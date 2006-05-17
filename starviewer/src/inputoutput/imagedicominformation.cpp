07c789135aa8924e7a3aa385d69ef5385e52c3c6
Marc Cals <cpmarc@gmail.com>
2006-05-17 15:54:34 +0000

    STYLE: Coding style de ImageDicomInformation

--- starviewer/src/inputoutput/imagedicominformation.cpp
+++ starviewer/src/inputoutput/imagedicominformation.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   Copyright (C) 2005 by Grup de Grï¿½ics de Girona                       *
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
@@ -20,7 +20,6 @@ ImageDicomInformation::ImageDicomInformation()
 
 }
 
-
 Status ImageDicomInformation:: openDicomFile(std::string imagePath)
 {
     Status state;
@@ -31,10 +30,9 @@ Status ImageDicomInformation:: openDicomFile(std::string imagePath)
     
     m_dicomFile = new DcmFileFormat();
     
-    OFCondition cond = m_dicomFile->loadFile(imagePath.c_str(), opt_transferSyntax, EGL_withoutGL,
-        DCM_MaxReadLength, opt_readAsDataset);
+    OFCondition cond = m_dicomFile->loadFile( imagePath.c_str() , opt_transferSyntax , EGL_withoutGL , DCM_MaxReadLength , opt_readAsDataset );
         
-    if (!cond.good()) return state.setStatus( cond );    
+    if ( !cond.good() ) return state.setStatus( cond );    
     
     m_dataset = m_dicomFile->getDataset();
         
@@ -46,9 +44,9 @@ std::string ImageDicomInformation::getStudyUID()
     DcmTagKey studyInstanceUIDTagKey( DCM_StudyInstanceUID ); 
     const char *studyUID = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( studyInstanceUIDTagKey, studyUID, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( studyInstanceUIDTagKey , studyUID , OFFalse  );
        
-    if (!cond.good() || studyUID == NULL)
+    if ( !cond.good() || studyUID == NULL )
     {
         return "";
     }
@@ -60,9 +58,9 @@ std::string ImageDicomInformation::getStudyID()
     DcmTagKey studyIDTagKey( DCM_StudyID ); 
     const char *studyID = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( studyIDTagKey, studyID, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( studyIDTagKey , studyID , OFFalse );
        
-    if (!cond.good() || studyID == NULL )
+    if ( !cond.good() || studyID == NULL )
     {
         return "";
     }
@@ -74,9 +72,9 @@ std::string ImageDicomInformation::getSeriesUID()
     DcmTagKey seriesInstanceUIDTagKey( DCM_SeriesInstanceUID ); 
     const char *seriesInstanceUID = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesInstanceUIDTagKey, seriesInstanceUID, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesInstanceUIDTagKey , seriesInstanceUID , OFFalse );
        
-    if (!cond.good() || seriesInstanceUID == NULL )
+    if ( !cond.good() || seriesInstanceUID == NULL )
     {
         return "";
     }
@@ -88,9 +86,9 @@ std::string ImageDicomInformation::getSeriesNumber()
     DcmTagKey seriesNumberTagKey( DCM_SeriesNumber );
     const char *seriesNumber = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesNumberTagKey, seriesNumber, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesNumberTagKey , seriesNumber , OFFalse );
        
-    if (!cond.good() || seriesNumber == NULL )
+    if ( !cond.good() || seriesNumber == NULL )
     {
         return "";
     }
@@ -102,9 +100,9 @@ std::string ImageDicomInformation::getSeriesModality()
     DcmTagKey seriesModalityTagKey( DCM_Modality ); 
     const char *seriesModality = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesModalityTagKey, seriesModality, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesModalityTagKey , seriesModality , OFFalse );
        
-    if (!cond.good() || seriesModality == NULL )
+    if ( !cond.good() || seriesModality == NULL )
     {
         return "";
     }
@@ -116,9 +114,9 @@ std::string ImageDicomInformation::getSeriesProtocolName()
     DcmTagKey seriesProtocolNameTagKey( DCM_ProtocolName ); 
     const char *seriesProtocolName = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesProtocolNameTagKey, seriesProtocolName, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesProtocolNameTagKey , seriesProtocolName , OFFalse );
        
-    if (!cond.good() || seriesProtocolName == NULL )
+    if ( !cond.good() || seriesProtocolName == NULL )
     {
         return "";
     }
@@ -130,13 +128,13 @@ std::string ImageDicomInformation::getSeriesDescription()
     DcmTagKey seriesDescriptionTagKey( DCM_SeriesDescription ); 
     const char *seriesDescription = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesDescriptionTagKey, seriesDescription, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesDescriptionTagKey , seriesDescription , OFFalse );
        
-    if (!cond.good() || seriesDescription == NULL )
+    if ( !cond.good() || seriesDescription == NULL )
     {
         return "";
     }
-    else if (seriesDescription == NULL)
+    else if ( seriesDescription == NULL )
     {
         return "";
     }
@@ -148,9 +146,9 @@ std::string ImageDicomInformation::getSeriesBodyPartExamined ()
     DcmTagKey seriesBodyPartExaminedTagKey( DCM_BodyPartExamined  ); 
     const char *seriesBodyPartExamined  = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesBodyPartExaminedTagKey, seriesBodyPartExamined , OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesBodyPartExaminedTagKey , seriesBodyPartExamined , OFFalse );
        
-    if (!cond.good() || seriesBodyPartExamined == NULL )
+    if ( !cond.good() || seriesBodyPartExamined == NULL )
     {
         return "";
     }
@@ -162,9 +160,9 @@ std::string ImageDicomInformation::getSeriesTime()
     DcmTagKey seriesTimeTagKey( DCM_SeriesTime ); 
     const char *seriesTime = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesTimeTagKey, seriesTime, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesTimeTagKey , seriesTime , OFFalse );
        
-    if (!cond.good() || seriesTime == NULL )
+    if ( !cond.good() || seriesTime == NULL )
     {
         return "";
     }
@@ -176,9 +174,9 @@ std::string ImageDicomInformation::getSeriesDate()
     DcmTagKey seriesDateTagKey( DCM_SeriesDate ); 
     const char *seriesDate = NULL;
 
-    OFCondition cond = m_dataset->findAndGetString( seriesDateTagKey, seriesDate, OFFalse );
+    OFCondition cond = m_dataset->findAndGetString( seriesDateTagKey , seriesDate , OFFalse );
        
-    if (!cond.good() || seriesDate == NULL )
+    if ( !cond.good() || seriesDate == NULL )
     {
         return "";
     }
@@ -188,6 +186,5 @@ std::string ImageDicomInformation::getSeriesDate()
 ImageDicomInformation::~ImageDicomInformation()
 {
 }
-
 
 }
--- starviewer/src/inputoutput/imagedicominformation.h
+++ starviewer/src/inputoutput/imagedicominformation.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   Copyright (C) 2005 by Grup de Grï¿½ics de Girona                       *
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
@@ -12,77 +12,74 @@
 
 namespace udg {
 
-/** Aquesta classe retorna els tags DICOM sol·licitats d'una imatge
-	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
+/** Aquesta classe retorna els tags DICOM sollicitats d'una imatge
+	@author Grup de GrÃ fics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
-
 
 class Status;
 
 class ImageDicomInformation{
 public:
 
-    /** Constructor de la classe
-      */
+    /// Constructor de la classe
     ImageDicomInformation();
 
-    /** Obre el DICOM del qual s'ha d'obtenir la informació
-      *         @param ruta del fitxer
-      */
-    Status openDicomFile(std::string filePath);
+    /** Obre el DICOM del qual s'ha d'obtenir la informaciï¿½      
+     *         @param ruta del fitxer
+     */
+    Status openDicomFile( std::string filePath );
     
     /** Retorna el study UID de l'imatge
-      *         @param study UID de l'imatge
-      */
+     *         @param study UID de l'imatge
+     */
     std::string getStudyUID();
     
     /** Retorna el ID de l'estudi de l'imatge
-    *         @param study ID de l'estudi de l'imatge
-    */
+     *         @param study ID de l'estudi de l'imatge
+     */
     std::string getStudyID();
     
     /** Retorna el series UID de l'imatge
-      *         @param series UID de l'imatge
-      */
+     *         @param series UID de l'imatge
+     */
     std::string getSeriesUID();
     
     /** Retorna el series number de l'imatge
-      *         @param series number
-      */
+     *         @param series number
+     */
     std::string getSeriesNumber();
     
-    /** Retorna la modalitat de la sèrie
-      *         @param modalitat de la sèrie
-      */
+    /** Retorna la modalitat de la sï¿½ie
+     *         @param modalitat de la sï¿½ie
+     */
     std::string getSeriesModality();
     
     /** Retorna el nom del Procotol utilitzat
-      *         @param Protocol utilitzat
-      */
+     *         @param Protocol utilitzat
+     */
     std::string getSeriesProtocolName();
     
-    /** Retorna la descripció de la sèrie
-      *         @param descripció de la sèrie
-      */
+    /** Retorna la descripciï¿½de la sï¿½ie
+     *         @param descripciï¿½de la sï¿½ie
+     */
     std::string getSeriesDescription();
     
     /** Retorna la part del cos examinada
-      *         @param part del cos examinada
-      */
+     *         @param part del cos examinada
+     */
     std::string getSeriesBodyPartExamined();
     
     /** Retorna l'hora en que s'ha capturat la serie
-      *         @return hora en que s'ha capturat la serie
-      */
+     *         @return hora en que s'ha capturat la serie
+     */
     std::string getSeriesTime();
     
     /** Retorna data en que s'ha capturat la serie
-      *         @return data en que s'ha capturat la serie
-      */
+     *         @return data en que s'ha capturat la serie
+     */
     std::string getSeriesDate();    
     
-    /** Destructor de la classe
-      */
+    /// Destructor de la classe
     ~ImageDicomInformation();
     
 private :
