d014dd710f6aa882fb20c9abae2154ded610c036
Jesús Spínola <jspinola@gmail.com>
2006-05-24 10:13:19 +0000

    ENH: Millores en els logs ( #83 )

--- starviewer/src/repositories/volumerepository.cpp
+++ starviewer/src/repositories/volumerepository.cpp
@@ -10,6 +10,7 @@
 
 #include "volumerepository.h"
 #include "volume.h"
+#include "logging.h"
 
 #include <iostream>
 
@@ -25,7 +26,7 @@ Identifier VolumeRepository::addVolume( Volume* model)
     
     id = this->addItem( model );
     emit itemAdded( id );
-    
+    INFO_LOG( qPrintable( "S'ha afegit al repositori el volum amb id: " + QString::number( id.getValue() ) )  );
     return id;
 }
 
@@ -39,6 +40,7 @@ void VolumeRepository::removeVolume( Identifier id )
 {
     this->removeItem( id );
     emit itemRemoved( id );
+    INFO_LOG( qPrintable( "S'ha esborrat del repositori el volum amb id: " + QString::number( id.getValue() ) ) );
 }
 
 int VolumeRepository::getNumberOfVolumes()
