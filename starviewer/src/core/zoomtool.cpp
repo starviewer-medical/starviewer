eff999003b645fd0083ff5400487a384acd279f0
Jesús Spínola <jspinola@gmail.com>
2006-10-19 15:15:30 +0000

    ENH: Re-estructuració de tools. No caldrà tenir una subclasse de tool per cada tool de zoom. Si s'associa a un visor o a un altre dependrà del constructor. Així serà més fàcil i reduïm el nombre de classes

---
+++ starviewer/src/core/zoomtool.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2006 by Grup de Gràfics de Girona                  *
+ *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
+ *                                                                         *
+ *   Universitat de Girona                                                 *
+ ***************************************************************************/
+#include "zoomtool.h"
+#include "q2dviewer.h"
+#include "q3dviewer.h"
+#include "q3dmprviewer.h"
+// #include <vtkInteractorStyleTrackballCamera.h> // per controlar el zoom
+#include <vtkInteractorStyleImage.h>
+#include <vtkInteractorStyle.h>
+#include <vtkCommand.h>
+
+namespace udg {
+
+ZoomTool::ZoomTool( Q2DViewer *viewer , QObject *parent, const char *name )
+//  : Tool(parent)
+{
+    m_state = NONE;
+    m_interactorStyle = viewer->getInteractorStyle();
+}
+
+ZoomTool::ZoomTool( Q3DViewer *viewer , QObject *parent, const char *name )
+//  : Tool(parent)
+{
+    m_state = NONE;
+    m_interactorStyle = viewer->getInteractorStyle();
+}
+
+ZoomTool::ZoomTool( Q3DMPRViewer *viewer , QObject *parent, const char *name )
+//  : Tool(parent)
+{
+    m_state = NONE;
+//     m_interactorStyle = viewer->getInteractorStyle();
+}
+
+ZoomTool::~ZoomTool()
+{
+}
+
+void ZoomTool::handleEvent( unsigned long eventID )
+{
+    switch( eventID )
+    {
+    case vtkCommand::LeftButtonPressEvent:
+        this->startZoom();
+    break;
+
+    case vtkCommand::MouseMoveEvent:
+        this->doZoom();
+    break;
+
+    case vtkCommand::LeftButtonReleaseEvent:
+        this->endZoom();
+    break;
+    
+    default:
+    break;
+    }
+}
+
+void ZoomTool::startZoom()
+{
+    m_state = ZOOMING;
+    // \TODO en comptes de fer això podríem crear crides específiques del propi visor que indirectament fan servir el vtkInteractorStyleImage
+    m_interactorStyle->StartDolly();
+}
+
+void ZoomTool::doZoom()
+{
+    if( m_state == ZOOMING )
+        m_interactorStyle->Dolly();
+}
+
+void ZoomTool::endZoom()
+{
+    m_state = NONE;
+    m_interactorStyle->EndDolly();
+}
+
+}
