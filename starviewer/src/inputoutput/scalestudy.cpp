adf21b175b531ff5cf278d1d903fbca06f57e90d
Jesús Spínola <jspinola@gmail.com>
2006-12-14 12:19:56 +0000

    BUG: L'extensió de la imatge escalada de previsualització era incorrecte. Es posava jpeg quan en realitat la imatge escalada era una PGM.

--- starviewer/src/inputoutput/scalestudy.cpp
+++ starviewer/src/inputoutput/scalestudy.cpp
@@ -35,22 +35,22 @@ void ScaleStudy::scale( std::string studyUID )
     StarviewerSettings settings;
     ScaleImage scaleImg;
     state = getSeriesOfStudy( studyUID,seriesList ); //busquem les sèries de l'estudi
-    
+
     seriesList.firstSeries();
-    
+
     while ( !seriesList.end() ) //escalem una imatge per cada sèrie
-    {  
+    {
         //preparem la màscara per buscar la imatge del mig de l'estudi
         mask.setSeriesUID( seriesList.getSeries().getSeriesUID().c_str() );
-        mask.setStudyUID( studyUID.c_str() );    
+        mask.setStudyUID( studyUID.c_str() );
         state = countImageNumber( mask , number );//comptem el número d'imatges, per saber quina és la imatge del mig
-        
+
         sprintf( imgNumX , "%i" , number / 2 + 1 );
         mask.setImageNumber( imgNumX );//indiquem per quin número d' imatge hem de buscar el path
-        
+
         relPath.clear();
         imageRelativePath( mask ,relPath );//busquem el path de la imatge del mig
-        
+
         if (  relPath.length() == 0 )
         {/*hi ha algunes màquines per error tenen el número d'imatge 1 sempre, encara que l'estudi tingui 10 imatges
           *En aquest cas no funciona buscar la imatge del mig, i el que es fa és mostrar la primera imatge que trobem
@@ -61,7 +61,7 @@ void ScaleStudy::scale( std::string studyUID )
         absPath.clear();
         absPath.append( settings.getCacheImagePath().toAscii().constData() );
         absPath.append( relPath ); //creem el path absolut a la imatge a la imatge
-        
+
         //creem el nom de la imatge resultant escalada
         absPathScal.clear();
         absPathScal.append( settings.getCacheImagePath().toAscii().constData() );
@@ -69,11 +69,11 @@ void ScaleStudy::scale( std::string studyUID )
         absPathScal.append( "/" );
         absPathScal.append( seriesList.getSeries().getSeriesUID().c_str() );
         absPathScal.append( "/" );
-        absPathScal.append( "scaled.jpeg" );
-        
+        absPathScal.append( "scaled.pgm" );
+
         scaleImg.dicom2lpgm( absPath.c_str() , absPathScal.c_str() , 100 );//creem la imatge escalada
-        
-        seriesList.nextSeries();     
+
+        seriesList.nextSeries();
     }
 }
 
@@ -81,16 +81,16 @@ Status ScaleStudy::getSeriesOfStudy( std::string studyUID , SeriesList &seriesLi
 {
     SeriesMask mask;
     CacheSeriesDAL cacheSeriesDAL;
-    
+
     mask.setStudyUID( studyUID.c_str() );
-    
+
     return cacheSeriesDAL.querySeries( mask , seriesList );
 }
 
 Status ScaleStudy::countImageNumber( ImageMask mask, int &number )
 {
     CacheImageDAL cacheImageDAL;
-    
+
     return cacheImageDAL.countImageNumber( mask , number );
 }
 
@@ -99,11 +99,11 @@ Status ScaleStudy::imageRelativePath( ImageMask mask , std::string &relPath )
     ImageList imageList;
     Image image;
     Status state;
-    
+
     CacheImageDAL cacheImageDAL;
 
     state = cacheImageDAL.queryImages( mask , imageList );
-    
+
     imageList.firstImage();
     if ( !imageList.end() )
     {
--- starviewer/src/interface/qserieslistwidget.cpp
+++ starviewer/src/interface/qserieslistwidget.cpp
@@ -3,7 +3,7 @@
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
- ***************************************************************************/ 
+ ***************************************************************************/
 #include "qserieslistwidget.h"
 
 #include <QString>
@@ -18,13 +18,13 @@ QSeriesListWidget::QSeriesListWidget(QWidget *parent )
  : QWidget( parent )
 {
     setupUi( this );
-    QSize size;    
+    QSize size;
 
     //Definim la mida de la imatge que mostrem
     size.setHeight( scaledSeriesSizeY );
     size.setWidth( scaledSeriesSizeX );
     m_seriesListWidget->setIconSize( size );
-    
+
     createConnections();
 }
 
@@ -40,10 +40,10 @@ void QSeriesListWidget::insertSeries( Series *serie )
     StarviewerSettings settings;
     QListWidgetItem *item = new QListWidgetItem( m_seriesListWidget );
     QString statusTip;
-    
+
     text.insert( 0 , tr( "Series " ) );
     text.append( serie->getSeriesNumber().c_str() );
-    
+
     if ( serie->getProtocolName().length() > 0 )
     {//si hi ha descripció la inserim
         text.append( " " );
@@ -51,7 +51,7 @@ void QSeriesListWidget::insertSeries( Series *serie )
         text.append( '\n' );
     }
     else text.append( '\n' );
-    
+
     if ( serie->getImageNumber() > 0 )
     {
         num.setNum( serie->getImageNumber() );
@@ -59,7 +59,7 @@ void QSeriesListWidget::insertSeries( Series *serie )
         text.append( tr(" images") );
         text.append( '\n' );
     }
-    
+
     nameClass.insert( 0 , this->objectName() );
     if ( nameClass == "m_seriesListWidgetCache" )
     {
@@ -67,17 +67,17 @@ void QSeriesListWidget::insertSeries( Series *serie )
         pathImage.append(serie->getStudyUID().c_str() );
         pathImage.append( "/" );
         pathImage.append( serie->getSeriesUID().c_str() );
-        pathImage.append( "/scaled.jpeg" );
+        pathImage.append( "/scaled.pgm" );
     }
 
     QIcon   icon(pathImage);
 
     item->setText(text);
     item->setIcon(icon);
-    
+
     /* A l'status Tip de cada item es guarda la UID de la serie, ja que aquest camp no el vull mostrar i no tinc
    enlloc per amagar-lo, ho utilitzo per identificar la sèrie */
-    item->setStatusTip(serie->getSeriesUID().c_str()); 
+    item->setStatusTip(serie->getSeriesUID().c_str());
 }
 
 void QSeriesListWidget::clicked( QListWidgetItem *item )
@@ -94,7 +94,7 @@ void QSeriesListWidget::selectedSeriesList( QString seriesUID )
 {
     QList< QListWidgetItem * > qSeriesList( m_seriesListWidget->findItems( "*" , Qt::MatchWildcard ) );
     QListWidgetItem *item;
-    
+
     for ( int i = 0;i < qSeriesList.count(); i++ )
     {
         item = qSeriesList.at( i );
@@ -104,7 +104,7 @@ void QSeriesListWidget::selectedSeriesList( QString seriesUID )
             m_seriesListWidget->setCurrentItem( item );
         }
         else m_seriesListWidget->setItemSelected( item , false );
-    }  
+    }
 }
 
 void QSeriesListWidget::clear()
