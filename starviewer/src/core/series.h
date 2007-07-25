046246072fe7d6795e3ccb7eb55ca641aafaf6ec
Jesús Spínola <jspinola@gmail.com>
2007-07-25 10:43:26 +0000

    ENH: millora general de la interfície i atributs de l'estructura Patient

--- starviewer/src/core/series.h
+++ starviewer/src/core/series.h
@@ -15,6 +15,10 @@
 
 namespace udg {
 
+class Volume;
+class Image;
+class Study;
+
 /**
 Classe que encapsula la sèrie d'un pacient.
 
@@ -22,11 +26,6 @@ La classe conté tot tipu d'informació relacionada amb la sèrie d'un pacient. 
 
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
-
-class Volume;
-class Image;
-class Study;
-
 class Series : public QObject
 {
 Q_OBJECT
@@ -36,22 +35,14 @@ public:
     ~Series();
 
     /// Assignar/Obtenir l'identificador universal de la sèrie
-    void setUID( QString uid );
-    QString getUID() const { return m_seriesUID; };
-
-    /// Assignar/Obtenir l'identificador de la sèrie
-    void setID( QString id );
-    QString getID() const { return m_seriesID; };
+    void setInstanceUID( QString uid );
+    QString getInstanceUID() const { return m_seriesInstanceUID; };
 
     /// assigna l'estudi pare de la sèrie
     void setParentStudy( Study *study ){ m_parentStudy = study; };
 
     /// afegeix un objecte imatge
     void addImage( Image *image );
-
-    /// Assignar/Obtenir l'orientació del pacient
-    void setPatientOrientation( QString orientation );
-    QString getPatientOrientation() const { return m_patientOrientation; };
 
     /// Assignar/Obtenir la modalitat de la sèrie
     void setModality( QString modality );
@@ -61,6 +52,10 @@ public:
     void setDescription( QString description );
     QString getDescription() const { return m_description; };
 
+    /// Assignar/Obtenir la posició del pacient relativa a la màquina
+    void setPatientPosition( QString position ){ m_patientPosition = position; }
+    QString getPatientPosition() const { return m_patientPosition; }
+
     /// Assignar/Obtenir el protocol de la sèrie
     void setProtocolName( QString protocolName );
     QString getProtocolName() const { return m_protocolName; };
@@ -68,9 +63,6 @@ public:
     /// Assignar/Obtenir el path de les imatges de la sèrie
     void setImagesPath( QString imagesPath );
     QString getImagesPath() const { return m_imagesPath; };
-
-    /// Retorna el camp clau que identificarà la sèrie de cares a la interfície. Es composarà pel protocolName més la descripció \TODO encara per determinar
-    QString getKey();
 
     /// Assignar/Obtenir la data i hora d'adquisició de la sèrie en format DD/MM/AAAA HH:MM. Retorna fals si hi ha algun error en el format
     bool setDateTime( int day , int month , int year , int hour , int minute );
@@ -87,26 +79,6 @@ public:
     QTime getTime();
     QString getTimeAsString();
 
-    /// Assignar/Obtenir nivells de contrast \TODO això hauria de ser a nivell de subvolum o a nivell d'imatge però no pas a nivell de series
-    void setWindowLevel( double window , double level );
-    double *getWindowLevel();
-    void setWindow( double window );
-    double getWindow() const { return m_window; }
-    void setLevel( double level );
-    double getLevel() const { return m_level; }
-
-    /// Assignar/Obtenir dimensions de la sèrie \TODO això hauria de ser a nivell de subvolum o a nivell d'imatge però no pas a nivell de series
-    void setDimensions( double dimensions[3] );
-    void setDimensions( double x , double y , double z );
-    double *getDimensions();
-    void getDimensions( double dimensions[3] );
-
-    /// Assignar/Obtenir espaiats de la sèrie \TODO això hauria de ser a nivell de subvolum o a nivell d'imatge però no pas a nivell de series
-    void setSpacing( double spacing[3] );
-    void setSpacing( double x , double y , double z );
-    double *getSpacing();
-    void getSpacing( double spacing[3] );
-
     /// Assignar/Obtenir identificador del volum al repositori corresponent a la sèrie \TODO estem assumint que un volum = una sèrie i això no és del tot cert. L'id, en tot cas, hauria d'anar relacionat amb el subvolum
     void setVolumeIdentifier( Identifier id );
     Identifier getVolumeIdentifier() const { return m_volumeID; }
@@ -117,47 +89,61 @@ public:
     /// Retorna el subvolum amb índex 'index', per defecte, el 0
     Volume *getVolume( int index = 0 );
 
+    /// Llista dels noms de fitxer de les imatges
+    QStringList getImagesPathList();
+
+    /// Ens diu si la sèrie està marcada com a seleccionada o no
+    inline bool isSelected() const { return m_selected; }
+
+public slots:
+    /// Selecciona/deselecciona aquesta sèrie
+    void select(){ setSelectStatus( true ); };
+    void unSelect(){ setSelectStatus( false ); };
+    void setSelectStatus( bool select ){ m_selected = select; };
+
 private:
-    /// Identidicador universal de la sèrie
-    QString m_seriesUID;
+    /// Informació comuna de la sèrie. C.7.3.1 General Series Module - PS 3.3.
 
-    /// Identificador de la sèrie
-    QString m_seriesID;
-
-    /// Orientació del pacient ( LR/AP/SI )
-    QString m_patientOrientation;
-
-    /// Modalitat d'imatge
+    /// Tipus d'equipament que originalment va adquirir les dades per crear les imatges creades en aquesta sèrie. Veure C.7.3.1.1.1 pels termes definits. (0008,0060) Tipus 1
     QString m_modality;
 
-    /// Descripció de la sèrie
+    /// Identidicador universal de la sèrie. (0020,000E) Tipus 1
+    QString m_seriesInstanceUID;
+
+    /// Nombre que identifica la sèrie. (0020,0011) Tipus 2
+    QString m_seriesNumber;
+
+    /// Data i hora en la que s'ha començat la sèrie. (0008,0021),(0008,0031) Tipus 3. \TODO Tractar per separat o conjuntament?
+    QDateTime m_dateTime;
+
+    /// Descriptor de la posició del pacient relativa a la màquina. Requerit per imatges CT i MR. No hi ha d'estar present si Patient Orientation Code Sequence (0054,0410) està present. Veure C.7.3.1.1.2 per termes definits i una explicació més profunda. (0018,5100) Tipus 2C
+    QString m_patientPosition;
+
+    /// Protocol que s'ha aplicat per obtenir les imatges definit per l'usuari. (0018,1030) Tipus 3
+    QString m_protocolName;
+
+    /// Descripció de la sèrie definida per l'usuari. (0008,103E) Tipus 3
     QString m_description;
 
-    /// Protocol que 'sha aplicat per obtenir la imatge
-    QString m_protocolName;
+    // FRAME OF REFERENCE MODULE. C.7.4 \TODO En principi no cal implementar-ho com a una entitat per separat i incloent-ho dins de sèries n'hauria d'haver prou per poder tractar el que necessitem
+    /// Identifica el frame of reference universalment. Veure C.7.4.1.1.1 per una explicació més profunda. (0020,0052) Tipus 1
+    QString m_frameOfReferenceUID;
+
+    /// Part de l'anatomia del pacient usat com a referència. Veure C.7.4.1.1.2, només per propòsits d'annotació. (0020,1040) Tipus 2
+    QString m_positionReferenceIndicator;
+
+
+    /// Indica si la sèrie està marcada com a seleccionada o no
+    bool m_selected;
 
     /// Directori sota el qual es trobem les imatges
     QString m_imagesPath;
 
-    /// Data i hora en que s'ha adquirit la sèrie
-    QDateTime m_dateTime;
-
     /// Imatge de previsualització associada a la sèrie
     QImage m_previewImage;
 
-    /// Window width i window level del contrast de la imatge \TODO podem tenir més d'un en una sèrie per tant això tendirà a quedar obsolet en aquest nivell
-    double m_window;
-    double m_level;
-
-    /// Dimensions ( x , y , z [nombre de llesques] )
-    double m_dimensions[3];
-
-    /// Espaiat en cada dimensió
-    double m_spacing[3];
-
     /// Identificador del volum al repositori
     Identifier m_volumeID;
-//     TransferFunctionList m_transferFunctionList;
 
     /// Llista de volums que composen la sèrie. La sèrie es pot separar en diversos volums per diverses raons, com pot ser mides d'imatge diferent, sèries amb dinàmics o fases, stacks, etc.
     QList<Volume *> m_volumesList;
