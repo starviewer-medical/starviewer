49ad7d689f00cf8bd3661f0bb89422e4450ae7ab
Jesús Spínola <jspinola@gmail.com>
2007-08-03 12:45:47 +0000

    STYLE: Neteja de la classe

--- starviewer/src/interface/qapplicationmainwindow.h
+++ starviewer/src/interface/qapplicationmainwindow.h
@@ -10,14 +10,12 @@
 // QT
 #include <QMainWindow>
 #include <QStringList>
-#include <QMenu>
 
 #include "identifier.h"
 
 // Forward declarations
 class QAction;
-class QToolBar;
-class QProgressDialog;
+class QMenu;
 class QCloseEvent;
 class QSignalMapper;
 
@@ -46,6 +44,10 @@ public:
     /// Retorna el numero de finestres amb diferents models que tenim obertes, el num. de QApplicationMainWindow
     unsigned int getCountQApplicationMainWindow();
 
+signals:
+    /// senyal emesa que indica si la finestra conté un volum o no
+    void containsVolume( bool );
+
 public slots:
     /// Fa les tasques que cal quan s'ha carregat un volum
     void onVolumeLoaded( Identifier id );
@@ -55,37 +57,37 @@ protected:
     void closeEvent(QCloseEvent *event);
 
 private:
+    ///Crea i inicialitza les accions de l'aplicació
+    void createActions();
+
+    /// Crea els menús.
+    void createMenus();
+
+    /// Crea el menú per escollir l'idioma de l'aplicació
+    void createLanguageMenu();
+
+    /// Llegeix la configuració inicial amb la que engega el programa
+    void readSettings();
+
+    /// Escriu la configuració amb la que s'engegarà el programa el pròxim cop. \TODO: s'hauria de cridar també quan obrim una finestra nova?
+    void writeSettings();
+
+private slots:
+    /// Obre una nova finestra
+    void newFile();
+
+    /// mostra el formulari d'about
+    void about();
+
+    /// canvia a l'idioma indicat
+    void switchToLanguage( int id );
+
+    /// canvia a pantalla completa o no
+    void switchFullScreen( bool full );
+
+private:
     /// Gestor de serveis i miniaplicacions
     ExtensionHandler *m_extensionHandler;
-
-    /// Tipus de fitxer a exportar
-    enum ExportFiles{ JpegExport , MetaIOExport , TiffExport, PngExport , BmpExport };
-
-//    /// Llista d'arxius recents
-//     QStringList m_recentFiles;
-//     enum { MaxRecentFiles = 5 };
-//     QAction *m_recentFileActions[ MaxRecentFiles ];
-
-    /// Arxiu actual que estem visualitzant
-    QString m_currentFile;
-
-    /// Filtres per als diàlegs d'obrir/exportar arxius
-//     QString m_exportFileFilters;
-//     QString m_exportToJpegFilter;
-//     QString m_exportToMetaIOFilter;
-//     QString m_exportToTiffFilter;
-//     QString m_exportToBmpFilter;
-//     QString m_exportToPngFilter;
-//     QString m_exportToDicomFilter;
-
-    /// Path actual del directori de treball ( obrir fitxer )
-    QString m_workingDirectory;
-
-    ///Path del directori de treball ( exportació a altres formats )
-    QString m_exportWorkingDirectory;
-
-    /// Aquesta string indica el path del directori que conté els arxius de traducció de l'aplicació
-    QString m_qmPath;
 
     /// Menús
     QMenu *m_fileMenu;
@@ -93,14 +95,6 @@ private:
     QMenu *m_windowMenu;
     QMenu *m_languageMenu;
     QMenu *m_helpMenu;
-
-    /// Sub menús
-//     QMenu *m_importFilesMenu;
-//     QMenu *m_exportFilesMenu;
-//     QMenu *m_recentFilesMenu;
-
-    /// Barres d'eines \TODO és possible que les fem desaparèixer
-    QToolBar *m_fileToolBar;
 
     /// Accions
     QAction *m_2DViewerAction;
@@ -114,18 +108,9 @@ private:
     QAction *m_aboutAction;
     QAction *m_fullScreenAction;
     QAction *m_logViewerAction;
-//     QAction *m_exportToJpegAction;
-//     QAction *m_exportToMetaIOAction;
-//     QAction *m_exportToTiffAction;
-//     QAction *m_exportToPngAction;
-//     QAction *m_exportToBmpAction;
-
     QAction *m_catalanAction;
     QAction *m_spanishAction;
     QAction *m_englishAction;
-
-    /// Per indicar el procés al obrir-se un fitxer
-    QProgressDialog *m_progressDialog;
 
     /// Mapeig de signals
     QSignalMapper *m_signalMapper;
@@ -133,76 +118,6 @@ private:
 
     /// Per veure els logs
     QLogViewer *m_logViewer;
-
-    // :::::::::::::::::::::::::::
-    // mètodes
-    // :::::::::::::::::::::::::::
-
-    ///Crea i inicialitza les accions de l'aplicació
-    void createActions();
-
-    /// Crea els menús.
-    void createMenus();
-
-    /// Crea el menú per escollir l'idioma de l'aplicació
-    void createLanguageMenu();
-
-    /// Crea la barra d'eines.
-    void createToolBars();
-
-    /// Crea la barra d'estat de l'aplicació
-    void createStatusBar();
-
-    /// Llegeix la configuració inicial amb la que engega el programa
-    void readSettings();
-
-    /// Escriu la configuració amb la que s'engegarà el programa el pròxim cop. \TODO: s'hauria de cridar també quan obrim una finestra nova?
-    void writeSettings();
-
-/*
-    /// Especifica el fitxer acctual que s'està tractant \TODO \deprecated aquest mètode esdevindrà antiquat en breu i no té gaire sentit desar el nom del fitxer, el metode continua existint perquè s'ha arrastrat des del principi
-    void setCurrentFile( const QString &fileName );
-
-    /// actualitza el llistat de noms de fitxers oberts recentment \TODO s'hauria d'adaptar, si cal quan hi hagi lo del pacs
-    void updateRecentFileActions();
-*/
-private slots:
-    /// Obre una nova finestra
-    void newFile();
-
-    /// mostra el formulari d'about
-    void about();
-
-    /// canvia a l'idioma indicat
-    void switchToLanguage( int id );
-
-    /// canvia a pantalla completa o no
-    void switchFullScreen( bool full );
-/*
-    /// obre un dels arxius recents::\TODO això pot esdevenir \deprecated
-    void openRecentFile();
-
-    /// Exporta la imatge a un format especificat
-    void exportFile( int type );
-
-    /// Exporta la imatge a JPEG
-    void exportToJpeg();
-
-    /// Exporta la imatge a MetaIO
-    void exportToMetaIO();
-
-    /// Exporta la imatge a TIFF
-    void exportToTiff();
-
-    /// Exporta la imatge a PNG
-    void exportToPng();
-
-    /// Exporta la imatge a BMP
-    void exportToBmp();
-*/
-signals:
-    /// senyal emesa que indica si la finestra conté un volum o no
-    void containsVolume( bool );
 };
 
 }; // fi namespace udg
