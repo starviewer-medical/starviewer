4b2b9a337f14413f5fc60cb140c431819fa8625a
Quim Rodríguez <quimrog.u@gmail.com>
2007-08-16 14:31:53 +0000

    ENH: Es fa que la classe DcmDataSetCache sigui thread-safe de manera que es pugui utilitzar DICOMTagReader per substituir el funcionament intern de ImageDicomInformation

--- starviewer/src/core/dcmdatasetcache.cpp
+++ starviewer/src/core/dcmdatasetcache.cpp
@@ -10,6 +10,10 @@
 #define HAVE_CONFIG_H 1
 #include "dcmtk/dcmdata/dcdatset.h"
 
+#include <QReadLocker>
+#include <QWriteLocker>
+#include <QThread>
+
 #include "logging.h"
 
 namespace udg {
@@ -19,14 +23,16 @@ static const int DcmDatasetCacheFilesLimit = 1050;
 DcmDatasetCache::DcmDatasetCache()
     : QObject(0), m_cache(DcmDatasetCacheFilesLimit), m_autoclearTimer(0), m_autoclearIsActive(false)
 {
+    connect(this, SIGNAL(resetTimer()), this, SLOT(resetAutoclearTimer()));
 }
 
 DcmDatasetCache::DcmDatasetCache(int seconds)
     : QObject(0), m_cache(DcmDatasetCacheFilesLimit), m_autoclearTimer(0), m_autoclearIsActive(false)
 {
-    // Només es marca com a activat, però el timer no s'activa perquè no cal: no hi ha elements.
+    // Només es marca com a activat, el timer no s'activa perquè no cal: no hi ha elements.
     m_autoclearIsActive = true;
     m_secondsForAutoclear = seconds;
+    connect(this, SIGNAL(resetTimer()), this, SLOT(resetAutoclearTimer()));
 }
 
 DcmDatasetCache::~DcmDatasetCache()
@@ -35,81 +41,91 @@ DcmDatasetCache::~DcmDatasetCache()
 
 DcmDataset* DcmDatasetCache::find(const QString &filePath)
 {
-    resetAutoclearTimer();
+    emit resetTimer();
+
+    QReadLocker locker(&m_cacheLock);
     DcmDataset *foundDataset = m_cache[filePath];
     return foundDataset ? dynamic_cast<DcmDataset*>( foundDataset->clone() ) :  NULL;
 }
 
 bool DcmDatasetCache::insert(const QString &filePath, DcmDataset *dataSet, int cost)
 {
-    resetAutoclearTimer();
+    emit resetTimer();
+
+    QWriteLocker locker(&m_cacheLock);
     return m_cache.insert(filePath, dataSet, cost);
 }
 
 bool DcmDatasetCache::remove(const QString &filePath)
 {
-    resetAutoclearTimer();
+    emit resetTimer();
+
+    QWriteLocker locker(&m_cacheLock);
     return m_cache.remove(filePath);
 }
 
 void DcmDatasetCache::clear()
 {
+    QWriteLocker locker(&m_cacheLock);
     m_cache.clear();
 }
 
-void DcmDatasetCache::activateAutoclear(int seconds)
+void DcmDatasetCache::startAutoclear(int seconds)
 {
     if (seconds > 0)
     {
         m_autoclearIsActive = true;
         m_secondsForAutoclear = seconds;
-        activateAutoclearTimer(seconds);
+        startAutoclearTimer(seconds);
     }
 }
 
-void DcmDatasetCache::deactivateAutoclear()
+void DcmDatasetCache::stopAutoclear()
 {
-    killAutoclearTimer();
+    stopAutoclearTimer();
 }
 
 void DcmDatasetCache::timerEvent(QTimerEvent *)
 {
-    killAutoclearTimer();
-    DEBUG_LOG("Buidem la cache DcmDataset perquè ha transcorregut el temps d'autoclear");
-    m_cache.clear();
+    stopAutoclearTimer();
+    QString thread; DEBUG_LOG(thread.sprintf("Buidem la cache DcmDataset perquè ha transcorregut el temps d'autoclear. Thread %p, Current Thread %p", this->thread(), QThread::currentThread()));
+    this->clear();
 }
 
 void DcmDatasetCache::resetAutoclearTimer()
 {
     if ( isAutoclearTimerActive() )
     {
-        killAutoclearTimer();
+        stopAutoclearTimer();
     }
 
     if (m_autoclearIsActive)
     {
-        activateAutoclearTimer(m_secondsForAutoclear);
+        startAutoclearTimer(m_secondsForAutoclear);
     }
 }
 
 bool DcmDatasetCache::isAutoclearTimerActive()
 {
+    QReadLocker locker(&m_timerLock);
     return m_autoclearTimer != 0;
 }
 
-void DcmDatasetCache::activateAutoclearTimer(int seconds)
+void DcmDatasetCache::startAutoclearTimer(int seconds)
 {
     if ( isAutoclearTimerActive() )
     {
-        killAutoclearTimer();
+        stopAutoclearTimer();
     }
+    QWriteLocker locker(&m_timerLock);
     m_autoclearTimer = startTimer(seconds*1000);
 }
 
-void DcmDatasetCache::killAutoclearTimer()
+void DcmDatasetCache::stopAutoclearTimer()
 {
     if ( isAutoclearTimerActive() )
     {
+        QWriteLocker locker(&m_timerLock);
         killTimer(m_autoclearTimer);
         m_autoclearTimer = 0;
     }
