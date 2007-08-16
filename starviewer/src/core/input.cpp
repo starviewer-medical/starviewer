5adb6cd5b829204fc840e757a2d9056ea7deae2a
Jesús Spínola <jspinola@gmail.com>
2007-08-16 15:38:21 +0000

    ENH: Hem traslladat la feina de llegir informació DICOM al VolumeSourceInformation. La principal raó és que estàvem llegint informació diferent des d'una classe i altre i ara queda més centralitzat

--- starviewer/src/core/input.cpp
+++ starviewer/src/core/input.cpp
@@ -16,8 +16,6 @@
 #include <QStringList>
 #include <QFileInfo>
 #include <QDir>
-// VTK
-#include <vtkMath.h> // pel cross
 
 namespace udg {
 
@@ -84,7 +82,6 @@ int Input::openFile( QString fileName )
         imageData->Delete();
 
         m_volumeData->getVolumeSourceInformation()->setFilenames( fileName );
-        this->setVolumeInformation();
         emit progress( 100 );
     }
     return errorCode;
@@ -133,7 +130,6 @@ int Input::readFiles( QStringList filenames )
             imageData->Delete();
 
             m_volumeData->getVolumeSourceInformation()->setFilenames( filenames );
-            this->setVolumeInformation();
             emit progress( 100 );
         }
     }
@@ -155,148 +151,13 @@ int Input::readSeries( QString dirPath )
     return readFiles( stdVectorOfStdStringToQStringList( filenames ) );
 }
 
-QString Input::getOrientation( double vector[3] )
+int Input::readImages( QList<Image *> imageList )
 {
-        char *orientation = new char[4];
-        char *optr = orientation;
-        *optr='\0';
-
-        char orientationX = vector[0] < 0 ? 'R' : 'L';
-        char orientationY = vector[1] < 0 ? 'A' : 'P';
-        char orientationZ = vector[2] < 0 ? 'I' : 'S';
-
-        double absX = fabs( vector[0] );
-        double absY = fabs( vector[1] );
-        double absZ = fabs( vector[2] );
-
-        int i;
-        for ( i = 0; i < 3; ++i )
-        {
-            if ( absX > .0001 && absX > absY && absX > absZ )
-            {
-                *optr++= orientationX;
-                absX = 0;
-            }
-            else if ( absY > .0001 && absY > absX && absY > absZ )
-            {
-                *optr++= orientationY;
-                absY = 0;
-            }
-            else if ( absZ > .0001 && absZ > absX && absZ > absY )
-            {
-                *optr++= orientationZ;
-                absZ = 0;
-            }
-            else break;
-            *optr='\0';
-        }
-        return QString( orientation );
+    QStringList filenames;
+    foreach( Image *image, imageList )
+        filenames << image->getPath();
+    return this->readFiles( filenames );
 }
-
-void Input::setVolumeInformation()
-{
-    // obtenim el primer arxiu de tots. \TODO Això ara mateix no és correcte però és pràcticament el mateix que havíem fet fins ara.
-    QString filename = m_volumeData->getVolumeSourceInformation()->getFilenames().at(0);
-    DICOMTagReader tagReader;
-    tagReader.setFile( filename );
-    QString value;
-
-    //obtenim l'string amb la posició del pacient relativa a la màquina(series level). Obligatori per MR i CT. No ha d'estar present si Patient Orientation Code Sequence (0054,0410) hi és, altrament és camp obligatori.
-    m_volumeData->getVolumeSourceInformation()->setPatientPosition( tagReader.getAttributeByTag( 0x0018, 0x5100 ).trimmed() );
-
-    //obtenim la orientació de la imatge(image level). Això ens designarà la direcció anatòmica (R,L,P,A,H,F) dels pixels que van d'esquerra-dreta/dalt-abaix. És un camp requerit si l'imatge no requereix Image Orientation(0020,0037) i Image Position(0020,0032)
-    value = tagReader.getAttributeByTag( 0x0020, 0x0020 );
-    if( !value.isEmpty() )
-    {
-        value.replace( QString( "\\" ) , QString( "," ) );
-        m_volumeData->getVolumeSourceInformation()->setPatientOrientationString( value );
-    }
-    else
-    {
-        // si no tenim la informació directament l'haurem de deduir a partir dels direction cosines
-        // l'Image Orientation
-        value = tagReader.getAttributeByTag( 0x0020,0x0037 );
-        if( !value.isEmpty() )
-        {
-            // passem de l'string als valors double
-            double dirCosinesValuesX[3] , dirCosinesValuesY[3] , dirCosinesValuesZ[3];
-            QStringList list = value.split( "\\" );
-            if( list.size() == 6 )
-            {
-                for ( int i = 0; i < 3; i++ )
-                {
-                    dirCosinesValuesX[ i ] = list.at( i ).toDouble();
-                    dirCosinesValuesY[ i ] = list.at( i+3 ).toDouble();
-                }
-
-                vtkMath::Cross( dirCosinesValuesX , dirCosinesValuesY , dirCosinesValuesZ );
-                // I ara ens disposem a crear l'string amb l'orientació del pacient
-                QString patientOrientationString;
-
-                patientOrientationString = this->getOrientation( dirCosinesValuesX );
-                patientOrientationString += ",";
-                patientOrientationString += this->getOrientation( dirCosinesValuesY );
-                patientOrientationString += ",";
-                patientOrientationString += this->getOrientation( dirCosinesValuesZ );
-                m_volumeData->getVolumeSourceInformation()->setPatientOrientationString( patientOrientationString );
-                m_volumeData->getVolumeSourceInformation()->setXDirectionCosines( dirCosinesValuesX );
-                m_volumeData->getVolumeSourceInformation()->setYDirectionCosines( dirCosinesValuesY );
-                m_volumeData->getVolumeSourceInformation()->setZDirectionCosines( dirCosinesValuesZ );
-
-                DEBUG_LOG("Patient orientation string: " + patientOrientationString );
-            }
-            else
-            {
-                // hi ha algun error en l'string ja que han de ser 2 parells de 3 valors
-                DEBUG_LOG( "No s'ha pogut determinar l'orientació del pacient (Tags 0020|0020 , 0020|0037) : " + value );
-            }
-        }
-        else
-        {
-            // no podem obtenir l'string d'orientació del pacient
-        }
-    }
-    // nom de la institució on s'ha fet l'estudi
-    value = tagReader.getAttributeByTag( 0x0008, 0x0080 );
-    if( !value.isEmpty() )
-        m_volumeData->getVolumeSourceInformation()->setInstitutionName( value );
-    else
-    {
-        // no tenim aquesta informació \TODO cal posar res?
-        m_volumeData->getVolumeSourceInformation()->setInstitutionName( tr( "Unknown" ) );
-    }
-
-    // nom del pacient
-    value = tagReader.getAttributeByTag( 0x0010,0x0010 );
-    if( !value.isEmpty() )
-    {
-        // pre-tractament per treure caràcters estranys com ^ que en alguns casos fan de separadors en comptes dels espais
-        while( value.indexOf("^") >= 0 )
-            value.replace( value.indexOf("^") , 1 , QString(" ") );
-        m_volumeData->getVolumeSourceInformation()->setPatientName( value );
-    }
-    // ID del pacient
-    m_volumeData->getVolumeSourceInformation()->setPatientID( tagReader.getAttributeByTag( 0x0010,0x0020 ) );
-
-    // data de l'estudi, la data està en format YYYYMMDD
-    m_volumeData->getVolumeSourceInformation()->setStudyDate( tagReader.getAttributeByTag( 0x0008,0x0020 ) );
-
-    // hora de l'estudi, format HHMMSS
-    m_volumeData->getVolumeSourceInformation()->setStudyTime( tagReader.getAttributeByTag( 0x0008,0x0030 ) );
-
-    // accession number
-    m_volumeData->getVolumeSourceInformation()->setAccessionNumber( tagReader.getAttributeByTag( 0x0008,0x0050 ) );
-
-    // Protocol name
-    m_volumeData->getVolumeSourceInformation()->setProtocolName( tagReader.getAttributeByTag( 0x0018,0x1030 ) );
-
-    //Number of Phases, TAG PRIVAT PHILIPS
-    m_volumeData->getVolumeSourceInformation()->setNumberOfPhases( tagReader.getAttributeByTag( 0x2001,0x1017 ).toInt() );
-
-    //Number of Slices Per Phase, nombre de llesques per cada fase, TAG PRIVAT PHILIPS
-    m_volumeData->getVolumeSourceInformation()->setNumberOfSlices( tagReader.getAttributeByTag( 0x2001,0x1018 ).toInt() );
-}
-
 
 QStringList Input::stdVectorOfStdStringToQStringList( std::vector< std::string > vector )
 {
--- starviewer/src/core/volumesourceinformation.cpp
+++ starviewer/src/core/volumesourceinformation.cpp
@@ -6,6 +6,7 @@
  ***************************************************************************/
 #include "volumesourceinformation.h"
 
+#include <vtkMath.h> // pel cross
 #include "dicomtagreader.h"
 #include "logging.h"
 
@@ -138,24 +139,139 @@ void VolumeSourceInformation::getZDirectionCosines( double zCosines[3] )
         zCosines[i] = m_directionCosines[i+6];
 }
 
-bool VolumeSourceInformation::loadDicomDataset( QString filename )
+bool VolumeSourceInformation::loadDicomData( QString filename )
 {
-    m_dicomTagReader->setFile( filename );
-    this->collectSerieInformation();
-    this->readWindowLevelData();
-}
+    if( m_dicomTagReader->setFile( filename ) )
+    {
+        // primer llegim els valors dels window level
+        QString windowWidth = m_dicomTagReader->getAttributeByName( DCM_WindowWidth );
+        QStringList windowWidthList = windowWidth.split("\\");
+        QString windowLevel = m_dicomTagReader->getAttributeByName( DCM_WindowCenter );
+        QStringList windowLevelList = windowLevel.split("\\");
 
-void VolumeSourceInformation::collectSerieInformation()
-{
-    this->setSeriesDescription( m_dicomTagReader->getAttributeByName( DCM_SeriesDescription ) );
-    this->setSeriesInstanceUID( m_dicomTagReader->getAttributeByName( DCM_SeriesInstanceUID ) );
+        double *wl;
+        for( int i = 0; i < windowWidthList.size(); i++ )
+        {
+            wl = new double[2];
+            wl[0] = windowWidthList.at(i).toDouble();
+            wl[1] = windowLevelList.at(i).toDouble();
+            m_windowLevelList.push_back( wl );
+        }
+        // i després les respectives descripcions si n'hi ha
+        QString descriptions = m_dicomTagReader->getAttributeByName( DCM_WindowCenterWidthExplanation );
+        QStringList descriptionsList = descriptions.split("\\");
+        foreach( QString desc , descriptionsList )
+        {
+            m_windowLevelDescriptions << desc;
+        }
+
+        this->setSeriesDescription( m_dicomTagReader->getAttributeByName( DCM_SeriesDescription ) );
+        this->setSeriesInstanceUID( m_dicomTagReader->getAttributeByName( DCM_SeriesInstanceUID ) );
+
+        //obtenim l'string amb la posició del pacient relativa a la màquina(series level). Obligatori per MR i CT. No ha d'estar present si Patient Orientation Code Sequence (0054,0410) hi és, altrament és camp obligatori.
+        this->setPatientPosition( m_dicomTagReader->getAttributeByTag( 0x0018, 0x5100 ).trimmed() );
+
+        //obtenim la orientació de la imatge(image level). Això ens designarà la direcció anatòmica (R,L,P,A,H,F) dels pixels que van d'esquerra-dreta/dalt-abaix. És un camp requerit si l'imatge no requereix Image Orientation(0020,0037) i Image Position(0020,0032)
+        QString value = m_dicomTagReader->getAttributeByTag( 0x0020, 0x0020 );
+        if( !value.isEmpty() )
+        {
+            value.replace( QString( "\\" ) , QString( "," ) );
+            this->setPatientOrientationString( value );
+        }
+        else
+        {
+            // si no tenim la informació directament l'haurem de deduir a partir dels direction cosines
+            // l'Image Orientation
+            value = m_dicomTagReader->getAttributeByTag( 0x0020,0x0037 );
+            if( !value.isEmpty() )
+            {
+                // passem de l'string als valors double
+                double dirCosinesValuesX[3] , dirCosinesValuesY[3] , dirCosinesValuesZ[3];
+                QStringList list = value.split( "\\" );
+                if( list.size() == 6 )
+                {
+                    for ( int i = 0; i < 3; i++ )
+                    {
+                        dirCosinesValuesX[ i ] = list.at( i ).toDouble();
+                        dirCosinesValuesY[ i ] = list.at( i+3 ).toDouble();
+                    }
+
+                    vtkMath::Cross( dirCosinesValuesX , dirCosinesValuesY , dirCosinesValuesZ );
+                    // I ara ens disposem a crear l'string amb l'orientació del pacient
+                    QString patientOrientationString;
+
+                    patientOrientationString = this->getOrientation( dirCosinesValuesX );
+                    patientOrientationString += ",";
+                    patientOrientationString += this->getOrientation( dirCosinesValuesY );
+                    patientOrientationString += ",";
+                    patientOrientationString += this->getOrientation( dirCosinesValuesZ );
+                    this->setPatientOrientationString( patientOrientationString );
+                    this->setXDirectionCosines( dirCosinesValuesX );
+                    this->setYDirectionCosines( dirCosinesValuesY );
+                    this->setZDirectionCosines( dirCosinesValuesZ );
+
+                    DEBUG_LOG("Patient orientation string: " + patientOrientationString );
+                }
+                else
+                {
+                    // hi ha algun error en l'string ja que han de ser 2 parells de 3 valors
+                    DEBUG_LOG( "No s'ha pogut determinar l'orientació del pacient (Tags 0020|0020 , 0020|0037) : " + value );
+                }
+            }
+            else
+            {
+                // no podem obtenir l'string d'orientació del pacient
+            }
+        }
+        // nom de la institució on s'ha fet l'estudi
+        value = m_dicomTagReader->getAttributeByTag( 0x0008, 0x0080 );
+        if( !value.isEmpty() )
+            this->setInstitutionName( value );
+        else
+        {
+            // no tenim aquesta informació \TODO cal posar res?
+            this->setInstitutionName( QObject::tr( "Unknown" ) );
+        }
+
+        // nom del pacient
+        value = m_dicomTagReader->getAttributeByTag( 0x0010,0x0010 );
+        if( !value.isEmpty() )
+        {
+            // pre-tractament per treure caràcters estranys com ^ que en alguns casos fan de separadors en comptes dels espais
+            while( value.indexOf("^") >= 0 )
+                value.replace( value.indexOf("^") , 1 , QString(" ") );
+            this->setPatientName( value );
+        }
+        // ID del pacient
+        this->setPatientID( m_dicomTagReader->getAttributeByTag( 0x0010,0x0020 ) );
+
+        // data de l'estudi, la data està en format YYYYMMDD
+        this->setStudyDate( m_dicomTagReader->getAttributeByTag( 0x0008,0x0020 ) );
+
+        // hora de l'estudi, format HHMMSS
+        this->setStudyTime( m_dicomTagReader->getAttributeByTag( 0x0008,0x0030 ) );
+
+        // accession number
+        this->setAccessionNumber( m_dicomTagReader->getAttributeByTag( 0x0008,0x0050 ) );
+
+        // Protocol name
+        this->setProtocolName( m_dicomTagReader->getAttributeByTag( 0x0018,0x1030 ) );
+
+        //Number of Phases, TAG PRIVAT PHILIPS
+        this->setNumberOfPhases( m_dicomTagReader->getAttributeByTag( 0x2001,0x1017 ).toInt() );
+
+        //Number of Slices Per Phase, nombre de llesques per cada fase, TAG PRIVAT PHILIPS
+        this->setNumberOfSlices( m_dicomTagReader->getAttributeByTag( 0x2001,0x1018 ).toInt() );
+    }
+    else
+        DEBUG_LOG("El DICOMTagReader no ha pogut llegir l'arxiu " + filename );
 }
 
 void VolumeSourceInformation::setFilenames( QStringList filenames )
 {
     m_filenamesList = filenames;
     if( !m_filenamesList.isEmpty() )
-        this->loadDicomDataset( m_filenamesList.at(0) ); //\TODO ara es fa així, però podria ser que tinguèssim un tracte més "refinat"
+        this->loadDicomData( m_filenamesList.at(0) ); //\TODO ara es fa així, però podria ser que tinguèssim un tracte més "refinat"
     else
         WARN_LOG("La llista de fitxers és buida");
 }
@@ -320,31 +436,6 @@ QString VolumeSourceInformation::getWindowLevelDescription( int position )
         return m_windowLevelDescriptions.at( position );
 }
 
-void VolumeSourceInformation::readWindowLevelData()
-{
-    // primer llegim els valors dels window level
-    QString windowWidth = m_dicomTagReader->getAttributeByName( DCM_WindowWidth );
-    QStringList windowWidthList = windowWidth.split("\\");
-    QString windowLevel = m_dicomTagReader->getAttributeByName( DCM_WindowCenter );
-    QStringList windowLevelList = windowLevel.split("\\");
-
-    double *wl;
-    for( int i = 0; i < windowWidthList.size(); i++ )
-    {
-        wl = new double[2];
-        wl[0] = windowWidthList.at(i).toDouble();
-        wl[1] = windowLevelList.at(i).toDouble();
-        m_windowLevelList.push_back( wl );
-    }
-    // i després les respectives descripcions si n'hi ha
-    QString descriptions = m_dicomTagReader->getAttributeByName( DCM_WindowCenterWidthExplanation );
-    QStringList descriptionsList = descriptions.split("\\");
-    foreach( QString value , descriptionsList )
-    {
-        m_windowLevelDescriptions << value;
-    }
-}
-
 bool VolumeSourceInformation::isMultiFrame()
 {
     return m_dicomTagReader->tagExists( DCM_RescaleIntercept );
@@ -382,6 +473,44 @@ void VolumeSourceInformation::setPatientPosition( QString patientPosition )
 //         setPatientOrientationString("LRAPIS");
 //     else // No tenim l'atribut esperat
 //         setPatientOrientationString("??????");
+}
+
+QString VolumeSourceInformation::getOrientation( double vector[3] )
+{
+    char *orientation = new char[4];
+    char *optr = orientation;
+    *optr='\0';
+
+    char orientationX = vector[0] < 0 ? 'R' : 'L';
+    char orientationY = vector[1] < 0 ? 'A' : 'P';
+    char orientationZ = vector[2] < 0 ? 'I' : 'S';
+
+    double absX = fabs( vector[0] );
+    double absY = fabs( vector[1] );
+    double absZ = fabs( vector[2] );
+
+    int i;
+    for ( i = 0; i < 3; ++i )
+    {
+        if ( absX > .0001 && absX > absY && absX > absZ )
+        {
+            *optr++= orientationX;
+            absX = 0;
+        }
+        else if ( absY > .0001 && absY > absX && absY > absZ )
+        {
+            *optr++= orientationY;
+            absY = 0;
+        }
+        else if ( absZ > .0001 && absZ > absX && absZ > absY )
+        {
+            *optr++= orientationZ;
+            absZ = 0;
+        }
+        else break;
+        *optr='\0';
+    }
+    return QString( orientation );
 }
 
 };  // end namespace udg
