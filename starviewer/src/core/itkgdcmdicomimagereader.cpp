ac5d66c4d013a2bc739612595e9e9b33d31b3296
Jesús Spínola <jspinola@gmail.com>
2007-08-21 12:57:39 +0000

    ENH: Implementació del mètode load() a itkGDCMDICOMImageReader

--- starviewer/src/core/itkgdcmdicomimagereader.cpp
+++ starviewer/src/core/itkgdcmdicomimagereader.cpp
@@ -6,6 +6,12 @@
  ***************************************************************************/
 #include "itkgdcmdicomimagereader.h"
 
+#include "logging.h"
+#include "image.h"
+
+#include <itkImageFileReader.h>
+#include <itkGDCMImageIO.h>
+
 namespace udg {
 
 itkGDCMDICOMImageReader::itkGDCMDICOMImageReader(QObject *parent)
@@ -13,10 +19,60 @@ itkGDCMDICOMImageReader::itkGDCMDICOMImageReader(QObject *parent)
 {
 }
 
-
 itkGDCMDICOMImageReader::~itkGDCMDICOMImageReader()
 {
 }
 
+bool itkGDCMDICOMImageReader::load()
+{
+    bool ok = readyToLoad();
+
+    if( ok )
+    {
+        typedef itk::Image<unsigned char, 3> ImageType;
+        typedef itk::ImageFileReader< ImageType >  ReaderType;
+        typedef itk::GDCMImageIO GDCMImageIOType;
+
+        ReaderType::Pointer reader = ReaderType::New();
+        GDCMImageIOType::Pointer gdcmIO = GDCMImageIOType::New();
+
+        // assignem al lector quin és el tipus de lector que volem
+        reader->SetImageIO( gdcmIO );
+
+        // nombre de bytes que copiem->debug
+        unsigned long bytes = 0;
+        // buffer on colocarem la llesca que hem llegit
+        unsigned char *dicomBuffer = NULL;
+        // imatges totals i comptador per calcular el progrés
+        int total = m_inputImageList.size();
+        int slice = 0;
+        emit started();
+        // Per cada imatge
+        foreach( Image *image, m_inputImageList )
+        {
+            reader->SetFileName( qPrintable( image->getPath() ) );
+            try
+            {
+                reader->Update();
+            }
+            catch ( itk::ExceptionObject & e )
+            {
+                ERROR_LOG( QString("Excepció llegint l'arxiu [%1]\nDescripció: [%2]")
+                        .arg( image->getPath() )
+                        .arg( e.GetDescription() )
+                        );
+            }
+            dicomBuffer = reader->GetOutput()->GetPixelContainer()->GetBufferPointer();
+            // copiem les dades del buffer d'imatge cap a vtk
+            memcpy( m_imageBuffer, dicomBuffer, m_sliceByteIncrement );
+            m_imageBuffer += m_sliceByteIncrement;
+            slice++;
+            emit progress( slice / total );
+        }
+        emit finished();
+    }
+
+    return ok;
+}
 
 }
--- starviewer/src/core/itkgdcmdicomimagereader.h
+++ starviewer/src/core/itkgdcmdicomimagereader.h
@@ -24,6 +24,7 @@ public:
 
     ~itkGDCMDICOMImageReader();
 
+    virtual bool load();
 };
 
 }
