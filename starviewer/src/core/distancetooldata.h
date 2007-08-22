271f15e709c915c3b2a3b095d7796510007b1752
Sergi Dote <sergidt@gmail.com>
2007-08-22 10:03:54 +0000

    ENH: s'han millorat les classes DistanceRepresentation i DistanceToolData: 	* En la DistanceToolData s'han afegit atributs i mètodes i signals per tal de que quan canvïi un atribut, la representació a la que està lligada 	  actualitzi els valors. 	* En la DistanceRepresentation, entre d'altres coses, s'han afegit els slots per tal de lligar els canvis de l'atribut de tipus DistanceToolData.

--- starviewer/src/core/distancetooldata.h
+++ starviewer/src/core/distancetooldata.h
@@ -9,23 +9,24 @@
 
 #include "tooldata.h"
 
+//Forward declarations
+class QString;
 
 namespace udg {
-
 /**
 Classe que implementa una especialització de la classe ToolData, per a contenir dades necessàries per a la Tool de creació de distàncies.
 
 @author Grup de Gràfics de Girona  ( GGG )
 */
 
-class DistanceToolData : public ToolData{
+class DistanceToolData : public ToolData {
 Q_OBJECT
 public:
     
     ///constructor amb paràmetres
     DistanceToolData( double p1[3], double p2[3] );
     
-    ~DistanceToolData(){}
+    ~DistanceToolData();
     
     ///assignem el primer punt
     void setFirstPoint( double p1[3] ); 
@@ -41,11 +42,36 @@ public:
     double* getSecondPoint()
     { return( m_secondPoint ); }
     
+    ///retornem el segon punt
+    double* getTextPosition()
+    { return( m_textPosition ); }
+    
+    ///retornem el text
+    QString getDistanceText()
+    { return( m_text ); }
+    
 private:
     
     ///atributs corresponents als punts de la distància
     double m_firstPoint[3];
     double m_secondPoint[3];
+    
+    ///atribut que conté el text de la distància
+    QString m_text;
+    
+    ///atribut corresponent a la posició del text de la distància
+    double m_textPosition[3];
+    
+    ///Mètode per calcular el la distància entre els dos punts, per tant el text que contindrà la distància i la posició
+    void calculateDistance();
+    
+signals:
+    
+    ///s'emet quan canvia el primer punt de la distància
+    void firstPointChanged();
+    
+    ///s'emet quan canvia el segon punt de la distància
+    void secondPointChanged();
 };
 
 };  //  end  namespace udg
