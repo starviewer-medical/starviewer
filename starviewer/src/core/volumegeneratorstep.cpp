7bc1c1ec588913dbb4be1d4b25e3f72c4a556c8e
Jesús Spínola <jspinola@gmail.com>
2007-08-23 11:40:42 +0000

    ENH: Millorem el tractament d'aquest filler accedint directament a les series que ens interessa

--- starviewer/src/core/volumegeneratorstep.cpp
+++ starviewer/src/core/volumegeneratorstep.cpp
@@ -7,7 +7,7 @@
 #include "volumegeneratorstep.h"
 #include "logging.h"
 #include "patientfillerinput.h"
-#include "patient.h"
+#include "series.h"
 #include "volumerepository.h"
 
 namespace udg {
@@ -15,9 +15,7 @@ namespace udg {
 VolumeGeneratorStep::VolumeGeneratorStep()
  : PatientFillerStep()
 {
-    m_requiredLabelsList << "DICOMFileClassifierFillerStep";
-    // TODO afegir el label a nivell de series VolumeGeneratorStep i TemporalDimension?
-    // aquest s'hauria de fer al final de tot, podríem possar-li prioritat mínima
+    m_requiredLabelsList << "DICOMFileClassifierFillerStep" << "TemporalDimensionFillerStep" << "ImageFillerStep";
 }
 
 VolumeGeneratorStep::~VolumeGeneratorStep()
@@ -30,13 +28,14 @@ bool VolumeGeneratorStep::fill()
     // processarem cadascun dels pacients que hi hagi en l'input i per cadascun totes les sèries que siguin de tipus imatge
     if( m_input )
     {
-        unsigned int i = 0;
-        while( i < m_input->getNumberOfPatients() )
+        QStringList requiredLabels;
+        requiredLabels << "TemporalDimensionFillerStep" << "ImageFillerStep"; 
+        QList<Series *> seriesToProcess = m_input->getSeriesWithLabels( requiredLabels );
+        foreach( Series *series, seriesToProcess )
         {
-            Patient *patient = m_input->getPatient( i );
-            this->processPatient( patient );
-            i++;
+            processSeries( series );
         }
+        ok = true;
     }
     else
     {
@@ -46,37 +45,15 @@ bool VolumeGeneratorStep::fill()
     return ok;
 }
 
-void VolumeGeneratorStep::processPatient( Patient *patient )
-{
-    QList<Study *> studyList = patient->getStudies();
-    foreach( Study *study, studyList )
-    {
-        QList<Series *> seriesList = study->getSeries();
-        foreach( Series *series, seriesList )
-        {
-            this->processSeries( series );
-        }
-    }
-}
-
 void VolumeGeneratorStep::processSeries( Series *series )
 {
-    // Podrem tenir o bé Images, o bé KINs o bé PresentationStates TODO si apliquéssim correctament lo dels labels
-    // a nivell de series això no caldria
-    if( isImageSeries(series) )
-    {
-        // TODO ara el que fem és que 1 Series equival a 1 Volume, més endavant es podrien fer un tracte més elaborat
-        Volume *volume = new Volume;
-        volume->setImages( series->getImages() );
-        Identifier volumeID = VolumeRepository::getRepository()->addVolume( volume );
-        series->setVolumeIdentifier( volumeID );
-        m_input->addLabelToSeries("VolumeGeneratorStep", series );
-        DEBUG_LOG("Generem un volum");
-    }
-    else
-    {
-        DEBUG_LOG("La serie amb uid " + series->getInstanceUID() + " no es processa perquè no és una sèrie d'Imatges. És de modalitat: " + series->getModality() );
-    }
+    // TODO ara el que fem és que 1 Series equival a 1 Volume, més endavant es podrien fer un tracte més elaborat
+    Volume *volume = new Volume;
+    volume->setImages( series->getImages() );
+    Identifier volumeID = VolumeRepository::getRepository()->addVolume( volume );
+    series->setVolumeIdentifier( volumeID );
+    m_input->addLabelToSeries("VolumeGeneratorStep", series );
+    DEBUG_LOG("Generem un volum");
 }
 
 }
