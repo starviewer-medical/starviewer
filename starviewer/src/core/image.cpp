da6af83a44f6a3216342704fdef9fee6461c086b
Jesús Spínola <jspinola@gmail.com>
2007-08-31 11:28:15 +0000

    BUG: S'havia pujat malament image.cpp en el commit anterior

--- starviewer/src/core/image.cpp
+++ starviewer/src/core/image.cpp
@@ -23,7 +23,7 @@
 namespace udg {
 
 Image::Image(QObject *parent)
- : QObject(parent), m_sliceThickness(1.0), m_numberOfFrames(1)
+ : QObject(parent), m_sliceThickness(1.0)
 {
 }
 
@@ -155,4 +155,375 @@ bool Image::setContentTime(QTime time)
     else
     {
         DEBUG_LOG("El time està en un mal format" );
-        retu
+        return false;
+    }
+}
+
+QDate Image::getContentDate() const
+{
+    return m_contentDate;
+}
+
+QString Image::getContentDateAsString()
+{
+    return m_contentDate.toString(Qt::LocaleDate);
+}
+
+QTime Image::getContentTime() const
+{
+    return m_contentTime;
+}
+
+QString Image::getContentTimeAsString()
+{
+    return m_contentTime.toString("HH:mm:ss");
+}
+
+void Image::setPixelSpacing( double x, double y )
+{
+    m_pixelSpacing[0] = x;
+    m_pixelSpacing[1] = y;
+}
+
+const double *Image::getPixelSpacing() const
+{
+    return m_pixelSpacing;
+}
+
+void Image::setSliceThickness( double z )
+{
+    m_sliceThickness = z;
+}
+
+double Image::getSliceThickness() const
+{
+    return m_sliceThickness;
+}
+
+void Image::setImagePositionPatient( double position[3] )
+{
+    memcpy( m_imagePositionPatient, position, 3 );
+}
+
+const double *Image::getImagePositionPatient() const
+{
+    return m_imagePositionPatient;
+}
+
+void Image::setSamplesPerPixel( int samples )
+{
+    m_samplesPerPixel = samples;
+}
+
+int Image::getSamplesPerPixel() const
+{
+    return m_samplesPerPixel;
+}
+
+void Image::setPhotometricInterpretation( QString value )
+{
+    m_photometricInterpretation = value;
+}
+
+QString Image::getPhotometricInterpretation() const
+{
+    return m_photometricInterpretation;
+}
+
+void Image::setRows( int rows  )
+{
+    m_rows = rows;
+}
+
+int Image::getRows() const
+{
+    return m_rows;
+}
+
+void Image::setColumns( int columns  )
+{
+    m_columns = columns;
+}
+
+int Image::getColumns() const
+{
+    return m_columns;
+}
+
+void Image::setBitsAllocated( int bits )
+{
+    m_bitsAllocated = bits;
+}
+
+int Image::getBitsAllocated() const
+{
+    return m_bitsAllocated;
+}
+
+void Image::setBitsStored( int bits )
+{
+    m_bitsStored = bits;
+}
+
+int Image::getBitsStored() const
+{
+    return m_bitsStored;
+}
+
+void Image::setPixelRepresentation( int representation )
+{
+    m_pixelRepresentation = representation;
+}
+
+int Image::getPixelRepresentation() const
+{
+    return m_pixelRepresentation;
+}
+
+void Image::setRescaleSlope( double slope )
+{
+    m_rescaleSlope = slope;
+}
+
+double Image::getRescaleSlope() const
+{
+    return m_rescaleSlope;
+}
+
+void Image::setRescaleIntercept( double intercept )
+{
+    m_rescaleIntercept = intercept;
+}
+
+double Image::getRescaleIntercept() const
+{
+    return m_rescaleIntercept;
+}
+
+void Image::addWindowLevel( double window, double level )
+{
+    QPair<double, double> windowLevel( window, level );
+    m_windowLevelList << windowLevel;
+}
+
+QPair<double,double> Image::getWindowLevel( int index ) const
+{
+    if( index > 0 && index < m_windowLevelList.size() )
+        return m_windowLevelList.at(index);
+    else
+    {
+        DEBUG_LOG("Index out of range");
+        return QPair<double,double>();
+    }
+}
+
+int Image::getNumberOfWindowLevels()
+{
+    return m_windowLevelList.size();
+}
+
+void Image::setNumberOfFrames( int frames )
+{
+    m_numberOfFrames = frames;
+}
+
+int Image::getNumberOfFrames() const
+{
+    return m_numberOfFrames;
+}
+
+void Image::addWindowLevelExplanation( QString explanation )
+{
+    m_windowLevelExplanationList << explanation;
+}
+
+void Image::setWindowLevelExplanations( const QStringList &explanations )
+{
+    m_windowLevelExplanationList = explanations;
+}
+
+QString Image::getWindowLevelExplanation( int index ) const
+{
+    if( index > 0 && index < m_windowLevelExplanationList.size() )
+        return m_windowLevelExplanationList.at(index);
+    else
+    {
+        DEBUG_LOG("Index out of range");
+        return QString();
+    }
+}
+
+void Image::setParentSeries( Series *series )
+{
+    m_parentSeries = series;
+}
+
+Series *Image::getParentSeries() const
+{
+    return m_parentSeries;
+}
+
+void Image::setPath( QString path )
+{
+    m_path = path;
+}
+
+QString Image::getPath() const
+{
+    return m_path;
+}
+
+void Image::addReferencedImage( Image *image )
+{
+    QString uid = image->getSOPInstanceUID();
+    if( uid.isEmpty() )
+    {
+        DEBUG_LOG("L'uid de la imatge està buit! No la podem insertar per inconsistent");
+    }
+    else
+    {
+        m_referencedImageSequence.push_back( image );
+    }
+
+}
+
+Image *Image::getReferencedImage( QString SOPInstanceUID )
+{
+    int i = 0;
+    bool found = false;
+    while( i < m_referencedImageSequence.size() && !found )
+    {
+        if( m_referencedImageSequence.at(i)->getSOPInstanceUID() == SOPInstanceUID )
+            found = true;
+        else
+            i++;
+    }
+    if( !found )
+        return 0;
+    else
+        return m_referencedImageSequence.at(i);
+}
+
+QList<Image *> Image::getReferencedImages()
+{
+    return m_referencedImageSequence;
+}
+
+bool Image::hasReferencedImages() const
+{
+    return ! m_referencedImageSequence.isEmpty();
+}
+
+void Image::setCTLocalizer( bool localizer )
+{
+    m_CTLocalizer = localizer;
+}
+
+bool Image::isCTLocalizer() const
+{
+    return m_CTLocalizer;
+}
+
+QPixmap Image::getThumbnail( int resolution )
+{
+    if( m_thumbnail.isNull() )
+    {
+        createThumbnail( resolution );
+    }
+    return m_thumbnail;
+}
+
+void Image::createThumbnail( int resolution )
+{
+    bool ok = false;
+
+    //TODO atenció! això està fet partint de la classe ScaleImage. No s'ha fet servir ScaleImage per no dependre de l'inputoutput des del core
+
+    //carreguem el fitxer dicom a escalar
+    DicomImage *dicomImage = new DicomImage( qPrintable( getPath() ) );
+
+    if( dicomImage == NULL )
+    {
+        ok = false;
+        DEBUG_LOG("Memòria insuficient");
+    }
+    else if( dicomImage->getStatus() == EIS_Normal )
+    {
+        dicomImage->hideAllOverlays();
+        dicomImage->setMinMaxWindow(1);
+        //escalem l'imatge
+        DicomImage *scaledImage;
+        //Escalem pel cantó més gran
+        unsigned long width, height;
+        if( dicomImage->getWidth() < dicomImage->getHeight() )
+        {
+            width = 0;
+            height = resolution;
+        }
+        else
+        {
+            width = resolution;
+            height = 0;
+        }
+        scaledImage = dicomImage->createScaledImage( width,height, 1, 1 );
+        if( scaledImage == NULL )
+        {
+            ok = false;
+            DEBUG_LOG("La imatge escalada s'ha retornat com a nul");
+        }
+        else if( scaledImage->getStatus() == EIS_Normal )
+        {
+            // el següent codi crea una imatge pgm a memòria i carreguem aquest buffer directament al pixmap
+            // obtingut de http://forum.dcmtk.org/viewtopic.php?t=120&highlight=qpixmap
+            // get image extension
+            const int width = (int)(scaledImage->getWidth());
+            const int height = (int)(scaledImage->getHeight());
+            char header[32];
+            // create PGM header
+            sprintf(header, "P5\n%i %i\n255\n", width, height);
+            const int offset = strlen(header);
+            const unsigned int length = width * height + offset;
+            // create output buffer for DicomImage class
+            Uint8 *buffer = new Uint8[length];
+            if (buffer != NULL)
+            {
+                // copy PGM header to buffer
+                OFBitmanipTemplate<Uint8>::copyMem((const Uint8 *)header, buffer, offset);
+                if( scaledImage->getOutputData((void *)(buffer + offset), length, 8))
+                {
+                    if( m_thumbnail.loadFromData((const unsigned char *)buffer, length, "PGM", Qt::AvoidDither) )
+                    {
+                        ok = true;
+                    }
+                    else
+                        DEBUG_LOG(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Ha fallat :(");
+
+                }
+                // delete temporary pixel buffer
+                delete[] buffer;
+            }
+        }
+        else
+        {
+            ok = false;
+            DEBUG_LOG( QString( "La imatge escalada té errors. Error: %1 ").arg( DicomImage::getString( scaledImage->getStatus() ) ) );
+        }
+    }
+    else
+    {
+        ok = false;
+        DEBUG_LOG( QString( "Error en carregar la DicomImage. Error: %1 ").arg( DicomImage::getString( dicomImage->getStatus() ) ) );
+    }
+
+    if( !ok ) // no hem pogut generar el thumbnail, creem un de buit
+    {
+        m_thumbnail = QPixmap(resolution,resolution);
+        m_thumbnail.fill(Qt::black);
+
+        QPainter painter( &m_thumbnail );
+        painter.setPen(Qt::white);
+        painter.drawText(0, 0, resolution, resolution, Qt::AlignCenter | Qt::TextWordWrap, tr("No Image Available"));
+    }
+}
+
+}
