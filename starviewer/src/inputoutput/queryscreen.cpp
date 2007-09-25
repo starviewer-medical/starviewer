3d8da65e8bccf9af9cd732e21a2148700053613a
Jesús Spínola <jspinola@gmail.com>
2007-09-25 15:22:54 +0000

    STYLE: Millores gramaticals

--- starviewer/src/inputoutput/queryscreen.cpp
+++ starviewer/src/inputoutput/queryscreen.cpp
@@ -1422,7 +1422,7 @@ void QueryScreen::errorQueringStudiesPacs( int PacsID )
     QString errorMessage;
 
     pacsListDB.queryPacs( &errorPacs, PacsID );
-    errorMessage = tr( "Can't query PACS %1 from %2\nBe sure that the IP and AETitle of the PACS is correct" )
+    errorMessage = tr( "Can't query PACS %1 from %2\nBe sure that the IP and AETitle of this PACS are correct" )
         .arg( errorPacs.getAEPacs() )
         .arg( errorPacs.getInstitution()
     );
