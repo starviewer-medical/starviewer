229b0e81fa75fdf2cee856b9200b6220635f1c39
Sergi Dote <sergidt@gmail.com>
2007-09-25 11:36:07 +0000

    ENH: traduccions de l'extensió cardiac2dviewer

--- starviewer/src/extensions/playground/cardiac2dviewer/translations_es_ES.ts
+++ starviewer/src/extensions/playground/cardiac2dviewer/translations_es_ES.ts
@@ -1,11 +1,21 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="es">
 <context>
     <name>udg::Cardiac2DViewerExtensionMediator</name>
     <message>
-        <location filename="cardiac2dviewerextensionmediator.cpp" line="24"/>
+        <location filename="cardiac2dviewerextensionmediator.cpp" line="27"/>
         <source>2D Cardiac Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Visor cardiaco 2D</translation>
+    </message>
+    <message>
+        <location filename="cardiac2dviewerextensionmediator.cpp" line="41"/>
+        <source>Starviewer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="cardiac2dviewerextensionmediator.cpp" line="41"/>
+        <source>The selected item is not an image</source>
+        <translation>El ítem seleccionado no es una imagen</translation>
     </message>
 </context>
 <context>
@@ -13,150 +23,150 @@
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="63"/>
         <source>&amp;Axial View</source>
-        <translation type="unfinished"></translation>
+        <translation>Vista &amp;Axial</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="64"/>
         <source>Ctrl+A</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="65"/>
         <source>Change Current View To Axial</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar la vista actual a Axial</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="70"/>
         <source>&amp;Sagital View</source>
-        <translation type="unfinished"></translation>
+        <translation>Vista &amp;Sagital</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="71"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="72"/>
-        <source>Change Current View To Sagital</source>
-        <translation type="unfinished"></translation>
+        <source>Change Current View To Saggital</source>
+        <translation>Cambiar la vista actual a Sagital</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="78"/>
         <source>&amp;Coronal View</source>
-        <translation type="unfinished"></translation>
+        <translation>Vista &amp;Coronal</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="79"/>
         <source>Ctrl+C</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="80"/>
         <source>Change Current View To Coronal</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar la vista actual a Coronal</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="86"/>
         <source>Space</source>
-        <translation type="unfinished"></translation>
+        <translation>Espacio</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="92"/>
         <source>Voxel Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Información de vóxel</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="93"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="94"/>
         <source>Enable voxel information over cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Activar la información de vóxel</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="102"/>
         <source>Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación en sentido horario</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="104"/>
         <source>Rotate the image in clockwise direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación de la imagen en sentido horario</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="111"/>
         <source>Rotate Counter Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación en sentido antihorario</translation>
     </message>
     <message>
         <location filename="qcardiac2dviewerextension.cpp" line="113"/>
         <source>Rotate the image in counter clockwise direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación de la imagen en sentido antihorario</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextension.cpp" line="313"/>
+        <location filename="qcardiac2dviewerextension.cpp" line="314"/>
         <source>Save video file as...</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar archivo de video como...</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextension.cpp" line="332"/>
+        <location filename="qcardiac2dviewerextension.cpp" line="333"/>
         <source>MPEG (*.mpg)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextension.cpp" line="337"/>
+        <location filename="qcardiac2dviewerextension.cpp" line="338"/>
         <source>AVI (*.avi)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextension.cpp" line="355"/>
+        <location filename="qcardiac2dviewerextension.cpp" line="356"/>
         <source>Making video</source>
-        <translation type="unfinished"></translation>
+        <translation>Produciendo video</translation>
     </message>
 </context>
 <context>
     <name>udg::QCardiac2DViewerExtensionBase</name>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="307"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="304"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="80"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="77"/>
         <source>window level</source>
-        <translation type="unfinished"></translation>
+        <translation>Brillo y contraste</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="102"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="99"/>
         <source>zoom</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="124"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="121"/>
         <source>move</source>
-        <translation type="unfinished"></translation>
+        <translation>mover</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="285"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="282"/>
         <source>voxel information</source>
-        <translation type="unfinished"></translation>
+        <translation>información de vóxel</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="352"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="349"/>
         <source>WW/WL</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="742"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="745"/>
         <source>Velocity</source>
-        <translation type="unfinished"></translation>
+        <translation>Velocidad</translation>
     </message>
     <message>
-        <location filename="qcardiac2dviewerextensionbase.ui" line="749"/>
+        <location filename="qcardiac2dviewerextensionbase.ui" line="752"/>
         <source>frames/sec</source>
-        <translation type="unfinished"></translation>
+        <translation>frames/seg</translation>
     </message>
 </context>
 </TS>
