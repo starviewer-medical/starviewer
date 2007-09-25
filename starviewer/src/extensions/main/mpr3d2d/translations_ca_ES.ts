c80c76460ea9ae1805b14e721e1e41115132c8ca
Sergi Dote <sergidt@gmail.com>
2007-09-25 10:59:17 +0000

    ENH: traduccions de l'extensió mpr3d2d.

--- starviewer/src/extensions/main/mpr3d2d/translations_ca_ES.ts
+++ starviewer/src/extensions/main/mpr3d2d/translations_ca_ES.ts
@@ -5,40 +5,50 @@
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="224"/>
         <source>Axial</source>
-        <translation type="unfinished">Axial</translation>
+        <translation>Axial</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="251"/>
         <source>Sagital</source>
-        <translation type="unfinished">Sagital</translation>
+        <translation>Sagital</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="270"/>
         <source>Coronal</source>
-        <translation type="unfinished">Coronal</translation>
+        <translation>Coronal</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="356"/>
         <source>WW/WL:</source>
-        <translation type="unfinished">WW/WL:</translation>
+        <translation>WW/WL:</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="313"/>
         <source>...</source>
-        <translation type="unfinished">...</translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextensionbase.ui" line="16"/>
         <source>Form1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>udg::MPR3D2DExtensionMediator</name>
     <message>
-        <location filename="mpr3d2dextensionmediator.cpp" line="24"/>
+        <location filename="mpr3d2dextensionmediator.cpp" line="27"/>
         <source>MPR 3D-2D</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mpr3d2dextensionmediator.cpp" line="41"/>
+        <source>Starviewer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mpr3d2dextensionmediator.cpp" line="41"/>
+        <source>The selected item is not an image</source>
+        <translation>L&apos;ítem seleccionat no és una imatge</translation>
     </message>
 </context>
 <context>
@@ -46,27 +56,27 @@
     <message>
         <location filename="qmpr3d2dextension.cpp" line="109"/>
         <source>Switch horizontal layout</source>
-        <translation type="unfinished">Canviar la distribució horitzontal</translation>
+        <translation>Canviar la distribució horitzontal</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextension.cpp" line="115"/>
         <source>Change views layout</source>
-        <translation type="unfinished">Canviar la distribució de les vistes</translation>
+        <translation>Canviar la distribució de les vistes</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextension.cpp" line="129"/>
         <source>Voxel Information</source>
-        <translation type="unfinished">Informació de voxel</translation>
+        <translation>Informació de vòxel</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextension.cpp" line="130"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished">Ctrl+I</translation>
+        <translation>Ctrl+I</translation>
     </message>
     <message>
         <location filename="qmpr3d2dextension.cpp" line="131"/>
         <source>Enable voxel information over cursor</source>
-        <translation type="unfinished">Habilitar la informació devoxel sobre el cursor</translation>
+        <translation>Habilitar la informació de vòxel sobre el cursor</translation>
     </message>
 </context>
 </TS>
