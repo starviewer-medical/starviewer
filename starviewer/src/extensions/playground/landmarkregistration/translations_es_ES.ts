bcbcf81598f1b95338143732e0852bc01e4b7e30
Sergi Dote <sergidt@gmail.com>
2007-09-26 13:52:18 +0000

    ENH: traducció de l'extensió de landmark registration

--- starviewer/src/extensions/playground/landmarkregistration/translations_es_ES.ts
+++ starviewer/src/extensions/playground/landmarkregistration/translations_es_ES.ts
@@ -1,99 +1,109 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="es">
 <context>
     <name>QLandmarkRegistrationExtensionBase</name>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="16"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="203"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="77"/>
         <source>window level</source>
-        <translation type="unfinished"></translation>
+        <translation>Brillo y contraste</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="99"/>
         <source>zoom</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="121"/>
         <source>move</source>
-        <translation type="unfinished"></translation>
+        <translation>mover</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="181"/>
         <source>voxel information</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="230"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacidad</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="295"/>
         <source>Seed List 1:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista de semillas 1:</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="389"/>
         <source>X</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="394"/>
         <source>Y</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="399"/>
         <source>Z</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="355"/>
         <source>Seed List 2:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista de semillas 2:</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="407"/>
         <source>Apply Registration</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicar registro</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="414"/>
         <source>Save Transform</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar transformada</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="421"/>
         <source>Load Transform</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargar transformada</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="428"/>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation>Restaurar</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextensionbase.ui" line="530"/>
         <source>Slice:</source>
-        <translation type="unfinished"></translation>
+        <translation>Corte:</translation>
     </message>
 </context>
 <context>
     <name>udg::LandmarkRegistrationExtensionMediator</name>
     <message>
-        <location filename="landmarkregistrationextensionmediator.cpp" line="24"/>
+        <location filename="landmarkregistrationextensionmediator.cpp" line="27"/>
         <source>Landmark Registration</source>
-        <translation type="unfinished"></translation>
+        <translation>registro por marcas</translation>
+    </message>
+    <message>
+        <location filename="landmarkregistrationextensionmediator.cpp" line="41"/>
+        <source>Starviewer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="landmarkregistrationextensionmediator.cpp" line="41"/>
+        <source>The selected item is not an image</source>
+        <translation>El ítem seleccionado no es una imagen</translation>
     </message>
 </context>
 <context>
@@ -101,72 +111,72 @@
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="89"/>
         <source>Voxel Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Información de vóxel</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="90"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="91"/>
         <source>Enable voxel information over cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Activar la información de vóxel</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="100"/>
         <source>Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación en sentido horario</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="102"/>
         <source>Rotate the image in clockwise direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación de la imagen en sentido horario</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="110"/>
         <source>Rotate Counter Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación en sentido antihorario</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="112"/>
         <source>Rotate the image in counter clockwise direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotación de la imagen en sentido antihorario</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="134"/>
         <source>SeedTool</source>
-        <translation type="unfinished"></translation>
+        <translation>Herramienta de semillas</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="135"/>
         <source>Enable/Disable seeding tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Activar/Desactivar la herramienta de semillas</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="275"/>
         <source>StarViewer</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qlandmarkregistrationextension.cpp" line="275"/>
-        <source>El nombre de llavors &#xc3;&#xa9;s diferent en les dues imatges</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="1097"/>
         <source>Save Transform file</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar fichero de transformada</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="1120"/>
         <source>all Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>todos los ficheros (*)</translation>
     </message>
     <message>
         <location filename="qlandmarkregistrationextension.cpp" line="1120"/>
         <source>Chose a transform filename</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione un fichero de trasformada</translation>
+    </message>
+    <message>
+        <location filename="qlandmarkregistrationextension.cpp" line="275"/>
+        <source>There are not the same number of seeds into images</source>
+        <translation>No hay el mismo número de semillas en la s dos imégenes</translation>
     </message>
 </context>
 </TS>
