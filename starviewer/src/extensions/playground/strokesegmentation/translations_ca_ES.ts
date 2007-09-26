f3d7acfbabf9ccf16e93f591ac6eb90af7aaddb4
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2007-09-26 10:18:48 +0000

    ENH: S'ha afegit els arxius de traduccions.

--- starviewer/src/extensions/playground/strokesegmentation/translations_ca_ES.ts
+++ starviewer/src/extensions/playground/strokesegmentation/translations_ca_ES.ts
@@ -1,91 +1,91 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="ca">
 <context>
     <name>QStrokeSegmentationExtensionBase</name>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="13"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="215"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="83"/>
         <source>window level</source>
-        <translation type="unfinished"></translation>
+        <translation>Lluentor</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="105"/>
         <source>zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoom</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="127"/>
         <source>move</source>
-        <translation type="unfinished"></translation>
+        <translation>Moure</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="193"/>
         <source>voxel information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informació del voxel</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="242"/>
         <source>View thresholds</source>
-        <translation type="unfinished"></translation>
+        <translation>Veure els umbrals</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="257"/>
         <source>Lower value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Valor mínim:</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="291"/>
         <source>Upper value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Valor màxim:</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="325"/>
         <source>Opacity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacitat:</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="395"/>
         <source>Seed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Llavor:</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="490"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicar</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="508"/>
         <source>Volume (in mm3):</source>
-        <translation type="unfinished"></translation>
+        <translation>Volum (en mm3):</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="550"/>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualitzar</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="577"/>
         <source>Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Editor</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="665"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamany</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextensionbase.ui" line="731"/>
         <source>Slice:</source>
-        <translation type="unfinished"></translation>
+        <translation>Llesca:</translation>
     </message>
 </context>
 <context>
@@ -93,55 +93,65 @@
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="99"/>
         <source>Voxel Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informació del voxel</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="100"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="101"/>
         <source>Enable voxel information over cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar l&apos;informació del voxel sota el cursor</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="109"/>
         <source>Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotació a la dreta</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="111"/>
         <source>Rotate the image in clockwise direction</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotació de la imatge en el sentit de les agulles del rellotge</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="136"/>
         <source>EditorTool</source>
-        <translation type="unfinished"></translation>
+        <translation>Eina d&apos;edició</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="137"/>
         <source>Enable/Disable editor tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar/Deshabilitar l&apos;eina d&apos;edició</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="246"/>
         <source>StarViewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Starviewer</translation>
     </message>
     <message>
         <location filename="qstrokesegmentationextension.cpp" line="246"/>
         <source>ERROR: no hi ha definida llavor o m&#xc3;&#xa0;scara</source>
-        <translation type="unfinished"></translation>
+        <translation>ERROR: no hi ha definida cap llavor o màscara</translation>
     </message>
 </context>
 <context>
     <name>udg::StrokeSegmentationExtensionMediator</name>
     <message>
-        <location filename="strokesegmentationextensionmediator.cpp" line="24"/>
+        <location filename="strokesegmentationextensionmediator.cpp" line="27"/>
         <source>Stroke Segmentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Segmentació d&apos;infarts</translation>
+    </message>
+    <message>
+        <location filename="strokesegmentationextensionmediator.cpp" line="41"/>
+        <source>Starviewer</source>
+        <translation>Starviewer</translation>
+    </message>
+    <message>
+        <location filename="strokesegmentationextensionmediator.cpp" line="41"/>
+        <source>The selected item is not an image</source>
+        <translation>L&apos;element seleccionat no es una imatge</translation>
     </message>
 </context>
 </TS>
