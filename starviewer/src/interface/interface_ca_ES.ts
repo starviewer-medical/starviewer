e8368352c1c84e5891a5b755e0c09dad84962471
Jesús Spínola <jspinola@gmail.com>
2007-10-10 10:00:25 +0000

    ENH: Actualització de les traduccions

--- starviewer/src/interface/interface_ca_ES.ts
+++ starviewer/src/interface/interface_ca_ES.ts
@@ -49,14 +49,24 @@
 <context>
     <name>udg::ExtensionHandler</name>
     <message>
-        <location filename="extensionhandler.cpp" line="139"/>
+        <location filename="extensionhandler.cpp" line="150"/>
         <source>Loading, please wait...</source>
         <translation>Carregant, si us plau espereu...</translation>
     </message>
     <message>
-        <location filename="extensionhandler.cpp" line="138"/>
+        <location filename="extensionhandler.cpp" line="149"/>
         <source>Patient loading</source>
         <translation>Carregant el pacient</translation>
+    </message>
+    <message>
+        <location filename="extensionhandler.cpp" line="236"/>
+        <source>Starviewer</source>
+        <translation>Starviewer</translation>
+    </message>
+    <message>
+        <location filename="extensionhandler.cpp" line="236"/>
+        <source>Sorry, an error ocurred while loading the data of patients:&lt;br&gt; %1</source>
+        <translation>Ho sentim, hi ha hagut algun error mentres es carregaven les dades dels pacients:&lt;br&gt; %1</translation>
     </message>
 </context>
 <context>
@@ -127,22 +137,22 @@
         <translation>Ctrl+W</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="210"/>
+        <location filename="qapplicationmainwindow.cpp" line="211"/>
         <source>&amp;File</source>
         <translation>&amp;Arxiva</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="241"/>
+        <location filename="qapplicationmainwindow.cpp" line="242"/>
         <source>&amp;Help</source>
         <translation>&amp;Ajuda</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="403"/>
+        <location filename="qapplicationmainwindow.cpp" line="420"/>
         <source>About Starviewer</source>
         <translation>Sobre l&apos;Starviewer</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="230"/>
+        <location filename="qapplicationmainwindow.cpp" line="231"/>
         <source>&amp;Language</source>
         <translation>&amp;Idioma</translation>
     </message>
@@ -167,7 +177,7 @@
         <translation>Obrir la finestra de consulta del PACS</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="221"/>
+        <location filename="qapplicationmainwindow.cpp" line="222"/>
         <source>&amp;Visualization</source>
         <translation>&amp;Visualització</translation>
     </message>
@@ -182,7 +192,7 @@
         <translation>Obrir una carpeta DICOM existent</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="292"/>
+        <location filename="qapplicationmainwindow.cpp" line="293"/>
         <source>Language Switch</source>
         <translation>Canvi d&apos;idioma</translation>
     </message>
@@ -217,7 +227,7 @@
         <translation>Mostrar arxiu de bitàcola</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="235"/>
+        <location filename="qapplicationmainwindow.cpp" line="236"/>
         <source>&amp;Window</source>
         <translation>&amp;Finestra</translation>
     </message>
@@ -227,12 +237,12 @@
         <translation>Obrir l&apos;aplicació %1</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="292"/>
+        <location filename="qapplicationmainwindow.cpp" line="293"/>
         <source>The changes will take effect the next time you startup the application</source>
         <translation>Els canvis tindran efecte el pròxim cop que iniciïs l&apos;aplicació</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="417"/>
+        <location filename="qapplicationmainwindow.cpp" line="434"/>
         <source>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2005-2007 Grup de Gr&#xc3;&#xa0;fics Girona&lt;p align=&apos;justify&apos;&gt;Starviewer is an image processing software dedicated to DICOM images produced by medical equipment (MRI, CT, PET, PET-CT...) It can also read many other file formats especified by the MetaIO estandard ( *.mhd files ). It is fully compliant with the DICOM standard for image comunication and image file formats. Starviewer is able to receive images transferred by DICOM communication protocol from any PACS or medical imaging modality (STORE SCP - Service Class Provider, STORE SCU - Service Class User, and Query/Retrieve).&lt;p align=&apos;justify&apos;&gt;Starviewer has been specifically designed for navigation and visualization of multimodality and multidimensional images: 2D Viewer, 2D MPR ( Multiplanar reconstruction ) Viewer , 3D MPR Viewer and Hybrid MPR Viewer and Maximum Intensity Projection(MIP).&lt;p align=&apos;justify&apos;&gt;Starviewer is at the same time a DICOM PACS workstation for medical imaging and an image processing software for medical research (radiology and nuclear imaging), functional imaging, 3D imaging, confocal microscopy and molecular imaging.&lt;p&gt;Version : %1 &lt;/p&gt;</source>
         <translation>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2005-2007 Grup de Gràfics de Girona&lt;p align=&apos;justify&apos;&gt;Starviewer és un software de processament d&apos;imatges DICOM produïdes a partir d&apos;equipament mèdic (MRI, CT, PET, PET-CT...). Pot llegir altres formats d&apos;arxiu especificats per l&apos;estàndard MetalO (arxius *.mhd). És completament compatible amb l&apos;estàndard DICOM per la comunicació d&apos;imatges i els seus formats. Starviewer està dissenyat específicament per la navegació i visualització d&apos;imatges multi modalitat i multidimensinals: visualitzador 2D, visualitzador MPR 2D (reconstrucció multi planar), visualitzador MPR 3D i un visualitzador híbrid MPR i MIP (Maximum Intensity Projection), &lt;p align=&apos;justify&apos;&gt;Starviewer és al mateix temps una estació de treball de DICOM PACS per a la visualització mèdica i un software per el processament d&apos;imatges mèdiques per la recerca (radiologia i visualització imatges nuclears), imatges funcionals, visualització imatges 3D i visualització imatges moleculars.&lt;p&gt;Versió: %1&lt;/p&gt;</translation>
     </message>
@@ -262,37 +272,37 @@
         <translation>Modificar la configuració de l&apos;Starviewer</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="229"/>
+        <location filename="qapplicationmainwindow.cpp" line="230"/>
         <source>&amp;Tools</source>
         <translation>&amp;Eines</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="250"/>
+        <location filename="qapplicationmainwindow.cpp" line="251"/>
         <source>Catalan</source>
         <translation>Català</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="251"/>
+        <location filename="qapplicationmainwindow.cpp" line="252"/>
         <source>Spanish</source>
         <translation>Castellà</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="252"/>
+        <location filename="qapplicationmainwindow.cpp" line="253"/>
         <source>English</source>
         <translation>Anglès</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="280"/>
+        <location filename="qapplicationmainwindow.cpp" line="281"/>
         <source>Switch to %1 Language</source>
         <translation>Canviar al llenguatge %1</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="457"/>
+        <location filename="qapplicationmainwindow.cpp" line="474"/>
         <source>Beta version</source>
         <translation>Versió Beta</translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="466"/>
+        <location filename="qapplicationmainwindow.cpp" line="483"/>
         <source>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p align=&apos;justify&apos;&gt;This version of Starviewer is a preview release of our next Starviewer version and it is being made available for testing purposes only.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;This version is intended for radiologist and our testing members that are helping us to improve this software. Current users of this version should not expect all of the extensions to work properly.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;If you  want to help us to improve our software, please, report any bug you found or any other feature request you have to us.&lt;/p&gt;&lt;h3&gt;We really appreciate that you give us your feedback!&lt;/h3&gt;</source>
         <translation>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p align=&apos;justify&apos;&gt;Aquesta versió d&apos;Starviewer és una versió prèvia de la propera versió oficial d&apos;Starviewer i està en una fase de proves.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;Aquesta versió prèvia està pensada per als radiòlegs i membres de testeig que ens ajuden a millorar aquesta aplicació. Els usuaris actuals d&apos;aquesta versió han de ser conscients que no totes les funcionalitats funcionaran a la perfecció durant aquesta fase.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;Si vols ajudar-nos a millorar l&apos;aplicació, si us plau informa&apos;ns de qualsevol error o qualsevol altre petició de funcionalitats que creguis convenients.&lt;/p&gt;&lt;h3&gt;El vostre feedback és fonamental per la millora de l&apos;Starviewer!&lt;/h3&gt;</translation>
     </message>
