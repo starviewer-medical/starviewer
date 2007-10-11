b054c46c552f4d7c7f55b0d1799fc4248328c062
Jesús Spínola <jspinola@gmail.com>
2007-10-11 11:06:31 +0000

    ENH: Re-colocació dels camps del QStudyTreeWidget. Li donem el protagonisme al nom del pacient

--- starviewer/src/inputoutput/qstudytreewidget.h
+++ starviewer/src/inputoutput/qstudytreewidget.h
@@ -41,6 +41,10 @@ class QStudyTreeWidget : public QWidget , private Ui::QStudyTreeWidgetBase
 {
 Q_OBJECT
 public:
+    enum ColumnIndex{ PatientName = 0, PatientID = 1, PatientAge = 2, Description = 3, Modality = 4, Date = 5, Time = 6,
+    PACSAETitle = 7, Institution = 8, UID = 9, StudyID = 10, ProtocolName = 11, AccNumber = 12, Type = 13,
+    ImageNumber = 14, RefPhysName = 15, PPStartDate = 16, PPStartTime = 17, ReqProcID = 18, SchedProcStep = 19
+    };
 
     /// Constructor de la classe
     QStudyTreeWidget( QWidget *parent = 0 );
--- starviewer/src/inputoutput/queryscreen.cpp
+++ starviewer/src/inputoutput/queryscreen.cpp
@@ -473,7 +473,7 @@ void QueryScreen::queryStudyPacs()
     m_studyTreeWidgetPacs->insertStudyList( m_studyListSingleton ); //fem que es visualitzi l'studyView seleccionat
     m_studyTreeWidgetPacs->insertSeriesList( m_seriesListSingleton );
     m_studyTreeWidgetPacs->insertImageList( m_imageListSingleton );
-    m_studyTreeWidgetPacs->setSortColumn( 2 );//ordenem pel nom TODO hauríem de tenir un mètode sortBy, que passant-li uns enums definits ho fes, perquè així si canviem la columna on tenim el nom, haurem de canviar tots els "setSortColumn( 2 )"
+    m_studyTreeWidgetPacs->setSortColumn( QStudyTreeWidget::PatientName );
 
     QApplication::restoreOverrideCursor();
 }
@@ -551,13 +551,13 @@ void QueryScreen::queryStudy( QString source )
         if( source == "Cache" )
         {
             m_studyTreeWidgetCache->insertStudyList( &studyList );//es mostra la llista d'estudis
-            m_studyTreeWidgetCache->setSortColumn( 2 ); //ordenem pel nom
+            m_studyTreeWidgetCache->setSortColumn( QStudyTreeWidget::PatientName ); //ordenem pel nom
         }
         else if( source == "DICOMDIR" )
         {
             m_studyTreeWidgetDicomdir->clear();
             m_studyTreeWidgetDicomdir->insertStudyList( &studyList );
-            m_studyTreeWidgetDicomdir->setSortColumn( 2 );//ordenem pel nom
+            m_studyTreeWidgetDicomdir->setSortColumn( QStudyTreeWidget::PatientName );//ordenem pel nom
         }
     }
     QApplication::restoreOverrideCursor();
