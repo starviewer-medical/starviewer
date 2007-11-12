ffd9ff8860eaee85e737542b76a63d10726845bb
Jesús Spínola <jspinola@gmail.com>
2007-11-12 17:42:05 +0000

    ENH: Refactoring de ToolData per OldToolData

--- starviewer/src/core/distancetooldata.h
+++ starviewer/src/core/distancetooldata.h
@@ -7,7 +7,7 @@
 #ifndef UDGDISTANCETOOLDATA_H
 #define UDGDISTANCETOOLDATA_H
 
-#include "tooldata.h"
+#include "oldtooldata.h"
 
 //Forward declarations
 class QString;
@@ -19,58 +19,58 @@ Classe que implementa una especialització de la classe ToolData, per a contenir
 @author Grup de Gràfics de Girona  ( GGG )
 */
 
-class DistanceToolData : public ToolData {
+class DistanceToolData : public OldToolData {
 Q_OBJECT
 public:
     ///constructor amb paràmetres
     DistanceToolData( double p1[3], double p2[3] );
-    
+
     ~DistanceToolData();
-    
+
     ///assignem el primer punt
-    void setFirstPoint( double p1[3] ); 
-    
+    void setFirstPoint( double p1[3] );
+
     ///assignem el segon punt
     void setSecondPoint( double p2[3] );
-    
+
     ///retornem el primer punt
     double* getFirstPoint()
-    { return( m_firstPoint ); } 
-    
+    { return( m_firstPoint ); }
+
     ///retornem el segon punt
     double* getSecondPoint()
     { return( m_secondPoint ); }
-    
+
     ///retornem el segon punt
     double* getTextPosition()
     { return( m_textPosition ); }
-    
+
     ///retornem el text
     QString getDistanceText()
     { return( m_text ); }
-    
+
     ///Mètode per calcular el la distància entre els dos punts, per tant el text que contindrà la distància i la posició
     void calculateDistance();
-    
+
 signals:
     ///s'emet quan canvia el primer punt de la distància
     void firstPointChanged();
-    
+
     ///s'emet quan canvia el segon punt de la distància
     void secondPointChanged();
-    
+
     ///s'emet quan canvia el text de la distància
     void distanceTextChanged();
 
 private:
-    
+
     ///atributs corresponents als punts de la distància
     double m_firstPoint[3];
     double m_secondPoint[3];
-    
+
     ///atribut que conté el text de la distància
     QString m_text;
-    
+
     ///atribut corresponent a la posició del text de la distància
     double m_textPosition[3];
 };
--- starviewer/src/core/roitooldata.h
+++ starviewer/src/core/roitooldata.h
@@ -7,7 +7,7 @@
 #ifndef UDGROITOOLDATA_H
 #define UDGROITOOLDATA_H
 
-#include "tooldata.h"
+#include "oldtooldata.h"
 #include <QList>
 
 //Forward declarations
@@ -20,64 +20,64 @@ Classe que implementa una especialització de la classe ToolData, per a contenir
 @author Grup de Gràfics de Girona  ( GGG )
 */
 
-class ROIToolData : public ToolData {
+class ROIToolData : public OldToolData {
 Q_OBJECT
 
 private:
-    
+
     typedef QList< double* > PointsList;
-    
+
     ///atribut corresponent als punts de la ROI
     PointsList m_points;
-    
+
     ///atribut que conté el text de la distància
     QString m_text;
-    
+
     ///ens diu el tipus de roi que encapsula
     QString m_typeOfROI;
-    
+
     ///atribut corresponent a la posició del text de la distància
     double m_textPosition[3];
 
 public:
-    
+
     ///constructor amb paràmetres
     ROIToolData( PointsList points, QString typeOfROI );
-    
+
     ~ROIToolData();
-    
+
     ///assignem els punts del polígon
-    void setPoints( PointsList points ); 
-    
+    void setPoints( PointsList points );
+
     ///retornem els punts de la ROI
     PointsList getPoints()
-    { return( m_points ); } 
+    { return( m_points ); }
 
-    
+
     ///retornem el segon punt
     double* getTextPosition()
     { return( m_textPosition ); }
-    
+
     ///retornem el text
     QString getROIText()
     { return( m_text ); }
-    
+
     ///ens retorna el tipus de roi
     QString getTypeOfROI()
     { return( m_typeOfROI ); }
-    
+
     ///ens permet assignar el tipus de roi
     void setTypeOfROI( QString type )
     { m_typeOfROI = type; }
-    
+
     ///a partir dels punts del rectangle que inscriu l'el·lipse, calcula l'àrea, crea el text i la posició d'aquest
     void calculateTextArea( double *topLeft, double *bottomRight, int view );
-    
+
 signals:
-    
+
     ///s'emet quan canvia algun punt de la ROI
     void listOfPointsChanged();
-    
+
     ///s'emet quan canvia el text de la ROI
     void roiTextChanged();
 };
