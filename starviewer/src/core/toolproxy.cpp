ae35dbeb29cc5a0e61e4ed1e5eb164ba801061f0
Sergi Dote <sergidt@gmail.com>
2007-11-15 12:22:59 +0000

    ENH: s'ha programat el ToolProxy amb les noves funcionalitats especificades en l'últim disseny de l'estructura de tools.

--- starviewer/src/core/toolproxy.cpp
+++ starviewer/src/core/toolproxy.cpp
@@ -5,18 +5,45 @@
  *   Universitat de Girona                                                 *
  ***************************************************************************/
 #include "toolproxy.h"
+#include "tool.h"
+
+#include <QString>
+#include <QList>
 
 namespace udg {
 
 ToolProxy::ToolProxy(QObject *parent)
  : QObject(parent)
-{
-}
-
+{}
 
 ToolProxy::~ToolProxy()
-{
-}
+{}
 
+void ToolProxy::addTool( Tool *tool )
+{
+    m_toolsMap.insert( tool->toolName(), tool );
+}
+    
+bool ToolProxy::removeTool( QString toolName )
+{
+    int numberRemoved = m_toolsMap.remove( toolName );
+    return numberRemoved > 0;
+}
+    
+void ToolProxy::removeAllTools()
+{
+    m_toolsMap.clear();
+}
+    
+void ToolProxy::forwardEvent( unsigned long eventID )
+{
+    //no es pot fer un foreach sobre un map perquè retorna parella d'elements, per això passem tots els elements del map a una QList.
+    QList <Tool*> toolsList = m_toolsMap.values();
+    
+    foreach( Tool *tool, toolsList )
+    {
+        tool->handleEvent( eventID );
+    }
+}
 
 }
--- starviewer/src/core/toolproxy.h
+++ starviewer/src/core/toolproxy.h
@@ -8,14 +8,28 @@
 #define UDGTOOLPROXY_H
 
 #include <QObject>
+#include <QMap>
+
+//forward declarations
+class QString;
 
 namespace udg {
 
+//forward declarations space udg
+class Tool;
+
 /**
-Classe encarregada de mantenir les tools actives per un visualitzador i de proporcionar-lis els events corresponents a aquestes
+Classe encarregada de mantenir les tools actives per un visualitzador i de proporcionar-lis els events corresponents.
+Les seves tasques són:
+    - Rep tots els events d'un viewer
+    - Coneix les tools actives per a un determinat viewer
+    - Passa els events rebuts a totes les tools actives
+    - Està associat a un viewer com atribut d'aquest
+    - Guarda el nom de cadascuna de les tools
 
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
+
 class ToolProxy : public QObject
 {
 Q_OBJECT
@@ -24,6 +38,32 @@ public:
 
     ~ToolProxy();
 
+    //mètodes
+
+    ///addTool: ens permet afegir una tool.
+    void addTool( Tool *tool );
+    
+    ///removeTool: ens permet eliminar una tool a partir del nom d'aquesta. Ens retorna true si s'ha trobat l'element i s'ha pogut eliminar, 
+    ///            false en cas contrari.
+    bool removeTool( QString toolName );
+    
+    ///removeAllTools: ens permet eliminar totes les tools que té guardades.
+    void removeAllTools();
+    
+public slots: 
+
+    ///Avalua l'event que ha rebut del visualitzador al que està associat i l'envia a les tools actives
+    void forwardEvent( unsigned long eventID );
+    
+private:
+     /// Tipus definit:  map on hi guardem les tools associades a una clau determinada pel nom de la tool.
+     ///                 en principi no es poden tenir dues tools amb el mateix nom, per tant podem fer-ho
+     ///                 amb un map, no cal un multimap.
+     
+    typedef QMap< QString, Tool* > ToolsMap;
+    
+    ///atribut de tipus ToolsMap
+    ToolsMap m_toolsMap;
 };
 
 }
