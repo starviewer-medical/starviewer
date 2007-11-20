6f879ccad95d76612d02d36edc619e75f13a1f1e
Marc Ruiz <marc.ruiz@gmail.com>
2007-11-20 13:40:03 +0000

    ENH: Actualitzats els fitxers de traducció amb noves cadenes.

--- starviewer/src/extensions/playground/optimalviewpoint/translations_en_GB.ts
+++ starviewer/src/extensions/playground/optimalviewpoint/translations_en_GB.ts
@@ -1,327 +1,282 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>OptimalViewpointInputParametersFormBase</name>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="22"/>
         <source>Segmentation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="34"/>
         <source>Load from file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="80"/>
         <source>Segmentation file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="87"/>
         <source>Open...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="97"/>
         <source>Automatic segmentation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="145"/>
         <source>&amp;Block length</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="155"/>
         <source>Block length (L) to compute excess entropy during segmentation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="189"/>
         <source>&amp;Iterations</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="199"/>
         <source>Number of iterations of the genetic algorithm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="233"/>
         <source>N&amp;oise</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="243"/>
         <source>Maximum variation in one step of the genetic algorithm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="280"/>
         <source>I&amp;mage sample distance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="290"/>
         <source>Horizontal and vertical distance between rays during segmentation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="330"/>
         <source>&amp;Number of clusters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="340"/>
         <source>Number of clusters (N) of the volume, including empty space</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="374"/>
         <source>Sample &amp;distance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="384"/>
         <source>Distance between samples on one ray during segmentation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="438"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="458"/>
         <source>&amp;Visualization parameters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="470"/>
         <source>Advanced controls</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="503"/>
         <source>If checked, shading will be applied when rendering the volume</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="506"/>
         <source>S&amp;hade</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="513"/>
         <source>Compute with opacity</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="530"/>
         <source>Interpolation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="541"/>
         <source>Nearest Neighbour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="546"/>
         <source>Linear (Interpolate &amp; Classify)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="551"/>
         <source>Linear (Classify &amp; Interpolate)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="569"/>
         <source>Specular</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="584"/>
         <source>Power</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="616"/>
         <source>Update plane</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="626"/>
         <source>All</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="646"/>
         <source>Cluster</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="655"/>
         <source>First</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="665"/>
         <source>Last</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="679"/>
         <source>Read extent from file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="697"/>
         <source>Number of &amp;planes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="707"/>
         <source>Number of planes to use</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="711"/>
         <source>0</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="716"/>
         <source>3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="721"/>
         <source>4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="726"/>
         <source>6</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="731"/>
         <source>8</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="736"/>
         <source>12</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="741"/>
         <source>20</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="746"/>
         <source>42</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="751"/>
         <source>162</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="769"/>
         <source>Block &amp;length</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="779"/>
         <source>Block length (L) to compute excess entropy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="805"/>
         <source>Ima&amp;ge sample distance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="815"/>
         <source>Horizontal and vertical distance between rays</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="847"/>
         <source>Sample dis&amp;tance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="857"/>
         <source>Distance between samples on one ray</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="881"/>
         <source>Compute entropy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="896"/>
         <source>Similarity threshold</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="933"/>
         <source>Load transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="936"/>
         <source>Load...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="943"/>
         <source>Save transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="946"/>
         <source>Save...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="956"/>
         <source>Graphic editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="961"/>
         <source>Editor by values</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="982"/>
         <source>Define the transfer function for the volume: left click on free space to add a new point; drag a point to move it; right click on a point to delete it</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersformbase.ui" line="1009"/>
         <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normalized</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QTransferFunctionEditorByValuesBase</name>
     <message>
-        <location filename="qtransferfunctioneditorbyvaluesbase.ui" line="30"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qtransferfunctioneditorbyvaluesbase.ui" line="37"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
@@ -329,12 +284,10 @@
 <context>
     <name>QTransferFunctionIntervalEditorBase</name>
     <message>
-        <location filename="qtransferfunctionintervaleditorbase.ui" line="22"/>
         <source>Interval</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qtransferfunctionintervaleditorbase.ui" line="55"/>
         <source>Select color...</source>
         <translation type="unfinished"></translation>
     </message>
@@ -342,17 +295,14 @@
 <context>
     <name>udg::OptimalViewpointExtensionMediator</name>
     <message>
-        <location filename="optimalviewpointextensionmediator.cpp" line="27"/>
         <source>Optimal Viewpoint</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointextensionmediator.cpp" line="41"/>
         <source>Starviewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointextensionmediator.cpp" line="41"/>
         <source>The selected item is not an image</source>
         <translation type="unfinished"></translation>
     </message>
@@ -360,47 +310,38 @@
 <context>
     <name>udg::OptimalViewpointInputParametersForm</name>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="390"/>
         <source>Open segmentation file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="391"/>
         <source>Segmentation files (*.seg);;All files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="413"/>
         <source>No segmentation file chosen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="414"/>
         <source>Please, choose a segmentation file or do an automatic segmentation.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="434"/>
         <source>Load transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="462"/>
         <source>Transfer function files (*.tf);;All files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="462"/>
         <source>Save transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="357"/>
         <source>Show parameters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="optimalviewpointinputparametersform.cpp" line="366"/>
         <source>Hide parameters</source>
         <translation type="unfinished"></translation>
     </message>
@@ -408,7 +349,6 @@
 <context>
     <name>udg::OptimalViewpointPlane</name>
     <message>
-        <location filename="optimalviewpointplane.cpp" line="51"/>
         <source>Plane %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -416,22 +356,18 @@
 <context>
     <name>udg::QOptimalViewpointExtension</name>
     <message>
-        <location filename="qoptimalviewpointextension.cpp" line="117"/>
         <source>Results</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qoptimalviewpointextension.cpp" line="123"/>
         <source>Plane</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qoptimalviewpointextension.cpp" line="69"/>
         <source>Segmentation error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qoptimalviewpointextension.cpp" line="70"/>
         <source>Cannot load segmentation from file %1.</source>
         <translation type="unfinished"></translation>
     </message>
