77fe78f00c50e1292d8901458f753737f34effa6
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2007-11-21 12:51:00 +0000

    ENH: S'ha afegit la tool de sincronitzar.

---
+++ starviewer/src/core/synchronizetool.cpp
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 by Grup de Gràfics de Girona                  *
+ *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
+ *                                                                         *
+ *   Universitat de Girona                                                 *
+ ***************************************************************************/
+#include "synchronizetool.h"
+#include "q2dviewer.h"
+#include "synchronizetooldata.h"
+#include "logging.h"
+
+//TODO treure aixoooooooo
+#include <vtkMath.h>
+
+namespace udg {
+
+SynchronizeTool::SynchronizeTool( QViewer *viewer, QObject *parent)
+: Tool( viewer, parent )
+{
+    m_toolData = 0;
+    m_toolName = "SynchronizeTool";
+    m_hasSharedData = true;
+    m_q2viewer = dynamic_cast<Q2DViewer*>(viewer);
+    m_lastSlice = m_q2viewer->getCurrentSlice();
+    connect( viewer, SIGNAL(sliceChanged( int ) ), this, SLOT( setIncrement( int ) ) );
+    setToolData( new SynchronizeToolData() );
+}
+
+SynchronizeTool::~SynchronizeTool()
+{
+
+}
+
+void SynchronizeTool::setToolData( ToolData *data )
+{
+    if( m_toolData )
+    {
+        disconnect( m_toolData, SIGNAL(dataChanged( ) ), this, SLOT( applyChanges() ) );
+    }
+    this->m_toolData = dynamic_cast<SynchronizeToolData*>(data);
+    connect( m_toolData, SIGNAL(dataChanged( ) ), this, SLOT( applyChanges() ) );
+}
+
+ToolData * SynchronizeTool::getToolData() const
+{
+    return this->m_toolData;
+}
+
+void SynchronizeTool::setIncrement( int slice )
+{
+
+    DEBUG_LOG( tr("Visualitzador: %1. Jo SI faig un setIncrement") );
+    int increment = slice - m_lastSlice;
+    m_lastSlice = slice;
+    disconnect( m_toolData, SIGNAL(dataChanged( ) ), this, SLOT( applyChanges() ) );
+    this->m_toolData->setIncrement( increment );
+    connect( m_toolData, SIGNAL(dataChanged( ) ), this, SLOT( applyChanges() ) );
+
+}
+
+void SynchronizeTool::applyChanges(  )
+{
+
+    DEBUG_LOG( tr("Visualitzador: %1. Jo SI actualitzo") );
+    int increment = this->m_toolData->getIncrement();
+    disconnect( m_viewer, SIGNAL(sliceChanged( int ) ), this, SLOT( setIncrement( int ) ) );
+    m_q2viewer->setSlice( m_lastSlice+increment );
+    m_lastSlice += increment;
+    connect( m_viewer, SIGNAL(sliceChanged( int ) ), this, SLOT( setIncrement( int ) ) );
+
+}
+
+void SynchronizeTool::handleEvent( unsigned long eventID )
+{
+   
+}
+}
---
+++ starviewer/src/core/synchronizetooldata.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 by Grup de Gràfics de Girona                  *
+ *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
+ *                                                                         *
+ *   Universitat de Girona                                                 *
+ ***************************************************************************/
+#include "synchronizetooldata.h"
+#include "q2dviewer.h"
+#include "logging.h"
+
+namespace udg {
+
+SynchronizeToolData::SynchronizeToolData()
+ : ToolData()
+{
+    m_increment = 0;
+}
+
+
+SynchronizeToolData::~SynchronizeToolData()
+{
+}
+
+void SynchronizeToolData::setIncrement( int value )
+{
+    m_increment = value;
+
+    DEBUG_LOG( tr("HE CANVIAT LES DADES!! INCREMENT: %1").arg(m_increment) );
+
+    emit( dataChanged() );
+}
+
+int SynchronizeToolData::getIncrement()
+{
+    return m_increment;
+}
+
+
+}
