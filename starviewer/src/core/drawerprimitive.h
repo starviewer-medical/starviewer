f4b2b23fb94e4fcfcd3045080784ffd94257270c
Jesús Spínola <jspinola@gmail.com>
2007-11-30 13:24:26 +0000

    ENH: Afegimla primitiva de polígon. S'amplia el ventall d'atributs de DrawerPrimitive i ja fem servir el nou drawer dins de ReferenceLinesTool

--- starviewer/src/core/drawerprimitive.h
+++ starviewer/src/core/drawerprimitive.h
@@ -11,6 +11,7 @@
 #include <QColor>
 
 class vtkProp;
+class vtkCoordinate;
 
 namespace udg {
 
@@ -55,6 +56,32 @@ public:
     void setColor( QColor color );
     QColor getColor() const;
 
+    /**
+     * Mètodes per configurar si la forma es pinta amb color de fons o no.
+     * Aquest atribut només tindrà sentit per formes tancades
+     */
+    void setFilled( bool fill );
+    bool isFilled() const;
+
+    enum LinePatternType{ ContinuousLinePattern = 0xFFFF, DiscontinuousLinePattern = 2000 };
+    /**
+     * Mètodes per configurar el patró de la línia
+     */
+    void setLinePattern( int pattern );
+    int getLinePattern() const;
+
+    /**
+     * Assigna/Obté l'amplada de la línia
+     */
+    void setLineWidth( double width );
+    double getLineWidth() const;
+
+    /**
+     * Assigna/Obté l'opacitat de l'objecte
+     */
+    void setOpacity( double opacity );
+    double getOpacity() const;
+
     // mètodes per construir l'objecte a representar gràficament
     /**
      * Construeix l'objecte vtk que representa aquesta primitiva
@@ -67,6 +94,22 @@ signals:
     /// s'emet quan alguna de les propietats ha canviat
     void changed();
 
+protected slots:
+    /**
+     * Aquest slot s'hauria de re-implementar per cada classe i és on s'actualitza
+     * l'estructura vtk que s'ha creat per representar la primitiva
+     */
+    virtual void updateVtkProp(){};
+
+protected:
+    /**
+     * Ens retorna un objecte vtkCoordinate configurat a partir del sistema de coordenades
+     * que té especificada la primitiva. Aquest mètode ens és útil per quan es vol construir
+     * la primitiva en format vtk
+     * @return L'objecte vtkCoordinate
+     */
+    vtkCoordinate *getVtkCoordinateObject();
+
 protected:
     /// Defineix si la primitiva és visible o no
     bool m_isVisible;
@@ -76,6 +119,18 @@ protected:
 
     /// Color de la primitiva, per defecte Qt::Green
     QColor m_color;
+
+    /// Controla si cal omplir les formes tancades amb el color per defecte. Per defecte no s'omplen
+    bool m_isFilled;
+
+    /// patró de la línia
+    int m_linePattern;
+
+    /// Ample de la línia. Per defecte 1.0
+    double m_lineWidth;
+
+    /// Opacitat de l'objecte, per defecte 1.0, és a dir, completament opac
+    double m_opacity;
 };
 
 }
