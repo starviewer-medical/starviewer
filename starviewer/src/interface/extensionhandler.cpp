5eeeac0f3fdc74f1313b70aa01c7c89f42e298ff
Quim Rodríguez <quimrog.u@gmail.com>
2008-02-27 23:34:15 +0000

    ENH: S'afegeix un mínim d'estadístiques: tools i extensions

--- starviewer/src/interface/extensionhandler.cpp
+++ starviewer/src/interface/extensionhandler.cpp
@@ -53,6 +53,7 @@ void ExtensionHandler::request( int who )
 {
     // \TODO: crear l'extensió amb el factory ::createExtension, no com està ara
     // \TODO la numeració és completament temporal!!! s'haurà de canviar aquest sistema
+    STAT_LOG("Activated open " + QString::number(who));
     switch( who )
     {
         case 1:
@@ -80,6 +81,7 @@ void ExtensionHandler::request( const QString &who )
 
     if (mediator && extension)
     {
+        STAT_LOG("Activated extension " + who);
         mediator->initializeExtension(extension, m_extensionContext );
         m_mainApp->getExtensionWorkspace()->addApplication(extension, mediator->getExtensionID().getLabel() );
     }
