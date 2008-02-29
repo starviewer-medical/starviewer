525715f2942b43a148998ebbe4d0bb9c8548e19d
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2008-02-29 13:12:42 +0000

    EMH: S'ha afegit les traduccions de extensions/main/mpr.

--- starviewer/src/extensions/main/mpr/translations_ca_ES.ts
+++ starviewer/src/extensions/main/mpr/translations_ca_ES.ts
@@ -1,14 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="ca">
+<defaultcodec></defaultcodec>
 <context>
     <name>QMPRExtensionBase</name>
     <message>
-        <location filename="qmprextensionbase.ui" line="314"/>
+        <location filename="qmprextensionbase.ui" line="364"/>
         <source>WW/WL:</source>
         <translation>WW/WL:</translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="257"/>
+        <location filename="qmprextensionbase.ui" line="307"/>
         <source>...</source>
         <translation>...</translation>
     </message>
@@ -18,29 +19,24 @@
         <translation>Moure llesques</translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="68"/>
+        <location filename="qmprextensionbase.ui" line="71"/>
         <source>Window Level</source>
         <translation>Lluentor/Contrast</translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="87"/>
+        <location filename="qmprextensionbase.ui" line="93"/>
         <source>Zoom</source>
         <translation>Zoom</translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="106"/>
+        <location filename="qmprextensionbase.ui" line="115"/>
         <source>Move</source>
         <translation>Moure</translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="213"/>
+        <location filename="qmprextensionbase.ui" line="263"/>
         <source>Thick Slab (mm)</source>
         <translation>Gruix de tall (mm)</translation>
-    </message>
-    <message>
-        <location filename="qmprextensionbase.ui" line="147"/>
-        <source>zoom</source>
-        <translation>zoom</translation>
     </message>
     <message>
         <location filename="qmprextensionbase.ui" line="13"/>
@@ -48,14 +44,34 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="qmprextensionbase.ui" line="279"/>
+        <location filename="qmprextensionbase.ui" line="329"/>
         <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#dc143c;&quot;&gt;*The current Series has multiple phases. &lt;/span&gt;&lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600; color:#dc143c;&quot;&gt;Currently the MPR 2D doesn&apos;t support &lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600; color:#dc143c;&quot;&gt;Series with multiple phases so it won&apos;t work propperly.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#dc143c;&quot;&gt;*La sèrie actual conté múltiples fases. &lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600; color:#dc143c;&quot;&gt;Actualment l&apos;MPR 2D no suporta &lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:600; color:#dc143c;&quot;&gt;sèries amb múltiples fases, per tant no funciona correctament.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="qmprextensionbase.ui" line="159"/>
+        <source>ROI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qmprextensionbase.ui" line="178"/>
+        <source>Distance</source>
+        <translation>Distància</translation>
+    </message>
+    <message>
+        <location filename="qmprextensionbase.ui" line="200"/>
+        <source>Rubber</source>
+        <translation>Borrar</translation>
     </message>
 </context>
 <context>
@@ -79,42 +95,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>udg::QMPRExtension</name>
     <message>
-        <location filename="qmprextension.cpp" line="142"/>
+        <location filename="qmprextension.cpp" line="147"/>
         <source>PNG Images (*.png);;PNM Images (*.pnm);;JPEG Images (*.jpg);;TIFF Images (*.tif);;BMP Images (*.bmp);;DICOM Images (*.dcm)</source>
         <translation>Imatges PNG (*.png);;Imatges PNM(*.pnm);;Imatges JPEG(*.jpg);;Imatges TIFF (*.tif);;Imatges BMP (*.bmp);;Imatges DICOM (*.dcm)</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="886"/>
+        <location filename="qmprextension.cpp" line="855"/>
         <source>Information</source>
         <translation>Informació</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="886"/>
+        <location filename="qmprextension.cpp" line="855"/>
         <source>There are not grabbed views to save</source>
         <translation>No hi ha vistes capturades per desar</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="890"/>
+        <location filename="qmprextension.cpp" line="859"/>
         <source>Save file</source>
         <translation>Desar fitxer</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="152"/>
+        <location filename="qmprextension.cpp" line="156"/>
         <source>Switch horizontal layout</source>
         <translation>Canviar la distribució horitzontal</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="157"/>
+        <location filename="qmprextension.cpp" line="161"/>
         <source>&amp;MIP</source>
         <translation>&amp;MIP</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="159"/>
+        <location filename="qmprextension.cpp" line="163"/>
         <source>Maximum Intensity Projection</source>
         <translation>Projecció de Màxima Intensitat</translation>
     </message>
     <message>
-        <location filename="qmprextension.cpp" line="158"/>
+        <location filename="qmprextension.cpp" line="162"/>
         <source>Ctrl+M</source>
         <translation></translation>
     </message>
