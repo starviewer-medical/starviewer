a367f064cb0eff4563d2f6f6bf2bd6e8fba6fc32
Jesús Spínola <jspinola@gmail.com>
2008-05-14 13:09:56 +0000

    ENH: Afegim uns TODOs en relació amb el ticket #376

--- starviewer/src/core/angletool.cpp
+++ starviewer/src/core/angletool.cpp
@@ -49,7 +49,7 @@ AngleTool::~AngleTool()
             delete m_mainPolyline;
         if (m_circumferencePolyline )
             delete m_circumferencePolyline;
-        
+
         m_2DViewer->getDrawer()->refresh();
     }
 }
@@ -59,7 +59,7 @@ void AngleTool::handleEvent( long unsigned eventID )
     switch( eventID )
     {
         case vtkCommand::LeftButtonPressEvent:
-        
+
             if ( m_2DViewer->getInteractor()->GetRepeatCount() == 0 )
             {
                 if ( m_state == NONE )
@@ -69,36 +69,39 @@ void AngleTool::handleEvent( long unsigned eventID )
                     this->fixFirstSegment();
                     this->findInitialDegreeArc();
                 }
-                else 
+                else
                 {
                     //voldrem enregistrar l'últim punt, pertant posem l'estat a none
                     m_state = NONE;
                     computeAngle();
                     delete m_circumferencePolyline;
-                }    
+                }
                 m_2DViewer->getDrawer()->refresh();
             }
         break;
 
         case vtkCommand::MouseMoveEvent:
-        
+
             if( m_mainPolyline && m_state == FIRST_POINT_FIXED  )
                 this->simulateFirstSegmentOfAngle();
             else if ( m_mainPolyline && m_state == CENTER_FIXED )
                 this->simulateSecondSegmentOfAngle();
-                
+
             m_2DViewer->getDrawer()->refresh();
         break;
-        
+
+// TODO aquest codi comentat, si es torna a fer servir, s'hauria d'unificar per totes les
+// tools d'anotació en un mètode virtual anomenat per exemple "interactionInterrupt()" o "reset()"
+//
 //         case vtkCommand::LeaveEvent:
 //             //en aquest cas considerem que l'usuari no vol anotar, per tant ens carreguem les primitives.
 //             if ( !m_mainPolyline.isNull() && m_state != NONE )
 //                 delete m_mainPolyline;
-//             
+//
 //             if ( !m_circumferencePolyline.isNull() &&  m_state != NONE )
 //                 delete m_circumferencePolyline;
-//         
-//             m_state = NONE;            
+//
+//             m_state = NONE;
 //             m_2DViewer->getDrawer()->refresh();
 //         break;
     }
@@ -107,12 +110,12 @@ void AngleTool::handleEvent( long unsigned eventID )
 void AngleTool::findInitialDegreeArc()
 {
     //Per saber quin l'angle inicial, cal calcular l'angle que forma el primer segment anotat i un segment fictici totalment horitzontal.
-    double horizontalP2[3], *vd1, *vd2, *pv; 
+    double horizontalP2[3], *vd1, *vd2, *pv;
     double *p1 = m_mainPolyline->getPoint( 0 );
     double *p2 = m_mainPolyline->getPoint( 1 );
-    
+
     int coord1, coord2;
-    
+
     switch( m_2DViewer->getView() )
     {
         case QViewer::AxialPlane:
@@ -130,16 +133,16 @@ void AngleTool::findInitialDegreeArc()
             coord2 = 1;
             break;
     }
-    
+
     for (int i = 0; i < 3; i++)
         horizontalP2[i] = p2[i];
-    
+
     vd1 = MathTools::directorVector( p1, p2 );
-    
+
     horizontalP2[coord1] += 10.0;
     vd2 = MathTools::directorVector( horizontalP2, p2 );
     pv = MathTools::vectorialProduct(vd1, vd2);
-    
+
     if ( pv[coord2] > 0 )
     {
         m_initialDegreeArc =(int)MathTools::angleInDegrees( vd1, vd2 );
@@ -165,11 +168,11 @@ void AngleTool::annotateFirstPoint()
     computed[0] = position[0];
     computed[1] = position[1];
     computed[2] = position[2];
-    
+
     //afegim el punt
     m_mainPolyline->addPoint( computed );
     m_mainPolyline->update( DrawerPrimitive::VTKRepresentation );
-    
+
     //actualitzem l'estat de la tool
     m_state = FIRST_POINT_FIXED;
 }
@@ -177,10 +180,10 @@ void AngleTool::annotateFirstPoint()
 void AngleTool::fixFirstSegment()
 {
     m_mainPolyline->update( DrawerPrimitive::VTKRepresentation );
-    
+
     //posem l'estat de la tool a CENTER_FIXED, així haurà agafat l'últim valor.
     m_state = CENTER_FIXED;
-    
+
     //creem la polilínia per a dibuixar l'arc de circumferència i l'afegim al drawer
     m_circumferencePolyline = new DrawerPolyline;
     m_2DViewer->getDrawer()->draw( m_circumferencePolyline , m_2DViewer->getView(), m_2DViewer->getCurrentSlice() );
@@ -188,20 +191,20 @@ void AngleTool::fixFirstSegment()
 
 void AngleTool::drawCircumference()
 {
-    double degrees, intersection[2], xAxis1[2], xAxis2[2], yAxis1[2], yAxis2[2], *newPoint, xRadius, yRadius; 
+    double degrees, intersection[2], xAxis1[2], xAxis2[2], yAxis1[2], yAxis2[2], *newPoint, xRadius, yRadius;
     int initialI, finalI, viewCoord;
-    
+
     int view = m_2DViewer->getView();
-        
+
     double *p1 = m_mainPolyline->getPoint( 0 );
     double *p2 = m_mainPolyline->getPoint( 1 );
     double *p3 = m_mainPolyline->getPoint( 2 );
-    
+
     double *vd1 = MathTools::directorVector( p1, p2 );
     double *vd2 = MathTools::directorVector( p3, p2 );
-    
+
     double angle = MathTools::angleInDegrees( vd1, vd2 );
-    
+
     switch( view )
     {
         case QViewer::AxialPlane:
@@ -216,7 +219,7 @@ void AngleTool::drawCircumference()
 
             yAxis2[0] = p2[0];
             yAxis2[1] = p1[1];
-            
+
             viewCoord = 2;
             break;
 
@@ -232,7 +235,7 @@ void AngleTool::drawCircumference()
 
             yAxis2[0] = p2[2];
             yAxis2[1] = p1[1];
-            
+
             viewCoord = 0;
             break;
 
@@ -248,41 +251,41 @@ void AngleTool::drawCircumference()
 
             yAxis2[0] = p2[0];
             yAxis2[1] = p1[2];
-            
+
             viewCoord = 1;
             break;
     }
-    
+
     Distance d1( p1, p2 );
     Distance d2( p2, p3 );
-    
+
     double distance1 = d1.getDistance3D();
     double distance2 = d2.getDistance3D();
-    
+
     xRadius =  MathTools::minimum( distance1, distance2 ) / 4.0;
     m_radius = xRadius;
     yRadius = xRadius;
 
     intersection[0] = ((yAxis2[0] - yAxis1[0]) / 2.0) + yAxis1[0];
     intersection[1] = ((xAxis2[1] - xAxis1[1]) / 2.0) + xAxis1[1];
-    
+
     double *pv = MathTools::vectorialProduct(vd1, vd2);
-    
+
     initialI = 360 - m_initialDegreeArc;
     finalI = (360 - ( angle+m_initialDegreeArc ) );
-    
+
     if ( pv[viewCoord] > 0 )
     {
         finalI = angle-m_initialDegreeArc;
     }
-    
+
     if ( (initialI-finalI) <= 180 )
     {
         for ( int i = initialI; i > finalI; i-- )
         {
             degrees = i*1.0*vtkMath::DoubleDegreesToRadians();
             newPoint = new double[3];
-            
+
             switch( view )
             {
                 case QViewer::AxialPlane:
@@ -294,11 +297,11 @@ void AngleTool::drawCircumference()
                 case QViewer::SagitalPlane:
                     newPoint[0] = 0.0;
                     newPoint[1] = cos( degrees )*yRadius + intersection[1];
-                    newPoint[2] = sin( degrees )*xRadius + intersection[0];     
+                    newPoint[2] = sin( degrees )*xRadius + intersection[0];
                     break;
 
                 case QViewer::CoronalPlane:
-                    newPoint[0] = sin( degrees )*xRadius + intersection[0];  
+                    newPoint[0] = sin( degrees )*xRadius + intersection[0];
                     newPoint[1] = 0.0;
                     newPoint[2] = cos( degrees )*yRadius + intersection[1];
                     break;
@@ -314,7 +317,7 @@ void AngleTool::drawCircumference()
         {
             degrees = i*1.0*vtkMath::DoubleDegreesToRadians();
             newPoint = new double[3];
-            
+
             switch( view )
             {
                 case QViewer::AxialPlane:
@@ -326,11 +329,11 @@ void AngleTool::drawCircumference()
                 case QViewer::SagitalPlane:
                     newPoint[0] = 0.0;
                     newPoint[1] = cos( degrees )*yRadius + intersection[1];
-                    newPoint[2] = sin( degrees )*xRadius + intersection[0];     
+                    newPoint[2] = sin( degrees )*xRadius + intersection[0];
                     break;
 
                 case QViewer::CoronalPlane:
-                    newPoint[0] = sin( degrees )*xRadius + intersection[0];  
+                    newPoint[0] = sin( degrees )*xRadius + intersection[0];
                     newPoint[1] = 0.0;
                     newPoint[2] = cos( degrees )*yRadius + intersection[1];
                     break;
@@ -355,7 +358,7 @@ void AngleTool::simulateFirstSegmentOfAngle()
     computed[0] = position[0];
     computed[1] = position[1];
     computed[2] = position[2];
-    
+
     if ( m_mainPolyline->getNumberOfPoints() == 2 ) //és que ja havíem assignat el segon punt que determina el primer segment de l'angle
     {
         //esborrem aquest segon punt
@@ -381,7 +384,7 @@ void AngleTool::simulateSecondSegmentOfAngle()
     computed[0] = position[0];
     computed[1] = position[1];
     computed[2] = position[2];
-    
+
     if ( m_mainPolyline->getNumberOfPoints() == 3 ) //és que ja havíem assignat el segon punt que determina el primer segment de l'angle
     {
         //esborrem aquest segon punt
@@ -391,7 +394,7 @@ void AngleTool::simulateSecondSegmentOfAngle()
     //afegim el nou segon punt que simula el nou punt
     m_mainPolyline->addPoint( computed );
     m_mainPolyline->update( DrawerPrimitive::VTKRepresentation );
-    
+
     m_circumferencePolyline->deleteAllPoints();
     drawCircumference();
 }
@@ -401,25 +404,25 @@ void AngleTool::computeAngle()
     double *p1 = m_mainPolyline->getPoint(0);
     double *p2 = m_mainPolyline->getPoint(1);
     double *p3 = m_mainPolyline->getPoint(2);
-    
+
     double textSituation[3];
-    
+
     double *vd1 = MathTools::directorVector( p1, p2 );
     double *vd2 = MathTools::directorVector( p3, p2 );
-         
+
     for (int i = 0; i < 3; i++)
     {
         textSituation[i] = (p1[i] + p3[i])/2;
-        
+
         if ( fabs( vd1[i] ) < 0.0001 )
             vd1[i] = 0.0;
-        
+
         if ( fabs( vd2[i] ) < 0.0001 )
             vd2[i] = 0.0;
     }
-    
-    double angle = MathTools::angleInDegrees( vd1, vd2 );    
-    
+
+    double angle = MathTools::angleInDegrees( vd1, vd2 );
+
     DrawerText * text = new DrawerText;
     text->setText( tr("%1 degrees").arg( angle,0,'f',1) );
     text->setAttatchmentPoint( textSituation );
