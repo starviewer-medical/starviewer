4c36e2e64f66b9f3b86eda403c85982fb0b78c0b
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2008-05-14 09:35:18 +0000

    DOC: Afegits uns TODO.

--- starviewer/src/extensions/main/q2dviewer/q2dviewerextension.cpp
+++ starviewer/src/extensions/main/q2dviewer/q2dviewerextension.cpp
@@ -211,6 +211,8 @@ void Q2DViewerExtension::setInput( Volume *input )
 
 void Q2DViewerExtension::resetViewToAxial()
 {
+    // TODO solucionat de forma temporal. L'extensió no té perquè saber que el visor
+    // està en axial
     if( !( m_selectedViewer->getViewer()->getView() == Q2DViewer::Axial ) )
     {
         m_thickSlabWidget->setProjectionMode( 0 );
@@ -220,6 +222,8 @@ void Q2DViewerExtension::resetViewToAxial()
 
 void Q2DViewerExtension::resetViewToSagital()
 {
+    // TODO solucionat de forma temporal. L'extensió no té perquè saber que el visor
+    // està en sagital
     if( !( m_selectedViewer->getViewer()->getView() == Q2DViewer::Sagital ) )
     {
         m_thickSlabWidget->setProjectionMode( 0 );
@@ -229,6 +233,8 @@ void Q2DViewerExtension::resetViewToSagital()
 
 void Q2DViewerExtension::resetViewToCoronal()
 {
+    // TODO solucionat de forma temporal. L'extensió no té perquè saber que el visor
+    // està en coronal
     if( !( m_selectedViewer->getViewer()->getView() == Q2DViewer::Coronal ) )
     {
         m_thickSlabWidget->setProjectionMode( 0 );
