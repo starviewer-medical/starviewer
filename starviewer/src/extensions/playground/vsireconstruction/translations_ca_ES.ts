6c36f965f9b51583f79e8d9af44016dae33d3f2a
Anton Bardera <anton.bardera@imae.udg.edu>
2008-05-22 12:40:14 +0000

    ENH: Afegim correctament els arxius de l'extensio vsirecontruction que no s'havien pogut pujar bé a la revisió r2453

---
+++ starviewer/src/extensions/playground/vsireconstruction/translations_ca_ES.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ca">
+<context>
+    <name>QStrokeSegmentationExtensionBase</name>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="13"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="215"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="83"/>
+        <source>window level</source>
+        <translation>Lluentor</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="105"/>
+        <source>zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="127"/>
+        <source>move</source>
+        <translation>Moure</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="193"/>
+        <source>voxel information</source>
+        <translation>Informació del voxel</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="242"/>
+        <source>View thresholds</source>
+        <translation>Veure els umbrals</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="257"/>
+        <source>Lower value:</source>
+        <translation>Valor mínim:</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="291"/>
+        <source>Upper value:</source>
+        <translation>Valor màxim:</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="325"/>
+        <source>Opacity:</source>
+        <translation>Opacitat:</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="395"/>
+        <source>Seed:</source>
+        <translation>Llavor:</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="490"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="508"/>
+        <source>Volume (in mm3):</source>
+        <translation>Volum (en mm3):</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="550"/>
+        <source>Update</source>
+        <translation>Actualitzar</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="577"/>
+        <source>Editor</source>
+        <translation>Editor</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="665"/>
+        <source>Size</source>
+        <translation>Tamany</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextensionbase.ui" line="731"/>
+        <source>Slice:</source>
+        <translation>Llesca:</translation>
+    </message>
+</context>
+<context>
+    <name>udg::QStrokeSegmentationExtension</name>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="98"/>
+        <source>Voxel Information</source>
+        <translation>Informació del voxel</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="99"/>
+        <source>Ctrl+I</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="100"/>
+        <source>Enable voxel information over cursor</source>
+        <translation>Habilitar l&apos;informació del voxel sota el cursor</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="108"/>
+        <source>Rotate Clockwise</source>
+        <translation>Rotació a la dreta</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="110"/>
+        <source>Rotate the image in clockwise direction</source>
+        <translation>Rotació de la imatge en el sentit de les agulles del rellotge</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="135"/>
+        <source>EditorTool</source>
+        <translation>Eina d&apos;edició</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="136"/>
+        <source>Enable/Disable editor tool</source>
+        <translation>Habilitar/Deshabilitar l&apos;eina d&apos;edició</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="245"/>
+        <source>StarViewer</source>
+        <translation>Starviewer</translation>
+    </message>
+    <message>
+        <location filename="qstrokesegmentationextension.cpp" line="245"/>
+        <source>ERROR: no hi ha definida llavor o m&#xc3;&#xa0;scara</source>
+        <translation>ERROR: no hi ha definida cap llavor o màscara</translation>
+    </message>
+</context>
+<context>
+    <name>udg::StrokeSegmentationExtensionMediator</name>
+    <message>
+        <location filename="strokesegmentationextensionmediator.cpp" line="27"/>
+        <source>Stroke Segmentation</source>
+        <translation>Segmentació d&apos;infarts</translation>
+    </message>
+    <message>
+        <location filename="strokesegmentationextensionmediator.cpp" line="41"/>
+        <source>Starviewer</source>
+        <translation>Starviewer</translation>
+    </message>
+    <message>
+        <location filename="strokesegmentationextensionmediator.cpp" line="41"/>
+        <source>The selected item is not an image</source>
+        <translation>L&apos;element seleccionat no es una imatge</translation>
+    </message>
+</context>
+</TS>
