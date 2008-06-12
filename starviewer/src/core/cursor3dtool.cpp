1c4df39cafd262921b0b4db5b3b337ac1d7c866f
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2008-06-12 11:44:33 +0000

    BUG: Si es canviava de llesca amb el cursor 3D visible per pantalla el punt que es donava era incorrecte. S'ha solucionat per la majoria de casos excepte el cas referent al ticket #557. Quan es solucioni el ticket, quedara solucionat el problema.

--- starviewer/src/core/cursor3dtool.cpp
+++ starviewer/src/core/cursor3dtool.cpp
@@ -40,6 +40,7 @@ Cursor3DTool::Cursor3DTool( QViewer *viewer, QObject *parent )
     // cada cop que el viewer canvïi d'input, hem d'actualitzar el frame of reference
     connect( m_2DViewer, SIGNAL(volumeChanged(Volume *) ), SLOT( refreshReferenceViewerData() ) );
     connect( m_2DViewer, SIGNAL(selected()),SLOT(refreshReferenceViewerData()) );
+    connect( m_2DViewer, SIGNAL(sliceChanged( int ) ), SLOT( hideCrossHair() ) );
 
     refreshReferenceViewerData();
 
@@ -300,6 +301,17 @@ void Cursor3DTool::refreshReferenceViewerData()
     if( m_2DViewer->getInput() )
     {
         updateFrameOfReference();
+    }
+}
+
+void Cursor3DTool::hideCrossHair()
+{
+    if( m_2DViewer->isActive() && m_state == NONE )
+    {
+        m_crossHair->setVisibility( false );
+        m_crossHair->update( DrawerPrimitive::VTKRepresentation );
+        m_2DViewer->refresh();
+        m_myData->setVisible( false );
     }
 }
 
