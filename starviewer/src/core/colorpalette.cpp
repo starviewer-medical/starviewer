31a63c0b4135fbebab95c126fda7fa71953e5caa
Jesús Spínola <jspinola@gmail.com>
2008-06-13 13:17:07 +0000

    COMP: Eliminem warnings (inicialitzacions de variables)

--- starviewer/src/core/colorpalette.cpp
+++ starviewer/src/core/colorpalette.cpp
@@ -9,50 +9,50 @@
 
 namespace udg {
 
-ColorPalette::ColorPalette() : grayScaleHighlightColor( Qt::darkGreen ), rgbHighlightColor( Qt::yellow ), grayScaleSelectionColor( Qt::darkGreen ), rgbSelectionColor( 236, 160, 28 ), rgbNormalColor( Qt::green ), grayScaleNormalColor( Qt::green )
+ColorPalette::ColorPalette() : grayScaleHighlightColor( Qt::darkGreen ), rgbHighlightColor( Qt::yellow ), grayScaleSelectionColor( Qt::darkGreen ), rgbSelectionColor( 236, 160, 28 ), grayScaleNormalColor( Qt::green ), rgbNormalColor( Qt::green )
 {
     m_colorMode = RGB;
 }
-    
-ColorPalette::ColorPalette( ColorMode mode ) : grayScaleHighlightColor( Qt::darkGreen ), rgbHighlightColor( Qt::yellow ), grayScaleSelectionColor( Qt::darkGreen ), rgbSelectionColor( 236, 160, 28 ), rgbNormalColor( Qt::green ), grayScaleNormalColor( Qt::green )
+
+ColorPalette::ColorPalette( ColorMode mode ) : grayScaleHighlightColor( Qt::darkGreen ), rgbHighlightColor( Qt::yellow ), grayScaleSelectionColor( Qt::darkGreen ), rgbSelectionColor( 236, 160, 28 ), grayScaleNormalColor( Qt::green ), rgbNormalColor( Qt::green )
 {
     m_colorMode = mode;
 }
-    
-    
+
+
 QColor ColorPalette::getHighlightColor() const
 {
     QColor highlight;
-    
+
     if ( m_colorMode == RGB )
         highlight = rgbHighlightColor;
     else
         highlight = grayScaleHighlightColor;
-    
+
     return( highlight );
 }
-    
+
 QColor ColorPalette::getSelectionColor() const
 {
     QColor selection;
-    
+
     if ( m_colorMode == RGB )
         selection = rgbSelectionColor;
     else
         selection = grayScaleSelectionColor;
-    
+
     return( selection );
 }
 
 QColor ColorPalette::getNormalColor() const
 {
     QColor normal;
-    
+
     if ( m_colorMode == RGB )
         normal = rgbNormalColor;
     else
         normal = grayScaleNormalColor;
-    
+
     return( normal );
 }
 
