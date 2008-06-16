fa8afb34216ab10a5cc81e5689e8d0d1e72938a3
Jesús Spínola <jspinola@gmail.com>
2008-06-16 11:32:07 +0000

    COMP: neteja de warnings i una miqueta de coding style

--- starviewer/src/extensions/playground/diffusionperfusionsegmentation/itkRegistre3DAffine.h
+++ starviewer/src/extensions/playground/diffusionperfusionsegmentation/itkRegistre3DAffine.h
@@ -54,30 +54,32 @@
 class CommandIterationUpdate : public itk::Command
 {
 public:
-  typedef  CommandIterationUpdate   Self;
-  typedef  itk::Command             Superclass;
-  typedef itk::SmartPointer<Self>  Pointer;
-  itkNewMacro( Self );
-protected:
-  CommandIterationUpdate() {};
-public:
-  //typedef itk::RegularStepGradientDescentOptimizer  OptimizerType;
-  //typedef itk::GradientDescentOptimizer  OptimizerType;
-  typedef itk::OnePlusOneEvolutionaryOptimizer   OptimizerType;
-  typedef   const OptimizerType   *    OptimizerPointer;
+    typedef  CommandIterationUpdate   Self;
+    typedef  itk::Command             Superclass;
+    typedef itk::SmartPointer<Self>  Pointer;
+    itkNewMacro( Self );
 
-  void Execute(itk::Object *caller, const itk::EventObject & event)
+protected:
+    CommandIterationUpdate(){};
+
+public:
+    //typedef itk::RegularStepGradientDescentOptimizer  OptimizerType;
+    //typedef itk::GradientDescentOptimizer  OptimizerType;
+    typedef itk::OnePlusOneEvolutionaryOptimizer OptimizerType;
+    typedef   const OptimizerType *OptimizerPointer;
+
+    void Execute(itk::Object *caller, const itk::EventObject & event)
     {
-      Execute( (const itk::Object *)caller, event);
+        Execute( (const itk::Object *)caller, event);
     }
 
-  void Execute(const itk::Object * object, const itk::EventObject & event)
+    void Execute(const itk::Object * object, const itk::EventObject & event)
     {
-      OptimizerPointer optimizer =
-        dynamic_cast< OptimizerPointer >( object );
-      if( typeid( event ) != typeid( itk::IterationEvent ) )
+        OptimizerPointer optimizer;
+        optimizer = dynamic_cast< OptimizerPointer >( object );
+        if( typeid( event ) != typeid( itk::IterationEvent ) )
         {
-        return;
+            return;
         }
 /*      std::cout << optimizer->GetCurrentIteration() << "   ";
       std::cout << optimizer->GetValue() << "   ";
