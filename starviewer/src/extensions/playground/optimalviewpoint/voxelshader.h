d51fdbe0be5db173eb70951b14c0c44320b76781
Marc Ruiz <marc.ruiz@gmail.com>
2008-07-29 15:52:18 +0000

    COMP: Eliminat un warning d'un paràmetre no utilitzat, i el segon shade de voxel shader ara també és pure virtual.

--- starviewer/src/extensions/playground/optimalviewpoint/voxelshader.h
+++ starviewer/src/extensions/playground/optimalviewpoint/voxelshader.h
@@ -28,7 +28,7 @@ public:
     /// Retorna el color corresponent al vòxel a la posició offset.
     virtual HdrColor shade( int offset, const Vector3 &direction, const HdrColor &baseColor = HdrColor() ) const = 0;
     /// Retorna el color corresponent al vòxel a la posició position, fent servir valors interpolats.
-    virtual HdrColor shade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor = HdrColor() ) const;
+    virtual HdrColor shade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor = HdrColor() ) const = 0;
     /// Retorna un string representatiu del voxel shader.
     virtual QString toString() const;
 
