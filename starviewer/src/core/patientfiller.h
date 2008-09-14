d6c6a02ebcd3e3205a3a840297f00aee96a5787c
Quim Rodríguez <quimrog.u@gmail.com>
2008-09-14 21:09:15 +0000

    DOC: S'amplia la documentació de la classe PatientFiller i s'actualitza als mètodes que hi ha ara.

--- starviewer/src/core/patientfiller.h
+++ starviewer/src/core/patientfiller.h
@@ -17,7 +17,21 @@ class Patient;
 class DICOMTagReader;
 
 /**
-Classe encarregada de a partir d'un Patient i un conjunt d'arxius classificar-los dins de l'estructura Patient i omplir les dades corresponents segons el tipus d'arxiu que es tracti.
+    Classe que s'encarrega de "omplir" un Patient a partir de fitxers DICOM. Bàsicament té dos modes d'operació: "asíncron" i "síncron".
+
+    En el mode d'operació asíncron el que podem fer és anar enviant fitxers dicom a processar i, un cop no tenim més fitxers, indiquem que
+    que finalitzi el procés per obtenir el resultat amb finishDICOMFilesProcess. El resultat ens el donarà en el signal
+    patientProcessed(Patient*).
+
+    En canvi, en el mode d'operació síncron, simplement cal cridar el mètode processDICOMFileList passant la llista de fitxers que
+    volem processar.
+
+    El mètode asíncron està pensant per poder processar fitxers DICOM un a un a mesura que els anem obtenit en threads diferents, siguent
+    recomenable el mètode síncron en qualsevol altra situació.
+
+    No es poden utilitzar els dos mètodes de processament alhora en el mateix fitxer.
+
+    \TODO De moment hi ha la limitiació de que es pressuposa que totes les imatges que se li passen són del mateix pacient.
 
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
@@ -30,22 +44,23 @@ public:
 
 public slots:
 
-    ///Procés encarregat de fer un processat de tots els mòduls per un fitxer DICOM passat.
+    ///Procem un fitxer DICOM. Ens permet anar passant fitxers un a un i, un cop acabem, cridar el mètode finishDICOMFilesProcess
+    ///per obtenir el resultat a partir del signal patientProcessed.
     ///Es presuposa que el fitxer DICOM passat no està buit.
     void processDICOMFile(DICOMTagReader *dicomTagReader);
 
-    ///Procés encarregat d'executar quelcom que s'hagi de fer un cop rebut tots els fitxers.
+    ///Indica que ja hem acabat de processar fitxers i ja podem obtenir el resultat final, és a dir, l'estructura Patient omplerta.
+    ///Aquesta se'ns dona a partir del signal patientProcessed.
     void finishDICOMFilesProcess();
 
-    /// Mètode que processa tots els fitxers que se li passin de cop, retornant la llista d'objectes
-    /// Patient que es generin.
+    ///Processa tots els fitxers que se li passin de cop, retornant la llista d'objectes Patient que es generin.
     QList<Patient*> processDICOMFileList(QStringList dicomFiles);
 
 signals:
-    /// Senyal que s'emet regularment al fer un fill o fillUntil indicant el % realitzat.
+    /// Senyal que s'emet cada vegada que es processa un fitxer indicant quin és dintre del "lot" a processar.
     void progress(int);
 
-    /// Senyal que s'emet quan el finishDICOMFilesProcess ha acabat.
+    /// Senyal que s'emet quan en el mode asíncron s'ha acabat de processar totes les images passades.
     void patientProcessed(Patient *);
 
 private:
