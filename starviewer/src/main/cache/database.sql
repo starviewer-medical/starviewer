522b26a17045e1dd14f519895af2912bef59e76a
Quim Rodríguez <quimrog.u@gmail.com>
2008-09-14 21:16:34 +0000

    ENH: Esborrem el codi de crear la base de dades vella. Quedem només lo imprescindible per funcionar actualment, mentre no s'esborra CachePool.

--- starviewer/src/main/cache/database.sql
+++ starviewer/src/main/cache/database.sql
@@ -88,67 +88,6 @@ CREATE TABLE Image
 CREATE INDEX  IndexImage_StudyInstanceUIDSeriesInstanceUID ON Image (StudyInstanceUID,SeriesInstanceUID); 
 
 
-CREATE TABLE PatientOld
-( PatID		VARCHAR2(64) PRIMARY KEY,
-  PatNam 	VARCHAR2(64),
-  PatBirDat	VARCHAR2(8),
-  PatSex	VARCHAR2(2)
-);
-
-CREATE TABLE StudyOld
-(
-  PatID		VARCHAR2(64) NOT NULL,
-  StuInsUID	VARCHAR2(64) PRIMARY KEY,
-  PatAge	VARCHAR2(4) DEFAULT "000Y",
-  StuID		VARCHAR2(16) NOT NULL,
-  StuDat	VARCHAR2(8),
-  StuTim	VARCHAR2(8),
-  RefPhyNam	VARCHAR2(64),
-  AccNum	VARCHAR2(16),
-  StuDes	VARCHAR2(64),
-  Modali	VARCHAR2(64),
-  OpeNam	VARCHAR2(64),
-  Locati	VARCHAR2(64),
-  AccDat	NUMBER(8),
-  AccTim	NUMBER(4),
-  AbsPath	VARCHAR2(1024),
-  Status	VARCHAR2(16),
-  PacsID        INTEGER NOT NULL
-);
-
-CREATE INDEX IND_STUDYOLD ON StudyOld (StuInsUID);
-
-CREATE TABLE SeriesOld
-( SerInsUID	VARCHAR2(64) PRIMARY KEY,
-  SerNum	VARCHAR2(12),
-  StuInsUID	VARCHAR2(64),
-  SerMod	VARCHAR2(16),
-  ProNam	VARCHAR2(16),
-  SerDes	VARCHAR2(64),
-  SerPath 	VARCHAR2(1024),
-  SerDat	VARCHAR2(8),
-  SerTim	VARCHAR2(8), 
-  BodParExa 	VARCHAR2(16)
-);
-
-CREATE INDEX IND_SERIES1OLD ON SeriesOld (StuInsUID,SerInsUID);
-CREATE INDEX IND_SERIES2OLD ON SeriesOld (SerInsUID);
-
-CREATE TABLE ImageOld
-(
-   SOPInsUID	VARCHAR2(64) PRIMARY KEY,
-   StuInsUID    VARCHAR2(64),
-   SerInsUID	VARCHAR2(64),
-   ImgNum	NUMBER(5),
-   ImgTim	VARCHAR2(8),
-   ImgDat	VARCHAR2(8),
-   ImgSiz	NUMBER(10),
-   ImgNam	VARCHAR2(255)	
-);
-
-CREATE INDEX IND_IMAGE1OLD ON ImageOld (StuInsUID,SerInsUID);
-CREATE INDEX IND_IMAGE2OLD ON ImageOld (SerInsUID);
-
 create table PoolOld
 (
     Param       VARCHAR2(12) UNIQUE,
