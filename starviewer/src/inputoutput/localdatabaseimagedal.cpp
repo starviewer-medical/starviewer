48441fe4d14b177bac22bd29341691860ff11021
Marc Cals <cpmarc@gmail.com>
2008-10-16 19:44:32 +0000

    BUG:Ticket #675, L'InstanceNumber és un string no un int

--- starviewer/src/inputoutput/localdatabaseimagedal.cpp
+++ starviewer/src/inputoutput/localdatabaseimagedal.cpp
@@ -175,7 +175,7 @@ QString LocalDatabaseImageDAL::buildSqlInsert(Image *newImage, int orderNumberIn
                                              "WindowLevelExplanations, SOPInstanceReferenceImage, SliceLocation,"
                                              "RescaleIntercept, NumberOfFrames, PhotometricInterpretation, OrderNumberInSeries,"
                                              "RetrievedDate, RetrievedTime, State) "
-                                     "values ('%1','%2','%3',%4,'%5','%6','%7',%8,'%9', %10, %11, %12, %13, %14, %15, %16,"
+                                     "values ('%1','%2','%3','%4','%5','%6','%7',%8,'%9', %10, %11, %12, %13, %14, %15, %16,"
                                               "'%17', '%18', '%19', '%20', '%21', %22, %23, '%24', %25, '%26' , '%27', %28)" )
                             .arg(newImage->getSOPInstanceUID())
                             .arg(newImage->getParentSeries()->getParentStudy()->getInstanceUID())
