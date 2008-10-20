0fcda6b03852ed4e5b4630e3cffe20b93420739d
Marc Cals <cpmarc@gmail.com>
2008-10-20 11:28:57 +0000

    BUG:El nom del mètode per inserir l'estudi descarregat a la bd és save no insert

--- starviewer/src/inputoutput/dicomdirimporter.cpp
+++ starviewer/src/inputoutput/dicomdirimporter.cpp
@@ -44,7 +44,7 @@ bool DICOMDIRImporter::import(QString dicomdirPath, QString studyUID, QString se
     connect(this, SIGNAL( importFinished() ), &patientFiller, SLOT( finishDICOMFilesProcess() ));
     
     //Connexió entre el processat i l'insersió al a BD
-    connect(&patientFiller, SIGNAL( patientProcessed(Patient*) ), &localDatabaseManagerThreaded, SLOT( insert(Patient*) ), Qt::DirectConnection);
+    connect(&patientFiller, SIGNAL( patientProcessed(Patient*) ), &localDatabaseManagerThreaded, SLOT( save(Patient*) ), Qt::DirectConnection);
     
     //Connexions per finalitzar els threads
     connect(&patientFiller, SIGNAL( patientProcessed(Patient*) ), &fillersThread, SLOT( quit() ), Qt::DirectConnection);
