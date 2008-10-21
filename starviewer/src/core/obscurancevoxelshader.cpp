6de898a2916789e4c79c06f3f908e8fb5370e4dc
Marc Ruiz <marc.ruiz@gmail.com>
2008-10-21 16:34:25 +0000

    ENH: Implementació dels mètodes nvShade a AmbientVoxelShader, DirectIlluminationVoxelShader i ObscuranceVoxelShader.

--- starviewer/src/core/obscurancevoxelshader.cpp
+++ starviewer/src/core/obscurancevoxelshader.cpp
@@ -1,8 +1,5 @@
 #include "obscurancevoxelshader.h"
 
-#include "trilinearinterpolator.h"
-#include "vector3.h"
-
 
 namespace udg {
 
@@ -36,53 +33,6 @@ void ObscuranceVoxelShader::setFactor( double factor )
 void ObscuranceVoxelShader::setFilters( double low, double high )
 {
     m_lowFilter = low; m_highFilter = high;
-}
-
-
-HdrColor ObscuranceVoxelShader::shade( int offset, const Vector3 &direction, const HdrColor &baseColor ) const
-{
-    Q_UNUSED( direction );
-
-    Q_CHECK_PTR( m_data );
-    Q_CHECK_PTR( m_obscurance );
-
-    if ( baseColor.isTransparent() || baseColor.isBlack() ) return baseColor;
-
-    double obscurance = m_obscurance[offset];
-    if ( obscurance < m_lowFilter ) obscurance = 0.0;
-    else if ( obscurance > m_highFilter ) obscurance = 1.0;
-    obscurance *= m_factor;
-
-    HdrColor shaded = baseColor;
-    shaded.multiplyColorBy( obscurance );
-
-    return shaded;
-}
-
-
-HdrColor ObscuranceVoxelShader::shade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor ) const
-{
-    Q_UNUSED( direction );
-
-    Q_CHECK_PTR( interpolator );
-    Q_CHECK_PTR( m_data );
-    Q_CHECK_PTR( m_obscurance );
-
-    if ( baseColor.isTransparent() || baseColor.isBlack() ) return baseColor;
-
-    int offsets[8];
-    double weights[8];
-    interpolator->getOffsetsAndWeights( position, offsets, weights );
-
-    double obscurance = TrilinearInterpolator::interpolate<double>( m_obscurance, offsets, weights );
-    if ( obscurance < m_lowFilter ) obscurance = 0.0;
-    else if ( obscurance > m_highFilter ) obscurance = 1.0;
-    obscurance *= m_factor;
-
-    HdrColor shaded = baseColor;
-    shaded.multiplyColorBy( obscurance );
-
-    return shaded;
 }
 
 
--- starviewer/src/core/obscurancevoxelshader.h
+++ starviewer/src/core/obscurancevoxelshader.h
@@ -1,9 +1,15 @@
 #ifndef UDGOBSCURANCEVOXELSHADER_H
 #define UDGOBSCURANCEVOXELSHADER_H
 
+
 #include "voxelshader.h"
 
+#include "trilinearinterpolator.h"
+#include "vector3.h"
+
+
 namespace udg {
+
 
 /**
  * Voxel shader que aplica obscurances.
@@ -26,6 +32,10 @@ public:
     virtual HdrColor shade( int offset, const Vector3 &direction, const HdrColor &baseColor = HdrColor() ) const;
     /// Retorna el color corresponent al vòxel a la posició position, fent servir valors interpolats.
     virtual HdrColor shade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor = HdrColor() ) const;
+    /// Retorna el color corresponent al vòxel a la posició offset.
+    HdrColor nvShade( int offset, const Vector3 &direction, const HdrColor &baseColor = HdrColor() ) const;
+    /// Retorna el color corresponent al vòxel a la posició position, fent servir valors interpolats.
+    HdrColor nvShade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor = HdrColor() ) const;
     /// Retorna un string representatiu del voxel shader.
     virtual QString toString() const;
 
@@ -36,6 +46,65 @@ protected:
     double m_lowFilter, m_highFilter;
 
 };
+
+
+inline HdrColor ObscuranceVoxelShader::shade( int offset, const Vector3 &direction, const HdrColor &baseColor ) const
+{
+    return nvShade( offset, direction, baseColor );
+}
+
+
+inline HdrColor ObscuranceVoxelShader::shade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor ) const
+{
+    return nvShade( position, direction, interpolator, baseColor );
+}
+
+
+inline HdrColor ObscuranceVoxelShader::nvShade( int offset, const Vector3 &direction, const HdrColor &baseColor ) const
+{
+    Q_UNUSED( direction );
+
+    Q_CHECK_PTR( m_data );
+    Q_CHECK_PTR( m_obscurance );
+
+    if ( baseColor.isTransparent() || baseColor.isBlack() ) return baseColor;
+
+    double obscurance = m_obscurance[offset];
+    if ( obscurance < m_lowFilter ) obscurance = 0.0;
+    else if ( obscurance > m_highFilter ) obscurance = 1.0;
+    obscurance *= m_factor;
+
+    HdrColor shaded = baseColor;
+    shaded.multiplyColorBy( obscurance );
+
+    return shaded;
+}
+
+
+inline HdrColor ObscuranceVoxelShader::nvShade( const Vector3 &position, const Vector3 &direction, const TrilinearInterpolator *interpolator, const HdrColor &baseColor ) const
+{
+    Q_UNUSED( direction );
+
+    Q_CHECK_PTR( interpolator );
+    Q_CHECK_PTR( m_data );
+    Q_CHECK_PTR( m_obscurance );
+
+    if ( baseColor.isTransparent() || baseColor.isBlack() ) return baseColor;
+
+    int offsets[8];
+    double weights[8];
+    interpolator->getOffsetsAndWeights( position, offsets, weights );
+
+    double obscurance = TrilinearInterpolator::interpolate<double>( m_obscurance, offsets, weights );
+    if ( obscurance < m_lowFilter ) obscurance = 0.0;
+    else if ( obscurance > m_highFilter ) obscurance = 1.0;
+    obscurance *= m_factor;
+
+    HdrColor shaded = baseColor;
+    shaded.multiplyColorBy( obscurance );
+
+    return shaded;
+}
 
 
 }
