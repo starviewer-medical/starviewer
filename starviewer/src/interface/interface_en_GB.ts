c721749207bb1ffdf1ec38cf8f0a9d53d4969848
Jesús Spínola <jspinola@gmail.com>
2008-11-21 18:14:05 +0000

    ENH: Actualitzem les traduccions d'interface per la release 0.7

--- starviewer/src/interface/interface_en_GB.ts
+++ starviewer/src/interface/interface_en_GB.ts
@@ -36,12 +36,12 @@
 <context>
     <name>udg::AppImportFile</name>
     <message>
-        <location filename="appimportfile.cpp" line="111"/>
+        <location filename="appimportfile.cpp" line="66"/>
         <source>Choose a directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="appimportfile.cpp" line="58"/>
+        <location filename="appimportfile.cpp" line="43"/>
         <source>Choose a file to open...</source>
         <translation type="unfinished"></translation>
     </message>
@@ -49,27 +49,27 @@
 <context>
     <name>udg::ExtensionHandler</name>
     <message>
-        <location filename="extensionhandler.cpp" line="163"/>
+        <location filename="extensionhandler.cpp" line="164"/>
         <source>Loading, please wait...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="extensionhandler.cpp" line="162"/>
+        <location filename="extensionhandler.cpp" line="163"/>
         <source>Patient loading</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="extensionhandler.cpp" line="255"/>
+        <location filename="extensionhandler.cpp" line="262"/>
         <source>Starviewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="extensionhandler.cpp" line="255"/>
+        <location filename="extensionhandler.cpp" line="262"/>
         <source>Sorry, an error ocurred while loading the data of patients:&lt;br&gt; %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="extensionhandler.cpp" line="179"/>
+        <location filename="extensionhandler.cpp" line="193"/>
         <source>Sorry, it seems that there is no patient data we can load.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -77,238 +77,248 @@
 <context>
     <name>udg::QApplicationMainWindow</name>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="167"/>
+        <location filename="qapplicationmainwindow.cpp" line="180"/>
         <source>&amp;Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="168"/>
+        <location filename="qapplicationmainwindow.cpp" line="181"/>
         <source>Ctrl+W</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="58"/>
+        <location filename="qapplicationmainwindow.cpp" line="59"/>
         <source>Starviewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="81"/>
+        <location filename="qapplicationmainwindow.cpp" line="95"/>
         <source>&amp;New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="82"/>
+        <location filename="qapplicationmainwindow.cpp" line="96"/>
         <source>Ctrl+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="88"/>
+        <location filename="qapplicationmainwindow.cpp" line="102"/>
         <source>&amp;Open file...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="90"/>
+        <location filename="qapplicationmainwindow.cpp" line="104"/>
         <source>Open an existing volume file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="160"/>
+        <location filename="qapplicationmainwindow.cpp" line="173"/>
         <source>&amp;About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="162"/>
+        <location filename="qapplicationmainwindow.cpp" line="175"/>
         <source>Show the application&apos;s About box</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="174"/>
+        <location filename="qapplicationmainwindow.cpp" line="187"/>
         <source>E&amp;xit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="175"/>
+        <location filename="qapplicationmainwindow.cpp" line="188"/>
         <source>Ctrl+Q</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="176"/>
+        <location filename="qapplicationmainwindow.cpp" line="189"/>
         <source>Exit the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="215"/>
+        <location filename="qapplicationmainwindow.cpp" line="228"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="235"/>
+        <location filename="qapplicationmainwindow.cpp" line="248"/>
         <source>&amp;Language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="246"/>
+        <location filename="qapplicationmainwindow.cpp" line="259"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="396"/>
+        <location filename="qapplicationmainwindow.cpp" line="410"/>
         <source>About Starviewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="83"/>
+        <location filename="qapplicationmainwindow.cpp" line="97"/>
         <source>Open a new working window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="89"/>
+        <location filename="qapplicationmainwindow.cpp" line="103"/>
         <source>Ctrl+O</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="104"/>
+        <location filename="qapplicationmainwindow.cpp" line="118"/>
         <source>&amp;PACS...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="105"/>
+        <location filename="qapplicationmainwindow.cpp" line="119"/>
         <source>Ctrl+P</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="106"/>
+        <location filename="qapplicationmainwindow.cpp" line="120"/>
         <source>Open PACS Query Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="226"/>
+        <location filename="qapplicationmainwindow.cpp" line="239"/>
         <source>&amp;Visualization</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="97"/>
+        <location filename="qapplicationmainwindow.cpp" line="111"/>
         <source>Ctrl+D</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="98"/>
+        <location filename="qapplicationmainwindow.cpp" line="112"/>
         <source>Open an existing DICOM folder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="297"/>
+        <location filename="qapplicationmainwindow.cpp" line="310"/>
         <source>Language Switch</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="169"/>
+        <location filename="qapplicationmainwindow.cpp" line="182"/>
         <source>Close the current extension page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="199"/>
+        <location filename="qapplicationmainwindow.cpp" line="212"/>
         <source>Show Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="200"/>
+        <location filename="qapplicationmainwindow.cpp" line="213"/>
         <source>Switch To Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="192"/>
+        <location filename="qapplicationmainwindow.cpp" line="205"/>
         <source>Retract Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="193"/>
+        <location filename="qapplicationmainwindow.cpp" line="206"/>
         <source>Switch To Normal Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="153"/>
+        <location filename="qapplicationmainwindow.cpp" line="166"/>
         <source>Show log file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="240"/>
+        <location filename="qapplicationmainwindow.cpp" line="253"/>
         <source>&amp;Window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="128"/>
+        <location filename="qapplicationmainwindow.cpp" line="141"/>
         <source>Open the %1 Application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="297"/>
+        <location filename="qapplicationmainwindow.cpp" line="310"/>
         <source>The changes will take effect the next time you startup the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="96"/>
+        <location filename="qapplicationmainwindow.cpp" line="110"/>
         <source>Open files from a directory...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="112"/>
+        <location filename="qapplicationmainwindow.cpp" line="126"/>
         <source>Open DICOMDIR...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="114"/>
+        <location filename="qapplicationmainwindow.cpp" line="127"/>
         <source>Open DICOMDIR from CD,DVD,Pendrive or HardDisk</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="181"/>
+        <location filename="qapplicationmainwindow.cpp" line="194"/>
         <source>&amp;Configuration...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="182"/>
+        <location filename="qapplicationmainwindow.cpp" line="195"/>
         <source>Modify Starviewer configuration</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="234"/>
+        <location filename="qapplicationmainwindow.cpp" line="247"/>
         <source>&amp;Tools</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="255"/>
+        <location filename="qapplicationmainwindow.cpp" line="268"/>
         <source>Catalan</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="256"/>
+        <location filename="qapplicationmainwindow.cpp" line="269"/>
         <source>Spanish</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="257"/>
+        <location filename="qapplicationmainwindow.cpp" line="270"/>
         <source>English</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="285"/>
+        <location filename="qapplicationmainwindow.cpp" line="298"/>
         <source>Switch to %1 Language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="448"/>
+        <location filename="qapplicationmainwindow.cpp" line="459"/>
         <source>Beta version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="457"/>
+        <location filename="qapplicationmainwindow.cpp" line="468"/>
         <source>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p align=&apos;justify&apos;&gt;This version of Starviewer is a preview release of our next Starviewer version and it is being made available for testing purposes only.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;This version is intended for radiologist and our testing members that are helping us to improve this software. Current users of this version should not expect all of the extensions to work properly.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;If you  want to help us to improve our software, please, report any bug you found or any other feature request you have to us.&lt;/p&gt;&lt;h3&gt;We really appreciate that you give us your feedback!&lt;/h3&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="409"/>
+        <location filename="qapplicationmainwindow.cpp" line="423"/>
         <source>&lt;h2&gt;Starviewer&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2005-2008 Graphics &amp; Imaging Laboratory (GILab), Girona&lt;p align=&apos;justify&apos;&gt;Starviewer is an image processing software dedicated to DICOM images produced by medical equipment (MRI, CT, PET, PET-CT...) It can also read many other file formats especified by the MetaIO estandard ( *.mhd files ). It is fully compliant with the DICOM standard for image comunication and image file formats. Starviewer is able to receive images transferred by DICOM communication protocol from any PACS or medical imaging modality (STORE SCP - Service Class Provider, STORE SCU - Service Class User, and Query/Retrieve).&lt;p align=&apos;justify&apos;&gt;Starviewer has been specifically designed for navigation and visualization of multimodality and multidimensional images: 2D Viewer, 2D MPR ( Multiplanar reconstruction ) Viewer , 3D MPR Viewer and Hybrid MPR Viewer and Maximum Intensity Projection(MIP).&lt;p align=&apos;justify&apos;&gt;Starviewer is at the same time a DICOM PACS workstation for medical imaging and an image processing software for medical research (radiology and nuclear imaging), functional imaging and 3D imaging.&lt;p&gt;Version: %1 &lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qapplicationmainwindow.cpp" line="70"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qapplicationmainwindow.cpp" line="71"/>
+        <source>Loading data, please wait...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
