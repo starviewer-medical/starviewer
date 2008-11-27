4d1923f4edaf22b591593e63a231d2f5e5b1d77a
Jesús Spínola <jspinola@gmail.com>
2008-11-27 18:16:45 +0000

    ENH: Afegim traduccions pel crash reporter ( ticket #813 )

---
+++ starviewer/src/crashreporter/crashreporter_ca_ES.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ca">
+<defaultcodec></defaultcodec>
+<context>
+    <name>QCrashReporterBase</name>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="13"/>
+        <source>Starviewer Crash Reporter</source>
+        <translation>Informador d&apos;incidències Starviewer</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="26"/>
+        <source>The application Starviewer crashed</source>
+        <translation>L&apos;aplicació Starviewer s&apos;ha tancat inesperadament</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="70"/>
+        <source>We&apos;re sorry Starviewer had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
+        <translation>Ho sentim però Starviewer ja tingut algun problema i s&apos;ha tancat inesperadament. Si dedica un moment per enviar-nos un informe de la incidència ens ajudarà a diagnosticar i arreglar el problema. La seva informació personal no s&apos;enviarà en aquest informe.</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="89"/>
+        <source>Tell Starviewer about this crash</source>
+        <translation>Informar d&apos;aquesta incidència</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="103"/>
+        <source>Comments</source>
+        <translation>Comentaris</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="109"/>
+        <source>Please describe any steps needed to trigger the problem (Optional):</source>
+        <translation>Si us plau descrigui alguns dels passos per poder tornar a reproduir el problema (Opcional):</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="119"/>
+        <source>Email me when more information is available (Optional):</source>
+        <translation>Envïi&apos;m un e-mail quan hi hagi més informació disponible sobre aquest problema (Opcional):</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="130"/>
+        <source>Problem Details</source>
+        <translation>Detalls del Problema</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="143"/>
+        <source>Sending report...</source>
+        <translation>Enviant informe...</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="170"/>
+        <source>Quit Starviewer</source>
+        <translation>Abandonar Starviewer</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="177"/>
+        <source>Restart Starviewer</source>
+        <translation>Re-iniciar Starviewer</translation>
+    </message>
+</context>
+</TS>
---
+++ starviewer/src/crashreporter/crashreporter_en_GB.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>QCrashReporterBase</name>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="13"/>
+        <source>Starviewer Crash Reporter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="26"/>
+        <source>The application Starviewer crashed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="70"/>
+        <source>We&apos;re sorry Starviewer had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="89"/>
+        <source>Tell Starviewer about this crash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="103"/>
+        <source>Comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="109"/>
+        <source>Please describe any steps needed to trigger the problem (Optional):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="119"/>
+        <source>Email me when more information is available (Optional):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="130"/>
+        <source>Problem Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="143"/>
+        <source>Sending report...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="170"/>
+        <source>Quit Starviewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qcrashreporterbase.ui" line="177"/>
+        <source>Restart Starviewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
