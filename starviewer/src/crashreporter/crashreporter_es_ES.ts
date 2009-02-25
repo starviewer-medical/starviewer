254377f171907432c7900493db5b3325015062ba
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2009-02-25 14:34:16 +0000

    ENH: Actualitzades les traduccions del crash reporter.

--- starviewer/src/crashreporter/crashreporter_es_ES.ts
+++ starviewer/src/crashreporter/crashreporter_es_ES.ts
@@ -4,59 +4,82 @@
 <context>
     <name>QCrashReporterBase</name>
     <message>
-        <location filename="qcrashreporterbase.ui" line="13"/>
-        <source>Starviewer Crash Reporter</source>
-        <translation>Informador de Incidencias Starviewer</translation>
-    </message>
-    <message>
-        <location filename="qcrashreporterbase.ui" line="26"/>
+        <location filename="qcrashreporterbase.ui" line="23"/>
         <source>The application Starviewer crashed</source>
         <translation>La aplicación Starviewer ha terminado inesperadamente</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="70"/>
+        <location filename="qcrashreporterbase.ui" line="67"/>
         <source>We&apos;re sorry Starviewer had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
         <translation>Lo sentimos pero Starviewer tuvo algun problema y terminó inesperadamente. Por favor dedique un momento para enviarnos un informe de incidencias que nos ayudará a diagnosticar y arreglar el problema. Su información personal no se enviará con el informe.</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="89"/>
+        <location filename="qcrashreporterbase.ui" line="86"/>
         <source>Tell Starviewer about this crash</source>
         <translation>Informar de la incidencia</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="103"/>
+        <location filename="qcrashreporterbase.ui" line="100"/>
         <source>Comments</source>
         <translation>Comentarios</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="109"/>
+        <location filename="qcrashreporterbase.ui" line="106"/>
         <source>Please describe any steps needed to trigger the problem (Optional):</source>
         <translation>Por favor describa algunos de los pasos necesarios para reproducir el problema (Opcional):</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="119"/>
+        <location filename="qcrashreporterbase.ui" line="116"/>
         <source>Email me when more information is available (Optional):</source>
         <translation>Envíeme un e-mail cuando haya más información disponible acerca del problema (Opcional):</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="130"/>
+        <location filename="qcrashreporterbase.ui" line="127"/>
         <source>Problem Details</source>
         <translation>Detalles del problema</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="143"/>
+        <location filename="qcrashreporterbase.ui" line="140"/>
         <source>Sending report...</source>
         <translation>Enviando informe...</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="170"/>
+        <location filename="qcrashreporterbase.ui" line="167"/>
         <source>Quit Starviewer</source>
         <translation>Abandonar Starviewer</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="177"/>
+        <location filename="qcrashreporterbase.ui" line="174"/>
         <source>Restart Starviewer</source>
         <translation>Reiniciar Starviewer</translation>
+    </message>
+</context>
+<context>
+    <name>udg::QCrashReporter</name>
+    <message>
+        <location filename="qcrashreporter.cpp" line="26"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporter.cpp" line="27"/>
+        <source>Quit %1</source>
+        <translation>Abandonar %1</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporter.cpp" line="28"/>
+        <source>Restart %1</source>
+        <translation>Reiniciar %1</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporter.cpp" line="29"/>
+        <source>Tell %1 about this crash</source>
+        <translation>Informar a %1 sobre el problema</translation>
+    </message>
+    <message>
+        <location filename="qcrashreporter.cpp" line="30"/>
+        <source>We&apos;re sorry %1 had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
+        <translation>Lo sentimos pero %1 tuvo algun problema y terminó inesperadamente. Por favor dedique un momento para enviarnos un informe de incidencias que nos ayudará a diagnosticar y arreglar el problema. Su información personal no se enviará con el informe.</translation>
     </message>
 </context>
 </TS>
