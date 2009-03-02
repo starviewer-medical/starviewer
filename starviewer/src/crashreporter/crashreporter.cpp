c4c4006ab9c82ae3b7b5c1b6cc9bfab012001b91
Quim Rodríguez <quimrog.u@gmail.com>
2009-03-02 23:26:10 +0000

    ENH: S'afegeix el codi necessari per tal de fer el crash reporter traduïble.

--- starviewer/src/crashreporter/crashreporter.cpp
+++ starviewer/src/crashreporter/crashreporter.cpp
@@ -5,17 +5,34 @@
  *   Universitat de Girona                                                 *
  ***************************************************************************/
 
+#include "../main/applicationtranslationsloader.h"
+#include "../core/starviewerapplication.h"
 #include "qcrashreporter.h"
 
 /* Programa principal per executar l'aplicació de crash report */
 
+void initializeTranslations(QApplication &app)
+{
+    udg::ApplicationTranslationsLoader translationsLoader(&app);
+	// li indiquem la locale corresponent
+    QLocale defaultLocale = translationsLoader.getDefaultLocale();
+	QLocale::setDefault( defaultLocale );
+
+    translationsLoader.loadTranslation(":/crashreporter_" + defaultLocale.name());
+}
+
 int main( int argc, char* argv[] )
 {
-
     QApplication app( argc, argv );
 
     if ( app.arguments().size() != 3 )
         return false;
+    
+    app.setOrganizationName( udg::OrganizationNameString );
+    app.setOrganizationDomain( udg::OrganizationDomainString );
+    app.setApplicationName( udg::ApplicationNameString );
+
+    initializeTranslations(app);
     
     udg::QCrashReporter reporter( app.arguments() );
     reporter.show();
