0d4a45166d99d41b1f813634a2a26858c290f7ee
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2009-03-02 11:58:40 +0000

    ENH: Actualització de les traduccions del Q2DViewer.

--- starviewer/src/extensions/main/q2dviewer/translations_ca_ES.ts
+++ starviewer/src/extensions/main/q2dviewer/translations_ca_ES.ts
@@ -1,20 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="ca">
-<defaultcodec></defaultcodec>
 <context>
     <name>udg::MenuGridWidget</name>
     <message>
-        <location filename="menugridwidget.cpp" line="259"/>
+        <location filename="menugridwidget.cpp" line="257"/>
         <source>%1x%2</source>
         <translation>%1x%2</translation>
     </message>
     <message>
-        <location filename="menugridwidget.cpp" line="246"/>
+        <location filename="menugridwidget.cpp" line="244"/>
         <source>%1,%2</source>
         <translation>%1,%2</translation>
     </message>
     <message>
-        <location filename="menugridwidget.cpp" line="203"/>
+        <location filename="menugridwidget.cpp" line="201"/>
         <source>%1</source>
         <translation>%1</translation>
     </message>
@@ -22,84 +21,104 @@
 <context>
     <name>udg::Q2DViewerExtension</name>
     <message>
-        <location filename="q2dviewerextension.cpp" line="103"/>
+        <location filename="q2dviewerextension.cpp" line="125"/>
         <source>Change Current View To Axial</source>
         <translation>Canviar la Vista Actual a Axial</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="109"/>
+        <location filename="q2dviewerextension.cpp" line="131"/>
         <source>Change Current View To Saggital</source>
         <translation>Canviar la Vista Actual a Sagital</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="115"/>
+        <location filename="q2dviewerextension.cpp" line="137"/>
         <source>Change Current View To Coronal</source>
         <translation>Canviar la Vista Actual a Coronal</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="131"/>
+        <location filename="q2dviewerextension.cpp" line="153"/>
         <source>Rotate the image in clockwise direction</source>
         <translation>Rotar la imatge en el sentit horari</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="138"/>
+        <location filename="q2dviewerextension.cpp" line="160"/>
         <source>Rotate Counter Clockwise</source>
         <translation>Rotar en sentit anti-horari</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="140"/>
+        <location filename="q2dviewerextension.cpp" line="162"/>
         <source>Rotate the image in counter clockwise direction</source>
         <translation>Rotar la imatge en sentit anti-horari</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="121"/>
+        <location filename="q2dviewerextension.cpp" line="143"/>
         <source>PS</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="122"/>
+        <location filename="q2dviewerextension.cpp" line="144"/>
         <source>Enable/Disable the current attached</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="147"/>
+        <location filename="q2dviewerextension.cpp" line="169"/>
         <source>Flip Horizontal</source>
         <translation>Capgira horitzontalment</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="148"/>
+        <location filename="q2dviewerextension.cpp" line="170"/>
         <source>Flip the image horizontally</source>
         <translation>Capgira la imatge horitzontalment</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="155"/>
+        <location filename="q2dviewerextension.cpp" line="177"/>
         <source>Flip Vertical</source>
         <translation>Capgira verticalment</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="156"/>
+        <location filename="q2dviewerextension.cpp" line="178"/>
         <source>Flip the image vertically</source>
         <translation>Capgira la imatge verticalment</translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="102"/>
+        <location filename="q2dviewerextension.cpp" line="124"/>
         <source>Axial</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="108"/>
+        <location filename="q2dviewerextension.cpp" line="130"/>
         <source>Sagital</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="114"/>
+        <location filename="q2dviewerextension.cpp" line="136"/>
         <source>Coronal</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="129"/>
+        <location filename="q2dviewerextension.cpp" line="151"/>
         <source>Rotate</source>
         <translation>Rotar</translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="453"/>
+        <source>Save current series image</source>
+        <translation>Guarda una imatge de la sèrie actual</translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="455"/>
+        <source>Save the current image in a standard image format</source>
+        <translation>Guarda una imatge de la sèrie actual amb el format estàndard</translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="458"/>
+        <source>Save all images from current series</source>
+        <translation>Guardar totes les imatges de la sèrie actual</translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="459"/>
+        <source>Save all the images in the selected viewer in a standard image format</source>
+        <translation>Guarda totes les imatges del visualitzador escollit en un format estàndard</translation>
     </message>
 </context>
 <context>
@@ -233,30 +252,25 @@
 <context>
     <name>udg::Q2DViewerExtensionMediator</name>
     <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="26"/>
+        <location filename="q2dviewerextensionmediator.cpp" line="27"/>
         <source>2D Viewer</source>
         <translation>Visor 2D</translation>
     </message>
     <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="41"/>
-        <source>Starviewer</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="41"/>
-        <source>The selected item is not an image</source>
-        <translation>L&apos;ítem seleccionat no és una imatge</translation>
+        <location filename="q2dviewerextensionmediator.cpp" line="42"/>
+        <source>There is not any viewable or supported Series in the selected Studies</source>
+        <translation>No hi ha cap sèrie suportada o que es pugui visualitzar a l&apos;estudi seleccionat</translation>
     </message>
 </context>
 <context>
     <name>udg::TableMenu</name>
     <message>
-        <location filename="tablemenu.cpp" line="98"/>
+        <location filename="tablemenu.cpp" line="97"/>
         <source>%1,%2</source>
         <translation>%1,%2</translation>
     </message>
     <message>
-        <location filename="tablemenu.cpp" line="140"/>
+        <location filename="tablemenu.cpp" line="139"/>
         <source>%1x%2</source>
         <translation>%1x%2</translation>
     </message>
--- starviewer/src/extensions/main/q2dviewer/translations_en_GB.ts
+++ starviewer/src/extensions/main/q2dviewer/translations_en_GB.ts
@@ -3,17 +3,17 @@
 <context>
     <name>udg::MenuGridWidget</name>
     <message>
-        <location filename="menugridwidget.cpp" line="259"/>
+        <location filename="menugridwidget.cpp" line="257"/>
         <source>%1x%2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="menugridwidget.cpp" line="246"/>
+        <location filename="menugridwidget.cpp" line="244"/>
         <source>%1,%2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="menugridwidget.cpp" line="203"/>
+        <location filename="menugridwidget.cpp" line="201"/>
         <source>%1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -21,83 +21,103 @@
 <context>
     <name>udg::Q2DViewerExtension</name>
     <message>
-        <location filename="q2dviewerextension.cpp" line="103"/>
+        <location filename="q2dviewerextension.cpp" line="125"/>
         <source>Change Current View To Axial</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="109"/>
+        <location filename="q2dviewerextension.cpp" line="131"/>
         <source>Change Current View To Saggital</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="115"/>
+        <location filename="q2dviewerextension.cpp" line="137"/>
         <source>Change Current View To Coronal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="131"/>
+        <location filename="q2dviewerextension.cpp" line="153"/>
         <source>Rotate the image in clockwise direction</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="138"/>
+        <location filename="q2dviewerextension.cpp" line="160"/>
         <source>Rotate Counter Clockwise</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="140"/>
+        <location filename="q2dviewerextension.cpp" line="162"/>
         <source>Rotate the image in counter clockwise direction</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="121"/>
+        <location filename="q2dviewerextension.cpp" line="143"/>
         <source>PS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="122"/>
+        <location filename="q2dviewerextension.cpp" line="144"/>
         <source>Enable/Disable the current attached</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="147"/>
+        <location filename="q2dviewerextension.cpp" line="169"/>
         <source>Flip Horizontal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="148"/>
+        <location filename="q2dviewerextension.cpp" line="170"/>
         <source>Flip the image horizontally</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="155"/>
+        <location filename="q2dviewerextension.cpp" line="177"/>
         <source>Flip Vertical</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="156"/>
+        <location filename="q2dviewerextension.cpp" line="178"/>
         <source>Flip the image vertically</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="102"/>
+        <location filename="q2dviewerextension.cpp" line="124"/>
         <source>Axial</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="108"/>
+        <location filename="q2dviewerextension.cpp" line="130"/>
         <source>Sagital</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="114"/>
+        <location filename="q2dviewerextension.cpp" line="136"/>
         <source>Coronal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextension.cpp" line="129"/>
+        <location filename="q2dviewerextension.cpp" line="151"/>
         <source>Rotate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="453"/>
+        <source>Save current series image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="455"/>
+        <source>Save the current image in a standard image format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="458"/>
+        <source>Save all images from current series</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q2dviewerextension.cpp" line="459"/>
+        <source>Save all the images in the selected viewer in a standard image format</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -232,30 +252,25 @@
 <context>
     <name>udg::Q2DViewerExtensionMediator</name>
     <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="26"/>
+        <location filename="q2dviewerextensionmediator.cpp" line="27"/>
         <source>2D Viewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="41"/>
-        <source>Starviewer</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="q2dviewerextensionmediator.cpp" line="41"/>
-        <source>The selected item is not an image</source>
+        <location filename="q2dviewerextensionmediator.cpp" line="42"/>
+        <source>There is not any viewable or supported Series in the selected Studies</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>udg::TableMenu</name>
     <message>
-        <location filename="tablemenu.cpp" line="98"/>
+        <location filename="tablemenu.cpp" line="97"/>
         <source>%1,%2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="tablemenu.cpp" line="140"/>
+        <location filename="tablemenu.cpp" line="139"/>
         <source>%1x%2</source>
         <translation type="unfinished"></translation>
     </message>
