7561ed342a511b6176790544f7be5910591c76ae
Quim Rodríguez <quimrog.u@gmail.com>
2009-03-05 22:35:03 +0000

    ENH: Es canvia la versió a la 0.8.0

--- starviewer/src/core/starviewerapplication.h
+++ starviewer/src/core/starviewerapplication.h
@@ -11,7 +11,7 @@
 
 namespace udg{
 
-const QString StarviewerVersionString("0.8.0.devel");
+const QString StarviewerVersionString("0.8.0");
 
 //Indica per aquesta versió d'starviewer quina és la revisió de bd necessària
 const int StarviewerDatabaseRevisionRequired(3095);
