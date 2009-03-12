534d973ecf4b75e5797da791088d7c8c221ae734
Marc Ruiz <marc.ruiz@gmail.com>
2009-03-12 16:03:38 +0000

    ENH: Càlcul de les mesures de teoria de la informació amb threads quan es fan sobre vectors molt grans. També s'han canviat alguns tipus de la classe per treballar sempre amb doubles, per tenir més precisió amb els càlculs intermitjos.

--- starviewer/src/extensions/playground/experimental3d/informationtheory.h
+++ starviewer/src/extensions/playground/experimental3d/informationtheory.h
@@ -23,25 +23,31 @@ public:
      * Retorna l'entropia de Shannon d'una variable aleatòria amb les probabilitats \p probabilities.
      * H(X) = -sum[0,n)( p(x) log p(x) )
      */
-    static T entropy( const QVector<T> &probabilities );
+    static double entropy( const QVector<T> &probabilities );
     /**
      * Retorna l'entropia de Shannon d'una variable aleatòria amb una distribució de probabilitat representada per \p histogram.
      * H(X) = -sum[0,n)( p(x) log p(x) )
      */
-    static T entropy( const Histogram &histogram );
+    static double entropy( const Histogram &histogram );
     /**
      * Retorna la divergència de Kullback-Leibler entre les distribucions de probabilitat P i Q, amb les probabilitats definides a \p probabilitiesP i \p probabilitiesQ.
      * D_KL(P||Q) = sum[0,n)( P(i) log ( P(i) / Q(i) ) )
      */
-    static T kullbackLeiblerDivergence( const QVector<T> &probabilitiesP, const QVector<T> &probabilitiesQ );
+    static double kullbackLeiblerDivergence( const QVector<T> &probabilitiesP, const QVector<T> &probabilitiesQ );
     /**
      * Retorna la divergència de Jensen-Shannon entre les distribucions de probabilitat P1 i P2, amb les probabilitats definides a \p probabilitiesP1 i \p probabilitiesP2 i
      * els pesos \p pi1 i \p pi2.
      * JSD(pi1,pi2; P1,P2) = H(pi1*P1 + pi2*P2) - ( pi1 * H(P1) + pi2 * H(P2) )
      */
-    static T jensenShannonDivergence( T pi1, T pi2, const QVector<T> &probabilitiesP1, const QVector<T> &probabilitiesP2 );
+    static double jensenShannonDivergence( double pi1, double pi2, const QVector<T> &probabilitiesP1, const QVector<T> &probabilitiesP2 );
 
 private:
+
+    static const int MIN_SIZE_TO_USE_THREADS = 100000;
+
+    class EntropyThread;
+    class KullbackLeiblerDivergenceThread;
+    class ProbabilitiesMixThread;
 
     InformationTheory();
     ~InformationTheory();
