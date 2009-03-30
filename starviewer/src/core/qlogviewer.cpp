07287e60e64f05c1524c21673ce0c049a6e4a242
Jesús Spínola <jspinola@gmail.com>
2009-03-30 14:01:56 +0000

    BUG: El missatge d'error no tenia argument ".arg()" de QString. Ampliem el missatge amb una mica més d'informació

--- starviewer/src/core/qlogviewer.cpp
+++ starviewer/src/core/qlogviewer.cpp
@@ -39,7 +39,7 @@ void QLogViewer::updateData()
     if ( !logFile.open( QFile::ReadOnly | QFile::Text) )
     {
         ERROR_LOG( "No s'ha pogut obrir l'arxiu de logs" );
-        m_logBrowser->setPlainText( tr("ERROR: No Log file found at this path: %1") + udg::UserLogsPath );
+        m_logBrowser->setPlainText( tr("ERROR: No Log file found at this path: %1\nEnvironment variable(logFilePath): %2").arg( udg::UserLogsFile ).arg( getenv("logFilePath") ) );
     }
     else
     {
