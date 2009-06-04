9a2a6c98c9f3b761b1e94e1e373bf0d8d0b6d327
Marc Cals <cpmarc@gmail.com>
2009-06-04 19:10:05 +0000

    ENH:S'ha creat widget QInputOutputPacsWidget a on s'han traslladat totes les responsabilitats de la consulta i operacions relacionades amb el PACS que tenia la QuerysScreen

--- starviewer/src/inputoutput/queryscreen.h
+++ starviewer/src/inputoutput/queryscreen.h
@@ -7,22 +7,14 @@
 #ifndef UDGQUERYSCREEN_H
 #define UDGQUERYSCREEN_H
 
-#include <QHash>
-
 #include "ui_queryscreenbase.h"
-#include "qexecuteoperationthread.h"
-#include "multiplequerystudy.h"
-#include "pacsparameters.h"
-#include "localdatabasemanager.h"
 #include "listenrisrequestthread.h"
 #include "qpopuprisrequestsscreen.h"
 
 namespace udg {
 
 class Status;
-class PacsServer;
 class QCreateDicomdir;
-class ProcessImageSingleton;
 class DicomMask;
 class QOperationStateScreen;
 class StatsWatcher;
@@ -80,36 +72,10 @@ private slots:
     /// Escull a on fer la cerca, si a nivell local o PACS
     void searchStudy();
 
-    /** Busca la informació d'una sèrie
-     * @param studyUID UID de l'estidi
-     */
-    void expandStudy( const QString &studyUID);
-
-    /** Busca la informació d'una sèrie
-     * @param studyUID UID de l'estidi
-     * @param seriesUID
-     */
-    void expandSeries( const QString &studyUID, const QString &seriesUID);
-
     /** Al canviar de pàgina del tab hem de canviar alguns paràmetres, com activar el boto Retrieve, etec..
      * @param index del tab al que s'ha canviat
      */
     void refreshTab( int index );
-
-    /** Aquest mètode s'encarrega de començar la descarrega d'un estudi, estableix la màscara, insereix l'estudi i la sèria a la caché, ademés de crear el thread per continuar amb la descàrrega de l'estdui
-     * @param view boolea que indica si després de la descarrega s'ha de visualitzar l'estudi
-     */
-    void retrieve(bool view = false);
-
-    /// Visualitza un estudi, si aquest estudi esta en el pacs el descarrega i posteriorment es visualitza, si es de la cache el carrega a la classe volum i es visualitza
-    void view();
-
-    /** Slot que s'activa per la classe qexecuteoperationthread, que quant un estudi ha estat descarregat el visualitzar, si l'usuari així ho ha indicat
-     * @param studyUID studyUID de l'estudi descarregat
-     * @param seriesUID seriesUID de la sèrie descarregada
-     * @param sopInstanceUID de la imatge descarregada
-     */
-    void studyRetrievedView( QString studyUID , QString seriesUID , QString sopInstanceUID );
 
     /** Slot que s'activa pel signal de la classe MultimpleQueryStudy, quan s'ha produit un error al connectar amb el pacs
      * @param pacsID ID del pacs a la base de ades local
@@ -121,17 +87,6 @@ private slots:
      */
     void errorQueringStudiesPacs( QString PacsID );
 
-    /** guarda els estudis seleccionats a m_studyTreeWidgetCache al PACS
-     */
-    void storeStudiesToPacs();
-
-    /** Cerca les imatges d'una sèrie al PACS
-     * @param StudyUID uid de l'estudi
-     * @param SeriesUID  uid de la sèrie
-     * @param PacsAETitle AETitle del PACS a buscar la sèrie
-     */
-    void queryImagePacs(QString StudyUID, QString SeriesUID);
-
     void updateOperationsInProgressMessage();
 
     /// Mostra/amaga els camps de cerca avançats
@@ -139,9 +94,6 @@ private slots:
 
     ///Mostra la pantalla QOperationStateScreen
     void showOperationStateScreen();
-
-    ///Ens Mostra un missatge indicant l'error produït a la QExecuteOperationThread, i com es pot solucionar
-    void showQExecuteOperationThreadError(QString studyInstanceUID, QString pacsID, QExecuteOperationThread::OperationError error);
 
     ///Ens mostra un message box indicant l'error produït mentre s'esperaven peticions del RIS
     void showListenRISRequestThreadError(ListenRISRequestThread::ListenRISRequestThreadError);
@@ -154,6 +106,9 @@ private slots:
     ///Fa un refresc del estudis que es mostren al tab de base de dades local
     void refreshLocalDatabaseTab();
 
+    ///Es comunica amb el widget de la base de dades i visualitzar un estudi descarregat del PACS
+    void viewRetrievedStudyFromPacs(QString studyInstanceUID);
+
 private:
 
     #ifdef STARVIEWER_LITE //Al fer remove de la pestanya del pacs es canvia el index de cada tab, per això hem de redefinir-lo pel cas de StarviewerLite
@@ -165,9 +120,7 @@ private:
     ///Connecta els signals i slots pertinents
     void createConnections();
 
-    DicomMask buildSeriesDicomMask(QString);
-
-    /** Construeix la màscara d'entrada pels dicom
+    /** Construeix la màscara d'entrada pels dicom a partir dels widgets de cerca
      * @return retorna la màscara d'un objecte dicom
      */
     DicomMask buildDicomMask();
@@ -177,24 +130,8 @@ private:
      */
     QString getStudyDatesStringMask();
 
-    /** Descarrega una estudi del pacs
-     * @param indica si l'estudi s'ha de visualitzar
-     * @param pacsIdToRetrieve indica l'id del Pacs del qual s'ha de descarregar l'estudi
-     * @param maskStudyToRetrieve la màscara dels objectes a descarregar
-     * @param studyToRetrieve L'estudi al qual pertanyen els objectes ad escarregar
-     */
-    void retrieveFromPacs(bool view, QString pacsIdToRetrieve, DicomMask mask, Study *studyToRetrieve);
-
-    /// Cerca als pacs seleccionats
-    void queryStudyPacs();
-
     ///Cerca els estudis que compleixen la màscara de cerca als pacs passats per paràmetre
     Status queryMultiplePacs(DicomMask searchMask, QList<PacsParameters> listPacsToQuery, MultipleQueryStudy *multipleQueryStudy);
-
-    /** Busca la informació d'una sèrie en el PACS i la mostra en la interfície
-     * @param studyUID UID de l'estidi
-     */
-    void querySeriesPacs(QString studyUID);
 
     ///Comprova els requeriments necessaris per poder utilitzar la QueryScreen
     void checkRequeriments();
@@ -212,19 +149,6 @@ private:
     ///inicialitza les variables necessaries, es cridat pel constructor
     void initialize();
 
-    ///Crear el menú contextual del QStudyTreeWidgetPacs
-    void CreateContextMenuQStudyTreeWidgetPacs();
-
-    ///Estableix la mida de les columnes de QStudyTreeWidget
-    void setQStudyTreeWidgetColumnsWidth();
-
-    /** Donat un AETitle busca les dades del PACS a la configuració i prepara un objecte PACSERVER, per poder
-     * connectar al PACS
-     * @param AETitlePACS Aetitle del PACS a connectar
-     * @return Objecte ParcsServer a punt per connectar amb el PACS
-     */
-    PacsServer getPacsServerByPacsID(QString pacsID);
-
     /**
      * Llegeix i aplica dades de configuració
      */
@@ -233,27 +157,7 @@ private:
     ///Guarda els settings de la QueryScreen
     void writeSettings();
 
-    ///Guarda la mida de les columnes del QStudyTreeView Pacs, dicomdir i cache
-    void saveQStudyTreeWidgetColumnsWidth();
-
-    ///Retorna l'ID del pacs al que pertany l'estudi passat per paràmetre, només té en compte els estudis que s'han consultat a la última query
-    QString getPacsIDFromQueriedStudies(QString studyInstanceUID);
-
 private:
-
-/// estructura necessària per passar els paràmetres al thread que descarregarrà les imatges
-struct retrieveParameters
-     {
-        QString studyUID;
-        PacsParameters pacs;
-      };
-
-    retrieveParameters retParam;
-
-    ProcessImageSingleton *m_processImageSingleton;
-
-    /// Ha de ser global, sino l'objecte es destrueix i QT no té temps d'atendre els signals dels threads
-    MultipleQueryStudy m_multipleQueryStudy;
 
     /** A la pestanya de dicomdir no s'ha de mostrar el QPacsList, per tant a la pestany de dicomdir
      * automaticament l'amaguem, i si tornem a la pestanya de la cache o del pacs, si anteriorment
@@ -266,15 +170,10 @@ struct retrieveParameters
 
     QOperationStateScreen *m_operationStateScreen;
     QCreateDicomdir *m_qcreateDicomdir;
-    QExecuteOperationThread m_qexecuteOperationThread;
-
-    QMenu m_contextMenuQStudyTreeWidgetCache, m_contextMenuQStudyTreeWidgetPacs;
 
     ListenRISRequestThread *m_listenRISRequestThread;
 
     QPopUpRisRequestsScreen *m_qpopUpRisRequestsScreen; //Popup que indica que el RIS ha fet una petició per descarregar un estudi
-
-    QHash<QString, QString> m_hashPacsIDOfStudyInstanceUID;
 
     StatsWatcher *m_statsWatcher;
 };
