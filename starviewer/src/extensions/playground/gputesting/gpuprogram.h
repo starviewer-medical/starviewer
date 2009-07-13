ee5f65b6f1ea6ad10a7569c753d60623adda06d9
Roger Bramon <rogerbramon@gmail.com>
2009-07-13 07:12:12 +0000

    COMP: Es modifiquen alguns fitxers per tal que l'Starviewer compili en Mac OS X. De moment, perquè l'Starviewer compili s'ha hagut de treure l'extensió perfusionmapreconstruction de extensions.inc ja que dóna un error de duplicació de símbols.

--- starviewer/src/extensions/playground/gputesting/gpuprogram.h
+++ starviewer/src/extensions/playground/gputesting/gpuprogram.h
@@ -2,7 +2,11 @@
 #define UDGGPUPROGRAM_H
 
 #include "glew.h" //Ha d'estar abans que includes d'opengl
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 
 #include <QHash>
 #include <QSet>
