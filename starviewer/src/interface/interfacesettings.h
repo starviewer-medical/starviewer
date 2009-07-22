8de2f7bbebd86b8e99180c9d9834894500b662e7
Jesús Spínola <jspinola@gmail.com>
2009-07-22 11:58:01 +0000

    ENH: Fem que es pugui canviar l'extensió per defecte via settings. També es contempla que si l'extensió per defecte configurada no existeix es faci un fallback per obrir igualment el 2D que sempre hi hauria de ser

--- starviewer/src/interface/interfacesettings.h
+++ starviewer/src/interface/interfacesettings.h
@@ -20,6 +20,8 @@ public:
     static const QString applicationMainWindowGeometryKey;
     // Indicarà si permetem tenir més d'una instància de cada extensió (true) o únicament una (false)
     static const QString allowMultipleInstancesPerExtensionKey;
+    // Defineix quina és l'extensió que s'obrirà per defecte
+    static const QString defaultExtensionKey;
 };
 
 } // end namespace udg 
