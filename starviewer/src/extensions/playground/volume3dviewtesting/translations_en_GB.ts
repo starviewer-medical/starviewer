6ad74a4abdd823c9333439beea022f166c5a2652
Jesús Spínola <jspinola@gmail.com>
2009-07-27 14:32:02 +0000

    ENH: Actualització dels arxius de traducció

--- starviewer/src/extensions/playground/volume3dviewtesting/translations_en_GB.ts
+++ starviewer/src/extensions/playground/volume3dviewtesting/translations_en_GB.ts
@@ -1,194 +1,205 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0">
 <context>
     <name>QVolume3DViewTestingExtensionBase</name>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="46"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="50"/>
         <source>Axial</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="74"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="78"/>
         <source>Sagital</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="102"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="106"/>
         <source>Coronal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="184"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="204"/>
         <source>Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="212"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="232"/>
         <source>Rotate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="265"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="285"/>
         <source>Pan</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="475"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="596"/>
         <source>Rendering Method</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="483"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="604"/>
         <source>Ray Casting</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="493"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="614"/>
         <source>MIP</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="498"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="619"/>
         <source>Texture 3D</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="503"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="624"/>
         <source>Texture 2D</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="508"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="629"/>
         <source>Iso Surface</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="140"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="142"/>
         <source>Orientation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="301"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="321"/>
         <source>Mouse Function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="240"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="260"/>
         <source>W/L</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="384"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="354"/>
+        <source>Clip Planes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="385"/>
+        <source>Widgets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="484"/>
         <source>Rendering style</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="396"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="496"/>
         <source>Custom style</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="488"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="609"/>
         <source>Ray Casting with Obscurances</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="513"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="634"/>
         <source>Contouring</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="540"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="668"/>
         <source>Shading</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="550"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="678"/>
         <source>Specular</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="560"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="688"/>
         <source>Specular power</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="603"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="738"/>
         <source>Contour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="613"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="748"/>
         <source>Threshold</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="656"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="809"/>
         <source>Obscurance quality</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="664"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="817"/>
         <source>Low</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="669"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="822"/>
         <source>Medium</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="674"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="827"/>
         <source>High</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="682"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="835"/>
         <source>Compute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="717"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="877"/>
         <source>Obscurance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="727"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="887"/>
         <source>Brightness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="772"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="939"/>
         <source>Iso-value</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="821"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="997"/>
         <source>Load transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="824"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="1000"/>
         <source>Load...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="831"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="1007"/>
         <source>Save transfer function</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="834"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="1010"/>
         <source>Save...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="841"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="1017"/>
         <source>Switch editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextensionbase.ui" line="888"/>
+        <location filename="qvolume3dviewtestingextensionbase.ui" line="1071"/>
         <source>Apply</source>
         <translation type="unfinished"></translation>
     </message>
@@ -196,77 +207,80 @@
 <context>
     <name>udg::QVolume3DViewTestingExtension</name>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="128"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="139"/>
         <source>Spine</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="141"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="152"/>
         <source>Thorax</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="154"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="165"/>
         <source>Pelvis</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="167"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="178"/>
         <source>Circle of Willis</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="179"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="190"/>
         <source>Carotids</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="192"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="203"/>
         <source>Bones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="205"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="216"/>
         <source>Bones 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="217"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="228"/>
         <source>Abdomen bones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="230"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="241"/>
         <source>Abdomen run-off</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="243"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="254"/>
         <source>Abdomen slab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="404"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="396"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="440"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="411"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="425"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="432"/>
         <source>Compute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="470"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="462"/>
         <source>Load CLUT</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="495"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="463"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="484"/>
         <source>Transfer function files (*.tf);;All files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qvolume3dviewtestingextension.cpp" line="495"/>
+        <location filename="qvolume3dviewtestingextension.cpp" line="484"/>
         <source>Save CLUT</source>
         <translation type="unfinished"></translation>
     </message>
