bbf32ee5945dac665c197a273b2512926c8de00a
Marius Vila Duran <marius.vila@gmail.com>
2009-07-28 10:00:25 +0000

    ENH: Afegida la classe QBurningApplicationConfigurationScreen que permet definir el programa de gravació que s'utilitzarà.

---
+++ starviewer/src/inputoutput/qburningapplicationconfigurationscreen.cpp
@@ -0,0 +1,122 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
+ *                                                                         *
+ *   Universitat de Girona                                                 *
+ ***************************************************************************/
+#include "qburningapplicationconfigurationscreen.h"
+
+
+//#include <QIntValidator>
+//#include <QDir>
+#include <QMessageBox>
+#include <QFileDialog>
+
+//#include "localdatabasemanager.h"
+#include "starviewerapplication.h"
+#include "inputoutputsettings.h"
+#include "logging.h"
+
+#include "settings.h"
+
+namespace udg {
+
+
+QBurningApplicationConfigurationScreen::QBurningApplicationConfigurationScreen( QWidget *parent ) : QWidget(parent)
+{
+    setupUi( this );
+    loadBurningDefaults();
+    createConnections();
+}
+
+QBurningApplicationConfigurationScreen::~QBurningApplicationConfigurationScreen()
+{
+}
+
+void QBurningApplicationConfigurationScreen::createConnections()
+{
+    //connecta el boto examinar programa de gravació amb el dialog per escollir el path del programa
+    connect( m_buttonExaminateBurningApplication , SIGNAL( clicked() ), SLOT( examinateBurningApplication() ) );
+}
+
+
+void QBurningApplicationConfigurationScreen::loadBurningDefaults()
+{
+	Settings settings;
+
+	m_textBurningApplicationPath->setText(settings.getValue(InputOutputSettings::burningApplicationPathKey).toString());
+	m_textBurningApplicationParameters->setText(settings.getValue(InputOutputSettings::burningApplicationParametersKey).toString());
+}
+
+bool QBurningApplicationConfigurationScreen::validateChanges()
+{
+    QDir dir;
+    bool valid = true;
+		
+    QString messageBoxText = tr("Some configuration options are not valid:\n");
+
+    if ( m_textBurningApplicationPath->isModified() )
+    {
+        if ( !dir.exists(m_textBurningApplicationPath->text() )) // si el fitxer indicat no existeix
+        {
+			QMessageBox::warning( this , ApplicationNameString , tr( "Invalid burning application path." ) );
+            return false;
+        }
+    }
+
+    if (!valid) QMessageBox::information(this, ApplicationNameString, messageBoxText);
+
+    return valid;
+}
+
+bool QBurningApplicationConfigurationScreen::applyChanges()
+{
+    if (validateChanges())
+    {
+		applyChangesBurningApplication();
+
+        return true;
+    }
+    else 
+	return false;
+	
+}
+
+void QBurningApplicationConfigurationScreen::examinateBurningApplication()
+{
+    //a la pàgina de QT indica que en el cas que nomes deixem seleccionar un fitxer, agafar el primer element de la llista i punt, no hi ha cap mètode que te retornin directament el fitxer selccionat
+    QFileDialog *dlg = new QFileDialog( 0 , QFileDialog::tr( "Open" ) , "./" , ApplicationNameString + " Burning Application (*.exe)" );
+    dlg->setFileMode( QFileDialog::ExistingFile );
+
+    if ( dlg->exec() == QDialog::Accepted )
+    {
+        if ( !dlg->selectedFiles().empty() )
+        {
+            m_textBurningApplicationPath->setText( dlg->selectedFiles().takeFirst() );
+            m_textBurningApplicationPath->setModified( true );// indiquem que m_textDatabaseRoot ha modificat el seu valor
+        }
+    }
+
+    delete dlg;
+}
+
+void QBurningApplicationConfigurationScreen::applyChangesBurningApplication()
+{
+    Settings settings;
+
+	settings.setValue(InputOutputSettings::burningApplicationPathKey, m_textBurningApplicationPath->text());
+	settings.setValue(InputOutputSettings::burningApplicationParametersKey, m_textBurningApplicationParameters->text());
+
+    if ( m_textBurningApplicationPath->isModified() )
+    {
+        INFO_LOG( "Es modificarà el path del programa de gravació per " + m_textBurningApplicationPath->text() );
+    }
+
+
+    if (m_textBurningApplicationParameters->isModified())
+    {
+        INFO_LOG("Es modificarà el parametres del programa de gravació per " + m_textBurningApplicationParameters->text() );
+    }
+}
+
+};
---
+++ starviewer/src/inputoutput/qburningapplicationconfigurationscreen.h
@@ -0,0 +1,91 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
+ *                                                                         *
+ *   Universitat de Girona                                                 *
+ ***************************************************************************/
+#ifndef UDGQBURNINGAPPLICATIONCONFIGURATIONSCREEN_H
+#define UDGQBURNINGAPPLICATIONCONFIGURATIONSCREEN_H
+
+#include "ui_qburningapplicationconfigurationscreenbase.h"
+
+namespace udg {
+
+class Status;
+
+/** Widget en el que es configuren els paràmetres del programa de grabació
+*/
+class QBurningApplicationConfigurationScreen : public QWidget, private ::Ui::QBurningApplicationConfigurationScreenBase
+{
+Q_OBJECT
+
+public:
+    /// Constructor de la classe
+    QBurningApplicationConfigurationScreen( QWidget *parent = 0 );
+
+    ///Destructor de classe
+    ~QBurningApplicationConfigurationScreen();
+
+public slots:
+    /// Aplica els canvis de la configuració
+    bool applyChanges();
+
+//signals:
+    /// Signal que s'emet cada vegada que hi ha un canvi a la configuració que pot afectar al queryscreen
+    //void configurationChanged(const QString& configuration);
+
+private slots:
+    /// Mostra un QDialog per especificar on es troba el programa de grabació en el disc dur
+    void examinateBurningApplication();
+
+    /// Mostra un QDialog per especificar on s'han de guardar les imatges descarregades
+    //void examinateCacheImagePath();
+
+    /// Esborra tota la caché
+    //void deleteStudies();
+
+    /// Compacta la base de dades de la cache
+    //void compactCache();
+
+    /// Slot que s'utilitza quant es fa algun canvi a la configuració, per activar els buttons apply
+    //void enableApplyButtons();
+
+    /// Slot que s'utilitza quant es fa algun canvia el path de la base de dades, per activar els buttons apply
+    //void configurationChangedDatabaseRoot();
+
+    /// Afegeix la '/' al final del path del directori si l'usuari no l'ha escrit
+    //void cacheImagePathEditingFinish();
+
+    /// crear base de dades
+    //void createDatabase();
+
+private:
+    ///crea els connects dels signals i slots
+    void createConnections();
+
+    /// Crea els input validators necessaris pels diferents camps d'edició.
+    //void configureInputValidator();
+
+    /// Tracta els errors que s'han produït a la base de dades en general
+    //void showDatabaseErrorMessage( const Status &state );
+
+    /// Carrega les dades de configuració del programa de grabació
+    void loadBurningDefaults();
+
+    ///  Aplica els canvis fets a la configuració de la cache
+    void applyChangesBurningApplication();
+
+    /** Valida que els canvis de la configuració siguin correctes
+     *  Path de la base de dades i directori dicom's existeix
+     *  @return indica si els canvis son correctes
+     */
+    bool validateChanges();
+
+private:
+    bool m_configurationChanged; ///Indica si la configuració ha canviat
+    bool m_createDatabase; /// Indica si s'ha comprovat demanat que es creï la base de dades indicada a m_textDatabaseRoot
+};
+
+};// end namespace udg
+
+#endif
