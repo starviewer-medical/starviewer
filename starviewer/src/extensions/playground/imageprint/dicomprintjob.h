7675b17e8d985cadde9cc66601aa6361ccdf8b55
Marc Cals <cpmarc@gmail.com>
2009-08-18 19:53:38 +0000

    ENH:Es treu de PrintJob els mètodes per especificar la prioritat de la impressió, aquesta propietat només  s'implementa a dicomprintjob, perquè poder escollir la prioritat és exclusiu de les impressores dicom.
    A DicomPrintJob es canvia el nom de la propietat Priority per PrintPriority, i es canvia el tipus de MediumType per QString

--- starviewer/src/extensions/playground/imageprint/dicomprintjob.h
+++ starviewer/src/extensions/playground/imageprint/dicomprintjob.h
@@ -18,22 +18,22 @@ public:
 	~DicomPrintJob();
     
     void setNumberCopies(const int &_numberCopies);
-    void setPriority(const QString &_priority);
-    void setMediumType(const int &_mediumType);
+    void setPrintPriority(const QString &_printPriority);
+    void setMediumType(const QString &_mediumType);
     void setLabel(const QString &_label);
     void setFilmDestination(const QString &_filmDestination);
 
     int getNumberCopies() const;
-    QString getPriority() const;
-    int getMediumType() const;
+    QString getPrintPriority() const;
+    QString getMediumType() const;
     QString getLabel() const;
     QString getFilmDestination() const;
 
 private:
 
     int         m_numberCopies;
-    QString     m_priority;
-    int         m_mediumType;
+    QString     m_printPriority;
+    QString     m_mediumType;
     QString     m_label;
     QString     m_filmDestination;
 };
