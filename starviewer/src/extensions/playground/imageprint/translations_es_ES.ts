ebb8b88cded684430636421ec7639db56821bffb
Miquel Serra <miquelserra@gmail.com>
2009-08-26 21:32:32 +0000

    STYLE: S'ha canviat el nom dels control de la interfície QDicomPrinterConfigurationWidgetBase per tal de seguir el Coding Style.

--- starviewer/src/extensions/playground/imageprint/translations_es_ES.ts
+++ starviewer/src/extensions/playground/imageprint/translations_es_ES.ts
@@ -1,4 +1,151 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="es_ES" sourcelanguage="ca_ES">
+<context>
+    <name>QDicomPrinterConfigurationWidgetBase</name>
+    <message utf8="true">
+        <source>Configuració de les Impressores DICOM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AETitle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HostName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <source>Configuració d&apos;una Impressora DICOM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hostname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Printer Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Smoothing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MEDIUM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Border Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BLACK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Maxim Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Minium Denstiy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>STANDARD\1,1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Orientation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LANDSCAPE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>24CMX30CM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Destination</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BIN_3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visible Trim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Magnification Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BILINEAR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Job Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CLEAR FILM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
