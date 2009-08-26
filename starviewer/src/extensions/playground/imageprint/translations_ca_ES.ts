23f4534496c4116121cc8fcb4c4c42a3d1b5b078
Miquel Serra <miquelserra@gmail.com>
2009-08-26 21:32:32 +0000

    STYLE: S'ha canviat el nom dels control de la interfície QDicomPrinterConfigurationWidgetBase per tal de seguir el Coding Style.

--- starviewer/src/extensions/playground/imageprint/translations_ca_ES.ts
+++ starviewer/src/extensions/playground/imageprint/translations_ca_ES.ts
@@ -5,15 +5,15 @@
     <name>QDicomPrinterConfigurationWidgetBase</name>
     <message>
         <source>Description</source>
-        <translation type="obsolete">Descripció</translation>
+        <translation type="unfinished">Descripció</translation>
     </message>
     <message utf8="true">
         <source>Configuració d&apos;una Impressora DICOM</source>
-        <translation type="obsolete">Configuració d&apos;una impressora DICOM</translation>
+        <translation type="unfinished">Configuració d&apos;una impressora DICOM</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="obsolete">Esborrar</translation>
+        <translation type="unfinished">Esborrar</translation>
     </message>
     <message>
         <source>Verify</source>
@@ -21,63 +21,135 @@
     </message>
     <message>
         <source>Printer</source>
-        <translation type="obsolete">Impressora</translation>
+        <translation type="unfinished">Impressora</translation>
     </message>
     <message>
         <source>Default Printer Settings</source>
-        <translation type="obsolete">Propietats per defecte de la Impressora</translation>
+        <translation type="unfinished">Propietats per defecte de la Impressora</translation>
     </message>
     <message>
         <source>Film Destination</source>
-        <translation type="obsolete">Destí del Film</translation>
+        <translation type="unfinished">Destí del Film</translation>
     </message>
     <message>
         <source>Maxim Density</source>
-        <translation type="obsolete">Densitat Màx</translation>
+        <translation type="unfinished">Densitat Màx</translation>
     </message>
     <message>
         <source>Minium Denstiy</source>
-        <translation type="obsolete">Densitat Mín</translation>
+        <translation type="unfinished">Densitat Mín</translation>
     </message>
     <message>
         <source>Layout</source>
-        <translation type="obsolete">Format</translation>
+        <translation type="unfinished">Format</translation>
     </message>
     <message>
         <source>Film Orientation</source>
-        <translation type="obsolete">Orientació del Film</translation>
+        <translation type="unfinished">Orientació del Film</translation>
     </message>
     <message>
         <source>Visible Trim</source>
-        <translation type="obsolete">Visualitzar Trim</translation>
+        <translation type="unfinished">Visualitzar Trim</translation>
     </message>
     <message>
         <source>Film Size</source>
-        <translation type="obsolete">Mida del Film</translation>
+        <translation type="unfinished">Mida del Film</translation>
     </message>
     <message>
         <source>Default Job Settings</source>
-        <translation type="obsolete">Propietats per defecte del Job</translation>
+        <translation type="unfinished">Propietats per defecte del Job</translation>
     </message>
     <message>
         <source>Copies</source>
-        <translation type="obsolete">Número de Còpies</translation>
+        <translation type="unfinished">Número de Còpies</translation>
     </message>
     <message>
         <source>Priority</source>
-        <translation type="obsolete">Prioritat</translation>
+        <translation type="unfinished">Prioritat</translation>
     </message>
     <message>
         <source>Medium</source>
-        <translation type="obsolete">Mitjà</translation>
+        <translation type="unfinished">Mitjà</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation type="obsolete">Afegir</translation>
+        <translation type="unfinished">Afegir</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="obsolete">Actualitzar</translation>
+        <translation type="unfinished">Actualitzar</translation>
+    </message>
+    <message utf8="true">
+        <source>Configuració de les Impressores DICOM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AETitle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HostName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hostname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Smoothing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MEDIUM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Border Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BLACK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>STANDARD\1,1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LANDSCAPE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>24CMX30CM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BIN_3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Magnification Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BILINEAR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CLEAR FILM</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
