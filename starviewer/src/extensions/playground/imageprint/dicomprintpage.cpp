f4304181e102b8392641a38fc2655315927cba58
Marc Cals <cpmarc@gmail.com>
2009-09-17 09:18:42 +0000

    ENH:S'afegeix mètode a dicomPrintPage que retorna el número de Files del layout i un altre que retorna el número de Columnes.
    Es fa que el valor del layout de la placa al crear l'spool no sigui hardcoded com fins ara, s'utilitzen els nous mètodes afegits.

--- starviewer/src/extensions/playground/imageprint/dicomprintpage.cpp
+++ starviewer/src/extensions/playground/imageprint/dicomprintpage.cpp
@@ -1,5 +1,8 @@
 #include "dicomprintpage.h"
 
+#include <QRegExp>
+#include <QStringList>
+
 namespace udg 
 {
 
@@ -21,6 +24,24 @@ void DicomPrintPage::setFilmLayout(const QString &filmLayout)
 QString DicomPrintPage::getFilmLayout() const
 {
     return m_filmLayout;
+}
+
+int DicomPrintPage::getFilmLayoutColumns()
+{
+    int columns, rows;
+
+    getFilmLayoutColumnsRows(columns, rows);
+
+    return columns;
+}
+
+int DicomPrintPage::getFilmLayoutRows()
+{
+    int columns, rows;
+
+    getFilmLayoutColumnsRows(columns, rows);
+
+    return rows;
 }
 
 void DicomPrintPage::setFilmOrientation(const QString &filmOrientation)
@@ -112,4 +133,28 @@ QString DicomPrintPage::getPolarity() const
 {
     return m_polarity;
 }
+
+void DicomPrintPage::getFilmLayoutColumnsRows(int &columns, int &rows)
+{
+    QStringList splittedFilmLayout;
+    QString filmLayout = getFilmLayout();
+    /*Comprovem amb expresions regulars que sigui un film layout correcte tenir en compte que en c++ per indicar '\' hem d'escriure '\\'
+      en el cas com és aquest que l'expresiò regular conté un '\', en expresió regular s'escriu '\\', i en codi ho hem d'escriur com '\\\\' */
+
+    if (filmLayout.contains(QRegExp("STANDARD\\\\\\d,\\d", Qt::CaseInsensitive))) //STANDARD\C,R
+    {
+        filmLayout.remove("STANDARD\\");
+        splittedFilmLayout = filmLayout.split(",");
+
+        columns = splittedFilmLayout.at(0).toInt();
+        rows = splittedFilmLayout.at(1).toInt();
+    }
+    else
+    {
+        columns = -1;
+        rows = -1;
+    }
+    //TODO: falten per implementar obtenir el número de columnes i files pels altres Layouts que també contempla el DICOM (PS 3.3 pàg 918 Tag Image Display Format)
+}
+
 }
