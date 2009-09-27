835e480aa062354f5df026e636f35a279addb1ab
Miquel Serra <miquelserra@gmail.com>
2009-09-27 21:06:14 +0000

    STYLE: S'aplica el Coding Styling a QDicomPritnerConfigurationWidget (documentació dels mètodes...) També es renombren alguns mètodes privats per tal de reflectir millor el seu funcionament.

--- starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.cpp
+++ starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.cpp
@@ -11,12 +11,12 @@ namespace udg {
 QDicomPrinterConfigurationWidget::QDicomPrinterConfigurationWidget()
 {
     setupUi( this );
-    createActions();	
     createConnections();
 
     m_listPrintersTreeWidget->setColumnHidden(0,true); 
-    clearPrinterParameters();
-    fillPrintersList();
+    clearPrinterSettings();
+    refreshPrinterList();
+    m_semafor=false;
 }
 
 QDicomPrinterConfigurationWidget::~QDicomPrinterConfigurationWidget()
@@ -24,81 +24,94 @@ QDicomPrinterConfigurationWidget::~QDicomPrinterConfigurationWidget()
 
 }
 
+
 //*** Public Slot***//
-void  QDicomPrinterConfigurationWidget::choosePrinter()
+
+void  QDicomPrinterConfigurationWidget::printerSelectionChanged()
 {
+    m_semafor=true;
     DicomPrinterManager dicomPrinterManager;
     QTreeWidgetItem *selectedItem = 0;
+    
+    this->clearPrinterSettings();
 
     if( !m_listPrintersTreeWidget->selectedItems().isEmpty() )
     {   
         selectedItem = m_listPrintersTreeWidget->selectedItems().first();
-        selectedPrinterId=selectedItem->text(0).toInt();
-        DicomPrinter selectedPrinter = dicomPrinterManager.getPrinterByID(selectedPrinterId);
+        m_selectedPrinterId = selectedItem->text(0).toInt();
+        
+        DicomPrinter selectedPrinter = dicomPrinterManager.getPrinterByID(m_selectedPrinterId);
     
-        // Printer
-        m_printerAetitleLineEdit->setText(selectedPrinter.getAETitle());
-        m_printerDescriptionLineEdit->setText(selectedPrinter.getDescription());
-        m_printerHostnameLineEdit->setText(selectedPrinter.getHostname());
-        m_printerPortLineEdit->setText(QString::number( selectedPrinter.getPort()));
+        this->setPrinterSettingsToControls(selectedPrinter);
+        this->setDefaultPrinterSettingsToControls(selectedPrinter);
+        this->setDefaultJobSettingsToControls(selectedPrinter);
+    }
+    m_semafor=false;
+}
 
-        // Default Job Settings
-        m_priorityComboBox->clear();
-        m_priorityComboBox->addItems(selectedPrinter.getAvailablePrintPriorityValues());
-        m_priorityComboBox->setCurrentIndex(m_priorityComboBox->findText(selectedPrinter.getDefaultPrintPriority()));
-        m_mediumTypeComboBox->clear();
-        m_mediumTypeComboBox->addItems(selectedPrinter.getAvailableMediumTypeValues());
-        m_mediumTypeComboBox->setCurrentIndex(m_mediumTypeComboBox->findText(selectedPrinter.getDefaultMediumType()));
+void QDicomPrinterConfigurationWidget::addPrinter()
+{
+    DicomPrinter dicomPrinter;
+    DicomPrinterManager dicomPrinterManager;
+    if (validatePrinterSettings())
+    {   
+        this->getPrinterSettingsFromControls(dicomPrinter);
+        this->getDefaultPrinterSettingsFromControls(dicomPrinter);
+        this->getDefaultJobSettingsFromControls(dicomPrinter);
 
-        // Default Printer Settings
-        m_layoutComboBox->clear();
-        m_layoutComboBox->addItems(selectedPrinter.getAvailableFilmLayoutValues());
-        m_layoutComboBox->setCurrentIndex(m_layoutComboBox->findText(selectedPrinter.getDefaultFilmLayout()));    
-        m_filmOrientationComboBox->clear();
-        m_filmOrientationComboBox->addItems(selectedPrinter.getAvailableFilmOrientationValues());
-        m_filmOrientationComboBox->setCurrentIndex(m_filmOrientationComboBox->findText(selectedPrinter.getDefaultFilmOrientation()));
-        m_filmSizeComboBox->clear();
-        m_filmSizeComboBox->addItems(selectedPrinter.getAvailableFilmSizeValues());
-        m_filmSizeComboBox->setCurrentIndex(m_filmSizeComboBox->findText(selectedPrinter.getDefaultFilmSize()));
-        m_filmDestinationComboBox->clear();
-        m_filmDestinationComboBox->addItems(selectedPrinter.getAvailableFilmDestinationValues());
-        m_filmDestinationComboBox->setCurrentIndex(m_filmDestinationComboBox->findText(selectedPrinter.getDefaultFilmDestination()));
-        m_magnifactionTypeComboBox->clear();
-        m_magnifactionTypeComboBox->addItems(selectedPrinter.getAvailableMagnificationTypeValues());
-        m_magnifactionTypeComboBox->setCurrentIndex(m_magnifactionTypeComboBox->findText(selectedPrinter.getDefaultMagnificationType()));
-        m_smoothingTypeComboBox->clear();
-        m_smoothingTypeComboBox->addItems(selectedPrinter.getAvailableSmoothingTypeValues());
-        m_smoothingTypeComboBox->setCurrentIndex(m_smoothingTypeComboBox->findText(selectedPrinter.getDefaultSmoothingType()));
-        m_borderDensityComboBox->clear();
-        m_borderDensityComboBox->addItems(selectedPrinter.getAvailableBorderDensityValues());
-        m_borderDensityComboBox->setCurrentIndex(m_borderDensityComboBox->findText(selectedPrinter.getDefaultBorderDensity()));
-        m_emptyDensityComboBox->clear();    
-        m_emptyDensityComboBox->addItems(selectedPrinter.getAvailableEmptyImageDensityValues());
-        m_emptyDensityComboBox->setCurrentIndex(m_emptyDensityComboBox->findText(selectedPrinter.getDefaultEmptyImageDensity()));
-        m_maximDensitySpinBox->setMaximum(selectedPrinter.getAvailableMaxDensityValues());    
-        m_maximDensitySpinBox->setValue(selectedPrinter.getDefaultMaxDensity());
-        m_miniumDensitySpinBox->setMaximum(selectedPrinter.getAvailableMinDensityValues());
-        m_miniumDensitySpinBox->setValue(selectedPrinter.getDefaultMinDensity());
-
-        if(selectedPrinter.getAvailableTrim())
+        if ( !dicomPrinterManager.addPrinter(dicomPrinter) )
         {
-            m_visibleTrimCheckBox->setCheckable(true);
-            m_visibleTrimCheckBox->setChecked(selectedPrinter.getDefaultTrim());
+            QMessageBox::warning(this, ApplicationNameString, tr("This Printer already exists."));
         }
         else
-        {   
-            m_visibleTrimCheckBox->setChecked(false);
-            m_visibleTrimCheckBox->setCheckable(false);
+        {
+            refreshPrinterList();
+            clearPrinterSettings();
         }
     }
-    else
+}
+
+void QDicomPrinterConfigurationWidget::updatePrinter()
+{
+    DicomPrinter dicomPrinter;
+    DicomPrinterManager dicomPrinterManager;
+    if (validatePrinterSettings())
     {   
-        clearPrinterParameters();
+        this->getPrinterSettingsFromControls(dicomPrinter);
+        this->getDefaultPrinterSettingsFromControls(dicomPrinter);
+        this->getDefaultJobSettingsFromControls(dicomPrinter);
+
+        if (!dicomPrinterManager.updatePrinter(m_selectedPrinterId,dicomPrinter))
+        {
+            QMessageBox::warning(this, ApplicationNameString, tr("This Printer already exists."));
+        }
+        else
+        {
+            refreshPrinterList();
+            clearPrinterSettings();
+        }
     }
+}
+
+void QDicomPrinterConfigurationWidget::deletePrinter()
+{
+    DicomPrinterManager dicomPrinterManager;
+    dicomPrinterManager.deletePrinter(m_selectedPrinterId);
+    this->refreshPrinterList();
+}
+
+void QDicomPrinterConfigurationWidget::testPrinter()
+{
+
 }
 
 void  QDicomPrinterConfigurationWidget::getAvailableParameters()
 {
+    if(m_semafor) 
+    {
+        return;
+    }
+
     QString text = m_printerAetitleLineEdit->text();
     if ( text.length() == 0 )
     {
@@ -165,109 +178,11 @@ void  QDicomPrinterConfigurationWidget::getAvailableParameters()
     }
 }
 
-void QDicomPrinterConfigurationWidget::addPrinter()
-{
-    DicomPrinter dicomPrinter;
-    DicomPrinterManager dicomPrinterManager;
-    if (validatePrinterParameters())
-    {   
-        // Printer Parameters
-        dicomPrinter.setAETitle(m_printerAetitleLineEdit->text());
-        dicomPrinter.setDescription(m_printerDescriptionLineEdit->text());
-        dicomPrinter.setHostname(m_printerHostnameLineEdit->text());
-        dicomPrinter.setPort(m_printerPortLineEdit->text().toInt());
-    
-         // Default Job Settings
-        dicomPrinter.setDefaultPrintPriority(m_priorityComboBox->currentText());   
-        dicomPrinter.setDefaultMediumType(m_mediumTypeComboBox->currentText());
+//*** Private Methods***//
 
-        // Default Printer Settings
-        dicomPrinter.setDefaultFilmLayout(m_layoutComboBox->currentText());
-        dicomPrinter.setDefaultFilmOrientation(m_filmOrientationComboBox->currentText());
-        dicomPrinter.setDefaultFilmSize(m_filmSizeComboBox->currentText());
-        dicomPrinter.setDefaultFilmDestination(m_filmDestinationComboBox->currentText());
-        dicomPrinter.setDefaultMagnificationType(m_magnifactionTypeComboBox->currentText());
-        dicomPrinter.setDefaultSmoothingType(m_smoothingTypeComboBox->currentText());
-        dicomPrinter.setDefaultBorderDensity(m_borderDensityComboBox->currentText());
-        dicomPrinter.setDefaultEmptyImageDensity(m_emptyDensityComboBox->currentText());
-        dicomPrinter.setDefaultMaxDensity(m_maximDensitySpinBox->value());
-        dicomPrinter.setDefaultMinDensity(m_miniumDensitySpinBox->value());
-        dicomPrinter.setDefaultTrim(m_visibleTrimCheckBox->isChecked());
-
-        if ( !dicomPrinterManager.addPrinter(dicomPrinter) )
-        {
-            QMessageBox::warning(this, ApplicationNameString, tr("This Printer already exists."));
-        }
-        else
-        {
-            fillPrintersList();
-            clearPrinterParameters();
-        }
-    }
-}
-
-void QDicomPrinterConfigurationWidget::updatePrinter()
-{
-    DicomPrinter dicomPrinter;
-    DicomPrinterManager dicomPrinterManager;
-    if (validatePrinterParameters())
-    {   
-        // Printer Parameters
-        dicomPrinter.setAETitle(m_printerAetitleLineEdit->text());
-        dicomPrinter.setDescription(m_printerDescriptionLineEdit->text());
-        dicomPrinter.setHostname(m_printerHostnameLineEdit->text());
-        dicomPrinter.setPort(m_printerPortLineEdit->text().toInt());
-    
-         // Default Job Settings
-        dicomPrinter.setDefaultPrintPriority(m_priorityComboBox->currentText());   
-        dicomPrinter.setDefaultMediumType(m_mediumTypeComboBox->currentText());
-
-        // Default Printer Settings
-        dicomPrinter.setDefaultFilmLayout(m_layoutComboBox->currentText());
-        dicomPrinter.setDefaultFilmOrientation(m_filmOrientationComboBox->currentText());
-        dicomPrinter.setDefaultFilmSize(m_filmSizeComboBox->currentText());
-        dicomPrinter.setDefaultFilmDestination(m_filmDestinationComboBox->currentText());
-        dicomPrinter.setDefaultMagnificationType(m_magnifactionTypeComboBox->currentText());
-        dicomPrinter.setDefaultSmoothingType(m_smoothingTypeComboBox->currentText());
-        dicomPrinter.setDefaultBorderDensity(m_borderDensityComboBox->currentText());
-        dicomPrinter.setDefaultEmptyImageDensity(m_emptyDensityComboBox->currentText());
-        dicomPrinter.setDefaultMaxDensity(m_maximDensitySpinBox->value());
-        dicomPrinter.setDefaultMinDensity(m_miniumDensitySpinBox->value());
-        dicomPrinter.setDefaultTrim(m_visibleTrimCheckBox->isChecked());
-
-        if (!dicomPrinterManager.updatePrinter(selectedPrinterId,dicomPrinter))
-        {
-            QMessageBox::warning(this, ApplicationNameString, tr("This Printer already exists."));
-        }
-        else
-        {
-            fillPrintersList();
-            clearPrinterParameters();
-        }
-    }
-}
-
-void QDicomPrinterConfigurationWidget::deletePrinter()
-{
-    DicomPrinterManager dicomPrinterManager;
-    dicomPrinterManager.deletePrinter(selectedPrinterId);
-    this->fillPrintersList();
-}
-
-void QDicomPrinterConfigurationWidget::testPrinter()
-{
-
-}
-
-//*** Private Methodes***//
 void QDicomPrinterConfigurationWidget::createConnections()
 { 
-	
-}
-
-void QDicomPrinterConfigurationWidget::createActions()
-{
-    connect( m_listPrintersTreeWidget , SIGNAL( itemSelectionChanged() ), SLOT( choosePrinter() ) );
+    connect( m_listPrintersTreeWidget , SIGNAL( itemSelectionChanged() ), SLOT( printerSelectionChanged() ) );
 
     connect( m_addPrinterPushButton , SIGNAL( clicked() ), SLOT( addPrinter() ));
     connect( m_updatePrinterPushButton , SIGNAL( clicked() ), SLOT( updatePrinter() ));
@@ -278,7 +193,7 @@ void QDicomPrinterConfigurationWidget::createActions()
     connect( m_printerPortLineEdit, SIGNAL( textChanged(const QString &) ), SLOT( getAvailableParameters() ) );
 }
 
-void QDicomPrinterConfigurationWidget::fillPrintersList()
+void QDicomPrinterConfigurationWidget::refreshPrinterList()
 {   
     DicomPrinterManager dicomPrinterManager;
     QList<DicomPrinter> dicomPrintersList = dicomPrinterManager.getDicomPrinterList();
@@ -297,42 +212,7 @@ void QDicomPrinterConfigurationWidget::fillPrintersList()
     }
 }
 
-void QDicomPrinterConfigurationWidget::clearPrinterParameters()
-{
-    // Printer
-    m_printerAetitleLineEdit->setText("");
-    m_printerDescriptionLineEdit->setText("");
-    m_printerHostnameLineEdit->setText("");
-    m_printerPortLineEdit->setText("");
-
-    // Default Job Settings
-    m_numberCopiesSpinBox->setValue(0);
-    m_priorityComboBox->setCurrentIndex(-1);
-    m_mediumTypeComboBox->setCurrentIndex(-1);
-
-    m_maximDensitySpinBox->setValue(0);
-    m_maximDensitySpinBox->setValue(0);
-    m_visibleTrimCheckBox->setChecked(false);
-
-     // Default Job Settings
-    m_priorityComboBox->clear();
-    m_mediumTypeComboBox->clear();        
-    m_mediumTypeComboBox->setCurrentIndex(-1);
-
-    // Default Printer Settings
-    m_layoutComboBox->clear();
-    m_filmOrientationComboBox->clear();
-    m_filmSizeComboBox->clear();
-    m_filmDestinationComboBox->clear();
-    m_magnifactionTypeComboBox->clear();
-    m_smoothingTypeComboBox->clear();
-    m_borderDensityComboBox->clear();    
-    m_emptyDensityComboBox->clear();    
-    m_maximDensitySpinBox->setValue(0);
-    m_miniumDensitySpinBox->setValue(0);
-}
-
-bool QDicomPrinterConfigurationWidget::validatePrinterParameters()
+bool QDicomPrinterConfigurationWidget::validatePrinterSettings()
 {
     QString text;
     
@@ -358,5 +238,110 @@ bool QDicomPrinterConfigurationWidget::validatePrinterParameters()
         return false;
     }
     return true;
+}
+
+void QDicomPrinterConfigurationWidget::clearPrinterSettings()
+{
+    // Printer
+    m_printerAetitleLineEdit->setText("");
+    m_printerDescriptionLineEdit->setText("");
+    m_printerHostnameLineEdit->setText("");
+    m_printerPortLineEdit->setText("");
+
+    // Default Job Settings
+    m_numberCopiesSpinBox->setValue(0);
+    m_priorityComboBox->setCurrentIndex(-1);
+    m_mediumTypeComboBox->setCurrentIndex(-1);
+    m_maximDensitySpinBox->setValue(0);
+    m_maximDensitySpinBox->setValue(0);
+    m_visibleTrimCheckBox->setChecked(false);
+
+     // Default Job Settings
+    m_priorityComboBox->clear();
+    m_mediumTypeComboBox->clear();        
+    m_mediumTypeComboBox->setCurrentIndex(-1);
+
+    // Default Printer Settings
+    m_layoutComboBox->clear();
+    m_filmOrientationComboBox->clear();
+    m_filmSizeComboBox->clear();
+    m_filmDestinationComboBox->clear();
+    m_magnifactionTypeComboBox->clear();
+    m_smoothingTypeComboBox->clear();
+    m_borderDensityComboBox->clear();    
+    m_emptyDensityComboBox->clear();    
+    m_maximDensitySpinBox->setValue(0);
+    m_miniumDensitySpinBox->setValue(0);
+}
+
+void QDicomPrinterConfigurationWidget::setPrinterSettingsToControls(DicomPrinter& printer)
+{
+    m_printerAetitleLineEdit->setText(printer.getAETitle());
+    m_printerDescriptionLineEdit->setText(printer.getDescription());
+    m_printerHostnameLineEdit->setText(printer.getHostname());
+    m_printerPortLineEdit->setText(QString::number( printer.getPort()));
+}
+
+void QDicomPrinterConfigurationWidget::getPrinterSettingsFromControls(DicomPrinter& printer)
+{
+    printer.setAETitle(m_printerAetitleLineEdit->text());
+    printer.setDescription(m_printerDescriptionLineEdit->text());
+    printer.setHostname(m_printerHostnameLineEdit->text());
+    printer.setPort(m_printerPortLineEdit->text().toInt());
+}
+
+void QDicomPrinterConfigurationWidget::setDefaultPrinterSettingsToControls(DicomPrinter& printer)
+{
+    m_layoutComboBox->addItems(printer.getAvailableFilmLayoutValues());
+    m_layoutComboBox->setCurrentIndex(m_layoutComboBox->findText(printer.getDefaultFilmLayout()));    
+    m_filmOrientationComboBox->addItems(printer.getAvailableFilmOrientationValues());
+    m_filmOrientationComboBox->setCurrentIndex(m_filmOrientationComboBox->findText(printer.getDefaultFilmOrientation()));
+    m_filmSizeComboBox->addItems(printer.getAvailableFilmSizeValues());
+    m_filmSizeComboBox->setCurrentIndex(m_filmSizeComboBox->findText(printer.getDefaultFilmSize()));
+    m_filmDestinationComboBox->addItems(printer.getAvailableFilmDestinationValues());
+    m_filmDestinationComboBox->setCurrentIndex(m_filmDestinationComboBox->findText(printer.getDefaultFilmDestination()));
+    m_magnifactionTypeComboBox->addItems(printer.getAvailableMagnificationTypeValues());
+    m_magnifactionTypeComboBox->setCurrentIndex(m_magnifactionTypeComboBox->findText(printer.getDefaultMagnificationType()));
+    m_smoothingTypeComboBox->addItems(printer.getAvailableSmoothingTypeValues());
+    m_smoothingTypeComboBox->setCurrentIndex(m_smoothingTypeComboBox->findText(printer.getDefaultSmoothingType()));
+    m_borderDensityComboBox->addItems(printer.getAvailableBorderDensityValues());
+    m_borderDensityComboBox->setCurrentIndex(m_borderDensityComboBox->findText(printer.getDefaultBorderDensity()));
+    m_emptyDensityComboBox->addItems(printer.getAvailableEmptyImageDensityValues());
+    m_emptyDensityComboBox->setCurrentIndex(m_emptyDensityComboBox->findText(printer.getDefaultEmptyImageDensity()));
+    m_maximDensitySpinBox->setMaximum(printer.getAvailableMaxDensityValues());    
+    m_maximDensitySpinBox->setValue(printer.getDefaultMaxDensity());
+    m_miniumDensitySpinBox->setMaximum(printer.getAvailableMinDensityValues());
+    m_miniumDensitySpinBox->setValue(printer.getDefaultMinDensity());
+    m_visibleTrimCheckBox->setCheckable(printer.getAvailableTrim());
+    m_visibleTrimCheckBox->setChecked(printer.getDefaultTrim());    
+}
+
+void QDicomPrinterConfigurationWidget::getDefaultPrinterSettingsFromControls(DicomPrinter& printer)
+{   
+    printer.setDefaultFilmLayout(m_layoutComboBox->currentText());
+    printer.setDefaultFilmOrientation(m_filmOrientationComboBox->currentText());
+    printer.setDefaultFilmSize(m_filmSizeComboBox->currentText());
+    printer.setDefaultFilmDestination(m_filmDestinationComboBox->currentText());
+    printer.setDefaultMagnificationType(m_magnifactionTypeComboBox->currentText());
+    printer.setDefaultSmoothingType(m_smoothingTypeComboBox->currentText());
+    printer.setDefaultBorderDensity(m_borderDensityComboBox->currentText());
+    printer.setDefaultEmptyImageDensity(m_emptyDensityComboBox->currentText());
+    printer.setDefaultMaxDensity(m_maximDensitySpinBox->value());
+    printer.setDefaultMinDensity(m_miniumDensitySpinBox->value());
+    printer.setDefaultTrim(m_visibleTrimCheckBox->isChecked());
+}
+
+void QDicomPrinterConfigurationWidget::setDefaultJobSettingsToControls(DicomPrinter& printer)
+{   
+    m_priorityComboBox->addItems(printer.getAvailablePrintPriorityValues());
+    m_priorityComboBox->setCurrentIndex(m_priorityComboBox->findText(printer.getDefaultPrintPriority()));
+    m_mediumTypeComboBox->addItems(printer.getAvailableMediumTypeValues());
+    m_mediumTypeComboBox->setCurrentIndex(m_mediumTypeComboBox->findText(printer.getDefaultMediumType()));
+}
+
+void QDicomPrinterConfigurationWidget::getDefaultJobSettingsFromControls(DicomPrinter& printer)
+{
+    printer.setDefaultPrintPriority(m_priorityComboBox->currentText());   
+    printer.setDefaultMediumType(m_mediumTypeComboBox->currentText());
 }
 }                                      
--- starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.h
+++ starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.h
@@ -4,14 +4,13 @@
 #include "ui_qdicomprinterconfigurationwidgetbase.h"
 #include "qprinterconfigurationwidget.h"
 
+namespace udg {
+
+class DicomPrinter;
 
 /**
  * Implementació de la interfície QPrinterConfigurationWidget per impressió d'imatge DICOM en DCMTK.
  */
-
-namespace udg {
-
-class Printer;
 
 class QDicomPrinterConfigurationWidget : public QPrinterConfigurationWidget , private::Ui::QDicomPrinterConfigurationWidgetBase
 {
@@ -22,22 +21,42 @@ public:
     ~QDicomPrinterConfigurationWidget();
 	
 public slots:
-    void choosePrinter();
+    // Acualiza la informació del Printer cada vegada que es selecciona al QTree una impressora.
+    void printerSelectionChanged();
+    // Afegeix una nova impressora Dicom a partir dels paràmetres introduits per l'usuari.
     void addPrinter();
+    // Modifica una impressora Dicom ja existen amb els paràmetres introduits per l'usuari.
     void updatePrinter();
+    // Esborra una impressora.
     void deletePrinter();
+    // Comprova la connexió amb una impressora entrada al sistema.
     void testPrinter();
+    // Mostra per pantalla els diferents valors que poden agafar cadascun dels paràmetres d'una impressora.
     void getAvailableParameters();
 
 private:
     void createConnections();
-    void createActions();
-    void fillPrintersList();
-    bool validatePrinterParameters();
-    void clearPrinterParameters();
+    // Actualitza la llista d'impressores entrades al sistema.
+    void refreshPrinterList();
+    // Comprova la validesa dels pàrametres d'entrada.
+    bool validatePrinterSettings();
+    // Neteja els valors de la interífice.
+    void clearPrinterSettings();
+    // Es mostra la informació bàsica de la impressora per pantalla (/p printer és la impressora que conté la informació a mostrar)
+    void setPrinterSettingsToControls(DicomPrinter& printer);
+    // Es guarda la informació bàsica de la impressora de l'interfície a un objecte DicomPrinter (/p printer és la impressora on es guarden les dades).
+    void getPrinterSettingsFromControls(DicomPrinter& printer);
+    // Es mostra la informació avançada de la impressora per pantalla (/p printer és la impressora que conté la informació a mostrar)
+    void setDefaultPrinterSettingsToControls(DicomPrinter& printer);
+     // Es guarda la informació avançada de la impressora de l'interfície a un objecte DicomPrinter (/p printer és la impressora on es guarden les dades).
+    void getDefaultPrinterSettingsFromControls(DicomPrinter& printer);
+    // Es mostra la informació del Job de la impressora per pantalla (/p printer és la impressora que conté la informació a mostrar)
+    void setDefaultJobSettingsToControls(DicomPrinter& printer);
+    // Es guarda la informació del Job de la impressora de l'interfície a un objecte DicomPrinter (/p printer és la impressora on es guarden les dades).
+    void getDefaultJobSettingsFromControls(DicomPrinter& printer);
     
-    int selectedPrinterId;
+    int m_selectedPrinterId;
+    bool m_semafor;
 };
-} 
-
+};
 #endif
