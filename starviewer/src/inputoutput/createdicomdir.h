a930c7d4c36647b4cd81264c95340e6ea2e9365f
Marc Cals <cpmarc@gmail.com>
2009-09-28 17:05:17 +0000

    BUG:Al intentar fer un DICOMDIR per imatges que no eren Little Endian, no podiem generar el dicomdir, perquè ens trobaven que en alguns casos la tansfer syntax de la imatge incomplien les que la normativa DICOM indica per un determinat profile

--- starviewer/src/inputoutput/createdicomdir.h
+++ starviewer/src/inputoutput/createdicomdir.h
@@ -15,7 +15,8 @@ namespace udg {
 
 class Status;
 
-/** Converteix un directori determinat a dicomdir. Per a que un directori es pugui convertir a dicomdir, els fitxer dicom han de tenir un nom com a molt de 8 caràcters, i han d'estar guardats en little endian
+/** Converteix un directori determinat a dicomdir. Per a que un directori es pugui convertir a dicomdir, els fitxer dicom han de tenir 
+    un nom com a molt de 8 caràcters.
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
 class CreateDicomdir
@@ -40,6 +41,22 @@ public:
      * @param enabled indica si s'activa el mode esctricte
      */
     void setStrictMode(bool enabled);
+
+    /** Indiquem si s'ha de comprovar si les imatges són del transfer syntax que indica la norma DICOM en funció del dispositiu de gravació escollit.
+     * En funció del dispositiu escollit on es crea el dicomdir la normativa DICOM indica que el DICOMDIR només pot contenir imatges 
+     * d'una determinada transfer syntax, per exemple pel profile STD-GEN-CD el  s'indica que només poden contenir imatges amb transfer syntax 
+     * Explicit Little Endian, o en el cas de STD-GEN-DVD-JPEG s'accepta Explicit Little Endian i algunes transfer syntax de jpeg lossless i Lossy,
+     * si alguna de les imatges no compleix el transfer syntax el DICOMDIR no és DICOM conformance.
+     * Aquesta funció permet habilitar o deshabilitat comprovar el transfer syntax de les imatges, fent que en un DICOMDIR poguem o no tenir imatges 
+     * que no siguin del transfer syntax que indica la normativa DICOM, si està habilitat i hi ha imatges que no compleixen el transfer syntax es dona error, 
+     * i si ho deshabilitem  es podem generar DICOMDIR per un determinat profile encara que contingui imatges que no compleixen els transfer syntax acceptats 
+     * per la normativa DICOM per aquell profile, ja que no es comprovarà el transfer syntax de les imatges
+     *
+     * Per defecte es comprova la transfer syntax de les imatges.
+     * ATENCIÓ! Si no comprovem la transfer syntax podem generar DICOMDIR que no siguin DICOM conformance
+     * A l'annex H del PS 3.11 es pot consulta cada profile quines transfer syntax suporta.
+     */
+    void setCheckTransferSyntax(bool checkTransferSyntax);
 
     /** Crea el fitxer DicomDir amb l'estructura dels estudis del directori passat per paràmetre
      * @param dicomdirPath directori a convertir a dicomdir
