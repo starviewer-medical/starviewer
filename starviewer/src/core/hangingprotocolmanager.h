92aaaca805e4b8ca73e3bb029c59a7648b9e753e
Jesús Spínola <jspinola@gmail.com>
2009-09-28 18:24:02 +0000

    STYLE: Es millora el coding style

--- starviewer/src/core/hangingprotocolmanager.h
+++ starviewer/src/core/hangingprotocolmanager.h
@@ -26,23 +26,20 @@ class Q2DViewer;
 /**
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
-class HangingProtocolManager : public QObject
-{
+class HangingProtocolManager : public QObject {
 Q_OBJECT
 public:
-
     HangingProtocolManager( QObject *parent = 0 );
-
     ~HangingProtocolManager();
 
-	/// Buscar i aplicar el millor hanging protocol
+    /// Buscar i aplicar el millor hanging protocol
     QList<HangingProtocol * > searchAndApplyBestHangingProtocol( ViewersLayout *layout, Patient *patient);
 
-	// Aplicar un hanging protocol concret
-	void applyHangingProtocol( int hangingProtocolNumber, ViewersLayout * layout, Patient * patient );
+    // Aplicar un hanging protocol concret
+    void applyHangingProtocol( int hangingProtocolNumber, ViewersLayout * layout, Patient * patient );
 
     /// TODO Mètode públic temporal per poder aplicar les transformacions desitjades per un viewer
-    /// es fa especialment per no haver de fer noves classes ni duplicar el mapa de transdormacions
+    /// es fa especialment per no haver de fer noves classes ni duplicar el mapa de transformacions
     /// i per poder aplicar-ho ràpidament sobre mamo. S'ha de plantejar bé on hauria d'anar tot això
     /// Donada la orientació actual i la desitjada, aplica sobre el viewer donat les transformacions pertinents
     /// El format de les orientacions és el mateix que el del DICOM, 2 strings separats per "\",
@@ -53,23 +50,21 @@ private:
     /// Mira si el protocol es pot aplicar al pacient
     bool isValid( HangingProtocol *protocol, Patient *patient);
 
-	// Busca la sèrie corresponent dins un grup de sèries. Si el booleà quitStudy és cert, a més, l'eliminarà del conjunt
+    // Busca la sèrie corresponent dins un grup de sèries. Si el booleà quitStudy és cert, a més, l'eliminarà del conjunt
     Series *searchSerie( Patient *patient, QList<Series *> &seriesList, HangingProtocolImageSet *imageSet, bool quitStudy, HangingProtocol * hangingProtocol );
 
-	/// Cert si la imatge compleix les restriccions
+    /// Cert si la imatge compleix les restriccions
     bool isValidImage( Image *image, HangingProtocolImageSet *imageSet, HangingProtocol * hangingProtocol );
 
     /// Cert si la sèrie compleix les restriccions de l'imageSet, fals altrament
     bool isValidSerie( Patient *patient, Series *serie, HangingProtocolImageSet *imageSet, HangingProtocol * hangingProtocol );
 
-	/// Aplicar les transformacions (rotacions, flips..) per mostrar per pantalla
-	void applyDisplayTransformations( Series *serie,  int imageNumber, Q2DViewerWidget *viewer, HangingProtocolDisplaySet *displaySet );
+    /// Aplicar les transformacions (rotacions, flips..) per mostrar per pantalla
+    void applyDisplayTransformations( Series *serie,  int imageNumber, Q2DViewerWidget *viewer, HangingProtocolDisplaySet *displaySet );
 
 private:
-
-	/// Map per guardar les operacions de rotacio i flip per aplicar a les imatges. Valors: String posicions, nombre de rotacions, nombre flips
-	QMap<QString, QString> m_operationsMap;
-
+    /// Map per guardar les operacions de rotacio i flip per aplicar a les imatges. Valors: String posicions, nombre de rotacions, nombre flips
+    QMap<QString, QString> m_operationsMap;
 };
 
 }
