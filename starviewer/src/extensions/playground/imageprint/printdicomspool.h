b644615d875181ef46b3405f96bc017c256ee224
Marc Cals <cpmarc@gmail.com>
2009-10-01 16:29:44 +0000

    ENH:Es millor el codi per fer-lo més intel·ligible

--- starviewer/src/extensions/playground/imageprint/printdicomspool.h
+++ starviewer/src/extensions/playground/imageprint/printdicomspool.h
@@ -7,12 +7,11 @@
 #include "dicomprintjob.h"
 #include "dicomprintpage.h"
 
-#include "../inputoutput/status.h"
-
 ///Representació d'una impressora DICOM amb les eines DCMTK.
 class DVPSStoredPrint;
 class DVPSPrintMessageHandler;
 class DcmDataset;
+class OFCondition;
 
 namespace udg
 {   
@@ -25,7 +24,6 @@ public:
 
 private:		
 	
-    DVPSStoredPrint	*m_storedPrintDcmtk;
     /*TODO: Potser que daria més entés no guardar les variables com a membre i desdle mètode printSpool passar-la al mètode que 
      *que les necessiti. Per exemple printSCUCreateBasicFilmSession necessita DicomPrintJob*/
     DicomPrinter m_dicomPrinter;
@@ -33,13 +31,13 @@ private:
 
     DVPSStoredPrint* loadStoredPrintFileDcmtk(const QString &pathStoredPrintDcmtkFile);
 
-    void printBasicGrayscaleSpool(const QString &spoolDirectoryPath);
+    void printStoredPrintDcmtkContent(DVPSPrintMessageHandler &printerConnection, const QString storedPrintDcmtkFilePath, const QString &spoolDirectoryPath);
 
     //Retorna els atributs del FilmSession en un Dataset
     DcmDataset getAttributesBasicFilmSession();
 
     ///Una vegada creada la FilmSession i FilmBox aquest mètode ens permet enviar una imatge a imprimir cap a la impressora
-    Status createAndSendBasicGrayscaleImageBox(DVPSPrintMessageHandler& printConnection, size_t imageNumber, const QString &spoolDirectoryPath);
+    OFCondition createAndSendBasicGrayscaleImageBox(DVPSPrintMessageHandler& printConnection, DVPSStoredPrint *storedPrintDcmtk, size_t imageNumber, const QString &spoolDirectoryPath);
 };
 }; 
 #endif
