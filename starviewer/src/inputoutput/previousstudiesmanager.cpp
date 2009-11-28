40d981aaf882a68179db1d8beb63fad1d2a1fd1c
Marc Cals <cpmarc@gmail.com>
2009-11-28 17:04:32 +0000

    ENH:S'implementen els mètode de la classe per cercar estudis previs

--- starviewer/src/inputoutput/previousstudiesmanager.cpp
+++ starviewer/src/inputoutput/previousstudiesmanager.cpp
@@ -6,32 +6,148 @@
  ***************************************************************************/
 
 #include "previousstudiesmanager.h"
+
 #include "study.h"
+#include "dicommask.h"
+#include "patient.h"
+#include "pacsmanager.h"
+#include "pacsdevicemanager.h"
 
 namespace udg {
 
 PreviousStudiesManager::PreviousStudiesManager()
 {
+	m_pacsManager = new PacsManager();
+	createConnections();
 }
 
 PreviousStudiesManager::~PreviousStudiesManager()
 {
+	m_pacsManager->cancelCurrentQueries();
+}
 
+void PreviousStudiesManager::createConnections()
+{
+    connect(m_pacsManager, SIGNAL(queryStudyResultsReceived(QList<Patient*>, QHash<QString, QString>)), SLOT(queryStudyResultsReceived(QList<Patient*>, QHash<QString, QString>)));
+
+    connect(m_pacsManager, SIGNAL(queryFinished()), SLOT(queryFinished()));
+
+    connect(m_pacsManager, SIGNAL(errorQueryingStudy(PacsDevice)), SLOT(errorQueryingStudy(PacsDevice)));
 }
 
 void PreviousStudiesManager::queryPreviousStudies(Study *study)
 {
-    Q_UNUSED( study );
+	PacsDeviceManager pacsDeviceManager;
+
+	m_pacsManager->cancelCurrentQueries();//Per si hi hagués una consulta executant-se
+
+	///Fem neteja de consultes anteriors
+	m_mergedHashPacsIDOfStudyInstanceUID.clear();
+	m_mergedStudyList.clear();
+	m_pacsDeviceIDErrorEmited.clear();
+
+	m_studyToFindPrevious = study;
+
+	//Preguntem al PACS per estudis
+	m_pacsManager->queryStudy(getPreviousStudyDicomMaskPatientID(study), pacsDeviceManager.getPACSList(true));
+	m_pacsManager->queryStudy(getPreviousStudyDicomMaskPatientName(study), pacsDeviceManager.getPACSList(true));
+	
 }
 
 void PreviousStudiesManager::cancelCurrentQuery()
 {
-
+	m_pacsManager->cancelCurrentQueries();
 }
 
 bool PreviousStudiesManager::isExecutingQueries()
 {
-    return false;
+	return m_pacsManager->isExecutingQueries();
 }
 
+void PreviousStudiesManager::queryFinished()
+{
+	/*Quan totes les query han acabat és quant fem l'emit amb els estudis previs trobats. A diferència de la 
+      PacsManager no podem emetre els resultats que anem rebent, perquè hem de fer un merge del resultats rebuts,
+	  per no tenir duplicats*/
+	emit queryPreviousStudiesFinished(m_mergedStudyList, m_mergedHashPacsIDOfStudyInstanceUID);
+}
+
+void PreviousStudiesManager::queryStudyResultsReceived(QList<Patient*> patientListResults, QHash<QString, QString> hashTablePacsIDOfStudyInstanceUID)
+{
+	foreach(Patient *patient, patientListResults)
+	{
+		foreach(Study *study, patient->getStudies())
+		{
+			if (!isStudyInMergedStudyList(study) && !isStudyToFindPrevious(study))
+			{
+				/*Si l'estudi no està a llista ja d'estudis afegits i no és el mateix estudi pel qua ens han demanat el 
+				 *previ l'afegim*/
+				m_mergedStudyList.append(study);
+				
+				m_mergedHashPacsIDOfStudyInstanceUID[study->getInstanceUID()] = hashTablePacsIDOfStudyInstanceUID[study->getInstanceUID()];
+			}
+		}
+	}
+}
+
+void PreviousStudiesManager::errorQueryingStudy(PacsDevice pacs)
+{
+	/*Com que fem dos cerques al mateix pacs si una falla, l'altra segurament també fallarà per evitar enviar
+	  dos signals d'error si les dos fallen, ja que per des de fora ha de ser transparent el número de consultes
+	  que es fa al PACS, i han de rebre un sol error comprovem si tenim l'ID del PACS a la llista de signals 
+	  d'errors en PACS emesos*/
+	if (!m_pacsDeviceIDErrorEmited.contains(pacs.getID()))
+	{
+		m_pacsDeviceIDErrorEmited.append(pacs.getID());
+		emit errorQueryingPreviousStudies(pacs);
+	}
+}
+
+bool PreviousStudiesManager::isStudyInMergedStudyList(Study *study)
+{
+	bool studyFoundInMergedList = false;
+	
+	foreach(Study *studyMerged, m_mergedStudyList)
+	{
+		if (study->getInstanceUID() == studyMerged->getInstanceUID())
+		{
+			studyFoundInMergedList = true;
+			break;
+		}
+	}
+
+	return studyFoundInMergedList;
+}
+
+bool PreviousStudiesManager::isStudyToFindPrevious(Study *study)
+{
+	return study->getInstanceUID() == m_studyToFindPrevious->getInstanceUID();
+}
+
+DicomMask PreviousStudiesManager::getPreviousStudyDicomMaskPatientID(Study *study)
+{
+	DicomMask dicomMask;
+
+	///Indiquem que volem buscar estudis igual o menors d'aquella data
+	dicomMask.setStudyDate(getPreviousStudyDateMask(study->getDate()));
+	dicomMask.setPatientId(study->getParentPatient()->getID());
+
+	return dicomMask;
+}
+
+DicomMask PreviousStudiesManager::getPreviousStudyDicomMaskPatientName(Study *study)
+{
+	DicomMask dicomMask;
+
+	///Indiquem que volem buscar estudis igual o menors d'aquella data
+	dicomMask.setStudyDate(getPreviousStudyDateMask(study->getDate()));
+	dicomMask.setPatientName(study->getParentPatient()->getFullName());
+
+	return dicomMask;
+}
+
+QString PreviousStudiesManager::getPreviousStudyDateMask(QDate studyDate)
+{
+	return "-" + studyDate.toString("yyyyMMdd");
+}
 }
--- starviewer/src/inputoutput/previousstudiesmanager.h
+++ starviewer/src/inputoutput/previousstudiesmanager.h
@@ -9,12 +9,26 @@
 
 #include <QObject>
 #include <QHash>
+#include <QStringList>
 #include "pacsdevice.h"
+
+class QDate;
 
 namespace udg {
 
+class Patient;
 class Study;
+class DicomMask;
+class PacsManager;
 
+/**Aquesta classe donat un Study demana els estudis previs en els PACS configurats per defecte, degut a que 
+  *ara actualment en el PACS tenim el mateix pacients amb PatientID diferents, també a part de cercar estudis
+  *que coincideixin amb el PatientID també es farà una altre cerca per Patient Name. 
+  */
+/*TODO: En teoria amb la implantació del SAP els problemes de que un Pacient té diversos Patient ID o que té el nom
+  *escrit de maneres diferents haurien de desapareixer, per tant d'aquí un temps quan la majoria d'estudis del PACS
+  *ja s'hagin fet a través del SAP i constatem que el Patient ID pel mateix pacient sempre és el mateix, 
+  *la cerca per nom de pacient podria desapareixer*/
 class PreviousStudiesManager : public QObject {
 Q_OBJECT
 public:
@@ -35,13 +49,56 @@ public:
 signals:
 
     /// Signal que s'emet quan ha finalitzat la consulta d'estudis previs
-    void queryPreviousStudiesFinished( QList<Study*>, QHash<QString, QString> hashPacsIDOfStudyInstanceUID );
-
-    /// Signal que s'emet per indicar que la query ha finalitzat
-    void queryFinished();
+    void queryPreviousStudiesFinished(QList<Study*>, QHash<QString, QString> hashPacsIDOfStudyInstanceUID);
 
     /// Signal que s'emet per indicar que s'ha produït un error a la consulta d'estudis d'un PACS
     void errorQueryingPreviousStudies(PacsDevice pacs);
+
+private slots:
+
+	///Slot que s'executa quan s'ha acabat la consulta d'estudis previs a PacsManager
+	void queryFinished();
+
+	///Slot que s'executa quan rebem els resultats d'una cerca a un PACS
+	void queryStudyResultsReceived(QList<Patient*>, QHash<QString, QString>);
+
+	///Slot que s'executa quan la consulta a un PACS a de PacsManager ha fallat
+	void errorQueryingStudy(PacsDevice);
+
+private:
+
+	PacsManager *m_pacsManager;
+	QList<Study*> m_mergedStudyList;
+	QHash<QString,QString> m_mergedHashPacsIDOfStudyInstanceUID;
+	Study *m_studyToFindPrevious;
+	/*Com fem una consulta dos consultes al mateix PACS si falla una segurament també fallarà la segona per això
+     *en aquesta llista registrarem l'ID dels Pacs pel quals hem emés el signal d'error i si rebem un segon error
+	 *com ja el tindrem aquesta llista ja no en farem signal*/
+	QStringList m_pacsDeviceIDErrorEmited;
+
+	///Crea les connexions
+	void createConnections();
+
+	///Retorna una màscara per buscar estudis previs que coincideixin amb ID del pacient del Study
+	DicomMask getPreviousStudyDicomMaskPatientID(Study *study);
+
+	///Retorna una màscara per buscar estudis previs que coincideixin amb Nom del pacient del Study
+	DicomMask getPreviousStudyDicomMaskPatientName(Study *study);	
+
+	///Comprova si l'estudi ja l'hem rebut d'un altre PACS si és així no l'afegim, si no l'hem trobat
+	void mergeStudyWithReceivedResults(Study *study, QString pacsID);
+
+	///Ens indica si aquell estudi està a la llista d'estudis ja rebuts de previs, per evitar duplicats
+	/* Hem de tenir en compte que com fem la cerca per ID i un altre per Patient Name per obtenir més resultats
+	 * potser que en les dos consultes ens retornin el mateix estudi, per tant hem d'evitar duplicats.
+	 */
+	bool isStudyInMergedStudyList(Study *study);
+
+	///Ens indica si aquest estudi és el mateix pel qual ens han demanat els previs, per evitar incloure'l a la llista 
+	bool isStudyToFindPrevious(Study *study);
+
+	///Donada una data la transforma a una màscara per cercar estudis previs a aquell
+	QString getPreviousStudyDateMask(QDate studyDate);
 
 };
 
