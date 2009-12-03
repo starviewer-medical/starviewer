ddcb11abe804a63a8b9df29f582b42064be88954
Marc Cals <cpmarc@gmail.com>
2009-12-03 17:39:27 +0000

    STYLE:Hi havia una línia de codi mal identada

--- starviewer/src/inputoutput/previousstudiesmanager.cpp
+++ starviewer/src/inputoutput/previousstudiesmanager.cpp
@@ -139,7 +139,7 @@ DicomMask PreviousStudiesManager::getBasicDicomMask()
     dicomMask.setStudyModality("");
     dicomMask.setStudyDate("");
     dicomMask.setStudyTime("");
-	dicomMask.setStudyUID("");
+    dicomMask.setStudyUID("");
 
     return dicomMask;
 }
