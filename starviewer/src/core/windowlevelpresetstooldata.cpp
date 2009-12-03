45689a37278954b2ee8a14f69d32214bcaf13fc6
Jesús Spínola <jspinola@gmail.com>
2009-12-03 13:23:48 +0000

    ENH: Actualitzem les traduccions per fer els noms dels presets de window level més curts en tots els idiomes i així estalviar espai de pantalla

--- starviewer/src/core/windowlevelpresetstooldata.cpp
+++ starviewer/src/core/windowlevelpresetstooldata.cpp
@@ -18,11 +18,11 @@ WindowLevelPresetsToolData::WindowLevelPresetsToolData(QObject *parent)
     addPreset( tr("CT Head"), 80, 40, StandardPresets );
     addPreset( tr("CT Bone"), 2000, 500, StandardPresets );
     addPreset( tr("CT Lung"), 1500, -650, StandardPresets );
-    addPreset( tr("CT Soft Tissues, Non Contrast"), 400, 40, StandardPresets );
-    addPreset( tr("CT Soft Tissues, Contrast Medium"), 400, 70, StandardPresets );
-    addPreset( tr("CT Liver, Contrast Medium"), 300, 60, StandardPresets ); // 60-100
-    addPreset( tr("CT Liver, Non Contrast"), 200, 40, StandardPresets );
-    addPreset( tr("CT Neck, Contrast Medium"), 300, 50, StandardPresets );
+    addPreset( tr("CT Soft Tissues"), 400, 40, StandardPresets );
+    addPreset( tr("CT Soft Tissues+Contrast"), 400, 70, StandardPresets );
+    addPreset( tr("CT Liver+Contrast"), 300, 60, StandardPresets ); // 60-100
+    addPreset( tr("CT Liver"), 200, 40, StandardPresets );
+    addPreset( tr("CT Neck+Contrast"), 300, 50, StandardPresets );
     addPreset( tr("Angiography"), 500, 100, StandardPresets ); // 100-200
     addPreset( tr("Osteoporosis"), 1000, 300, StandardPresets ); //100-1500:window!
     addPreset( tr("Emphysema"), 800, -800, StandardPresets );
