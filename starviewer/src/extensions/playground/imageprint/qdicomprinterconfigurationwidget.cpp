76837f1aec22a7712b498ab28668b23ed24f1349
Jesús Spínola <jspinola@gmail.com>
2009-12-16 12:51:46 +0000

    ENH: Afegim la possibilitat de canviar el directori d'spool dins del quadre de configuració d'impresores. Potser no és la ubicació més adequada ,ja que és un setting general per totes les impresions i no només per una impresora en concret, el seu lloc més idoni hauria de ser el de configuracions generals, però ara mateix es faria força difícil, ja que implicaria fer dependre interface d'una extensió, i per tant de moment s'ha descartat i s'ha anat per la via ràpida

--- starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.cpp
+++ starviewer/src/extensions/playground/imageprint/qdicomprinterconfigurationwidget.cpp
@@ -5,8 +5,9 @@
 #include "starviewerapplication.h"
 #include "qdicomaddprinterwidget.h"
 #include "dicomprint.h"
-
+#include "imageprintsettings.h"
 #include <QMessageBox>
+#include <QFileDialog>
 
 namespace udg {
 
@@ -27,6 +28,10 @@ QDicomPrinterConfigurationWidget::QDicomPrinterConfigurationWidget()
 
     m_maximumDensitySpinBox->setSpecialValueText(tr("Automatic"));//Afegim valor "" per si no volen especificar densitat
     m_minimumDensitySpinBox->setSpecialValueText(tr("Automatic"));//Afegim valor "" per si no volen especificar densitat
+
+    Settings settings;
+    m_spoolPathLineEdit->setText( settings.getValue( ImagePrintSettings::SpoolDirectory ).toString() );
+    connect( m_chooseSpoolPathPushButton, SIGNAL( clicked() ), SLOT( chooseSpoolPath() ) );
 
     printerSelectionChanged();
 }
@@ -390,6 +395,17 @@ void QDicomPrinterConfigurationWidget::accept()
         m_listPrintersTreeWidget->clearSelection();
         clearPrinterSettings();
         close();
+    }
+}
+
+void QDicomPrinterConfigurationWidget::chooseSpoolPath()
+{
+    QString path = QFileDialog::getExistingDirectory( this, tr( "Choose a directory..." ), m_spoolPathLineEdit->text() );
+    if( !path.isEmpty() )
+    {
+        m_spoolPathLineEdit->setText( path );
+        Settings settings;
+        settings.setValue( ImagePrintSettings::SpoolDirectory, path );
     }
 }
 
