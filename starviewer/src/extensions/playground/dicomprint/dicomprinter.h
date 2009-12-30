9f513cd0585f9a54c4e0e4890d0d164115465338
Miquel Serra <miquelserra@gmail.com>
2009-12-30 10:43:34 +0000

    STYLE: S'aplica el Coding Styling a les classes Printer i DicomPrinter.

--- starviewer/src/extensions/playground/dicomprint/dicomprinter.h
+++ starviewer/src/extensions/playground/dicomprint/dicomprinter.h
@@ -6,130 +6,235 @@
 #include <QList>
 #include "printer.h"
 
-///Representació dels paràmetres d'una impressora DICOM.
+/** 
+  * Implementació de la interfície Printer per una impressora del tipus Dicom
+  */
 
 namespace udg
-{   
+{
 
 class DicomPrinter : public Printer
 {
 public:
+
     DicomPrinter();
+
     ~DicomPrinter();
-    
-    /// ID de l'impressora
-    int getID();
+
+    /// Afegeix l'ID de l'impressora    
     void setID(int ID);
 
-    /// Posa el nom de la impressora.
-    void setName(const QString &name);
+    /// Retorna l'ID de l'impressora    
+    int getID();
+
+    /// Afegeix el nom de la impressora.
+    void setName( const QString& name );
+
+    /// Retorna el nom de la impressora.
     QString getName() const;
 
-    /// Posa el tipus de la impressora.
-    void setType(const QString &type);
+    /// Afegeix el tipus de la impressora.
+    void setType( const QString& type );
+
+    /// Retorna el tipus de la impressora.
     QString getType() const;
 
-    /// Posa la descripció de la impressora.
-    void setDescription(const QString &description);	
+    /// Afegeix la descripció de la impressora.
+    void setDescription( const QString& description );	
+
+    /// Retorna la descripció de la impressora.    
     QString getDescription() const;
 
-    ///Especifica/retorna el AETitle de la impressora
-    void setAETitle(const QString &aeTitle);
+    /// Afageix el AETitle de la impressora
+    void setAETitle( const QString& aeTitle );
+
+    /// Retorna el AETitle de la impressora
     QString getAETitle() const;
 
-    /// Posa el HostName (IP) de la impressora.
-    void setHostname(const QString &hostname);
+    /// Afegeix el HostName (IP) de la impressora.
+    void setHostname( const QString& hostname );
+
+    /// Retorna el AETitle de la impressora
     QString getHostname() const;
 
-    /// Posa el Port de la impressora.
-    void setPort(const int &port);
-    int getPort() const;
+    /// Afegeix el Port de la impressora.
+    void setPort( int port );
 
-    /// Posa els mitjans disponibles per la impressió (\p mediumType és una llista d'opcions separades per "\").
-    void setDefaultMediumType(const QString &mediumType);
+    /// Retorna el Port de la impressora.
+    int getPort();
+
+    /// Especifica el valor per defecte del MediumType
+    void setDefaultMediumType( const QString& mediumType );
+
+    /// Retorna el valor per defecte del MediumType
     QString getDefaultMediumType() const; 
-    void setAvailableMediumTypeValues(QStringList mediumTypeValues);
+
+    /// Especifica una llista de valors disponibles del MediumType
+    void setAvailableMediumTypeValues( QStringList mediumTypeValues );
+
+    /// Retorna una llista de valors disponibles del MediumType
     QStringList getAvailableMediumTypeValues();
 
-    /// Posa les mides disponibles d'impressió (\p filmSize és una llista d'opcions separades per "\").
-    void setDefaultFilmSize(const QString &filmSize);	
+    /// Especifica el valor per defecte del FilmSize
+    void setDefaultFilmSize( const QString& filmSize );	
+
+    /// Retorna el valor per defecte del FilmSize
     QString getDefaultFilmSize() const;
-    void setAvailableFilmSizeValues(QStringList filmSizeValues);
+
+    /// Especifica una llista de valors disponibles del FilmSize
+    void setAvailableFilmSizeValues( QStringList filmSizeValues );
+
+    /// Retorna una llista de valors disponibles del FilmSize
     QStringList getAvailableFilmSizeValues();
 
-    /// Posa els layouts disponibles per la impressió (\p layout és una llista d'opcions separades per "\").
-    void setDefaultFilmLayout(const QString &filmLayout);	
+     /// Especifica el valor per defecte del FilmLayout
+    void setDefaultFilmLayout( const QString& filmLayout );	
+
+    /// Retorna el valor per defecte del FilmLayout
     QString getDefaultFilmLayout() const;
+
+    /// Especifica una llista de valors disponibles del FilmLayout
     void setAvailableFilmLayoutValues(QStringList filmLayoutValues);	
+
+    /// Retorna una llista de valors disponibles del FilmLayout
     QStringList getAvailableFilmLayoutValues();	
-    ///Retorna el número de columnes/files del layout
+        
+     /// Retorna el número de columnes que té el layout del Film per defecte de la impressora.
     int getDefaultFilmLayoutColumns();
+
+    /// Retorna el número de files que té el layout del Film per defecte de la impressora.
     int getDefaultFilmLayoutRows();
 
-    ///Especifica retorna la orientació (horitzontal/vertical) per defecte amb la que s'ha d'imprimir la placa
+    /// Especifica la orientació (horitzontal/vertical) per defecte amb la que s'ha d'imprimir la placa
     void setDefaultFilmOrientation(const QString &filmOrientation);	
+
+    /// Retorna la orientació (horitzontal/vertical) per defecte amb la que s'ha d'imprimir la placa
     QString getDefaultFilmOrientation() const;
+
+    /// Especifica la llista d'orientacions (horitzontal/vertical) disponibles amb la que s'ha d'imprimir la placa
     void setAvailableFilmOrientationValues(QStringList filmOrientationValues);
+
+     /// Retorna la llista d'orientacions (horitzontal/vertical) disponibles amb les que es pot imprimir la placa
     QStringList getAvailableFilmOrientationValues();
 
-    /// Posa el Magnification type disponibles per la impressió (\p magnificationType és una llista d'opcions separades per "\").
-    void setDefaultMagnificationType(const QString &magnificationType);	
+    /// Afegeix el Magnification Type per defecte a la impressora
+    void setDefaultMagnificationType( const QString& magnificationType );	
+
+    /// Retorna el Magnification Type per defecte a la impressora
     QString getDefaultMagnificationType() const;
+
+    /// Afegeix una llista de Magnification Type disponibles a la impressora
     void setAvailableMagnificationTypeValues(QStringList magnificationTypeValues);
+
+    /// Retorna una llista de Magnification Type disponibles a la impressora
     QStringList getAvailableMagnificationTypeValues();
 
-    ///Especifica/retorna la resolució màxima de la impressió.
-    void setDefaultMaxDensity(ushort maxDensity);
+    /// Afegeix la Densitat Màxima per defecte a la impressora
+    void setDefaultMaxDensity( ushort maxDensity );
+
+    /// Retorna la Densitat Màxima per defecte a la impressora
     ushort getDefaultMaxDensity() const;
+
+    /// Afegeix la Densitat Màxima que pot agafar la impressora
     void setAvailableMaxDensityValues(ushort maxDensityValues);
+
+    /// Retorna la Densitat Màxima que pot agafar la impressora
     ushort getAvailableMaxDensityValues();
 
-    ///Especifica/retorna la resolució mínima de la impressió. Si té valor 0 vol dir que no s'aplica
+    /// Especifica la resolució mínima de la impressió. Si té valor 0 vol dir que no s'aplica
     void setDefaultMinDensity(ushort minDensity);
-    ushort getDefaultMinDensity() const;
-    void setAvailableMinDensityValues(ushort minDensityValues);
-    ushort getAvailableMinDensityValues();
-    
-    /// Posa si es suporta l'ús de TimBox o no.
-    void setDefaultTrim(bool trim);
-	bool getDefaultTrim() const;
-    void setAvailableTrim(bool availableTrim);
-    bool getAvailableTrim() const;
 
-    /// Posa les densitats disponibles en el Border  (\p borderDensity és una llista d'opcions separades per "\").
+    /// Retorna la resolució mínima de la impressió. Si té valor 0 vol dir que no s'aplica
+    ushort getDefaultMinDensity() const;
+
+    /// Especifica la resolució mínima que pot agafar la impressió. Si té valor 0 vol dir que no s'aplica
+    void setAvailableMinDensityValues(ushort minDensityValues);
+
+    /// Retorna la resolució mínima que pot agafar la impressió. Si té valor 0 vol dir que no s'aplica
+    ushort getAvailableMinDensityValues();
+
+    /// Especifica si s'accepta TrimBox per defecte
+    void setDefaultTrim(bool trim);
+
+    /// Retorna si s'accepta TrimBox per defecte
+    bool getDefaultTrim();
+
+    /// Especifica si s'accepta TrimBox.
+    void setAvailableTrim(bool availableTrim);
+
+    /// Retorna si s'accepta TrimBox.
+    bool getAvailableTrim();
+
+    /// Especifica el valor per defecte de la Densitat del Border.
     void setDefaultBorderDensity(const QString &borderDensity);
+
+    /// Retorna el valor per defecte de la Densitat del Border.
     QString getDefaultBorderDensity() const;
+
+    /// Especifica una llista de  valors disponibles de la Densitat del Border.
     void setAvailableBorderDensityValues(QStringList borderDensityValues);
+
+    /// Retorna una llista de  valors disponibles de la Densitat del Border.
     QStringList getAvailableBorderDensityValues();
 
-    /// Posa les densitats disponibles en la zona buida (EmptyImage) de la impressió  (\p borderDensity és una llista d'opcions separades per "\").
+    /// Especifica el valor per defecte de l'EmptyImageDensity
     void setDefaultEmptyImageDensity(const QString &emptyImageDensity);
+
+    /// Retorna el valor per defecte de l'EmptyImageDensity
     QString getDefaultEmptyImageDensity() const;
+
+    /// Especifica una llista de  valors disponibles de l'EmptyImageDensity    
     void setAvailableEmptyImageDensityValues(QStringList emptyImageDensityValues);
+
+    /// Retorna una llista de  valors disponibles de l'EmptyImageDensity    
     QStringList getAvailableEmptyImageDensityValues();
 
-    /// Posa els destins disponibles per la impressió  (\p borderDensity és una llista d'opcions separades per "\").
+    /// Especifica el valor per defecte del FilmDestination
     void setDefaultFilmDestination(const QString &filmDestination);
+
+    /// Retorna el valor per defecte del FilmDestination
     QString getDefaultFilmDestination() const;
+
+    /// Especifica una llista de  valors disponibles del FilmDestination    
     void setAvailableFilmDestinationValues(QStringList filmDestinationValues);
+
+    /// Retorna una llista de  valors disponibles del FilmDestination
     QStringList getAvailableFilmDestinationValues();
 
-    ///Especifica/Retorna el smoothing type per defecte amb la que s'imprimirà. Només es pot configurar si Magnification Type té com a Valor "CUBIC"
+    ///Especifica el Smoothing type per defecte amb la que s'imprimirà
     void setDefaultSmoothingType(const QString &smoothingType);
+
+    /// Retorna el Smoothing type per defecte amb la que s'imprimirà
     QString getDefaultSmoothingType() const;
+
+    /// Especifica una llista de Smoothing type disponibles amb la que es pot imprimir.
     void setAvailableSmoothingTypeValues(QStringList smoothingTypeValues);
+
+    /// Retorna una llista de Smoothing type disponibles amb la que es pot imprimir.
     QStringList getAvailableSmoothingTypeValues();
-    
-	///Especifica/retorna la prioritat per defecte dels jobs que s'imprimeixin amb aquesta impressora
-    void setDefaultPrintPriority(const QString &priority);
+
+    /// Especifica la prioritat per defecte dels jobs que s'imprimeixin amb aquesta impressora
+    void setDefaultPrintPriority(const QString& priority);
+
+    /// Retorna la prioritat per defecte dels jobs que s'imprimeixin amb aquesta impressora
     QString getDefaultPrintPriority() const;
+
+    /// Especifica una llista de prioritats disponibles dels jobs que s'imprimeixin amb aquesta impressora
     void setAvailablePrintPriorityValues(QStringList printPriorityValues);
+
+    /// Retorna una llista de prioritats disponibles dels jobs que s'imprimeixin amb aquesta impressora
     QStringList getAvailablePrintPriorityValues();
-    
-    ///Especifica/retorna la polaritat per defecte de l'impressora
-    void setDefaultPolarity(const QString &polarity);
+
+    /// Especifica la polaritat per defecte de l'impressora
+    void setDefaultPolarity(const QString& polarity);
+
+    /// Retorna la polaritat per defecte de l'impressora
     QString getDefaultPolarity() const;
+
+    /// Especifica una llista de polaritats per defecte de l'impressora
     void setAvailablePolarityValues(QStringList polarityValues);
+
+    /// Retorna una llista de polaritats per defecte de l'impressora
     QStringList getAvailablePolarityValues();
 
     /**Especifica/retorna el valor de ConfigurationInformation per la impressora, depén de la impressora, per exemple per agfa permet afegir 
@@ -139,14 +244,24 @@ public:
     void setDefaultConfigurationInformation(const QString configurationInformation);
     QString getDefaultConfigurationInformation() const;
 
-    void setIsDefault(const bool &isDefault);
+    /// Especifica si la impressora és la impressora per defecte del sistema.
+    void setIsDefault( bool isDefault );
+
+    /// Retorna si la impressora és la impressora per defecte del sistema.
     bool getIsDefault() const;
 
+private:
+    
+    /**Mètode que retorna el número de columnes i files del layout, s'implementa en un sol mètode per no tenir codi duplicat
+    si s'implementés un mètode per obtenir el número de columnes i una altre per obtenir el número de files*/
+    void getDefaultFilmLayoutColumnsRows(int &colums, int &rows);   
 
 private:		
 	
     int m_ID;
     QString m_aeTitle;
+    QString m_hostname;
+    int m_port;
     QString	m_defaultDescription;
     QString	m_defaultMediumType;
     QStringList m_mediumTypeValues;
@@ -177,10 +292,6 @@ private:
     QString m_defaultPolarity;
     QStringList m_polarityValues;    
     QString m_configurationInformation;
-
-    /**Mètode que retorna el número de columnes i files del layout, s'implementa en un sol mètode per no tenir codi duplicat
-       si s'implementés un mètode per obtenir el número de columnes i una altre per obtenir el número de files*/
-    void getDefaultFilmLayoutColumnsRows(int &colums, int &rows);   
 };
 }; 
 #endif
