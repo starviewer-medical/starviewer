aab1bf009171eca719a74cef4509cbf44a596105
Jesús Spínola <jspinola@gmail.com>
2010-01-28 17:49:52 +0000

    STYLE: Millorem el coding style eliminant l'estil de dcmtk que encara perdurava en alguns talls de codi

--- starviewer/src/inputoutput/retrieveimages.h
+++ starviewer/src/inputoutput/retrieveimages.h
@@ -36,9 +36,6 @@ struct StoreCallbackData;
 
 class DcmDataset;
 
-/** This class helps to interactive with the pacs, retrieve images that match with the mask
- */
-
 namespace udg{
 
 class Status;
@@ -46,71 +43,82 @@ class PacsConnection;
 class DicomMask;
 class DICOMTagReader;
 
+/** 
+    This class helps to interactive with the pacs, retrieve images that match with the mask
+ */
 class RetrieveImages
 {
-
 public:
-
-   ///constructor de la classe
    RetrieveImages();
 
-    /** This action sets the connection that we will use to connect to the pacs
-     * @param   connection [in] Study's Open connection to the pacs
-     */
+   /** 
+    * This action sets the connection that we will use to connect to the pacs
+    * @param connection [in] Study's Open connection to the pacs
+    */
    void setConnection(PacsConnection connection);
 
-   /** Sets the network to use for retrieve the images
+   /** 
+    * Sets the network to use for retrieve the images
     * @param Network to use
     */
-   void setNetwork (T_ASC_Network * network);
+   void setNetwork(T_ASC_Network * network);
 
-    /** This action sets the mask that we will use to retrieve the image in to the pacs.
-    * @param mask Màscara de cerca
-    */
+    /** 
+     * This action sets the mask that we will use to retrieve the image in to the pacs.
+     * @param mask Màscara de cerca
+     */
    void setMask( DicomMask mask );
 
-   /** Download the study's Image
+   /** 
+    * Download the study's Image
     * @return state of the method
     */
    Status retrieve();
 
 private:
-
-    T_ASC_Association *m_assoc; // request DICOM association;
+    /// Request DICOM association;
+    T_ASC_Association *m_assoc; 
     T_ASC_Network *m_net;
     DcmDataset *m_mask;
 
     /// En aquesta funció acceptem la connexió que se'ns sol·licita per transmetre'ns imatges, i indiquem quins transfer syntax suportem
-    static OFCondition acceptSubAssoc( T_ASC_Network * aNet , T_ASC_Association ** assoc );
+    static OFCondition acceptSubAssoc( T_ASC_Network * aNet, T_ASC_Association ** assoc );
 
-    static void moveCallback( void *callbackData , T_DIMSE_C_MoveRQ *req, int responseCount , T_DIMSE_C_MoveRSP *response );
+    static void moveCallback( void *callbackData, T_DIMSE_C_MoveRQ *req, int responseCount, T_DIMSE_C_MoveRSP *response );
 
-    ///Responem a una petició d'echo
-    static OFCondition echoSCP( T_ASC_Association * assoc , T_DIMSE_Message * msg , T_ASC_PresentationContextID presID );
+    /// Responem a una petició d'echo
+    static OFCondition echoSCP( T_ASC_Association * assoc, T_DIMSE_Message * msg, T_ASC_PresentationContextID presID );
 
-    /// Aquesta funció s'encarrega de guardar cada trama DICOM que rebem
-    static void storeSCPCallback(void *callbackData ,
-                                 T_DIMSE_StoreProgress *progress ,    /* progress state */
-                                 T_DIMSE_C_StoreRQ *req ,             /* original store request */
-                                 char *imageFileName, DcmDataset **imageDataSet , /* being received into */
-                                 T_DIMSE_C_StoreRSP *rsp ,            /* final store response */
-                                 DcmDataset **statusDetail );
+    /** 
+     * Aquesta funció s'encarrega de guardar cada trama DICOM que rebem
+     * Paràmetres d'entrada
+     * @param callbackData 
+     * @param progress Progress state
+     * @param req Original store request
+     * @param imageFileName
+     * @param imageDataSet Being received into
+     * Paràmetres de sortida
+     * @param rsp Final store response
+     * @param statusDetail
+     */
+    static void storeSCPCallback( void *callbackData, T_DIMSE_StoreProgress *progress, T_DIMSE_C_StoreRQ *req, char *imageFileName, DcmDataset **imageDataSet, T_DIMSE_C_StoreRSP *rsp, DcmDataset **statusDetail );
 
-    static OFCondition storeSCP( T_ASC_Association *assoc , T_DIMSE_Message *msg , T_ASC_PresentationContextID presID );
+    static OFCondition storeSCP( T_ASC_Association *assoc, T_DIMSE_Message *msg, T_ASC_PresentationContextID presID );
 
     /// Accepta la connexió que ens fa el PACS, per convertir-nos en un scp
     static OFCondition subOpSCP( T_ASC_Association **subAssoc );
 
-    static void subOpCallback(void * /*subOpCallbackData*/ , T_ASC_Network *aNet , T_ASC_Association **subAssoc );
+    static void subOpCallback( void * /*subOpCallbackData*/, T_ASC_Network *aNet, T_ASC_Association **subAssoc );
 
-    ///Guarda una composite instance descarregada
+    /// Guarda una composite instance descarregada
     static OFCondition save(StoreCallbackData *storeCallbackData, QString imageFileNameToSave);
 
-    ///Retorna el nom del fitxer amb que s'ha de guardar l'objecte descarregat, composa el path on s'ha de guardar + el nom del fitxer, si el path on s'ha de guardar la imatge no existeix el crea
+    /// Retorna el nom del fitxer amb que s'ha de guardar l'objecte descarregat, composa el path on s'ha de guardar + el nom del fitxer, si el path on s'ha de guardar la imatge no existeix el crea
     static QString getCompositeInstanceFileName(DcmDataset *imageDataset);
 
-    ///En cas d'error processa la resposta rebuda per part del SCP, grava l'error el log i el retorna en forma d'objecte Status
+    /// En cas d'error processa la resposta rebuda per part del SCP, grava l'error el log i el retorna en forma d'objecte Status
     Status processErrorResponseFromMoveSCP(T_DIMSE_C_MoveRSP *response, DcmDataset *statusDetail);
 };
+
 };
 #endif
