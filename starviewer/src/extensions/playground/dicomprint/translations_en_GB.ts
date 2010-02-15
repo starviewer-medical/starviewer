670db0d4c017ef9f60decc600feb74a3d57bb382
Marc Cals <cpmarc@gmail.com>
2010-02-15 19:14:05 +0000

    ENH:Es fan les traduccions al català i castellà per DICOMPrint

--- starviewer/src/extensions/playground/dicomprint/translations_en_GB.ts
+++ starviewer/src/extensions/playground/dicomprint/translations_en_GB.ts
@@ -2,17 +2,185 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_GB" sourcelanguage="ca_ES">
 <context>
-    <name>QDicomPrinterConfigurationWidgetBase</name>
-    <message utf8="true">
-        <source>Configuració de les Impressores DICOM</source>
+    <name>QDicomAddPrinterWidgetBase</name>
+    <message>
+        <source>Add new DICOM printer</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>AETitle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hostname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Printer Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDicomJobCreatorWidgetBase</name>
+    <message>
+        <source>QDicomJobCreatorWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDicomPrintExtensionBase</name>
+    <message>
+        <source>DICOM print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of copies:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Images to print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Interval:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>From:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pages to print:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>#m_noSupportedSeriesFrame {
+background-color: #dc8a8a;
+border-radius: 4px;
+border: 1px solid  #680000;
+}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>margin-right: 5px;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This series cannot be printed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Slice:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configure DICOM printers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hostname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Images sent to DICOM print successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDicomPrinterBasicSettingsWidgetBase</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Print Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Print priority:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Destination:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Layout:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Orientation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDicomPrinterConfigurationWidgetBase</name>
     <message>
         <source>AETitle</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>HostName</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -23,128 +191,247 @@
         <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <source>Configuració d&apos;una Impressora DICOM</source>
+    <message>
+        <source>DICOM Printers Configuration</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Delete</source>
+        <source>Id</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Test</source>
+        <source>Add New Printer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Printer</source>
+        <source>Remove Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Test Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Printer Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AETitle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hostname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Advanced Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Magnification Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Border Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Minimum Density:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Polarity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Smoothing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty Image Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Maximum Denstiy:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Trim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configuration Information:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Hostname</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>QDicomPrintingConfigurationWidgetBase</name>
     <message>
-        <source>Default Printer Settings</source>
+        <source>QDicomPrintingConfigurationWidget</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Smoothing</source>
+        <source>test</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>udg::DicomPrintExtensionMediator</name>
+    <message>
+        <source>DICOM Print</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>MEDIUM</source>
+        <source>Starviewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Border Density</source>
+        <source>The selected item is not an image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>udg::QDicomAddPrinterWidget</name>
+    <message>
+        <source>%1 can&apos;t add the printer %2 because a printer with the same AETitle already exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>udg::QDicomPrintExtension</name>
+    <message>
+        <source>Some of the pages film can&apos;t be printed because </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>BLACK</source>
+        <source>the printer doesn&apos;t respond.
+
+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Empty Density</source>
+        <source>the printer doesn&apos;t respond as expected.
+
+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Maxim Density</source>
+        <source>%1 can&apos;t create print spool.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Minium Denstiy</source>
+        <source>%1 can&apos;t load some of the images to print.
+
+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Layout</source>
+        <source>This series cannot be printed because color is not supported.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>STANDARD\1,1</source>
+        <source>This series cannot be printed because multi-frame images are not supported.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Film Orientation</source>
+        <source>The images can&apos;t be printed because </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>LANDSCAPE</source>
+        <source>In most cases this error is produced because the printer doesn&apos;t support some of the print configuration parameters.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Film Size</source>
+        <source>Close &apos;DICOM print&apos; tab and try it again.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>24CMX30CM</source>
+        <source>an unknow error has produced.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Film Destination</source>
+        <source>Check printer DICOM Conformance to be sure that it accepts all your print parameters.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>BIN_3</source>
+        <source>Be sure the computer is connected on the network and the printer network parameters are correct.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>udg::QDicomPrinterConfigurationWidget</name>
+    <message>
+        <source>Automatic</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Visible Trim</source>
+        <source>Error can&apos;t apply changes to the printer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Magnification Type</source>
+        <source>Test of printer %1 is correct.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>BILINEAR</source>
+        <source>No printer is selected to test.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Default Job Settings</source>
+        <source>No printer is selected to apply the changes.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Copies</source>
+        <source>Printer %1 doesn&apos;t respond correclty.
+Be sure that hostname and AETitle are correct.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Priority</source>
+        <source>Printer %1 doesn&apos;t respond.
+Be sure that hostname and AETitle are correct.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>CLEAR FILM</source>
+        <source>AETitle field can&apos;t be empty.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Medium</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Update</source>
+        <source>Hostname can&apos;t be empty.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
