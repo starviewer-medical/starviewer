4d37e3cd380beed8e42f188b12ead6e47c33d2d9
Jesús Spínola <jspinola@gmail.com>
2010-04-28 12:23:53 +0000

    STYLE: Millores en el coding style

--- starviewer/src/core/windowleveltool.h
+++ starviewer/src/core/windowleveltool.h
@@ -15,23 +15,19 @@ namespace udg {
 	@author Grup de Gràfics de Girona  ( GGG ) <vismed@ima.udg.es>
 */
 
-//class Q2DViewer;
 class QViewer;
 
-class WindowLevelTool : public Tool
-{
+class WindowLevelTool : public Tool {
 Q_OBJECT
 public:
-    /// estats de la tool
+    /// Estats de la tool
     enum { None , WindowLevelling };
 
     WindowLevelTool( QViewer *viewer, QObject *parent = 0 );
-
     ~WindowLevelTool();
 
     void handleEvent( unsigned long eventID );
 
-/// \TODO potser aquests mètodes slots passen a ser públics
 private slots:
     /// Comença el window level
     void startWindowLevel();
@@ -43,11 +39,11 @@ private slots:
     void endWindowLevel();
 
 private:
-    /// valors per controlar el mapeig del window level
+    /// Valors per controlar el mapeig del window level
     double m_initialWindow, m_initialLevel, m_currentWindow, m_currentLevel;
     int m_windowLevelStartPosition[2], m_windowLevelCurrentPosition[2];
     
-    /// estats d'execució de la tool
+    /// Estats d'execució de la tool
     int m_state;
 };
 
