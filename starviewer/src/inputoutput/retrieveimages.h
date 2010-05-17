f323a4b57ca230c4ddeea1ba09e5762423afc8f0
Marc Cals <cpmarc@gmail.com>
2010-05-17 18:24:17 +0000

    ENH:Com a dada de callback passem un punter a la pròpia classe RetrieveImage, això ens permet que tots els mètodes de la classe excepte els mètodes callback no hagin de ser estàtics. Això és un primer pas per poder-nos desfer de les classes ProcesImage.

--- starviewer/src/inputoutput/retrieveimages.h
+++ starviewer/src/inputoutput/retrieveimages.h
@@ -70,32 +70,42 @@ private:
     T_ASC_Network *m_net;
     DcmDataset *m_mask;
 
-    /// En aquesta funció acceptem la connexió que se'ns sol·licita per transmetre'ns imatges, i indiquem quins transfer syntax suportem
-    static OFCondition acceptSubAssociation( T_ASC_Network * associationNetwork, T_ASC_Association ** association );
+    struct StoreSCPCallbackData
+    {
+        DcmFileFormat *dcmFileFormat;
+        RetrieveImages *retrieveImages;
+        QString fileName;
+    };
 
-    static void moveCallback( void *callbackData, T_DIMSE_C_MoveRQ *moveRequest, int responseCount, T_DIMSE_C_MoveRSP *moveResponse );
+    /// En aquesta funció acceptem la connexió que se'ns sol·licita per transmetre'ns imatges, i indiquem quins transfer syntax suportem
+    OFCondition acceptSubAssociation( T_ASC_Network * associationNetwork, T_ASC_Association ** association );
 
     /// Responem a una petició d'echo
-    static OFCondition echoSCP( T_ASC_Association * association, T_DIMSE_Message * dimseMessage, T_ASC_PresentationContextID presentationContextID );
+    OFCondition echoSCP( T_ASC_Association * association, T_DIMSE_Message * dimseMessage, T_ASC_PresentationContextID presentationContextID );
+
+    ///Responem a una petició per guardar una imatge
+    OFCondition storeSCP( T_ASC_Association *association, T_DIMSE_Message *messagge, T_ASC_PresentationContextID presentationContextID );
+
+    /// Accepta la connexió que ens fa el PACS, per convertir-nos en un scp
+    OFCondition subOperationSCP( T_ASC_Association **subAssociation );
+
+    /// Guarda una composite instance descarregada
+    OFCondition save(DcmFileFormat *fileRetrieved, QString dicomFileAbsolutePath);
+
+    /// Retorna el nom del fitxer amb que s'ha de guardar l'objecte descarregat, composa el path on s'ha de guardar + el nom del fitxer, si el path on s'ha de guardar la imatge no existeix el crea
+    QString getAbsoluteFilePathCompositeInstance(DcmDataset *imageDataset, QString fileName);
+
+    /// En cas d'error processa la resposta rebuda per part del SCP, grava l'error el log i el retorna en forma d'objecte Status
+    Status processErrorResponseFromMoveSCP(T_DIMSE_C_MoveRSP *moveResponse, DcmDataset *statusDetail);
+
+    ///Callback de move, semblaria que s'executa cada vegada que s'ha descarregat una imatge
+    static void moveCallback( void *callbackData, T_DIMSE_C_MoveRQ *moveRequest, int responseCount, T_DIMSE_C_MoveRSP *moveResponse );
 
     ///Aquesta funció s'encarrega de guardar cada trama DICOM que rebem
     static void storeSCPCallback( void *callbackData, T_DIMSE_StoreProgress *progress, T_DIMSE_C_StoreRQ *storeRequest, char *imageFileName, DcmDataset **imageDataSet, T_DIMSE_C_StoreRSP *storeResponse, DcmDataset **statusDetail );
 
-    static OFCondition storeSCP( T_ASC_Association *association, T_DIMSE_Message *messagge, T_ASC_PresentationContextID presentationContextID );
-
-    /// Accepta la connexió que ens fa el PACS, per convertir-nos en un scp
-    static OFCondition subOperationSCP( T_ASC_Association **subAssociation );
-
+    ///Callbac que s'ha executa quan s'inicia un suboperació de descàrrega d'imatges
     static void subOperationCallback( void * /*subOperationCallbackData*/, T_ASC_Network *associationNetwork, T_ASC_Association **subAssociation );
-
-    /// Guarda una composite instance descarregada
-    static OFCondition save(DcmFileFormat *fileRetrieved, QString imageFileNameToSave);
-
-    /// Retorna el nom del fitxer amb que s'ha de guardar l'objecte descarregat, composa el path on s'ha de guardar + el nom del fitxer, si el path on s'ha de guardar la imatge no existeix el crea
-    static QString getCompositeInstanceFileName(DcmDataset *imageDataset);
-
-    /// En cas d'error processa la resposta rebuda per part del SCP, grava l'error el log i el retorna en forma d'objecte Status
-    Status processErrorResponseFromMoveSCP(T_DIMSE_C_MoveRSP *moveResponse, DcmDataset *statusDetail);
 };
 
 };
