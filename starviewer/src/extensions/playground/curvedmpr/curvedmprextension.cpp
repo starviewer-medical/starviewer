2fbd72db574015eed99259700d13f8dd9e30f27d
Dolors Segura <dolmaseg@gmail.com>
2010-05-22 16:23:10 +0000

    ENH: Primera versió implementació MPR Curvilini utilitzant la nova tool LinePathTool.

--- starviewer/src/extensions/playground/curvedmpr/curvedmprextension.cpp
+++ starviewer/src/extensions/playground/curvedmpr/curvedmprextension.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   Copyright (C) 2010 by Grup de Gràfics de Girona                       *
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
@@ -8,6 +8,16 @@
 
 #include "curvedmprsettings.h"
 #include "toolmanager.h"
+#include "volume.h"
+#include "toolproxy.h"
+#include "tool.h"
+#include "linepathtool.h"
+#include "mathtools.h"
+#include "drawerpolyline.h"
+// Vtk's
+#include <vtkImageData.h>
+#include <vtkPointData.h>
+#include <vtkCell.h>
 
 namespace udg {
 
@@ -22,6 +32,12 @@ CurvedMPRExtension::CurvedMPRExtension( QWidget *parent )
     m_viewersLayout->setGrid(2,1);
     
     initializeTools();
+
+    // Quan l'usuari indiqui la línia sobre la que caldrà projectar, llavors s'iniciarà el procés
+    // de creació del reslicedVolume que caldrà visualitzar al segon viewer.
+    ToolProxy *toolProxy = m_viewersLayout->getViewerWidget(0)->getViewer()->getToolProxy();
+    LinePathTool *linePathTool = qobject_cast<LinePathTool *>( toolProxy->getTool( "LinePathTool" ) );
+    connect( linePathTool, SIGNAL( finished( QPointer<DrawerPolyline> ) ), SLOT( updateReslice( QPointer<DrawerPolyline> )) );
 }
 
 CurvedMPRExtension::~CurvedMPRExtension()
@@ -39,6 +55,7 @@ void CurvedMPRExtension::initializeTools()
     m_polylineROIToolButton->setDefaultAction( m_toolManager->registerTool("PolylineROITool") );
     m_eraserToolButton->setDefaultAction( m_toolManager->registerTool("EraserTool") );
     m_voxelInformationToolButton->setDefaultAction( m_toolManager->registerTool("VoxelInformationTool") );
+    m_linePathToolButton->setDefaultAction( m_toolManager->registerTool("LinePathTool") );
     // Tools sense botó
     m_toolManager->registerTool("WindowLevelTool");
     m_toolManager->registerTool("TranslateTool");
@@ -48,7 +65,7 @@ void CurvedMPRExtension::initializeTools()
 
     // Definim els grups exclusius
     QStringList leftButtonExclusiveTools;
-    leftButtonExclusiveTools << "ZoomTool" << "SlicingTool" << "DistanceTool" << "PolylineROITool" << "EraserTool";
+    leftButtonExclusiveTools << "ZoomTool" << "SlicingTool" << "DistanceTool" << "PolylineROITool" << "EraserTool" << "LinePathTool";
     m_toolManager->addExclusiveToolsGroup("LeftButtonGroup", leftButtonExclusiveTools);
 
     // Activem les tools que volem tenir per defecte, això és com si clickéssim a cadascun dels ToolButton
@@ -59,6 +76,9 @@ void CurvedMPRExtension::initializeTools()
     // Registrem al manager les tools que van als diferents viewers
     m_toolManager->setupRegisteredTools( m_viewersLayout->getViewerWidget(0)->getViewer() );
     m_toolManager->setupRegisteredTools( m_viewersLayout->getViewerWidget(1)->getViewer() );
+
+    // Activem per defecte la tool LinePathTool
+    m_toolManager->getRegisteredToolAction("LinePathTool")->trigger();
 }
 
 void CurvedMPRExtension::setInput( Volume *input )
@@ -67,6 +87,197 @@ void CurvedMPRExtension::setInput( Volume *input )
     m_viewersLayout->getViewerWidget(1)->getViewer()->setInput(input);
 }
 
-};  // end namespace udg
+void CurvedMPRExtension::updateReslice( QPointer<DrawerPolyline> polyline )
+{
+    Q2DViewer *mainViewer = m_viewersLayout->getViewerWidget(0)->getViewer();
+    Volume *volume = mainViewer->getInput();
+
+    // Es porta a terme l'MPR Curvilini per obtenir el nou volum amb la reconstrucció
+    Volume *reslicedVolume = doCurvedReslice( volume, polyline );
+
+    // Visualitzem nou volum de dades al segon viewer
+    Q2DViewer *reconstructionViewer = m_viewersLayout->getViewerWidget(1)->getViewer();
+    // TODO: dóna error perquè no s'ha afegit cap punt a les dades VTK...
+    //reconstructionViewer->setInput( reslicedVolume );
+}
+
+Volume* CurvedMPRExtension::doCurvedReslice( Volume *volume, QPointer<DrawerPolyline> polyline )
+{
+    Volume *reslicedVolume = new Volume;
+
+    // Distància entre els píxels de les imatges que formen el volum
+    double spacing[3];
+    volume->getSpacing( spacing );
+    double pixelsDistance = sqrt( spacing[0] * spacing[0] + spacing[1] * spacing[1] );
+
+    // Es construeix una llista amb tots els punts que hi ha sobre la polyline indicada per
+    // l'usuari i que cal tenir en compte al fer la reconstrucció
+    QList< double* > *pointsPath = new QList< double* >;
+    getPointsPath( polyline, pixelsDistance, pointsPath );
+
+    // S'inicialitzen i s'emplenen les dades VTK que han de formar el volum de la reconstrucció.
+    vtkImageData *imageDataVTK = vtkImageData::New();
+    initAndFillImageDataVTK( volume, pointsPath, imageDataVTK );
+    
+    DEBUG_LOG(QString("Abans assignar data al reslicedVolume."));
+
+    // S'assignen les dades VTK al nou volum
+    reslicedVolume->setData( imageDataVTK );
+
+    return reslicedVolume;
+}
+
+void CurvedMPRExtension::getPointsPath( QPointer<DrawerPolyline> polyline, double pixelsDistance, QList< double * > *pointsPath )
+{
+    QList< double* > pointsList = polyline->getPointsList();
+    double *previousPoint;
+    double *currentPoint;
+    double *newLinePoint;
+    
+    //DEBUG_LOG(QString("PolyLine points size = %1").arg(pointsList.length()));
+
+    for ( int i = 0; i < pointsList.size(); i++)
+    {
+        currentPoint = pointsList.at(i);
+        
+        if ( i > 0 )
+        {
+            // S'anoten tots els punts que hi ha sobre la línia que formen el punt anterior 
+            // i l'actual de la polyline indicada per l'usuari
+            // La distància entre aquests punts per defecte serà la distància entre píxels
+
+            double *directorVector = MathTools::directorVector( previousPoint, currentPoint );
+            double distanceLine = MathTools::getDistance3D( previousPoint, currentPoint );
+            double numPointsLine = distanceLine / pixelsDistance;
+
+            MathTools::normalize( directorVector );
+
+            //DEBUG_LOG(QString("DirectorVector [%1,%2,%3]").arg(directorVector[0]).arg(directorVector[1]).arg(directorVector[2]));
+
+            //DEBUG_LOG(QString("numPointsLine = %1").arg(numPointsLine));
+
+            for (int j = 0; j < numPointsLine - 1; j++)
+            {
+                newLinePoint = new double[3];
+                newLinePoint[0] = previousPoint[0] + directorVector[0] * pixelsDistance * (j + 1);
+                newLinePoint[1] = previousPoint[1] + directorVector[1] * pixelsDistance * (j + 1);
+                newLinePoint[2] = previousPoint[2] + directorVector[2] * pixelsDistance * (j + 1);
+
+                //DEBUG_LOG(QString("New line point [%1,%2,%3]").arg(newLinePoint[0]).arg(newLinePoint[1]).arg(newLinePoint[2]));
+
+                pointsPath->append( newLinePoint );
+            }
+        }
+
+        // S'inclou a la llista el punt actual
+        if ( previousPoint[0] != currentPoint[0] || previousPoint[1] != currentPoint[1] || previousPoint[2] != currentPoint[2] )
+        {
+            //DEBUG_LOG(QString("append current point [%1,%2,%3]").arg(currentPoint[0]).arg(currentPoint[1]).arg(currentPoint[2]));
+            pointsPath->append( currentPoint );
+        }
+
+        previousPoint = currentPoint;
+    }
+
+    /*for ( int i = 0; i < pointsPath->size(); i++ )
+    {
+        double *point = pointsPath->at( i );
+        DEBUG_LOG(QString("Point path [%1,%2,%3]").arg(point[0]).arg(point[1]).arg(point[2]));
+    }*/
+}
+
+void CurvedMPRExtension::initAndFillImageDataVTK( Volume * volume, QList< double* > *pointsPath, vtkImageData *imageDataVTK )
+{
+    // Inicialització les dades VTK que formaran el volum de la reconstrucció.
+    /*
+    imageDataVTK->SetOrigin( .0, .0, .0 );
+    imageDataVTK->SetSpacing( 1., 1., 1. );
+    imageDataVTK->SetDimensions( 10, 10, 1 );
+    imageDataVTK->SetWholeExtent( 0, 9, 0, 9, 0, 0 );
+    imageDataVTK->SetScalarTypeToShort();
+    imageDataVTK->SetNumberOfScalarComponents(1);
+    imageDataVTK->AllocateScalars();
+    */
+
+    double maxX = (double) pointsPath->length();
+    double maxY = (double) volume->getImages().length();
+
+    imageDataVTK->SetOrigin( .0, .0, .0 );
+    imageDataVTK->SetSpacing( 1., 1., 1. ); //???
+    imageDataVTK->SetDimensions( maxX, maxY, 1 );
+    imageDataVTK->SetWholeExtent( 0, maxX, 0, maxY, 0, 0 );
+    imageDataVTK->SetScalarTypeToShort();
+    imageDataVTK->SetNumberOfScalarComponents(1);
+    imageDataVTK->AllocateScalars();
+
+    // S'emplena el dataset del volum amb el valor dels píxels resultat de projectar en profunditat
+    // la línia indicada per l'usuari
+
+    // Píxels que formen la imatge i dels quals modificarem el valor
+    signed short * scalarPointer = (signed short *) imageDataVTK->GetScalarPointer();
+
+    //DEBUG_LOG(QString("maxY %1").arg(maxY));
+    //DEBUG_LOG(QString("maxX %1").arg(maxX));
+
+    // Per cada fila del nou volum (tantes files com llesques (núm. imatges))
+    for( int y = 0; y < maxY; y++ )
+    {
+        // Obtenim el valor del pixel a la llesca actual per tots els punts indicats per 
+        // l'usuari i que formen les columnes de la imatge resultat
+        for ( int x = 0; x < pointsPath->size(); x++ )
+        {
+            double *point = pointsPath->at(x);
+        
+            Volume::VoxelType voxelValue;
+            // Es calcula el valor del voxel allà on es troba el punt actual a la llesca actual
+            point[2] = point[2] + y;
+
+            //DEBUG_LOG(QString("Point imatge [%1,%2,%3]").arg(point[0]).arg(point[1]).arg(point[2]));
+            if ( getPointImageVoxelValue( volume, point, voxelValue ) )
+            {
+                //DEBUG_LOG(QString("Valor pixel %1").arg(voxelValue));
+                // S'afegeix el valor del píxel a les dades VTK
+                *scalarPointer = voxelValue;
+                *scalarPointer++;
+            }
+        }
+    }
+}
+
+bool CurvedMPRExtension::getPointImageVoxelValue( Volume *volume, double *point, Volume::VoxelType &voxelValue )
+{
+    bool found = false;
+    if( point )
+    {
+        double tolerance;
+        int subCellId;
+        double parametricCoordinates[3], interpolationWeights[8];
+
+        vtkPointData *pointData = volume->getVtkData()->GetPointData();
+        vtkPointData* outPointData = vtkPointData::New();
+        outPointData->InterpolateAllocate( pointData , 1 , 1 );
+
+        // Use tolerance as a function of size of source data
+        tolerance = volume->getVtkData()->GetLength();
+        tolerance = tolerance ? tolerance*tolerance / 1000.0 : 0.001;
+
+        Volume::VtkImageTypePointer dades = volume->getVtkData();
+
+        // Find the cell that contains point and get it
+        vtkCell *cell = volume->getVtkData()->FindAndGetCell( point , NULL , -1 , tolerance , subCellId , parametricCoordinates , interpolationWeights );
+        if ( cell )
+        {
+            // Interpolate the point data
+            outPointData->InterpolatePoint( pointData , 0 , cell->PointIds , interpolationWeights );
+            voxelValue = outPointData->GetScalars()->GetTuple1(0);
+            found = true;
+        }
+        outPointData->Delete();
+    }
+
+    return found;
+}
+
+}; // end namespace udg
 
 
--- starviewer/src/extensions/playground/curvedmpr/curvedmprextension.h
+++ starviewer/src/extensions/playground/curvedmpr/curvedmprextension.h
@@ -9,11 +9,16 @@
 
 #include "ui_curvedmprextensionbase.h"
 
+// Qt's
+#include <QPointer>
+
 namespace udg {
 
 // FWD declarations
 class Volume;
 class ToolManager;
+class DrawerPolyline;
+
 /**
     Aquesta extensió integra les eines per poder realitzar un MPR Curvilini
 */
@@ -27,9 +32,35 @@ public slots:
     /// Assigna el volum amb el que s'aplica l'MPR Curvilini
     void setInput( Volume *input );
 
+    /// Inicia el procés de creació del reslicedVolume que caldrà visualitzar al segon viewer
+    /// Es crida quan l'usuari indica la línia sobre la que caldrà projectar
+    void updateReslice( QPointer<DrawerPolyline> polyline );
+
 private:
     /// Inicialitza les tools que tindrà l'extensió
     void initializeTools();
+
+    /// Porta a terme l'MPR Curvilini per obtenir el nou volum amb la reconstrucció
+    /// que cal assignar el segon viewer
+    Volume* doCurvedReslice( Volume *volume, QPointer<DrawerPolyline> polyline );
+
+    /// Es costrueix una llista amb tots els punts que hi ha sobre la polyline indicada per
+    /// l'usuari i que cal tenir en compte al fer la reconstrucció
+    void getPointsPath( QPointer<DrawerPolyline> polyline, double pixelsDistance, QList< double * > *pointsPath );
+
+    /// S'inicialitzen i s'emplenen les dades VTK que han de formar el volum de la reconstrucció.
+    void initAndFillImageDataVTK( Volume *volume, QList< double * > *pointsPath, vtkImageData *imageDataVTK );
+
+    /**
+     * COPIAT del mètode getCurrentCursorImageVoxel de la classe Q2Viewer
+     * Calcula el valor del voxel allà on es troba el punt indicat
+     * @param volume. Volum de dades de treball.
+     * @param point. Coordenades [x,y,z] del punt de la imatge que volem saber el valor.
+     * @param voxelValue Valor d'entrada/sortida. Ens dóna el valor del vòxel calculat
+     * En cas que el punt indicat estigui fora dels límits de la imatge, el valor retornat serà indefinit
+     * @return Cert si el punt indicat es troba dins dels límits de la imatge, fals altrament
+     */
+    bool getPointImageVoxelValue( Volume *volume, double *point, Volume::VoxelType &voxelValue );
 
 private:
     /// El volum al que se li practica l'MPR Curvilini
