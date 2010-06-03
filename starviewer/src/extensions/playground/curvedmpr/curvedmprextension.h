5f5636acac2ec2819cad07925d17ebdc40918d84
Jesús Spínola <jspinola@gmail.com>
2010-06-03 08:08:48 +0000

    ENH: Eliminem el pas de paràmetre de Volume ja que sempre fem servir el mainVolume, així es simplifica.

--- starviewer/src/extensions/playground/curvedmpr/curvedmprextension.h
+++ starviewer/src/extensions/playground/curvedmpr/curvedmprextension.h
@@ -44,14 +44,15 @@ private:
     void initializeTools();
 
     /// Porta a terme l'MPR Curvilini retornant un nou volum al que se li ha assignat la reconstrucció calculada
-    Volume* doCurvedReslice( Volume *volume, QPointer<DrawerPolyline> polyline );
+    /// L'MPR Curvilini es calcula sobre el volum del visor principal
+    Volume* doCurvedReslice(QPointer<DrawerPolyline> polyline);
 
     /// Retorna una llista amb tots els punts que hi ha sobre la polyline indicada per
     /// l'usuari i que cal tenir en compte al fer la reconstrucció
     QList<double *> getPointsPath(QPointer<DrawerPolyline> polyline, double pixelsDistance);
 
     /// S'inicialitzen i s'emplenen les dades VTK que han de formar el volum de la reconstrucció.
-    void initAndFillImageDataVTK(Volume *volume, const QList<double *> &pointsPath, vtkImageData *imageDataVTK);
+    void initAndFillImageDataVTK(const QList<double *> &pointsPath, vtkImageData *imageDataVTK);
 
 private:
     /// El volum al que se li practica l'MPR Curvilini
