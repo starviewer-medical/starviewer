4c3654b8d55c6cedea69a19561eb8269cd6c43ee
Jesús Spínola <jspinola@gmail.com>
2010-08-04 18:11:04 +0000

    ENH: Millorem el processament d'imatges en color. Segons l'espai de color i la seva definició amb lookup tables calia fer un post-procés perquè aquestes s'apliquiéssin finalment.

--- starviewer/src/core/volumepixeldatareadervtkgdcm.h
+++ starviewer/src/core/volumepixeldatareadervtkgdcm.h
@@ -20,6 +20,12 @@ public:
 
     int read(const QStringList &filenames);
 
+private:
+    /// Un cop llegides les dades, les processa segons l'espai de color 
+    /// en que estiguin definides i les assigna l'objecte vtkImageData
+    // TODO Potser aquest processament s'hauria de fer al corresponent visor i no aquí?
+    void applyColorProcessing();
+
 private slots:
     /// Captura el senyal de vtk perquè poguem notificar el progrés de la lectura
     void slotProgress();
