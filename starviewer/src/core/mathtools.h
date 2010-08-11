e80952ce08818e3a8af4846ee7a0fc9116416f2f
Jesús Spínola <jspinola@gmail.com>
2010-08-11 16:32:31 +0000

    STYLE: Ajustem el coding style

--- starviewer/src/core/mathtools.h
+++ starviewer/src/core/mathtools.h
@@ -18,7 +18,7 @@ namespace udg{
  */
 class MathTools {
 public:
-    enum IntersectionResults{ ParallelLines, SkewIntersection, LinesIntersect };
+    enum IntersectionResults { ParallelLines, SkewIntersection, LinesIntersect };
 
 ///
 /// Declaració de constants
@@ -40,16 +40,16 @@ static const double DoubleMaximumValue;
 ///
 
 /// Ens retorna el mínim dels dos valors
-static double minimum( double a, double b);
+static double minimum(double a, double b);
 
 /// Ens retorna el màxim dels dos valors
-static double maximum( double a, double b);
+static double maximum(double a, double b);
 
 /// Ens retorna el valor truncat
-static double truncate( double x);
+static double truncate(double x);
 
 /// Arrodoneix un nombre real a l'enter més proper
-static int roundToNearestInteger( double x );
+static int roundToNearestInteger(double x);
 
 /// Transforma el valor passat per paràmetre de graus a radians i vice-versa
 static float degreesToRadians(float degrees);
@@ -62,15 +62,15 @@ static float radiansToDegrees(float radians);
 static double logTwo(const double x, const bool zero = true);
 
 /// Arrel cúbica
-static double cubeRoot( double x );
+static double cubeRoot(double x);
 
 /// Determina si podem considerar pràcticament iguals els dos 
 /// valors passats per paràmetre si la seva diferència és menor al valor d'Epsilon 
 static bool closeEnough(float f1, float f2);
 
 /// Distància entre punts 2D i 3D
-static double getDistance2D( const double firstPoint[2], const double secondPoint[2] );
-static double getDistance3D( const double firstPoint[3], const double secondPoint[3] );
+static double getDistance2D(const double firstPoint[2], const double secondPoint[2]);
+static double getDistance3D(const double firstPoint[3], const double secondPoint[3]);
 
 ///
 /// Càlculs d'interseccions
@@ -78,11 +78,11 @@ static double getDistance3D( const double firstPoint[3], const double secondPoin
 
 /// Calcula la intersecció de dos plans definits per un punt i una normal (p,n) i (q,m) respectivament
 /// Retorna 0 si els plans no intersecten perquè són paral·lels , altrament >0
-static int planeIntersection( double p[3] , double n[3], double q[3] , double m[3], double r[3] , double t[3] );
+static int planeIntersection(double p[3] , double n[3], double q[3] , double m[3], double r[3] , double t[3]);
 
 /// Calcula la intersecció de tres plans definits per un punt i una normal (p,n) , (q,m) , (r,t) respectivament
 /// Retorna 0 si ... -1 si ... perquè són paral·lels , altrament >0
-static int planeIntersection( double p[3] , double n[3], double q[3] , double m[3], double r[3] , double t[3] , double intersectionPoint[3] );
+static int planeIntersection(double p[3] , double n[3], double q[3] , double m[3], double r[3] , double t[3] , double intersectionPoint[3]);
 
 /**
 * Retorna el punt d'intersecció de dues línies 
@@ -100,23 +100,23 @@ static double *infiniteLinesIntersection(double *p1, double *p2, double *p3, dou
 ///
 
 /// A partir del segment determinat pels dos punts passats com a paràmetres, calcula un dels possibles vectors directors
-static double* directorVector( const double point1[3], const double point2[3] );
+static double* directorVector(const double point1[3], const double point2[3]);
 
 /// Calcula el mòdul d'un vector
-static double modulus( double vector[3] );
+static double modulus(double vector[3]);
 
 /// Producte escalar dels dos vectors passats per paràmetre
-static double dotProduct( double vector1[3], double vector2[3] );
+static double dotProduct(double vector1[3], double vector2[3]);
 
 /// Calcula el producte vectorial dels dos vectors directors passats per paràmetre i ens deixa el resultat en el tercer paràmetre
-static void crossProduct( double vectorDirector1[3], double vectorDirector2[3], double crossProductVector[3] );
+static void crossProduct(double vectorDirector1[3], double vectorDirector2[3], double crossProductVector[3]);
 
 /// Normalitza el vector passat per paràmetre
-static double normalize( double vector[3] );
+static double normalize(double vector[3]);
 
 /// Calcula l'angle entre dos vectors. Retorna el valor en radians o graus
-static double angleInRadians( double vec1[3] , double vec2[3] );
-static double angleInDegrees( double vec1[3] , double vec2[3] );
+static double angleInRadians(double vec1[3] , double vec2[3]);
+static double angleInDegrees(double vec1[3] , double vec2[3]);
 
 /// Retorna cert si \a x és NaN.
 static bool isNaN(double x);
