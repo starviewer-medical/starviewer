38acc4c2f7af777f1663611da0ed5e58d8fb9660
Marc Cals <cpmarc@gmail.com>
2010-08-18 19:53:54 +0000

    STYLE: S'ha aplicat coding style a QueryPacsJob.

--- starviewer/src/inputoutput/querypacsjob.cpp
+++ starviewer/src/inputoutput/querypacsjob.cpp
@@ -20,7 +20,7 @@
 
 namespace udg {
 
-QueryPacsJob::QueryPacsJob(PacsDevice pacsDevice , DicomMask mask, QueryLevel queryLevel, QObject *parent)
+QueryPacsJob::QueryPacsJob(PacsDevice pacsDevice, DicomMask mask, QueryLevel queryLevel, QObject *parent)
  : Job(parent)
 {
     //creem l'objecte fer la query
@@ -34,35 +34,37 @@ QueryPacsJob::QueryPacsJob(PacsDevice pacsDevice , DicomMask mask, QueryLevel qu
 
 void QueryPacsJob::run()
 {
-    QString missatgeLog;
-    //creem la connexió
     PacsServer pacsServer(m_pacsDevice);
     Settings settings;
 
-    INFO_LOG( "Thread iniciat per cercar al PACS: AELocal= " + settings.getValue(InputOutputSettings::LocalAETitle).toString() + "; AEPACS= " + m_pacsDevice.getAETitle() + "; PACS Adr= " + m_pacsDevice.getAddress() + "; PACS Port= " + QString().setNum(m_pacsDevice.getQueryRetrieveServicePort()) + ";" );
+    INFO_LOG("Thread iniciat per cercar al PACS: AELocal= " + settings.getValue(InputOutputSettings::LocalAETitle).toString() + "; AEPACS= " + 
+        m_pacsDevice.getAETitle() + "; PACS Adr= " + m_pacsDevice.getAddress() + "; PACS Port= " + 
+        QString().setNum(m_pacsDevice.getQueryRetrieveServicePort()) + ";");
 
     m_queryStatus = pacsServer.connect(PacsServer::query);
 
-    if ( !m_queryStatus.good() )
+    if (!m_queryStatus.good())
     {
-        ERROR_LOG( QString("Error al connectar al PACS %1. PACS ERROR: %2").arg( m_pacsDevice.getAETitle() ).arg( m_queryStatus.text() ) );
+        ERROR_LOG(QString("Error al connectar al PACS %1. PACS ERROR: %2").arg(m_pacsDevice.getAETitle()).arg(m_queryStatus.text()));
     }
     else
     {
         m_queryPacs->setConnection(pacsServer);
         //busquem els estudis
-        m_queryStatus = m_queryPacs->query( m_mask );
-        if (! m_queryStatus.good() && !isAbortRequested() )
+        m_queryStatus = m_queryPacs->query(m_mask);
+        if (! m_queryStatus.good() && !isAbortRequested())
         {
-            //En el cas que hem abortat la query, i per abortar s'hagi abortat l'assocació, retorna el missatge DIMSE Failed to receive message, per això comprovem si hem abortat, perquè no donem aquest missatge com error ja que és normal
-            ERROR_LOG( QString("Error al connectar al PACS %1. PACS ERROR: %2").arg( m_pacsDevice.getAETitle() ).arg( m_queryStatus.text() ) );
+            /*En el cas que hem abortat la query, i per abortar s'hagi abortat l'assocació, retorna el missatge DIMSE Failed to receive message, 
+            per això comprovem si hem abortat, perquè no donem aquest missatge com error ja que és normal*/
+            ERROR_LOG(QString("Error al connectar al PACS %1. PACS ERROR: %2").arg(m_pacsDevice.getAETitle()).arg(m_queryStatus.text()));
         }
 
-        INFO_LOG ( QString("Thread del PACS %1 finalitzant").arg( m_pacsDevice.getAETitle() ) );
+        INFO_LOG (QString("Thread del PACS %1 finalitzant").arg(m_pacsDevice.getAETitle()));
 
-        //desconnectem
         pacsServer.disconnect();
     }
+
+    //TODO:Cal?
     setFinished(true);
 }
 
