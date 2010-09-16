692e43c53408ffe0d97812218cb474ac9974d548
Roger Bramon <rogerbramon@gmail.com>
2010-09-16 06:24:03 +0000

    STYLE: S'aplica coding style a la classe QPreviousStudiesWidget.

--- starviewer/src/inputoutput/qpreviousstudieswidget.h
+++ starviewer/src/inputoutput/qpreviousstudieswidget.h
@@ -23,13 +23,12 @@ class QPreviousStudiesWidget : public QFrame
 {
 Q_OBJECT
 public:
-    QPreviousStudiesWidget( QWidget * parent = 0 );
+    QPreviousStudiesWidget(QWidget *parent = 0);
     ~QPreviousStudiesWidget();
 
-    /** Mètode per activar la cerca d'estudis previs. Es mostraran al widget els estudis del pacient anteriors a la data
-      * de l'estudi proporcionat. Es farà la consulta als PACS que estan seleccionats a la llista de nodes de la QueryScreen
-      */
-    void searchPreviousStudiesOf( Study * study);
+    /// Mètode per activar la cerca d'estudis previs. Es mostraran al widget els estudis del pacient anteriors a la data
+    /// de l'estudi proporcionat. Es farà la consulta als PACS que estan seleccionats a la llista de nodes de la QueryScreen
+    void searchPreviousStudiesOf(Study *study);
 
 signals:
     /// S'emet només quan no hi ha altres estudis ja descarregant-se.
@@ -46,7 +45,7 @@ private:
     /// Inicialització del widget mostrat quan s'estan cercant estudis previs.
     void initializeLookinForStudiesWidget();
     /// Insereix un estudi a l'arbre i genera el contenidor associat a aquest estudi.
-    void insertStudyToTree(Study * study , QString pacsID  );
+    void insertStudyToTree(Study *study, QString pacsID);
     /// Actualitza l'amplada del QTreeWidget per aconseguir que l'scroll horitzontal no apareixi i tota la info sigui visible.
     void updateWidthTree();
 
@@ -55,19 +54,19 @@ private:
     void decreaseNumberOfDownladingStudies();
 
     /// Retorna la llista d'estudis ordenada per Data i Hora en ordre ascendent o descendent segons es vulgui.
-    QList<Study*> orderStudiesByDateTime( QList<Study*> & inputList , bool descendingOrder = false );
+    QList<Study*> orderStudiesByDateTime(QList<Study*> &inputList, bool descendingOrder = false);
 
 private slots:
     /// Insereix els estudis a l'arbre.
-    void insertStudiesToTree(  QList<Study*> studiesList , QHash<QString, QString> hashPacsIDOfStudyInstanceUID );
+    void insertStudiesToTree(QList<Study*> studiesList, QHash<QString, QString> hashPacsIDOfStudyInstanceUID);
     
     /// Invoca la descàrrega i càrrega de l'estudi identificat amb l'uid proporcionat.
-    void retrieveAndLoadStudy( const QString & studyInstanceUID  );
+    void retrieveAndLoadStudy(const QString &studyInstanceUID);
 
     /// Actualitzen l'estat de l'objecte i la interfície de l'estudi en qüestió.
-    void studyRetrieveStarted( QString studyInstanceUID );
-    void studyRetrieveFinished( QString studyInstanceUID );
-    void studyRetrieveFailed( QString studyInstanceUID );
+    void studyRetrieveStarted(QString studyInstanceUID);
+    void studyRetrieveFinished(QString studyInstanceUID);
+    void studyRetrieveFailed(QString studyInstanceUID);
 
 private:
 
@@ -77,28 +76,28 @@ private:
     /// Contenidor d'objectes associats a l'estudi que serveix per facilitar la intercomunicació
     struct StudyInfo
     {
-        Study * study;
+        Study *study;
         QString pacsID;
-        QTreeWidgetItem * item;
+        QTreeWidgetItem *item;
         Status status;
-        QLabel * statusIcon;
-        QPushButton * downloadButton;
+        QLabel *statusIcon;
+        QPushButton *downloadButton;
     };
 
     /// Estructura que s'encarrega de guardar els contenidors associats a cada Study
-    QHash< QString, StudyInfo * > m_infomationPerStudy;
+    QHash< QString, StudyInfo *> m_infomationPerStudy;
     /// Widget utilitzat per mostrar la llista dels estudis previs
-    QTreeWidget * m_previousStudiesTree;
+    QTreeWidget *m_previousStudiesTree;
     /// Widget que apareix quan s'està fent la consulta dels possibles estudis previs.
-    QWidget * m_lookingForStudiesWidget;
+    QWidget *m_lookingForStudiesWidget;
     /// Label per mostrar que no hi ha estudis previs.
-    QLabel * m_noPreviousStudiesLabel;
+    QLabel *m_noPreviousStudiesLabel;
     /// Objecte encarregat de cercar estudis previs
-    PreviousStudiesManager * m_previousStudiesManager;
+    PreviousStudiesManager *m_previousStudiesManager;
     /// Mapper utilitzat per saber cada botó de descàrrega a quin estudi està associat.
-    QSignalMapper * m_signalMapper;
+    QSignalMapper *m_signalMapper;
     /// Objecte utilitzat per invocar la descàrrega d'estudis.
-    QueryScreen * m_queryScreen;
+    QueryScreen *m_queryScreen;
     /// Ens permet saber els estudis que s'estan descarregant.
     int m_numberOfDownloadingStudies;
 };
