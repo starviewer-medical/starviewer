5bbb70846bda6da71384c41cb87e89aaef7f3258
Quim Rodríguez <quimrog.u@gmail.com>
2010-11-07 20:28:37 +0000

    MERGE: Merge dels canvis fets al trunk a 6305-6306,6309 on s'intenten apal·liar els problemes amb els estudis BAV de Lleida (#1312)

--- starviewer/src/core/qviewer.cpp
+++ starviewer/src/core/qviewer.cpp
@@ -610,7 +610,7 @@ void QViewer::getDefaultWindowLevel( double windowLevel[2] )
         if (m_mainVolume)
         {
             double *range = m_mainVolume->getVtkData()->GetScalarRange();
-            this->setDefaultWindowLevel(range[1] - range[0], (range[1] / 2.) + range[0]);
+            this->setDefaultWindowLevel(range[1] - range[0], ((range[1] - range[0]) / 2.) + range[0]);
         }
         else
         {
@@ -667,6 +667,7 @@ void QViewer::updateWindowLevelData()
         return;
 
     const QString DefaultWindowLevelName =  tr("Default");
+    const QString AutomaticWindowLevelName = tr("Auto");
 
     m_windowLevelData->removePresetsFromGroup( WindowLevelPresetsToolData::FileDefined );
 
@@ -702,12 +703,16 @@ void QViewer::updateWindowLevelData()
                 m_windowLevelData->activatePreset( description );
         }
     }
-    else // no n'hi ha de definits al volum, agafem el que ens doni el viewer
+    
+    // Calculem un window level automàtic que sempre posarem disponible a l'usuari
+    double *range = m_mainVolume->getVtkData()->GetScalarRange();
+    double automaticWindowWidth = range[1] - range[0];
+    double automaticWindowLevel = range[0] + (automaticWindowWidth * 0.5);
+    m_windowLevelData->addPreset(AutomaticWindowLevelName, automaticWindowWidth, automaticWindowLevel, WindowLevelPresetsToolData::FileDefined);
+    // Si no hi ha window levels definits per defecte activarem l'automàtic
+    if (wlCount <= 0)
     {
-        double windowLevel[2];
-        this->getDefaultWindowLevel( windowLevel );
-        m_windowLevelData->addPreset( DefaultWindowLevelName, windowLevel[0], windowLevel[1], WindowLevelPresetsToolData::FileDefined );
-        m_windowLevelData->activatePreset(DefaultWindowLevelName);
+        m_windowLevelData->activatePreset(AutomaticWindowLevelName);
     }
 }
 
