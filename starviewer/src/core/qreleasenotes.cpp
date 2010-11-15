0526aeb56d8b546c71080fb81e514c8793da89c3
Marc Puigdemont <fraijilverto@hotmail.com>
2010-11-15 11:14:31 +0000

    ENH: Es canvia la codificació de les classes QReleaseNotes de Western European a UTF-8

--- starviewer/src/core/qreleasenotes.cpp
+++ starviewer/src/core/qreleasenotes.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Grup de Gràfics de Girona                       *
+ *   Copyright (C) 2005 by Grup de GrÃ fics de Girona                       *
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
@@ -16,7 +16,7 @@ QReleaseNotes::QReleaseNotes(QWidget *parent)
 {
     setupUi(this);
 
-    //no cal fer un metode a part per les connexions si només en tenim una
+    //no cal fer un metode a part per les connexions si nomÃ©s en tenim una
     connect(m_closePushButton, SIGNAL(clicked()), this, SLOT(close()));
 
     //Fer que la finestra sempre quedi davant i no es pugui fer res fins que no es tanqui
@@ -44,7 +44,7 @@ void QReleaseNotes::setUrl(QUrl url)
 
 void QReleaseNotes::closeEvent(QCloseEvent *event)
 {
-    //llençar el senyal de que es tanca la finestra
+    //llenÃ§ar el senyal de que es tanca la finestra
     closing();
     //i tancar la finestra
     event->accept();
--- starviewer/src/core/qreleasenotes.h
+++ starviewer/src/core/qreleasenotes.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Grup de Gr‡fics de Girona                       *
+ *   Copyright (C) 2005 by Grup de Gr√†fics de Girona                       *
  *   http://iiia.udg.es/GGG/index.html?langu=uk                            *
  *                                                                         *
  *   Universitat de Girona                                                 *
@@ -11,11 +11,10 @@
 #include "ui_qreleasenotesbase.h"
 
 #include <QWidget>
-class QNetworkProxy;
 
 namespace udg {
 
-/** Aquesta classe s'utilitza per mostrar les release notes, contÈ un QWebView per tal de mostrar-les com a html.
+/** Aquesta classe s'utilitza per mostrar les release notes, cont√© un QWebView per tal de mostrar-les com a html.
     Es gestionada per ApplicationVersionChecker.
   */
 class QReleaseNotes : public QWidget, Ui::QReleaseNotesBase {
@@ -23,33 +22,27 @@ Q_OBJECT
 
 public:
     /// Constructor amb els atributs del QWidget on es posa (0 per que sigui una finestra nova)
-    /// i la referËncia al pare per tal de poder-li indicar quan es tanca la finestra
+    /// i la refer√®ncia al pare per tal de poder-li indicar quan es tanca la finestra
     QReleaseNotes(QWidget *parent);
     /// Destructor
     ~QReleaseNotes();
-    /// Retorna si esta activat el checkbox de no mostrar mÈs
+    /// Retorna si esta activat el checkbox de no mostrar m√©s
     bool isDontShowAnymoreChecked();
     /// Mostra o amaga el checkbox don't show anymore
     void setDontShowVisible(bool visible);
-    /// Determina quin url haur‡ d'obrir la finestra de les release notes
+    /// Determina quin url haur√† d'obrir la finestra de les release notes
     void setUrl(QUrl url);
     
 signals:
-    // Senyal per indicar a ApplicationVersionChecker que es tanca la finestra
+    /// Senyal per indicar a ApplicationVersionChecker que es tanca la finestra
     void closing();
 
-//public slots:
-
 protected:
-    /// Redifinir el close event
+    /// Redifinir el close event per quan es tanqui, guardar els settings
     void closeEvent(QCloseEvent *event);
-
-//private:
-
-//private:
     
 };
 
 } // end namespace udg
 
-#endif
+#endif //UDGQRELEASENOTES_H
