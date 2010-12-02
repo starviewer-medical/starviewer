6ef9edeb6699c666df7bc67787e3f14d2c9383db
Quim Rodríguez <quimrog.u@gmail.com>
2010-12-02 19:15:18 +0000

    ENH: Es fan els canvis necessaris per la 0.9.2 final

--- starviewer/src/core/starviewerapplication.h
+++ starviewer/src/core/starviewerapplication.h
@@ -11,8 +11,8 @@
 
 namespace udg{
 
-const QString StarviewerVersionString("0.9.2-RC7");
-const QString StarviewerBuildID("2010112500");
+const QString StarviewerVersionString("0.9.2");
+const QString StarviewerBuildID("2010112600");
 
 //Indica per aquesta versió d'starviewer quina és la revisió de bd necessària
 const int StarviewerDatabaseRevisionRequired(5114);
