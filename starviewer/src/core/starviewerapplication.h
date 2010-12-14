aae9bceac134bde2f6342737ab4214ffb5e63cd2
Quim Rodríguez <quimrog.u@gmail.com>
2010-12-14 19:56:53 +0000

    Merge branch 'working/documentar-packages'

--- starviewer/src/core/starviewerapplication.h
+++ starviewer/src/core/starviewerapplication.h
@@ -11,7 +11,7 @@
 
 namespace udg{
 
-const QString StarviewerVersionString("0.9.0.devel");
+const QString StarviewerVersionString("0.10.0-devel");
 const QString StarviewerBuildID("2010032100");
 
 //Indica per aquesta versió d'starviewer quina és la revisió de bd necessària
