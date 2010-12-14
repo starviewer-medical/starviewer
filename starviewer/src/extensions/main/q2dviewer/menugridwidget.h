506bba1e762d109ef34e3330a72abd28e9752f58
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2010-12-14 09:34:08 +0000

    ENH: S'elimina codi que ja no s'utilitza al treure la funcionalitat del GridLayout a la r6490. Referent al tiquet #1366.

--- starviewer/src/extensions/main/q2dviewer/menugridwidget.h
+++ starviewer/src/extensions/main/q2dviewer/menugridwidget.h
@@ -34,22 +34,7 @@ public:
 
     ~MenuGridWidget();
 
-    /// Lista que guarda la configuració de grids
-    QStringList m_predefinedGridsList;
-
-    /// Màxim nombre de columnes a mostrar els grids predefinits
-    void setMaxColumns( int columns );
-
-    /// Màxim nombre d'elements a mostrar
-    void setMaxElements( int elements );
-
     void createHangingProtocolsWidget();
-    
-    /// Mètode que omple el widget amb les els grids predefinits
-    void createPredefinedGrids( int numberSeries );
-
-    /// Mètode que omple el widget amb les els grids predefinits
-    void createPredefinedGrids( QStringList list );
 
     /// Elimina tot el contingut del widget
     void dropContent();
@@ -75,14 +60,10 @@ public slots:
 signals:
     
     /// Emet que s'ha escollit un grid
-    void selectedGrid( int , int );
     void selectedGrid( int );
 
 protected:
-
-    /// Mètode que crea una icona de rows x columns
-    ItemMenu * createIcon( int rows, int columns );
-
+   
     /// Mètode que crea una icona segons un hanging protocol
     ItemMenu * createIcon( const HangingProtocol * hangingProtocol );
 
@@ -94,9 +75,6 @@ protected:
 
     /// Nombre de columnes a mostrar
     int m_maxColumns;
-
-    /// Màxim nombre d'elements a mostrar;
-    int m_maxElements;
 
     /// Grid per mostrar els elements predefinits
     QGridLayout *m_gridLayout;
