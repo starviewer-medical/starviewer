3780cb9abe019db69f2bf53f5aaef943c92a8ce2
Quim Rodríguez <quimrog.u@gmail.com>
2011-01-07 18:27:50 +0000

    COMP: S'elimina warning que avisa d'inicialitzacions al revés de les declaracions.

--- starviewer/src/core/qviewer.cpp
+++ starviewer/src/core/qviewer.cpp
@@ -46,7 +46,7 @@ namespace udg {
 
 QViewer::QViewer(QWidget *parent)
  : QWidget(parent), m_mainVolume(0), m_contextMenuActive(true), m_mouseHasMoved(false), m_windowLevelData(0), m_defaultWindow(.0),
-   m_defaultLevel(.0), m_isRenderingEnabled(true), m_isActive(false), m_hasDefaultWindowLevelDefined(false), m_transferFunction(0)
+   m_defaultLevel(.0), m_transferFunction(0), m_isRenderingEnabled(true), m_isActive(false), m_hasDefaultWindowLevelDefined(false)
 {
     // TODO: De moment es desactiven els warnings en release i windows perquè no apareixi la finestra vtkOutputWindow
     // però la solució bona és que els viewers no donin warnings.
