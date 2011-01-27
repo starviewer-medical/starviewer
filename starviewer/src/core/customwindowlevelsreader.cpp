181c11b688d80d27cae378b169a5a8186b3ec787
Roger Bramon <rogerbramon@gmail.com>
2011-01-27 12:10:59 +0000

    ENH: S'afegeix l'estructura de classes per llegir Custom Window Levels des de fitxer XML tot i que encara no s'utilitza. S'afegeix també el validador. L'estructura és molt similiar a la de DICOMDumpDefaultTags o HP i queda pendent el refactoring tal i com diu el tiquet #1413.

---
+++ starviewer/src/core/customwindowlevelsreader.cpp
@@ -0,0 +1,77 @@
+#ifndef UDGCUSTOMWINDOWLEVELSREADER_CPP
+#define UDGDCUSTOMWINDOWLEVELSREADER_CPP
+
+#include "customwindowlevelsreader.h"
+#include "customwindowlevel.h"
+#include "logging.h"
+
+#include <QFile>
+#include <QXmlStreamReader>
+#include <QXmlSchema>
+#include <QList>
+
+namespace udg {
+
+CustomWindowLevelsReader::CustomWindowLevelsReader()
+{
+   QXmlSchema schema;
+   schema.load(QUrl("qrc:xmlSchemes/CustomWindowLevels.xml"));
+   m_customWindowLevelsSchemaValidator.setSchema(schema);
+}
+
+CustomWindowLevelsReader::~CustomWindowLevelsReader()
+{
+}
+
+QList<CustomWindowLevel*> CustomWindowLevelsReader::readFile(const QString &path)
+{
+    QFile file(path);
+    if (!file.open(QFile::ReadOnly | QFile::Text))
+    {
+        return QList<CustomWindowLevel*>();
+    }
+
+    if (!m_customWindowLevelsSchemaValidator.validate(QUrl::fromLocalFile(file.fileName())))
+    {
+        DEBUG_LOG("No es processa perquè es invalid");
+        return QList<CustomWindowLevel*>();
+    }
+
+    QList<CustomWindowLevel*> listOfCustomWindowLevels;
+
+    QXmlStreamReader reader(&file);
+    if (reader.readNextStartElement())
+    {
+        if (reader.name() == "CustomWindowLevels")
+        {
+            while (reader.readNextStartElement())
+            {
+                if (reader.name() == "CustomWindowLevel")
+                {
+                    CustomWindowLevel *customWindowLevel = new CustomWindowLevel();
+                    QString name = reader.attributes().first().value().toString();
+                    customWindowLevel->setName(name);
+
+                    while (reader.readNextStartElement())
+                    {
+                        if (reader.name() == "window")
+                        {
+                            customWindowLevel->setWindow(reader.readElementText().toDouble());
+                        }
+                        else if (reader.name() == "level")
+                        {
+                            customWindowLevel->setLevel(reader.readElementText().toDouble());
+                        }
+                    }
+                    listOfCustomWindowLevels.append(customWindowLevel);
+                }
+            }
+        }
+    }
+
+    return listOfCustomWindowLevels;
+}
+
+}
+
+#endif
