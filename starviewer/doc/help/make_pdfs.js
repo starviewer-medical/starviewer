ce73f9c21481ee842a114fd5cc5e6451e292eb70
Quim Rodríguez <quimrog.u@gmail.com>
2011-03-13 19:24:22 +0000

    ENH: S'afegeix script per crear els pdf's dels manual d'ajuda i quick guide. Només funciona sota windows. Quan hi hagi el "source" també s'haurà d'afegir el documents de shortcuts.

---
+++ starviewer/doc/help/make_pdfs.js
@@ -0,0 +1,39 @@
+/*
+ Genera els documents d'ajuda en format PDF al directori /bin a punt per empaquetar amb l'instal·lador.
+ Funciona només sota Windows.
+ Per fer-ho fa servir el Word de l'Office. Ha de ser versió Office >= 2007.
+
+ TODO: Falta afegir el document de shortcuts i tenirn en compte la versió Lite.
+*/
+
+var shell = WScript.CreateObject("WScript.Shell");
+var saveToFolderName = shell.CurrentDirectory + "\\..\\..\\bin\\"
+
+function docxToPdf(msword, sourceFileName)
+{
+    var fileSystem = WScript.CreateObject("Scripting.FileSystemObject");
+    //var shell = WScript.CreateObject("WScript.Shell");
+    //var saveToFileName = shell.CurrentDirectory + "\\..\\..\\bin\\" + fileSystem.GetBaseName(sourceFileName) + ".pdf";
+    var saveToFileName = saveToFolderName + fileSystem.GetBaseName(sourceFileName) + ".pdf";
+    if (fileSystem.FileExists(saveToFileName))
+    {
+        fileSystem.DeleteFile(saveToFileName);
+    }
+    // Obrim el document amb el word i el convertim a pdf
+    msword.Documents.Open(sourceFileName);
+    msword.ActiveDocument.SaveAs(saveToFileName, 17); // 17 magic number per wdFormatPDF
+}
+
+var shell = WScript.CreateObject("WScript.Shell");
+var filenames = [shell.CurrentDirectory + "\\usermanual\\Starviewer_User_guide.docx",
+                 shell.CurrentDirectory + "\\quickstartguide\\Starviewer_Quick_start_guide.docx"];
+var msword = WScript.CreateObject("Word.Application");
+
+for(var i = 0; i < filenames.length; ++i)
+{
+    docxToPdf(msword, filenames[i]);
+}
+
+msword.quit();
+
+WScript.Echo("The help files have benn generated successfully on " + saveToFolderName);
