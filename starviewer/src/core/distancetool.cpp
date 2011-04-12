0e6ebeaf8e72783f079452f83e598c9ac3414699
Jesús Spínola <jspinola@gmail.com>
2011-04-12 12:43:05 +0000

    STYLE: Apliquem correccions d'espais i salts de línia

--- starviewer/src/core/distancetool.cpp
+++ starviewer/src/core/distancetool.cpp
@@ -20,17 +20,17 @@
 
 namespace udg {
 
-DistanceTool::DistanceTool( QViewer *viewer, QObject *parent )
+DistanceTool::DistanceTool(QViewer *viewer, QObject *parent)
  : Tool(viewer, parent)
 {
     m_toolName = "DistanceTool";
     m_hasSharedData = false;
 
-    m_2DViewer = qobject_cast<Q2DViewer *>( viewer );
-    if( !m_2DViewer )
-        DEBUG_LOG(QString("El casting no ha funcionat!!! És possible que viewer no sigui un Q2DViewer!!!-> ")+ viewer->metaObject()->className() );
+    m_2DViewer = qobject_cast<Q2DViewer *>(viewer);
+    if (!m_2DViewer)
+        DEBUG_LOG(QString("El casting no ha funcionat!!! És possible que viewer no sigui un Q2DViewer!!!-> ")+ viewer->metaObject()->className());
 
-    connect( m_2DViewer, SIGNAL( volumeChanged(Volume *) ), SLOT( initialize() ) );
+    connect(m_2DViewer, SIGNAL(volumeChanged(Volume *)), SLOT(initialize()));
     initialize();
 }
 
@@ -39,20 +39,20 @@ DistanceTool::~DistanceTool()
     bool hasToRefresh = false;
     // Cal decrementar el reference count perquè 
     // l'annotació s'esborri si "matem" l'eina
-    if ( m_line )
+    if (m_line)
     {
         m_line->decreaseReferenceCount();
         delete m_line;
         hasToRefresh = true;
     }
 
-    if( hasToRefresh )
+    if (hasToRefresh)
         m_2DViewer->render();
 }
 
-void DistanceTool::handleEvent( long unsigned eventID )
+void DistanceTool::handleEvent(long unsigned eventID)
 {
-    switch( eventID )
+    switch (eventID)
     {
         case vtkCommand::LeftButtonPressEvent:
             handlePointAddition();
@@ -65,9 +65,9 @@ void DistanceTool::handleEvent( long unsigned eventID )
 
 void DistanceTool::handlePointAddition()
 {
-    if( m_2DViewer->getInput() )
+    if (m_2DViewer->getInput())
     {
-        if ( m_2DViewer->getInteractor()->GetRepeatCount() == 0 )
+        if (m_2DViewer->getInteractor()->GetRepeatCount() == 0)
         {
             this->annotateNewPoint();
         }
@@ -76,7 +76,7 @@ void DistanceTool::handlePointAddition()
 
 void DistanceTool::annotateNewPoint()
 {
-    if ( !m_line )
+    if (!m_line)
     {
         m_line = new DrawerLine;
         // Així evitem que durant l'edició la primitiva pugui ser esborrada per events externs
@@ -84,20 +84,20 @@ void DistanceTool::annotateNewPoint()
     }
 
     double clickedWorldPoint[3];
-    m_2DViewer->getEventWorldCoordinate( clickedWorldPoint );
+    m_2DViewer->getEventWorldCoordinate(clickedWorldPoint);
 
     // Afegim el punt
-    if( m_lineState == NoPointFixed )
+    if (m_lineState == NoPointFixed)
     {
-        m_line->setFirstPoint( clickedWorldPoint );
-        m_line->setSecondPoint( clickedWorldPoint );
+        m_line->setFirstPoint(clickedWorldPoint);
+        m_line->setSecondPoint(clickedWorldPoint);
         m_lineState = FirstPointFixed;
 
-        m_2DViewer->getDrawer()->draw( m_line , m_2DViewer->getView(), m_2DViewer->getCurrentSlice() );
+        m_2DViewer->getDrawer()->draw(m_line , m_2DViewer->getView(), m_2DViewer->getCurrentSlice());
     }
     else
     {
-        m_line->setSecondPoint( clickedWorldPoint );
+        m_line->setSecondPoint(clickedWorldPoint);
         m_line->update();
 
         // Posem el text
@@ -107,9 +107,9 @@ void DistanceTool::annotateNewPoint()
         double *vtkSpacing = m_2DViewer->getInput()->getSpacing();
         const double *pixelSpacing = m_2DViewer->getInput()->getImage(0)->getPixelSpacing();
 
-        if ( pixelSpacing[0] == 0.0 && pixelSpacing[1] == 0.0 )
+        if (pixelSpacing[0] == 0.0 && pixelSpacing[1] == 0.0)
         {
-            text->setText( tr("%1 px").arg( m_line->computeDistance( vtkSpacing ), 0, 'f', 0 ) );
+            text->setText(tr("%1 px").arg(m_line->computeDistance(vtkSpacing), 0, 'f', 0));
         }
         else
         {
@@ -117,17 +117,17 @@ void DistanceTool::annotateNewPoint()
             double distance;
             // En cas de Ultrasons es fa un tractament especial perquè VTK no agafa l'spacing correcte. \TODO S'hauria d'unificar.
             // Podem tenir imatges de la mateixa sèrie amb spacings diferents
-            if ( m_2DViewer->getInput()->getImage(0)->getParentSeries()->getModality() == "US" )
+            if (m_2DViewer->getInput()->getImage(0)->getParentSeries()->getModality() == "US")
             {
                 Image *image = m_2DViewer->getCurrentDisplayedImage();
-                if( image )
+                if (image)
                 {
                     const double *usSpacing = image->getPixelSpacing();
                     double * firstPoint = m_line->getFirstPoint();
                     double * secondPoint = m_line->getSecondPoint();
 
-                    double xx = ( firstPoint[0] - secondPoint[0] ) / vtkSpacing[0] * usSpacing[0];
-                    double yy = ( firstPoint[1] - secondPoint[1] ) / vtkSpacing[1] * usSpacing[1];
+                    double xx = (firstPoint[0] - secondPoint[0]) / vtkSpacing[0] * usSpacing[0];
+                    double yy = (firstPoint[1] - secondPoint[1]) / vtkSpacing[1] * usSpacing[1];
                     double value = std::pow(xx, 2) + std::pow(yy, 2);
                     distance = std::sqrt(value);
                     distanceIsComputed = true;
@@ -141,22 +141,22 @@ void DistanceTool::annotateNewPoint()
                 }
             }
 
-            if( !distanceIsComputed )
+            if (!distanceIsComputed)
             {
                 distance = m_line->computeDistance();
             }
 
-            text->setText( tr("%1 mm").arg( distance, 0, 'f', 2 ) );
+            text->setText(tr("%1 mm").arg(distance, 0, 'f', 2));
         }
 
         // Coloquem el texte a prop del punt que quedi més a l'esquerra de la línia
         int i;
-        int xIndex = Q2DViewer::getXIndexForView( m_2DViewer->getView() );
+        int xIndex = Q2DViewer::getXIndexForView(m_2DViewer->getView());
         double leftPoint[3];
         double *firstPoint = m_line->getFirstPoint();
         double *secondPoint = m_line->getSecondPoint();
 
-        if ( firstPoint[xIndex] <= secondPoint[xIndex] )
+        if (firstPoint[xIndex] <= secondPoint[xIndex])
         {
             for (i = 0; i < 3; i++)
                 leftPoint[i] = firstPoint[i]; 
@@ -174,10 +174,10 @@ void DistanceTool::annotateNewPoint()
         m_2DViewer->computeDisplayToWorld(leftPointInDisplay[0] - Padding, leftPointInDisplay[1], leftPointInDisplay[2], leftPoint);    // apliquem el padding i tornem a coordenades de món
         // ara leftPoint és l'attachment point que volem
 
-        text->setAttachmentPoint( leftPoint );
-        text->setHorizontalJustification( "Right" );
+        text->setAttachmentPoint(leftPoint);
+        text->setHorizontalJustification("Right");
         text->shadowOn();
-        m_2DViewer->getDrawer()->draw( text , m_2DViewer->getView(), m_2DViewer->getCurrentSlice() );
+        m_2DViewer->getDrawer()->draw(text , m_2DViewer->getView(), m_2DViewer->getCurrentSlice());
         // Reiniciem l'estat de la tool
         initialize();
     }
@@ -185,12 +185,12 @@ void DistanceTool::annotateNewPoint()
 
 void DistanceTool::simulateLine()
 {
-    if( m_line )
+    if (m_line)
     {
         double clickedWorldPoint[3];
-        m_2DViewer->getEventWorldCoordinate( clickedWorldPoint );
+        m_2DViewer->getEventWorldCoordinate(clickedWorldPoint);
 
-        m_line->setSecondPoint( clickedWorldPoint );
+        m_line->setSecondPoint(clickedWorldPoint);
         m_line->update();
         m_2DViewer->render();
     }
@@ -198,7 +198,7 @@ void DistanceTool::simulateLine()
 
 void DistanceTool::initialize()
 {
-    if( m_line )
+    if (m_line)
     {
         // Així alliberem la primitiva perquè pugui ser esborrada
         m_line->decreaseReferenceCount();
