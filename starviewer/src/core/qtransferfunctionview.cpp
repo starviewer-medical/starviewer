2365cc20245478a0bb73879a259ac3f1383126a2
Marc Ruiz <marc.ruiz@gmail.com>
2011-04-26 16:50:46 +0000

    BUG: No es feia una connexió perquè el nom de l'slot estava malament.

--- starviewer/src/core/qtransferfunctionview.cpp
+++ starviewer/src/core/qtransferfunctionview.cpp
@@ -18,7 +18,7 @@ void QTransferFunctionView::makeConnections()
     connect(m_editor, SIGNAL(transferFunctionChanged(TransferFunction)), SLOT(setTransferFunction(TransferFunction)));
     connect(m_editor, SIGNAL(nameChanged(QString)), SLOT(setName(QString)));
     connect(m_editor, SIGNAL(colorTransferFunctionChanged(ColorTransferFunction)), SLOT(setColorTransferFunction(ColorTransferFunction)));
-    connect(m_editor, SIGNAL(colorPointAdded(double,QColor)), SLOT(addColorNode(double,QColor)));
+    connect(m_editor, SIGNAL(colorPointAdded(double,QColor)), SLOT(addColorPoint(double,QColor)));
     connect(m_editor, SIGNAL(colorPointRemoved(double)), SLOT(removeColorPoint(double)));
     connect(m_editor, SIGNAL(colorPointMoved(double,double)), SLOT(moveColorPoint(double,double)));
     connect(m_editor, SIGNAL(colorPointChanged(double,QColor)), SLOT(changeColorPoint(double,QColor)));
