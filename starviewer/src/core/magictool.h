6709a3680531b7357473220c8573a7aca98d966e
Roger Bramon <rogerbramon@gmail.com>
2011-05-06 16:15:30 +0000

    ENH: Es fa un refactoring general de MagicTool i es fa que heredi de ROITool. Això permet mostrar la mitjana i la desviació a més de l'àrea. També s'afegeix que el moviment del mouse serveixi per variar el Magic Factor. Fartaria treure la limitació que té que només pugui ser utilitzada en el pla d'adquisició. Tiquet #1590.

--- starviewer/src/core/magictool.h
+++ starviewer/src/core/magictool.h
@@ -1,13 +1,9 @@
 #ifndef UDGMAGICTOOL_H
 #define UDGMAGICTOOL_H
 
-#include "tool.h"
+#include "roitool.h"
 
-#include <QPointer>
 #include <QVector>
-
-class vtkActor;
-class vtkImageData;
 
 namespace udg {
 
@@ -19,7 +15,7 @@ class DrawerPolygon;
 /**
     Tool que serveix per editar el volum sobreposat en un visor 2D
 */
-class MagicTool : public Tool {
+class MagicTool : public ROITool {
 Q_OBJECT
 public:
     MagicTool(QViewer *viewer, QObject *parent = 0);
@@ -28,88 +24,66 @@ public:
     void handleEvent(unsigned long eventID);
 
 private:
-    /**
-     * Crida a la generació de la regió màgica
-     */
-    void setMagicPoint();
+    /// Crida a la generació de la regió màgica
+    void generateRegion();
 
-    /**
-     * Versió iterativa del region Growing
-     */
-    void paintRegionMask();
+    /// Calcula el rang de valors d'intensitat vàlid a partir de \sa #m_magicSize i \see #m_magicFactor
+    void computeLevelRange();
 
-    /**
-     * Fer un moviment dses d'un índex cap a una direcció
-     * @param a, @param b índex del volum de la màscara que estem mirant en cada crida
-     * @param movement direcció en el moviment
-     */
+    /// Versió iterativa del region Growing
+    void computeRegionMask();
+
+    /// Fer un moviment des d'un índex cap a una direcció
+    /// @param a, @param b índex del volum de la màscara que estem mirant en cada crida
+    /// @param movement direcció en el moviment
     void doMovement(int &a, int &b, int movement);
 
-    /**
-     * Desfer un moviment dses d'un índex cap a una direcció
-     * @param a, @param b índex del volum de la màscara que estem mirant en cada crida
-     * @param movement direcció en el moviment
-     */
+    /// Desfer un moviment dses d'un índex cap a una direcció
+    /// @param a, @param b índex del volum de la màscara que estem mirant en cada crida
+    /// @param movement direcció en el moviment
     void undoMovement(int &a, int &b, int movement);
 
-    /**
-     * Detecta les vores
-     */
-    void detectBorder();
+    /// Genera el polígon a partir de la màscara
+    void computePolygon();
+
+    /// Mètodes auxiliar per la generació del polígon
     void getNextIndex(int direction, int x1, int y1, int &x2, int &y2);
     int getNextDirection(int direction);
     int getInverseDirection(int direction);
     void addPoint(int direction, int x1, int y1, double z1);
     bool isLoopReached();
 
-    /**
-     * Retorna la desviació estàndard dins la regió marcada per la magicSize
-     * @param a, @param b, @param c índex del volum de la màscara que estem mirant
-     */
+    /// Retorna la desviació estàndard dins la regió marcada per la magicSize
+    /// @param a, @param b, @param c índex del volum de la màscara que estem mirant
     double getStandardDeviation(int a, int b, int c);
 
-    /**
-     * Incrementa la mida de l'editor
-     */
-    void increaseMagicSize();
+    /// Comença la generació de la regió màgica
+    void startMagicRegion();
 
-    /**
-     * Decrementa la mida de l'editor
-     */
-    void decreaseMagicSize();
+    /// Calcula la regió definitiva i mostra per pantalla les mesures
+    void closeForm();
+
+    /// Modifica el Magic Factor \sa #m_magicFactor segons el desplaçament del ratolí
+    void modifyMagicFactor();
 
 private slots:
-    /**
-     * Inicialitza diverses dades necessàries per la tool: Valors de m_insideValue, m_outsideValue i m_volumeCont
-     */
+    /// Inicialitza la tool
     void initialize();
 
 private:
-    /// Ens guardem aquest punter per ser més còmode
-    Q2DViewer *m_2DViewer;
-
-    ///Mida de la tool
+    /// Mida de la tool
     int m_magicSize;
-
-    QVector<bool> m_mask;
-
     double m_magicFactor;
 
-    int m_lowerlevel;
-    int m_upperlevel;
+    /// Màscara de la regió que formarà el polígon
+    QVector<bool> m_mask;
 
-    int m_a;
-    int m_b;
-    int m_c;
-    int m_i;
-    int m_ext[6];
+    /// Rang de valors que es tindran en compte pel region growing
+    double m_lowerLevel;
+    double m_upperLevel;
 
-    DrawerText * m_text;
-
-    vtkImageData * m_input;
-
-    /// Polígon principal: és la polígon que ens marca la forma que hem segmentat
-    QPointer<DrawerPolygon> m_mainPolygon;
+    /// Coordenades de món a on s'ha fet el click inicial
+    double m_pickedPosition[3];
 };
 
 }
