91845b8cce79334f0868545d3b0e45f98e1838c3
Marc Ruiz <marc.ruiz@gmail.com>
2011-05-11 11:47:40 +0000

    ENH: Es poden afegir nodes a la funció de transferència d'opacitat escalar des de la interfície gràfica.

--- starviewer/src/core/qopacitytransferfunctiongraphicalview.cpp
+++ starviewer/src/core/qopacitytransferfunctiongraphicalview.cpp
@@ -5,13 +5,15 @@
 #include "qopacitytransferfunctiongraphicalviewnode.h"
 
 #include <cmath>
+#include <typeinfo>
 
 #include <QWheelEvent>
+#include <QtConcurrentFilter>
 
 namespace udg {
 
 QOpacityTransferFunctionGraphicalView::QOpacityTransferFunctionGraphicalView(QWidget *parent)
-    : QGraphicsView(parent)
+    : QGraphicsView(parent), m_state(Ready)
 {
     setFrameShape(QFrame::NoFrame);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
@@ -48,7 +50,6 @@ void QOpacityTransferFunctionGraphicalView::setOpacityTransferFunction(const Opa
             line->setRightNode(node);
             previousNode->setRightLine(line);
             node->setLeftLine(line);
-            line->update();
             scene()->addItem(line);
         }
 
@@ -86,6 +87,52 @@ void QOpacityTransferFunctionGraphicalView::setOpacityTransferFunction(const Opa
     fitInView(rect);
 }
 
+void QOpacityTransferFunctionGraphicalView::mousePressEvent(QMouseEvent *event)
+{
+    QGraphicsView::mousePressEvent(event);
+
+    if (m_state != Ready)
+    {
+        DEBUG_LOG(QString("Mouse press inesperat (estat %1)").arg(m_state));
+        return;
+    }
+
+    if (event->button() == Qt::LeftButton)
+    {
+        if (scene()->selectedItems().isEmpty()) // si no hi ha items seleccionats
+        {
+            m_state = Adding;
+            addNode(mapToScene(event->pos()));
+            QGraphicsView::mousePressEvent(event);  // Al lloro! Tornem a fer el mousePressEvent per poder arrossegar el nou node!
+        }
+        else
+        {
+            //beginMoveNodes();
+        }
+    }
+//    else if (event->button() == Qt::RightButton)
+//    {
+//        removeNode(mapToScene(event->pos()).x());
+//    }
+}
+
+void QOpacityTransferFunctionGraphicalView::mouseMoveEvent(QMouseEvent *event)
+{
+    QGraphicsView::mouseMoveEvent(event);
+}
+
+void QOpacityTransferFunctionGraphicalView::mouseReleaseEvent(QMouseEvent *event)
+{
+    QGraphicsView::mouseReleaseEvent(event);
+
+    if (m_state == Adding)
+    {
+        QGraphicsItem *node = scene()->selectedItems().first();
+        emit nodeAdded(node->x(), node->y());
+        m_state = Ready;
+    }
+}
+
 void QOpacityTransferFunctionGraphicalView::wheelEvent(QWheelEvent *event)
 {
     double scale = pow(2.0, event->delta() / 240.0);
@@ -107,6 +154,90 @@ void QOpacityTransferFunctionGraphicalView::fitInView(const QRectF &rect)
 
     // cridem el mètode del pare per ajustar el zoom
     QGraphicsView::fitInView(rect);
+}
+
+namespace {
+
+bool isNode(const QGraphicsItem *item)
+{
+    return typeid(*item) == typeid(QOpacityTransferFunctionGraphicalViewNode);
+}
+
+bool lessThan(const QGraphicsItem *item1, const QGraphicsItem *item2)
+{
+    return item1->x() < item2->x();
+}
+
+}
+
+void QOpacityTransferFunctionGraphicalView::addNode(const QPointF &position)
+{
+    QOpacityTransferFunctionGraphicalViewNode *node = new QOpacityTransferFunctionGraphicalViewNode();  // (n*)
+    node->setPos(position);
+    node->setToolTip(QString("(%1, %2").arg(position.x()).arg(position.y()));
+    scene()->addItem(node);
+
+    // Busquem els nodes anterior i posterior
+    QList<QGraphicsItem*> items = this->items();    // tots els items (nodes i línies)
+    QtConcurrent::blockingFilter(items, isNode);    // filtrem per quedar-nos només amb els nodes
+    qSort(items.begin(), items.end(), lessThan);    // ordenem per x
+    QList<QGraphicsItem*>::const_iterator it = qBinaryFind(items.begin(), items.end(), node, lessThan); // busquem el nou node (ja l'hem inserit abans)
+    Q_ASSERT(it != items.end());
+    Q_ASSERT(*it == node);
+    QOpacityTransferFunctionGraphicalViewNode *leftNode = 0;    // (ln)
+    QOpacityTransferFunctionGraphicalViewNode *rightNode = 0;   // (rn)
+    if (it != items.begin())    // existeix un node a l'esquerra
+    {
+        leftNode = dynamic_cast<QOpacityTransferFunctionGraphicalViewNode*>(*(it - 1));
+        Q_ASSERT(leftNode);
+    }
+    if (it + 1 != items.end())  // existeix un node a la dreta
+    {
+        rightNode = dynamic_cast<QOpacityTransferFunctionGraphicalViewNode*>(*(it + 1));
+        Q_ASSERT(rightNode);
+    }
+
+    // Busquem les línies anterior i posterior
+    QOpacityTransferFunctionGraphicalViewLine *leftLine = 0;    // (ll)
+    QOpacityTransferFunctionGraphicalViewLine *rightLine = 0;   // (rl)
+    if (leftNode && rightNode)              // ?-(ln)--ll--(rn)-? ==> ?-(ln)--ll--(n*)--rl*--(rn)-?
+    {
+        Q_ASSERT(leftNode->rightLine());
+        Q_ASSERT(leftNode->rightLine()->rightNode() == rightNode);
+        Q_ASSERT(rightNode->leftLine());
+        Q_ASSERT(rightNode->leftLine()->leftNode() == leftNode);
+        leftLine = leftNode->rightLine();
+        rightLine = new QOpacityTransferFunctionGraphicalViewLine();
+        scene()->addItem(rightLine);
+    }
+    else if (leftNode)  // && !rightNode    // ?-(ln) ==> ?-(ln)--ll*--(n*)
+    {
+        Q_ASSERT(!leftNode->rightLine());
+        leftLine = new QOpacityTransferFunctionGraphicalViewLine();
+        scene()->addItem(leftLine);
+    }
+    else if (rightNode) // && !leftNode     // (rn)-? ==> (n*)--rl*--(rn)-?
+    {
+        Q_ASSERT(!rightNode->leftLine());
+        rightLine = new QOpacityTransferFunctionGraphicalViewLine();
+        scene()->addItem(rightLine);
+    }
+
+    // Enllacem bé els nodes i les línies
+    if (leftLine)
+    {
+        leftNode->setRightLine(leftLine);
+        leftLine->setLeftNode(leftNode);
+        leftLine->setRightNode(node);
+        node->setLeftLine(leftLine);
+    }
+    if (rightLine)
+    {
+        node->setRightLine(rightLine);
+        rightLine->setLeftNode(node);
+        rightLine->setRightNode(rightNode);
+        rightNode->setLeftLine(rightLine);
+    }
 }
 
 } // namespace udg
--- starviewer/src/core/qtransferfunctionview.cpp
+++ starviewer/src/core/qtransferfunctionview.cpp
@@ -18,8 +18,15 @@ void QTransferFunctionView::makeConnections()
     connect(m_editor, SIGNAL(transferFunctionChanged(TransferFunction)), SLOT(setTransferFunction(TransferFunction)));
     connect(m_editor, SIGNAL(nameChanged(QString)), SLOT(setName(QString)));
     connect(m_editor, SIGNAL(colorTransferFunctionChanged(ColorTransferFunction)), SLOT(setColorTransferFunction(ColorTransferFunction)));
-    connect(m_editor, SIGNAL(colorPointAdded(double, QColor)), SLOT(addColorPoint(double, QColor)));
+    connect(m_editor, SIGNAL(scalarOpacityTransferFunctionChanged(OpacityTransferFunction)), SLOT(setScalarOpacityTransferFunction(OpacityTransferFunction)));
+//    connect(m_editor, SIGNAL(gradientOpacityTransferFunctionChanged(OpacityTransferFunction)),
+//            SLOT(setGradientOpacityTransferFunction(OpacityTransferFunction)));
+    connect(m_editor, SIGNAL(colorPointAdded(double,QColor)), SLOT(addColorPoint(double,QColor)));
     connect(m_editor, SIGNAL(colorPointRemoved(double)), SLOT(removeColorPoint(double)));
+    connect(m_editor, SIGNAL(scalarOpacityPointAdded(double,double)), SLOT(addScalarOpacityPoint(double,double)));
+    connect(m_editor, SIGNAL(scalarOpacityPointRemoved(double)), SLOT(removeScalarOpacityPoint(double)));
+//    connect(m_editor, SIGNAL(gradientOpacityPointAdded(double,double)), SLOT(addGradientOpacityPoint(double,double)));
+//    connect(m_editor, SIGNAL(gradientOpacityPointRemoved(double)), SLOT(removeGradientOpacityPoint(double)));
 }
 
 } // namespace udg
