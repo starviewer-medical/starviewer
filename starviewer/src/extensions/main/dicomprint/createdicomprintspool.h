7794d40dbf6e967a42cfad8ffb28911459891a6b
Marc Cals <cpmarc@gmail.com>
2011-05-12 17:29:29 +0000

    COMP: Es treu warning que indicava que no es podia esborrar la variable pixelData perquè està declarada com a tipus void*. El que es fa és convertir la variable pixelData a tipus char* i en els llocs on es necessita com a void* se li fa un cast.

--- starviewer/src/extensions/main/dicomprint/createdicomprintspool.h
+++ starviewer/src/extensions/main/dicomprint/createdicomprintspool.h
@@ -26,7 +26,7 @@ private:
 
     void setBasicFilmBoxAttributes();
 
-    bool createHardcopyGrayscaleImage(Image *imageToPrint, const void *pixelData, unsigned long bitmapWidth, unsigned long bitmapHeight, double pixelAspectRatio, const QString &spoolDirectoryPath);
+    bool createHardcopyGrayscaleImage(Image *imageToPrint, const char *pixelData, unsigned long bitmapWidth, unsigned long bitmapHeight, double pixelAspectRatio, const QString &spoolDirectoryPath);
 
     void setImageBoxAttributes();
 
