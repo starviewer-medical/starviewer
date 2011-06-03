0b583d93b5eeb9e71494395b5de226c405278062
Roger Bramon <rogerbramon@gmail.com>
2011-06-03 16:03:10 +0000

    STYLE: Es passen els tabuladors a espais.

--- starviewer/tests/auto/shared/autotest.h
+++ starviewer/tests/auto/shared/autotest.h
@@ -12,46 +12,46 @@ typedef QList<QObject*> TestList;
 
 inline TestList& testList()
 {
-	static TestList list;
-	return list;
+    static TestList list;
+    return list;
 }
 
 inline bool findObject(QObject* object)
 {
-	TestList& list = testList();
-	if (list.contains(object))
-	{
-	    return true;
-	}
-	foreach (QObject* test, list)
-	{
-	    if (test->objectName() == object->objectName())
-	    {
+    TestList& list = testList();
+    if (list.contains(object))
+    {
+        return true;
+    }
+    foreach (QObject* test, list)
+    {
+        if (test->objectName() == object->objectName())
+        {
             return true;
-	    }
-	}
-	return false;
+        }
+    }
+    return false;
 }
 
 inline void addTest(QObject* object)
 {
-	TestList& list = testList();
-	if (!findObject(object))
-	{
-	    list.append(object);
-	}
+    TestList& list = testList();
+    if (!findObject(object))
+    {
+        list.append(object);
+    }
 }
 
 inline int run(int argc, char *argv[])
 {
-	int ret = 0;
+    int ret = 0;
 
-	foreach (QObject* test, testList())
-	{
-	    ret += QTest::qExec(test, argc, argv);
-	}
+    foreach (QObject* test, testList())
+    {
+        ret += QTest::qExec(test, argc, argv);
+    }
 
-	return ret;
+    return ret;
 }
 }
 
