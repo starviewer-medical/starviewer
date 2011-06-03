a3e091c3c84ff9be8d9382c6525aa4495cdbc0cc
Roger Bramon <rogerbramon@gmail.com>
2011-06-03 16:03:19 +0000

    S'afegeix un parametre a l'executable dels autotests que permet guardar en fitxers la sortida dels testos.

--- starviewer/tests/auto/autotests.cpp
+++ starviewer/tests/auto/autotests.cpp
@@ -1,4 +1,8 @@
 #include "autotest.h"
 #include "gtest/gtest.h"
 
+/// A part dels paràmetres que QTest defineix de cada test, se n'han afegint més:
+///     -saveOutputToDir <dirPath>: guarda l'output de cada testsuite a un fitxer del format <nomDelTest>.txt.
+///                                 Requeriments: el directori NO ha d'existir o ha d'estar buit.
+
 TEST_MAIN
--- starviewer/tests/auto/shared/autotest.h
+++ starviewer/tests/auto/shared/autotest.h
@@ -5,6 +5,8 @@
 #include <QList>
 #include <QString>
 #include <QSharedPointer>
+#include <QDir>
+#include <iostream>
 
 namespace AutoTest
 {
@@ -42,13 +44,73 @@ inline void addTest(QObject* object)
     }
 }
 
-inline int run(int argc, char *argv[])
+
+inline int run()
 {
     int ret = 0;
+    QString dirToSaveTests;
 
-    foreach (QObject* test, testList())
+    // Comprovem paràmetres nostres. Si ni ha, els eliminem perquè al executar el test no es queixi.
+    QStringList arguments = qApp->arguments();
+    if (arguments.contains("-saveOutputToDir"))
     {
-        ret += QTest::qExec(test, argc, argv);
+        int argumentPosition = arguments.indexOf("-saveOutputToDir");
+        arguments.removeAt(argumentPosition);
+        dirToSaveTests = arguments.value(argumentPosition);
+        if (dirToSaveTests.isEmpty() || dirToSaveTests.startsWith("-"))
+        {
+            std::cerr << "ERROR: Argument -saveOutputToDir needs a directory path as option" << std::endl;
+            return -1;
+        }
+        else
+        {
+            arguments.removeAt(argumentPosition);
+        }
+
+        QDir dir(dirToSaveTests);
+        if (dir.exists()) // Si és correcte eliminem els paràmetres
+        {
+            if (!dir.entryList(QDir::AllEntries | QDir::NoDotAndDotDot).isEmpty())
+            {
+                std::cerr << "ERROR: The directory to save the ouput files has to be empty." << std::endl;
+                return -1;
+            }
+        }
+        else
+        {
+            if (!dir.mkpath(dir.absolutePath()))
+            {
+                std::cerr << qPrintable(QString("ERROR: Unable to create dir: %1").arg(dir.absolutePath())) << std::endl;
+                return -1;
+            }
+        }
+    }
+
+    // Generem els paràmetres argc i argv correctes sense els nostres paràmetres
+    int modifiedArgc = arguments.count();
+    char *modifiedArgv[modifiedArgc];
+    int i = 0;
+    foreach (const QString &argument, arguments)
+    {
+        char *tempArgument = new char[argument.length()];
+        strcpy(tempArgument, argument.toStdString().c_str());
+        modifiedArgv[i] = tempArgument;
+        i++;
+    }
+
+    foreach (QObject *test, testList())
+    {
+        if (!dirToSaveTests.isEmpty())
+        {
+            freopen(qPrintable(QString("%1/%2.txt").arg(dirToSaveTests).arg(test->objectName())), "w", stdout);
+        }
+        ret += QTest::qExec(test, modifiedArgc, modifiedArgv);
+    }
+
+    //Eliminem la llista de paràmetres
+    for (int i = 0; i < modifiedArgc; i++)
+    {
+        delete modifiedArgv[i];
     }
 
     return ret;
@@ -74,7 +136,7 @@ public:
 int main(int argc, char *argv[]) \
 { \
     QApplication app(argc, argv); \
-    return AutoTest::run(argc, argv); \
+    return AutoTest::run(); \
 }
 
 #endif // AUTOTEST_H
