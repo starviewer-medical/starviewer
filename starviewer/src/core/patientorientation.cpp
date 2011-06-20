b8e77cfcd730812c51c361ed3f81c61ec0464c10
Jesús Spínola <jspinola@gmail.com>
2011-06-20 10:17:26 +0000

    ENH: Afegim nou mètode per assignar la orientació de pacient en format DICOM i els corresponents tests.

--- starviewer/src/core/patientorientation.cpp
+++ starviewer/src/core/patientorientation.cpp
@@ -1,5 +1,9 @@
 #include "patientorientation.h"
 
+#include <QRegExp>
+
+#include "dicomvaluerepresentationconverter.h"
+
 namespace udg {
 
 const QString PatientOrientation::LeftLabel("L");
@@ -8,6 +12,25 @@ const QString PatientOrientation::PosteriorLabel("P");
 const QString PatientOrientation::AnteriorLabel("A");
 const QString PatientOrientation::HeadLabel("H");
 const QString PatientOrientation::FeetLabel("F");
+
+bool PatientOrientation::setDICOMFormattedPatientOrientation(const QString &patientOrientation)
+{
+    if (validateDICOMFormattedPatientOrientationString(patientOrientation))
+    {
+        m_patientOrientationString = patientOrientation;
+        return true;
+    }
+    else
+    {
+        m_patientOrientationString = "";
+        return false;
+    }
+}
+
+QString PatientOrientation::getDICOMFormattedPatientOrientation() const
+{
+    return m_patientOrientationString;
+}
 
 QString PatientOrientation::getOppositeOrientationLabel(const QString &label)
 {
@@ -47,5 +70,37 @@ QString PatientOrientation::getOppositeOrientationLabel(const QString &label)
     }
     return oppositeLabel;
 }
+
+bool PatientOrientation::validateDICOMFormattedPatientOrientationString(const QString &string)
+{
+    // Construim l'expressió regular que ens comprova que la cadena està en el format correcte: Cadena buida, o amb 2 o 3 elements.
+    
+    // Etiquetes vàlides: RLAPHF
+    QString validOrientationLabels = RightLabel + LeftLabel + AnteriorLabel + PosteriorLabel + HeadLabel + FeetLabel;
+    
+    // Expressió per les etiquetes [RLAPHF]+
+    QString validLabelsExpression = "[" + validOrientationLabels + "]+";
+    
+    // Expressió pels separadors 
+    // Hem de posar dos cops seguits DICOMValueRepresentationConverter::ValuesSeparator, ja que tal com diu la documentació de QRegExp:
+    //
+    // Note: The C++ compiler transforms backslashes in strings. To include a \ in a regexp, enter it twice, i.e. \\. 
+    // To match the backslash character itself, enter it four times, i.e. \\\\.
+    //
+    // Llavors cal posar \\\\ perquè l'expressió sigui correcta
+    QString separatorExpression = DICOMValueRepresentationConverter::ValuesSeparator + DICOMValueRepresentationConverter::ValuesSeparator;
+    
+    // L'expressió final: ([RLAPHF]+\\\\[RLAPHF]+(\\\\[RLAPHF]+)?)*
+    // És a dir, 2 o 3 ítems amb etiquetes vàlides separats per \\ 
+    QString regExpString = "(" + validLabelsExpression + separatorExpression + validLabelsExpression +
+        "(" + separatorExpression + validLabelsExpression + ")?)*";
+
+    QRegExp validStringExpression(regExpString);
+    return validStringExpression.exactMatch(string);
+}
+
+
+
+
 
 } // End namespace udg
