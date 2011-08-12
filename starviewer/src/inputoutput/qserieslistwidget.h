fc047de95cc0ee5208eb72e8211db1c00359af2e
Marc Cals <cpmarc@gmail.com>
2011-08-12 18:15:56 +0000

    ENH: Es fa refactoring sobre la classe QSeriesListWidget perquè sigui més comprensible i fàcil de mantenir.

--- starviewer/src/inputoutput/qserieslistwidget.h
+++ starviewer/src/inputoutput/qserieslistwidget.h
@@ -2,12 +2,13 @@
 #define UDGSERIESLISTWIDGET_H
 
 #include "ui_qserieslistwidgetbase.h"
+
 namespace udg {
 
 class Series;
 
 /**
-    Aquesta classe és un widget millorat del ListWidget de QT, fet  modificat per poder mostrar les sèries d'un estudi.
+    Ens mostrar una previsualització en forma de thumbnail de les sèries passades al Widget.
   */
 class QSeriesListWidget : public QWidget, private Ui::QSeriesListWidgetBase {
 Q_OBJECT
@@ -18,11 +19,6 @@ public:
     /// Insereix l'informació d'una sèrie
     void insertSeries(QString studyInstanceUID, Series *series);
 
-    QString getCurrentSeriesUID();
-
-    /// Retorna l'estudi uid de la sèrie seeleccionada
-    QString getCurrentStudyUID();
-
     /// Esborra de la llista la serie amb el UID passat per paràmetre
     void removeSeries(const QString &seriesInstanceUID);
 
@@ -30,18 +26,22 @@ public slots:
     /// Estableix quina és la série seleccionada
     void setCurrentSeries(const QString &seriesUID);
 
+    /// Neteja el widget i el deixa sense cap thumbnail de Series
     void clear();
 
 signals:
     /// Quan seleccionem una sèrie emeiteix una signal per a que el QStudyTreeWidget, tingui seleccionada la mateixa sèrie
-    void selectedSeriesIcon(const QString studyInstanceUID, const QString seriesInstanceUID);
+    void seriesThumbnailClicked(const QString studyInstanceUID, const QString seriesInstanceUID);
 
     /// Quan es fa doble click emiteix un signal, perquè la sèrie sigui visualitzada
-    void viewSeriesIcon();
+    void seriesThumbnailDoubleClicked(QString studyInstanceUID, QString seriesInstanceUID);
 
 private:
     /// Crea les connexions
     void createConnections();
+
+    /// Retorna la descripció pel thumbnail de la sèrie
+    QString getSeriesThumbnailDescription(Series *series);
 
 private slots:
     /// Slot que s'activa quan s'ha fet click sobre un thumbnail
@@ -51,16 +51,15 @@ private slots:
     void seriesDoubleClicked(QString IDThumbnail);
 
 private:
+    //Guardem per cada sèrie a quin estudi pertany
+    QHash<QString, QString> m_studyInstanceUIDBySeriesInstanceUID;
 
-    /// Guardem per cada sèrie a quin estudi pertany
-    QHash<QString, QString> m_HashSeriesStudy;
-    /// Modalitats de sèries que no són images, com (KO, PR, SR)
-    QStringList m_nonDicomImageSeriesList;
-    // Indica a quina ha estat la última fila que hem inseritat una sèrie que era una imatge
-    int m_lastInsertedImageRow;
-
+    //Modalitats de sèries que no són images, com (KO, PR, SR)
+    QStringList m_DICOMModalitiesNonImage;
+    //Indica a quina ha estat la última fila que hem inseritat una sèrie que era una imatge
+    int m_positionOfLastInsertedThumbnail;
 };
 
-};
+}
 
 #endif
