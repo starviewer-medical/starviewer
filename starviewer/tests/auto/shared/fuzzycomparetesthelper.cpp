c76ca93caf1496fc441cfaef641c33d69ea01a23
Jesús Spínola <jspinola@gmail.com>
2011-09-08 11:23:02 +0000

    ENH: Afegim una funció fuzzy compare per comparar QVector<double>

--- starviewer/tests/auto/shared/fuzzycomparetesthelper.cpp
+++ starviewer/tests/auto/shared/fuzzycomparetesthelper.cpp
@@ -8,4 +8,22 @@ bool FuzzyCompareTestHelper::fuzzyCompare(const double &v1, const double &v2, co
     return (diff < epsilon) && (diff > -epsilon);
 }
 
+bool FuzzyCompareTestHelper::fuzzyCompare(const QVector<double> &v1, const QVector<double> &v2, const double &epsilon)
+{
+    if (v1.size() != v2.size())
+    {
+        return false;
+    }
+
+    int i = 0;
+    bool equal = true;
+    while (i < v1.size() && equal)
+    {
+        equal = fuzzyCompare(v1.at(i), v2.at(i), epsilon);
+        ++i;
+    }
+
+    return equal;
+}
+
 }
--- starviewer/tests/auto/shared/fuzzycomparetesthelper.h
+++ starviewer/tests/auto/shared/fuzzycomparetesthelper.h
@@ -1,6 +1,8 @@
 #ifndef FUZZYCOMPARETESTHELPER_H
 #define FUZZYCOMPARETESTHELPER_H
 
+#include <QVector>
+
 namespace testing {
 
 /// Classe que contindrà les funcions de comparació fuzzy que necessitem
@@ -9,6 +11,10 @@ public:
     /// Fa una comparació fuzzy de dos valors double. Tenim un tercer paràmetre opcional epsilon que ens 
     /// permetrà fixar la precisió que volem en la comparació.
     static bool fuzzyCompare(const double &v1, const double &v2, const double &epsilon = 0.000000001);
+
+    /// Fa una comparació fuzzy de dos valors QVector<double>. Tots dos hauran de ser de la mateixa longitud.
+    /// Tenim un tercer paràmetre opcional epsilon que ens permetrà fixar la precisió que volem en la comparació.
+    static bool fuzzyCompare(const QVector<double> &v1, const QVector<double> &v2, const double &epsilon = 0.000000001);
 };
 
 }
