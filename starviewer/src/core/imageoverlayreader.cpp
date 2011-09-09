1ea0a3a3600c5753300f1352c7a5dc639c733962
Jesús Spínola <jspinola@gmail.com>
2011-09-09 10:54:30 +0000

    BUG: Fem que getGDCMImageFromFile() retorni un objecte gdcm::Image i no un punter, ja que sinó l'objecte retornat no queda copiat i ens quedem amb una referència a un gdcm::Image buit. Adaptem els corresponents tests al nou comportament.

--- starviewer/src/core/imageoverlayreader.cpp
+++ starviewer/src/core/imageoverlayreader.cpp
@@ -24,17 +24,13 @@ bool ImageOverlayReader::read()
 {
     m_overlaysList.clear();
 
-    gdcm::Image *image = getGDCMImageFromFile(m_filename);
-    if (!image)
-    {
-        return false;
-    }
+    gdcm::Image image = getGDCMImageFromFile(m_filename);
     
-    for (size_t overlayIndex = 0; overlayIndex < image->GetNumberOfOverlays(); ++overlayIndex)
+    for (size_t overlayIndex = 0; overlayIndex < image.GetNumberOfOverlays(); ++overlayIndex)
     {
-        m_overlaysList << ImageOverlay::fromGDCMOverlay(image->GetOverlay(overlayIndex));
+        m_overlaysList << ImageOverlay::fromGDCMOverlay(image.GetOverlay(overlayIndex));
     }
-    
+
     return true;
 }
 
@@ -43,7 +39,7 @@ QList<ImageOverlay> ImageOverlayReader::getOverlays() const
     return m_overlaysList;
 }
 
-gdcm::Image* ImageOverlayReader::getGDCMImageFromFile(const QString &filename)
+gdcm::Image ImageOverlayReader::getGDCMImageFromFile(const QString &filename)
 {
     gdcm::ImageReader imageReader;
     imageReader.SetFileName(qPrintable(filename));
@@ -51,10 +47,10 @@ gdcm::Image* ImageOverlayReader::getGDCMImageFromFile(const QString &filename)
     {
         ERROR_LOG("Ha fallat la lectura del fitxer: " + filename + " [ImageOverlayReader]");
         DEBUG_LOG("Ha fallat la lectura del fitxer: " + filename);
-        return 0;
+        return gdcm::Image();
     }
     
-    return &imageReader.GetImage();
+    return imageReader.GetImage();
 }
 
 }
