5ee650ea71681ac65ba413f6d76218615f7fa71b
Roger Bramon <rogerbramon@gmail.com>
2011-09-16 12:35:08 +0000

    BUG: El destructor havia de ser public perquè sino no podrem eliminar els objectes d'aquest tipus.

--- starviewer/src/core/diagnosistest.h
+++ starviewer/src/core/diagnosistest.h
@@ -7,13 +7,13 @@ namespace udg {
 
 class DiagnosisTest {
 public:
+    /// Destructor per defecte. És virtual per que les subclasses puguin reimplementar-lo
+    virtual ~DiagnosisTest(){}
     /// Mètode principal de la interfície. No està implementat, i es força a les subclasses a fer-ho.
     virtual DiagnosisTestResult run() = 0;
 protected:
     /// Constructor per defecte protected perque no es pugui instanciar objectes DiagnosisTest
     DiagnosisTest(){}
-    /// Destructor per defecte. És virtual per que les subclasses puguin reimplementar-lo
-    virtual ~DiagnosisTest(){};
 };
 
 }
