4bcfba142d052da5529bb1e88e49c782a93ce731
Roger Bramon <rogerbramon@gmail.com>
2011-09-16 12:35:06 +0000

    ENH: Posem el constructor protected perquè no es puguin instanciar objectes d'aquest tipus.

--- starviewer/src/core/diagnosistest.h
+++ starviewer/src/core/diagnosistest.h
@@ -10,6 +10,8 @@ public:
     /// Mètode principal de la interfície. No està implementat, i es força a les subclasses a fer-ho.
     virtual DiagnosisTestResult run() = 0;
 protected:
+    /// Constructor per defecte protected perque no es pugui instanciar objectes DiagnosisTest
+    DiagnosisTest(){}
     /// Destructor per defecte. És virtual per que les subclasses puguin reimplementar-lo
     virtual ~DiagnosisTest(){};
 };
