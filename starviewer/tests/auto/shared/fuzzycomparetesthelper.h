a9f5dbbed6d3985c4b1b8a125d7a7a5c19560c38
Marc Ruiz <marc.ruiz@gmail.com>
2011-10-19 11:09:50 +0000

    ENH: S'afegeixen tests per al mètode getStackDirection de Volume i per a l'eina d'angle obert.

--- starviewer/tests/auto/shared/fuzzycomparetesthelper.h
+++ starviewer/tests/auto/shared/fuzzycomparetesthelper.h
@@ -3,6 +3,8 @@
 
 #include <QVector>
 
+class QVector3D;
+
 namespace testing {
 
 /// Classe que contindrà les funcions de comparació fuzzy que necessitem
@@ -15,6 +17,10 @@ public:
     /// Fa una comparació fuzzy de dos valors QVector<double>. Tots dos hauran de ser de la mateixa longitud.
     /// Tenim un tercer paràmetre opcional epsilon que ens permetrà fixar la precisió que volem en la comparació.
     static bool fuzzyCompare(const QVector<double> &v1, const QVector<double> &v2, const double &epsilon = 0.000000001);
+
+    /// Fa una comparació fuzzy de dos valors QVector3D.
+    /// Tenim un tercer paràmetre opcional epsilon que ens permetrà fixar la precisió que volem en la comparació.
+    static bool fuzzyCompare(const QVector3D &v1, const QVector3D &v2, const double &epsilon = 0.000000001);
 };
 
 }
