449b0bf4ba4c8cfc6ee34eb989361cf9bd253745
Marc Puigdemont <fraijilverto@hotmail.com>
2011-11-21 10:58:45 +0000

    ENH: S'afegeix el test de diagnosi per comprovar la versió de l'aplicació (ApplicationVersionTest), i el seu unit testing.

---
+++ starviewer/src/core/applicationversiontest.cpp
@@ -0,0 +1,74 @@
+// Starviewer
+#include "applicationversiontest.h"
+#include "starviewerapplication.h"
+#include "applicationupdatechecker.h"
+// Qt
+#include <QString>
+#include <QThread>
+
+namespace udg {
+
+ApplicationVersionTest::ApplicationVersionTest(QObject *parent)
+ : DiagnosisTest(parent)
+{
+}
+
+ApplicationVersionTest::~ApplicationVersionTest()
+{
+}
+
+DiagnosisTestResult ApplicationVersionTest::run()
+{
+    /// Fer el check online. Després de cridar aquest mètode, les variables m_onlineCheckOk i m_newVersionAvailable prenen valor.
+    onlineCheck();
+
+    DiagnosisTestResult::DiagnosisTestResultState state;
+    QString description = "";
+    QString solution = "";
+    if (m_onlineCheckOk)
+    {
+        if (m_newVersionAvailable)
+        {
+            state = DiagnosisTestResult::Warning;
+            description = "There is a new version";
+            solution = "Update";
+        }
+        else
+        {
+            state = DiagnosisTestResult::Ok;
+        }
+    }
+    else
+    {
+        // En mode Debug retorna error, ja que la caden de text de la versió de l'aplicació que s'envia al webservice conté -devel
+        state = DiagnosisTestResult::Error;
+        description = m_olineCheckerErrorDescription;
+        // TODO Proposar una solució quan hi ha hagut error en el check online de les release notes
+        solution = "";
+    } 
+
+    return DiagnosisTestResult(state, description, solution);
+}
+
+void ApplicationVersionTest::onlineCheck()
+{
+    ApplicationUpdateChecker onlineChecker;
+    QThread onlineCheckerThread;
+    // Moure l'onlineChecker a un altre thread
+    onlineChecker.moveToThread(&onlineCheckerThread);
+    // La conexió de checkFinished s'ha de fer en mode DirectConnection per tal de què s'executi correctament al thread que toca
+    connect(&onlineChecker, SIGNAL(checkFinished()), &onlineCheckerThread, SLOT(quit()), Qt::DirectConnection);
+    // Per tal d'executar el mètode check for updates a l'altre thread, el que farem és connectar-lo a un signal, i fer un emit quan sigui el moment
+    connect(this, SIGNAL(startCheckForUpdates()), &onlineChecker, SLOT(checkForUpdates()));
+
+    // Iniciar el thread, cridar el checkForUpdates i esperar síncronament a qué acabi
+    onlineCheckerThread.start();
+    emit startCheckForUpdates();
+    onlineCheckerThread.wait();
+
+    m_onlineCheckOk = onlineChecker.isOnlineCheckOk();
+    m_newVersionAvailable = onlineChecker.isNewVersionAvailable();
+    m_olineCheckerErrorDescription = onlineChecker.getErrorDescription();
+}
+
+}
---
+++ starviewer/src/core/applicationversiontest.h
@@ -0,0 +1,40 @@
+#ifndef UDGAPPLICATIONVERSIONTEST_H
+#define UDGAPPLICATIONVERSIONTEST_H
+
+#include "diagnosistest.h"
+#include "diagnosistestresult.h"
+#include "diagnosistestfactoryregister.h"
+
+#include <QObject>
+
+namespace udg {
+
+class ApplicationVersionTest : public DiagnosisTest {
+Q_OBJECT
+public:
+    /// Constructor per defecte
+    ApplicationVersionTest(QObject *parent = 0);
+    /// Destructor
+    ~ApplicationVersionTest();
+    /// Mètode implementat de la interfície DiagnosisTest.
+    /// DiagnosisTestResult valdrà OK quan l'aplicació estigui actualitzada, Warning si hi ha alguna actualització disponible,
+    /// i Error si hi ha hagut algun error.
+    DiagnosisTestResult run();
+
+signals:
+    void startCheckForUpdates();
+
+protected:
+    virtual void onlineCheck();
+
+protected:
+    bool m_onlineCheckOk;
+    bool m_newVersionAvailable;
+    QString m_olineCheckerErrorDescription;
+};
+
+static DiagnosisTestFactoryRegister<ApplicationVersionTest> registerApplicationVersionTest("ApplicationVersionTest");
+
+}
+
+#endif
