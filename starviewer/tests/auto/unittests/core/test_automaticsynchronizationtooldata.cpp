79c69567ee3df248ce3501510cc5f5e65e509017
Ester Bonmatí <esterbonmati+starviewer@gmail.com>
2011-11-24 09:39:48 +0000

    ENH: Millora del test del mètode setPosition_Works() de la automaticSincronizationToolData. Ara té en compte el desplaçament afegit.

--- starviewer/tests/auto/unittests/core/test_automaticsynchronizationtooldata.cpp
+++ starviewer/tests/auto/unittests/core/test_automaticsynchronizationtooldata.cpp
@@ -22,10 +22,12 @@ void test_AutomaticSynchronizationToolData::setPosition_Works_data()
     QTest::addColumn<QString>("frameOfReferenceUID_1");
     QTest::addColumn<QString>("view_1");
     QTest::addColumn<double*>("position_1");
+    QTest::addColumn<double>("displacement_1");
 
     QTest::addColumn<QString>("frameOfReferenceUID_2");
     QTest::addColumn<QString>("view_2");
     QTest::addColumn<double*>("position_2");
+    QTest::addColumn<double>("displacement_2");
 
     QTest::addColumn<double*>("result");
 
@@ -34,15 +36,19 @@ void test_AutomaticSynchronizationToolData::setPosition_Works_data()
     position_1[1] = 2.0;
     position_1[2] = 3.0;
 
+    double displacement1 = 2.6;
+
     double *position_2 = new double[3];
     position_2[0] = 10.0; 
     position_2[1] = 20.0;
     position_2[2] = 30.0;
 
-    QTest::newRow("same UID, same view") << "1" << "VIEW_1" << position_1 << "1" << "VIEW_1" << position_2; 
-    QTest::newRow("same UID, different view") << "1" << "VIEW_1" << position_1 << "1" << "VIEW_2" << position_2;
-    QTest::newRow("different UID, same view") << "1" << "VIEW_1" << position_1 << "2" << "VIEW_1" << position_2;
-    QTest::newRow("different UID, different view") << "1" << "VIEW_1" << position_1 << "2" << "VIEW_2" << position_2;
+    double displacement2 = 5.2;
+
+    QTest::newRow("same UID, same view") << "1" << "VIEW_1" << position_1 << displacement1 << "1" << "VIEW_1" << position_2 << displacement2; 
+    QTest::newRow("same UID, different view") << "1" << "VIEW_1" << position_1 << displacement1 << "1" << "VIEW_2" << position_2 << displacement2; 
+    QTest::newRow("different UID, same view") << "1" << "VIEW_1" << position_1 << displacement1 << "2" << "VIEW_1" << position_2 << displacement2; 
+    QTest::newRow("different UID, different view") << "1" << "VIEW_1" << position_1 << displacement1 << "2" << "VIEW_2" << position_2 << displacement2; 
 }
 
 void test_AutomaticSynchronizationToolData::setPosition_Works()
@@ -50,22 +56,40 @@ void test_AutomaticSynchronizationToolData::setPosition_Works()
     QFETCH(QString, frameOfReferenceUID_1);
     QFETCH(QString, view_1);
     QFETCH(double*, position_1);
+    QFETCH(double, displacement_1);
     QFETCH(QString, frameOfReferenceUID_2);
     QFETCH(QString, view_2);
     QFETCH(double*, position_2);
+    QFETCH(double, displacement_2);
 
     AutomaticSynchronizationToolData *toolData = new AutomaticSynchronizationToolData();
 
-    toolData->setPosition(frameOfReferenceUID_1, view_1, position_1, 0);
-    toolData->setPosition(frameOfReferenceUID_2, view_2, position_2, 0);
+    toolData->setPosition(frameOfReferenceUID_1, view_1, position_1, displacement_1);
+    
+    QCOMPARE(toolData->hasPosition(frameOfReferenceUID_1, view_1), true);
+    bool same = (frameOfReferenceUID_1 == frameOfReferenceUID_2) && (view_1 == view_2);
+    QCOMPARE(toolData->hasPosition(frameOfReferenceUID_2, view_2), same);
+
+    double *savedPosition = toolData->getPosition(frameOfReferenceUID_1, view_1);
+    QCOMPARE(savedPosition[0], position_1[0]);
+    QCOMPARE(savedPosition[1], position_1[1]);
+    QCOMPARE(savedPosition[2], position_1[2]);
+    
+    double displacement = toolData->getDisplacement();
+    QCOMPARE(displacement, displacement_1);
+
+    toolData->setPosition(frameOfReferenceUID_2, view_2, position_2, displacement_2);
 
     QCOMPARE(toolData->hasPosition(frameOfReferenceUID_1, view_1), true);
     QCOMPARE(toolData->hasPosition(frameOfReferenceUID_2, view_2), true);
 
-    double *savedPosition = toolData->getPosition(frameOfReferenceUID_2, view_2);
+    savedPosition = toolData->getPosition(frameOfReferenceUID_2, view_2);
     QCOMPARE(savedPosition[0], position_2[0]);
     QCOMPARE(savedPosition[1], position_2[1]);
     QCOMPARE(savedPosition[2], position_2[2]);
+    
+    displacement = toolData->getDisplacement();
+    QCOMPARE(displacement, displacement_2);
 
     delete toolData;
 }
