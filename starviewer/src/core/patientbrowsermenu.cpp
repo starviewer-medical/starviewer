45743fbcd79e3739117b5f47cd11102fe0acc53c
Jesús Spínola <jspinola@gmail.com>
2011-12-20 13:37:19 +0000

    ENH: Fem que els estudis al menú de pacient quedin ordenats de més recent a més antic (#1743)

--- starviewer/src/core/patientbrowsermenu.cpp
+++ starviewer/src/core/patientbrowsermenu.cpp
@@ -23,12 +23,22 @@ PatientBrowserMenu::~PatientBrowserMenu()
 {
 }
 
+// Mètode per indicar a qSort com ordenar una llista d'estudis de més recent a més antic
+bool studyNewerThan(Study *study1, Study *study2)
+{
+    return study1->getDateTime() > study2->getDateTime();
+}
+
 void PatientBrowserMenu::setPatient(Patient *patient)
 {
     createWidgets();
     QString caption;
     QString label;
-    foreach (Study *study, patient->getStudies())
+    // TODO Hauríem de tenir els mètodes perquè Patient ens retornés els estudis ordenats en 
+    // ascendent o descendent en comptes de fer-ho així
+    QList<Study*> descendingSortedStudies = patient->getStudies();
+    qSort(descendingSortedStudies.begin(), descendingSortedStudies.end(), studyNewerThan);
+    foreach (Study *study, descendingSortedStudies)
     {
         // Extreiem el caption de l'estudi
         caption = tr("Study %1 : %2 [%3] %4")
