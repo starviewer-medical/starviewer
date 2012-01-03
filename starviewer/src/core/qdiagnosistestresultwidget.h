5ba6a76905c5795698fa58c1876d4e694a369e3b
Marc Puigdemont <fraijilverto@hotmail.com>
2012-01-03 12:45:20 +0000

    ENH: S'afegeix una funcionalitat per calcular manualment la mida que ocupen els resultats dels tests de diagnosi a QDiagnosisTest. D'aquesta manera, els labels amb word wrap es mostren correctament.

--- starviewer/src/core/qdiagnosistestresultwidget.h
+++ starviewer/src/core/qdiagnosistestresultwidget.h
@@ -30,6 +30,9 @@ public:
     /// Indica si es pot expandir. Es poden expandir tots excepte els que mostra un DiagnosisTestResult que no ha fallat
     bool isExpandable();
 
+    // Assignar la mida del widget pare per poder calcular la mida correcte dels labels amb wordwrap
+    void setParentWidgetWidth(int parentWidgetWidth);
+
 signals:
     ///Signal que s'emet quan s'ha canviat la mida del Widget
     void clicked(QDiagnosisTestResultWidget *);
@@ -37,6 +40,8 @@ signals:
 protected:
     /// Comprova si s'ha fet click al frame que mostra la descripció del test, si és així es mostren/s'amaguen la drescripció de l'error del test i la solució
     void mouseReleaseEvent (QMouseEvent * event);
+    /// Retorna la mida adequada del widget
+    QSize sizeHint() const;
 
 private:
 
@@ -46,10 +51,20 @@ private:
     ///Indica si el click s'ha fet a l'àrea del Frame que mostrar la descripció del test
     bool clickIsInAreaTestDescriptionFrame(QMouseEvent *event);
 
+    /// Calcula quantes línies de text ocupa un QString en un QLabel amb word wrap
+    int countNumberOfLinesWithWordWrap(QTextEdit *textEdit) const;
+
 private:
     bool m_isExpanded;
     bool m_isExpandable;
 
+    /// Nombre de línies que ocupa la descripció del resultat del test en un label amb word wrap
+    int m_diagnosisTestResultDescriptionLines;
+    /// Nombre de línies que ocupa la solució del resultat del test en un label amb word wrap
+    int m_diagnosisTestResultSolutionLines;
+
+    // Mida del widget pare
+    int m_parentWidgetWidth;
 };
 
 }
