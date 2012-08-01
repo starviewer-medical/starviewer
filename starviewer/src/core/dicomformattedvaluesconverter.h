93a07a659e52f5abab1fb1515d9ffa5cb5193d51
Jesús Spínola <jspinola@gmail.com>
2012-08-01 09:24:48 +0000

    ﻿ENH: Millorem el comportament pel cas en que el nombre de window widths i window centers difereix

--- starviewer/src/core/dicomformattedvaluesconverter.h
+++ starviewer/src/core/dicomformattedvaluesconverter.h
@@ -18,7 +18,8 @@ public:
 
     /// Donats els valors en string de window width/center i explanation construeix la llista d'objectes Window Level pertinent
     /// Si els valors de les cadenes windowWidthString i windowCenterString no són Decimal Strings es retornarà una llista buida
-    /// Si el nombre d'elements de windowWidthString i windowCenterString no és el mateix es retornarà una llista buida
+    /// Si el nombre d'elements de windowWidthString i windowCenterString no és el mateix es crearà una llista que tingui tants
+    /// elements com la llista amb menys elements. Els elements de la llista més gran seran descartats
     /// explanationString no cal que tingui el mateix nombre d'elements que windowWidthString i windowCenterString, ja que es considera opcional.
     /// En cas que el nombre d'elements d'explanationString difereixi, només s'afegiran aquells que estiguin dins del rang del nombre de window levels
     static QList<WindowLevel> parseWindowLevelValues(const QString &windowWidthString, const QString &windowCenterString, const QString &explanationString = QString());
