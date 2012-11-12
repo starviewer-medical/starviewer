809dca3c18d9831755724cda443d60a880869430
Jesús Spínola <jspinola@gmail.com>
2012-11-12 12:16:00 +0000

    ﻿COMP: Fem que els mètodes siguin const

--- starviewer/src/core/screenlayout.h
+++ starviewer/src/core/screenlayout.h
@@ -35,20 +35,20 @@ public:
     
     /// Ens indica quina és la pantalla que es troba a la dreta/esquerra de la pantalla amb ID indicada.
     /// Retorna -1 si no n'hi ha cap pantalla en el costat indicat
-    int getScreenOnTheRightOf(int screenID);
-    int getScreenOnTheLeftOf(int screenID);
-    
+    int getScreenOnTheRightOf(int screenID) const;
+    int getScreenOnTheLeftOf(int screenID) const;
+
     /// Indica si la primera pantalla està per sobre de la segona. Comprova que el bottom de la primera estigui per sobre del top de la segona.
-    bool isOver(int screen1, int screen2);
+    bool isOver(int screen1, int screen2) const;
     
     /// Indica si la primera pantalla està per sota de la segona. Comprova que el top de la primera estigui per sota del bottom de la segona.
-    bool isUnder(int screen1, int screen2);
+    bool isUnder(int screen1, int screen2) const;
     
     /// Indica si la primera pantalla està a l'esquerra de la segona. Comprova que el right de la primera sigui menor que el left de la segona.
-    bool isOnLeft(int screen1, int screen2);
+    bool isOnLeft(int screen1, int screen2) const;
     
     /// Indica si la primera pantalla està a la dreta de la segona. Comprova que el left de la primera sigui major que el right de la segona.
-    bool isOnRight(int screen1, int screen2);
+    bool isOnRight(int screen1, int screen2) const;
 
 private:
     /// Ens retorna l'índex de la pantalla amb ID screenID. Si no existeix, l'índex serà -1
