38709e7d0f9ba15b4573dfc82696a71d67976657
Jesús Spínola <jspinola@gmail.com>
2012-11-30 09:26:50 +0000

    ﻿ENH: Afegim primera versió de la classe Study Layout Mapper que servirà per distribuir les imatges d'un o varis estudis en un layout segons la configuració que volguem aplicar

---
+++ starviewer/src/core/studylayoutmapper.h
@@ -0,0 +1,29 @@
+#ifndef UDGSTUDYLAYOUTMAPPER_H
+#define UDGSTUDYLAYOUTMAPPER_H
+
+#include <QList>
+#include <QPair>
+
+namespace udg {
+
+class StudyLayoutConfig;
+class ViewersLayout;
+class Patient;
+class Volume;
+
+class StudyLayoutMapper {
+public:
+    StudyLayoutMapper();
+    ~StudyLayoutMapper();
+
+    /// Aplica la configuració sobre el layout amb els estudis del pacient donats
+    void applyConfig(const StudyLayoutConfig &config, ViewersLayout *layout, Patient *patient);
+
+private:
+    /// Donada una llista de volums amb la corresponent llesca, els col·loca al layout segons la configuració donada
+    void placeImagesInCurrentLayout(const QList<QPair<Volume*, int> > &volumesToPlace, const StudyLayoutConfig &config, ViewersLayout *layout);
+};
+
+} // End namespace udg
+
+#endif
