04b561d4c7eb1b4b462251f51f13e525c7f1f62d
Jesús Spínola <jspinola@gmail.com>
2012-11-30 09:26:36 +0000

    ﻿ENH: Fem que ChangeSliceQViewerCommand pugui escollir en quina llesca situar el visor, no només la primera, central i última

--- starviewer/src/core/changesliceqviewercommand.h
+++ starviewer/src/core/changesliceqviewercommand.h
@@ -13,10 +13,14 @@ class Q2DViewer;
 class ChangeSliceQViewerCommand : public QViewerCommand {
 Q_OBJECT
 public:
-    enum SlicePosition { MinimumSlice, MiddleSlice, MaximumSlice };
+    enum SlicePosition { MinimumSlice, MiddleSlice, MaximumSlice, CustomSlice };
 
     /// Constructor que permet especificar en quina posició volem situar el viewer sense haver de saber quantes imatges té el volume
     ChangeSliceQViewerCommand(Q2DViewer *viewer, SlicePosition slice, QObject *parent = 0);
+
+    /// Constructor que permet especificar lliurement en quina llesca volem situar el volum. En cas que la llesca indicada estigui per sota del mínim
+    /// la llesca assignada serà la mínima, i en cas que estigui per sobre del màxim, s'assignarà la màxima
+    ChangeSliceQViewerCommand(Q2DViewer *viewer, int slice, QObject *parent = 0);
 
 public slots:
     void execute();
@@ -24,6 +28,7 @@ public slots:
 private:
     Q2DViewer *m_viewer;
     SlicePosition m_slicePosition;
+    int m_customSliceNumber;
 };
 
 }
