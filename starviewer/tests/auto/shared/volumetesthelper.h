c0f13f220a7a0f516f32a21f468dbc6cb9fa95a2
Marc Ruiz <marc.ruiz@gmail.com>
2013-01-04 11:45:31 +0000

    COMP: M'havia oblidat d'incloure aquests canvis al commit r8752.

--- starviewer/tests/auto/shared/volumetesthelper.h
+++ starviewer/tests/auto/shared/volumetesthelper.h
@@ -20,6 +20,9 @@ public:
     /// Torna un Volume amb el num d'imatges, fases i imatges, a més d'assignar-li al vtkImageData l'origin, spacing, dimensions i l'extent passats per paràmetre
     static udg::Volume* createVolumeWithParameters(int numberOfImages, int numberOfPhases, int numberOfSlicesPerPhase, double origin[3], double spacing[3], int dimensions[3], int extent[6]);
 
+    /// Torna un Volume multiframe (amb més d'una imatge i totes amb el mateix path) amb el número d'imatges, fase i imatges per fase en cada sèrie indicat.
+    static udg::Volume* createMultiframeVolume(int numberOfImages = 2, int numberOfPhases = 1, int numberOfSlicesPerPhase = 1);
+
     /// Elimina les imatges del volum cridant el destructor de l'estructura Patient/Study/Series/Image que pertany a cada imatge
     static void cleanUp(udg::Volume *volume);
 };
