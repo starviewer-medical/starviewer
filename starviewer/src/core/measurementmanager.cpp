d663863b55c46e7ccb4c07115374db1839be0440
Jesús Spínola <jspinola@gmail.com>
2013-05-09 10:57:56 +0000

    ﻿ENH: Added two new methods for proper measurement computation. This methods enable the correction of the measure when pixel spacing values on the image differ from the ones on the Volume

--- starviewer/src/core/measurementmanager.cpp
+++ starviewer/src/core/measurementmanager.cpp
@@ -1,6 +1,8 @@
 #include "measurementmanager.h"
 
+#include "drawerline.h"
 #include "image.h"
+#include "mathtools.h"
 
 namespace udg {
 
@@ -42,6 +44,49 @@ QString MeasurementManager::getMeasurementUnitsAsQString(Image *image)
     }
 
     return units;
+}
+
+double* MeasurementManager::amendCoordinate(double coordinate[3], double dataSpacing[3], Image *image)
+{
+    double *amendedCoordinate = new double[3];
+
+    if (!image)
+    {
+        memcpy(amendedCoordinate, coordinate, sizeof(double) * 3);
+        return amendedCoordinate;
+    }
+
+    const double *pixelSpacing = image->getPixelSpacing();
+    double multiplierFactor[2];
+    
+    if (pixelSpacing[0] == 0.0 && pixelSpacing[1] == 0.0)
+    {
+        multiplierFactor[0] = multiplierFactor[1] = 1.0;
+        
+        amendedCoordinate[2] = coordinate[2] / dataSpacing[2];
+    }
+    else
+    {
+        multiplierFactor[0] = pixelSpacing[0];
+        multiplierFactor[1] = pixelSpacing[1]; 
+
+        amendedCoordinate[2] = coordinate[2];
+    }
+    
+    for (int i = 0; i < 2; ++i)
+    {
+        amendedCoordinate[i] = coordinate[i] / dataSpacing[i] * multiplierFactor[i];
+    }
+    
+    return amendedCoordinate;
+}
+
+double MeasurementManager::computeDistance(DrawerLine *line, Image *image, double dataSpacing[3])
+{
+    double *p1 = amendCoordinate(line->getFirstPoint(), dataSpacing, image);
+    double *p2 = amendCoordinate(line->getSecondPoint(), dataSpacing, image);
+
+    return MathTools::getDistance3D(p1, p2);
 }
 
 }; // End namespace udg
--- starviewer/src/core/measurementmanager.h
+++ starviewer/src/core/measurementmanager.h
@@ -5,6 +5,7 @@
 
 namespace udg {
 
+class DrawerLine;
 class Image;
 
 class MeasurementManager {
@@ -14,6 +15,15 @@ public:
     /// Returns the corresponding measurement units for the given image
     static MeasurementUnitsType getMeasurementUnits(Image *image);
     static QString getMeasurementUnitsAsQString(Image *image);
+
+    /// Given a coordinate, returns an amended coordinate depending on the spacing of the image data and the actual pixel spacing parameters from its image
+    /// If there's no pixel spacing information on the image, coordinate will be normalized upon dataSpacing
+    /// If image's pixel spacing and dataSpacing are different, coordinate will be normalized upon dataSpacing and multiplied by image's pixel spacing
+    /// Otherwise, coordinate will remain the same
+    static double* amendCoordinate(double coordinate[3], double dataSpacing[3], Image *image);
+
+    /// Computes the distance relative to the line, the spacing of the volume it's laying in and the source image
+    static double computeDistance(DrawerLine *line, Image *image, double dataSpacing[3]);
 };
 
 } // End namespace udg
