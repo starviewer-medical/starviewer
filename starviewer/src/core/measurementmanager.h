8d7e00187d389812e11cc568f1931181f539d8aa
Jesús Spínola <jspinola@gmail.com>
2013-06-04 12:42:19 +0000

    ﻿ENH: Changed the way we are computing measurements. Instead of having specific methods for each kind of measure, we take advantage of the new class MeasureComputer.
    amendCoordinate(), computeDistance/Area() are no longer needed and we use getMeasurementForDisplay() which uses MeasureComputer. Two specific methods for each primitive are provided to get distances and
    areas, but it could be extended through the MeasureComputer subclasses we make and could be used with other objects that represent geometric shapes.

--- starviewer/src/core/measurementmanager.h
+++ starviewer/src/core/measurementmanager.h
@@ -8,8 +8,7 @@ namespace udg {
 class DrawerLine;
 class DrawerPolygon;
 class Image;
-class PixelSpacing2D;
-
+class MeasureComputer;
 /**
     This is a helper class to manage measurements safely.
     This class should be used when we want to give the right measurement for diagnostics.
@@ -21,27 +20,57 @@ public:
     enum MeasurementType { Detector, Magnified, Calibrated, Physical, NoDefinedUnits, UnknownMeaning };
 
     enum MeasurementUnitsType { Pixels, Millimetres, NotAvailable };
+
+    /// Enumarated type to choice the verbosity of the information to display for a given measurement
+    /// Minimal: Only the preferred default measure is computed and its type is only explicity pointed out when it's different from the expected for its modality
+    /// MinimalExplicit:Same as Minimal, but if measure is on Detector is also explicity pointed out
+    /// Verbose: Same as Minimal plus Detector measure if available (not explicitly pointed out)
+    /// VerboseExplicit: Same as Verbose and Detector measure is explicitly pointed out if available
+    enum MeasurementDisplayVerbosityType { Minimal, MinimalExplicit, Verbose, VerboseExplicit };
     
     /// Returns the corresponding measurement units for the given image
     static MeasurementUnitsType getMeasurementUnits(Image *image);
     static QString getMeasurementUnitsAsQString(Image *image);
 
-    /// Given a coordinate, returns an amended coordinate depending on the spacing of the image data and the actual pixel spacing parameters from its image
-    /// If there's no pixel spacing information on the image, coordinate will be normalized upon dataSpacing
-    /// If image's pixel spacing and dataSpacing are different, coordinate will be normalized upon dataSpacing and multiplied by image's pixel spacing
-    /// Otherwise, coordinate will remain the same
-    static double* amendCoordinate(double coordinate[3], double dataSpacing[3], Image *image);
-
-    /// Computes the distance relative to the line, the spacing of the volume it's laying in and the source image
-    static double computeDistance(DrawerLine *line, Image *image, double dataSpacing[3]);
-
-    /// Computes the area relative to the polygon, the spacing of the volume it's laying in and the source image
-    static double computeArea(DrawerPolygon *polygon, Image *image, double dataSpacing[3]);
-
+    /// Computes the corresponding measurement and returns it formatted for display
+    /// @param verbosity Will determine how much extra information will be added in order to be more descriptive about de measurement
+    static QString getMeasurementForDisplay(MeasureComputer *measureComputer, Image *image, double dataSpacing[3], 
+        MeasurementDisplayVerbosityType verbosity = Minimal);
+    /// Overloaded methods specifying the primitive on which we want the measure instead of the computer class.
+    /// It takes the proper measure computer for each primitive.
+    static QString getMeasurementForDisplay(DrawerLine *line, Image *image, double dataSpacing[3], MeasurementDisplayVerbosityType verbosity = Minimal);
+    static QString getMeasurementForDisplay(DrawerPolygon *polygon, Image *image, double dataSpacing[3], MeasurementDisplayVerbosityType verbosity = Minimal);
+    
     /// Returns the default meaurement type regarding image properties, such as pixel spacing tags and modality
     static MeasurementType getDefaultMeasurementType(Image *image);
-    /// Amends coordinate taken on coordinateSpacing with the given amenderSpacing
-    static double* amendCoordinateExplicit(double coordinate[3], double coordinateSpacing[3], const PixelSpacing2D &amenderSpacing);
+
+private:
+    /// Return the associated information string for the provided MeasurementType
+    static QString getMeasurementTypeInformationString(MeasurementType type);
+
+    /// Computes measurement as getMeasurementForDisplay() method does but we can specify explicitly which kind of measurement we want to do
+    /// If the specified measurement type requires attributes which are not present or valid, the result would be an empty string
+    static QString getMeasurementForDisplayExplicit(MeasureComputer *measureComputer, Image *image, double dataSpacing[3], 
+        MeasurementType explicitMeasurementType, bool verbose);
+    
+    /// Computes the measure specifying explicitly which kind of measurement we want to do
+    /// If the specified measurement type requires attributes which are not present or valid, the result would be the same as the default measurement type
+    /// for the given image. In any case, appliedMeasurementType will return the measurement type finally applied
+    static double computeMeasureExplicit(MeasureComputer *measureComputer, Image *image, double dataSpacing[3], MeasurementType explicitMeasurementType, 
+        MeasurementType &appliedMeasurementType);
+    
+    /// Formats the measurement made on the given image in a string suitable for display. Dimensions should be used to indicate if 
+    /// the measure is an area (2) or a volume (3) and append the proper sufix to the units
+    static QString formatMeasurementForDisplay(double measurementValue, Image *image, MeasurementDisplayVerbosityType verbosity, int dimensions);
+    
+    /// Same as formatMeasurementForDisplay() but indicating explicitly which is the measurement type
+    static QString formatMeasurementForDisplayExplicit(double measurementValue, Image *image, MeasurementType explicitMeasurementType, bool verbose, int dimensions);
+
+    /// Returns true if for the given image and verbosity, "detector" should be explicitly displayed, false otherwise
+    static bool hasToAddDetectorMeasurementInformation(Image *image, MeasurementDisplayVerbosityType verbosity);
+
+    /// Given the measurement type and verbosity tells if this type should be explicitly displayed
+    static bool hasToBeExplicitAboutMeasurementType(MeasurementType measurementType, MeasurementDisplayVerbosityType verbosity);
 };
 
 } // End namespace udg
