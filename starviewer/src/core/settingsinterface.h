598d1b6e460e61ed14a1e72aa0c784bb21902df3
Marc Ruiz <marc.ruiz@gmail.com>
2013-06-19 12:31:43 +0000

    ENH: Add SettingsInterface as the common interface for settings classes.

---
+++ starviewer/src/core/settingsinterface.h
@@ -0,0 +1,31 @@
+#ifndef SETTINGSINTERFACE_H
+#define SETTINGSINTERFACE_H
+
+class QString;
+class QVariant;
+
+namespace udg {
+
+/**
+ *  Common interface for classes that implement settings.
+ */
+class SettingsInterface {
+
+public:
+
+    virtual ~SettingsInterface() = 0 {};
+
+    /// Returns the value of the setting identified by the given key. If the value is not set, returs the default value of the setting.
+    virtual QVariant getValue(const QString &key) const = 0;
+    /// Sets the value of the setting identified by the given key.
+    virtual void setValue(const QString &key, const QVariant &value) = 0;
+    /// Returns true if the setting identified by the given key has a set value, and false otherwise.
+    virtual bool contains(const QString &key) const = 0;
+    /// Unsets the value of the setting identified by the given key.
+    virtual void remove(const QString &key) = 0;
+
+};
+
+} // namespace udg
+
+#endif // SETTINGSINTERFACE_H
