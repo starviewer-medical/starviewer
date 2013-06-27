51c6fe6235da25eddb18e7ee4d7c951718558394
Jesús Spínola <jspinola@gmail.com>
2013-06-27 14:01:23 +0000

    ﻿ENH: Adding a new class to represent the response of a DIMSE-C opertaion

---
+++ starviewer/src/core/dicomserviceresponsestatus.cpp
@@ -0,0 +1,255 @@
+#include "dicomserviceresponsestatus.h"
+
+#include "dicomdictionary.h"
+
+namespace udg {
+
+const unsigned int DICOMServiceResponseStatus::RefusedOutOfResources = 0xa700;
+const unsigned int DICOMServiceResponseStatus::RefusedOutOfResourcesUnableToCalculateNumberOfMatches = 0xa701;
+const unsigned int DICOMServiceResponseStatus::RefusedOutOfResourcesUnableToPerformSubOperations = 0xa702;
+const unsigned int DICOMServiceResponseStatus::RefusedMoveDestinationUnknown = 0xa801;
+const unsigned int DICOMServiceResponseStatus::IdentifierDoesNotMatchSOPClass = 0xa900;
+const unsigned int DICOMServiceResponseStatus::ErrorDataSetDoesNotMatchSOPClass = 0xa900;
+const unsigned int DICOMServiceResponseStatus::ErrorCannotUnderstand = 0xc000;
+const unsigned int DICOMServiceResponseStatus::UnableToProcess = 0xc000;
+const unsigned int DICOMServiceResponseStatus::TerminatedDueToCancelRequest = 0xfe00;
+const unsigned int DICOMServiceResponseStatus::WarningSubOperationsCompleteOneOrMoreFailures = 0xb000;
+const unsigned int DICOMServiceResponseStatus::WarningCoercionOfDataElements = 0xb000;
+const unsigned int DICOMServiceResponseStatus::WarningDatasetDoesNotMatchSOPClass = 0xb007;
+const unsigned int DICOMServiceResponseStatus::WarningElementsDiscarded = 0xb006;
+const unsigned int DICOMServiceResponseStatus::Pending = 0xff00;
+const unsigned int DICOMServiceResponseStatus::PendingWarningUnsupportedOptionalKeys = 0xff01;
+const unsigned int DICOMServiceResponseStatus::Success = 0x0000;
+
+DICOMServiceResponseStatus::DICOMServiceResponseStatus()
+{
+    m_statusCode = Success;
+    m_serviceType = UnknownService;
+}
+
+DICOMServiceResponseStatus::~DICOMServiceResponseStatus()
+{
+}
+
+void DICOMServiceResponseStatus::setStatusCode(unsigned int code)
+{
+    m_statusCode = code;
+}
+
+unsigned int DICOMServiceResponseStatus::getStatusCode() const
+{
+    return m_statusCode;
+}
+
+void DICOMServiceResponseStatus::setServiceType(ServiceType service)
+{
+    m_serviceType = service;
+}
+
+DICOMServiceResponseStatus::ServiceType DICOMServiceResponseStatus::getServiceType() const
+{
+    return m_serviceType;
+}
+
+QString DICOMServiceResponseStatus::getStatusCodeAsString() const
+{
+    // dcmtk's dimse.h functions (DU_cmoveStatusString, DU_cfindStatusString, DU_storeStatusString,...) could be also used
+    // to translate codes into strings, but we'll loss the possibility of translate them
+    switch (m_statusCode)
+    {
+        case RefusedOutOfResources:
+            return QObject::tr("Refused: Out of resources");
+        
+        case RefusedOutOfResourcesUnableToCalculateNumberOfMatches:
+            return QObject::tr("Refused: Out of resources - Unable to calculate number of matches");
+
+        case RefusedOutOfResourcesUnableToPerformSubOperations:
+            return QObject::tr("Refused: Out of resources - Unable to perform sub-operations");
+
+        case RefusedMoveDestinationUnknown:
+            return QObject::tr("Refused: Move destination unknown");
+
+        case IdentifierDoesNotMatchSOPClass:
+            if (m_serviceType == CStore)
+            {
+                // ErrorDataSetDoesNotMatchSOPClass
+                return QObject::tr("Error: Data set does not match SOP Class");
+            }
+            else
+            {
+                return QObject::tr("Identifiers does not match SOP Class");
+            }
+
+        case UnableToProcess:
+            if (m_serviceType == CStore)
+            {
+                // ErrorCannotUnderstand
+                return QObject::tr("Error: Cannot understand");
+            }
+            else
+            {
+                return QObject::tr("Unable to process");
+            }
+
+        case TerminatedDueToCancelRequest:
+            return QObject::tr("Terminated due to cancel request");
+
+        case WarningSubOperationsCompleteOneOrMoreFailures:
+            if (m_serviceType == CStore)
+            {
+                // WarningCoercionOfDataElements
+                return QObject::tr("Warning: Coercion of data elements");
+            }
+            else
+            {
+                return QObject::tr("Warning: Sub-operations complete, one or more failures");
+            }
+
+        case WarningDatasetDoesNotMatchSOPClass:
+            return QObject::tr("Warning: Data set does not match SOP Class");
+
+        case WarningElementsDiscarded:
+            return QObject::tr("Warning: Elements discarded");
+        
+        case Pending:
+            return QObject::tr("Pending");
+        
+        case PendingWarningUnsupportedOptionalKeys:
+            return QObject::tr("Pending: Warning - One or more optional keys were not supported");
+
+        case Success:
+            return QObject::tr("Success");
+        
+        default:
+            return QObject::tr("Unkown status");
+    }
+}
+
+DICOMServiceResponseStatus::ServiceStatusType DICOMServiceResponseStatus::getServiceStatus() const
+{
+    switch (m_statusCode)
+    {
+        case RefusedOutOfResources:
+        case RefusedOutOfResourcesUnableToCalculateNumberOfMatches:
+        case RefusedOutOfResourcesUnableToPerformSubOperations:
+        case RefusedMoveDestinationUnknown:
+        case IdentifierDoesNotMatchSOPClass:
+        case UnableToProcess:
+            return FailureStatus;
+            break;
+
+        case TerminatedDueToCancelRequest:
+            return CancelStatus;
+            break;
+
+        case WarningSubOperationsCompleteOneOrMoreFailures:
+        case WarningDatasetDoesNotMatchSOPClass:
+        case WarningElementsDiscarded:
+            return WarningStatus;
+            break;
+
+        case Pending:
+        case PendingWarningUnsupportedOptionalKeys:
+            return PendingStatus;
+            break;
+
+        case Success:
+            return SuccessStatus;
+            break;
+
+        default:
+            return UnknownStatus;
+            break;
+    }
+}
+
+QList<DICOMTag> DICOMServiceResponseStatus::getRelatedFields()
+{
+    QList<DICOMTag> relatedFields;
+
+    switch (m_statusCode)
+    {
+        case RefusedOutOfResources:
+            relatedFields << DICOMErrorComment;
+            break;
+
+        case RefusedOutOfResourcesUnableToCalculateNumberOfMatches:
+            relatedFields << DICOMErrorComment;
+            break;
+
+        case RefusedOutOfResourcesUnableToPerformSubOperations:
+            relatedFields << DICOMNumberOfCompletedSubOperations << DICOMNumberOfFailedSubOperations << DICOMNumberOfWarningSubOperations;
+            break;
+
+        case RefusedMoveDestinationUnknown:
+            relatedFields << DICOMErrorComment;
+            break;
+
+        case IdentifierDoesNotMatchSOPClass:
+            relatedFields << DICOMOffendingElement << DICOMErrorComment;
+            break;
+
+        case UnableToProcess:
+            relatedFields << DICOMOffendingElement << DICOMErrorComment;
+            break;
+
+        case TerminatedDueToCancelRequest:
+            relatedFields << DICOMNumberOfRemainingSubOperations << DICOMNumberOfCompletedSubOperations << DICOMNumberOfFailedSubOperations 
+                << DICOMNumberOfWarningSubOperations;
+            break;
+
+        case WarningSubOperationsCompleteOneOrMoreFailures:
+            relatedFields << DICOMNumberOfCompletedSubOperations << DICOMNumberOfFailedSubOperations << DICOMNumberOfWarningSubOperations;
+            break;
+
+        case WarningDatasetDoesNotMatchSOPClass:
+        case WarningElementsDiscarded:
+            relatedFields << DICOMOffendingElement << DICOMErrorComment;
+            break;
+        
+        case Pending:
+            relatedFields << DICOMNumberOfRemainingSubOperations << DICOMNumberOfCompletedSubOperations << DICOMNumberOfFailedSubOperations 
+                << DICOMNumberOfWarningSubOperations;
+            break;
+
+        case Success:
+            relatedFields << DICOMNumberOfCompletedSubOperations << DICOMNumberOfFailedSubOperations << DICOMNumberOfWarningSubOperations;
+            break;
+    }
+
+    return relatedFields;
+}
+
+void DICOMServiceResponseStatus::addRelatedFieldValue(DICOMValueAttribute *value)
+{
+    if (value)
+    {
+        m_relatedFieldsValues << value;
+    }
+}
+
+QString DICOMServiceResponseStatus::toString() const
+{
+    QString result;
+
+    result = QObject::tr("DIMSE Status: ") + getStatusCodeAsString();
+    foreach (DICOMValueAttribute *value, m_relatedFieldsValues)
+    {
+        result += "\n" + value->toString(true);
+    }
+
+    return result;
+}
+
+void DICOMServiceResponseStatus::clear()
+{
+    m_statusCode = Success;
+    
+    foreach (DICOMValueAttribute *value, m_relatedFieldsValues)
+    {
+        delete value;
+    }
+    m_relatedFieldsValues.clear();
+}
+
+} // end namespace udg
---
+++ starviewer/src/core/dicomserviceresponsestatus.h
@@ -0,0 +1,146 @@
+#ifndef UDGDICOMRESPONSESTATUS_H
+#define UDGDICOMRESPONSESTATUS_H
+
+#include <QString>
+#include <QList>
+
+#include "dicomtag.h"
+#include "dicomvalueattribute.h"
+
+namespace udg {
+
+/**
+    Represents the DICOM response status that could be obtained from DIMSE-C service requests as C-GET, C-FIND, C-MOVE and C-STORE
+    For more information, see PS 3.4 Annex C.4 and PS 3.7 Annex C - Status Type Enconding
+ */
+class DICOMServiceResponseStatus {
+public:
+    DICOMServiceResponseStatus();
+    ~DICOMServiceResponseStatus();
+    
+    /// Status Codes
+    // Failure
+    static const unsigned int RefusedOutOfResources; // C-FIND & C-STORE
+    static const unsigned int RefusedOutOfResourcesUnableToCalculateNumberOfMatches; // C-MOVE & C-GET
+    static const unsigned int RefusedOutOfResourcesUnableToPerformSubOperations; // C-MOVE & C-GET
+    static const unsigned int RefusedMoveDestinationUnknown; // C-MOVE
+    static const unsigned int IdentifierDoesNotMatchSOPClass; // C-FIND, C-MOVE, C-GET & C-STORE
+    static const unsigned int ErrorDataSetDoesNotMatchSOPClass; // C-STORE
+    static const unsigned int ErrorCannotUnderstand; // C-STORE
+    static const unsigned int UnableToProcess; // C-FIND, C-MOVE, C-GET
+    // Cancel
+    static const unsigned int TerminatedDueToCancelRequest; // C-FIND (Matching terminated..), C-MOVE, C-GET (Suboperations terminated...)
+    // Warning
+    static const unsigned int WarningSubOperationsCompleteOneOrMoreFailures; // C-MOVE & C-GET
+    static const unsigned int WarningCoercionOfDataElements; // C-STORE
+    static const unsigned int WarningDatasetDoesNotMatchSOPClass; // C-STORE
+    static const unsigned int WarningElementsDiscarded; // C-STORE
+    // Pending
+    static const unsigned int Pending; // C-FIND, C-MOVE, C-GET
+    static const unsigned int PendingWarningUnsupportedOptionalKeys; // C-FIND
+    // Success
+    static const unsigned int Success; // C-FIND, C-MOVE, C-GET & C-STORE;
+    
+    /// Classification of the different possible status
+    enum ServiceStatusType { FailureStatus, CancelStatus, WarningStatus, SuccessStatus, PendingStatus, UnknownStatus };
+    
+    /// Kinds of services the response could belong to
+    enum ServiceType { CFind, CMove, CGet, CStore, UnknownService };
+
+    /// Set/get the status code
+    void setStatusCode(unsigned int code);
+    unsigned int getStatusCode() const;
+
+    /// Set/get the service the response belongs to
+    void setServiceType(ServiceType service);
+    ServiceType getServiceType() const;
+
+    /// Gets the status code as a human-readable string
+    QString getStatusCodeAsString() const;
+    
+    /// Returns the service status type corresponding to the current status code
+    ServiceStatusType getServiceStatus() const;
+
+    /// Returns the list of related DICOM field tags for the current status code
+    QList<DICOMTag> getRelatedFields();
+    
+    /// Adds a related field value
+    void addRelatedFieldValue(DICOMValueAttribute *value);
+
+    /// Returns the status details in human-readable formatted string
+    QString toString() const;
+
+    /// Resets the object to its initial values
+    void clear();
+
+private:
+    /// Status code
+    unsigned int m_statusCode;
+    
+    /// Service type which response belongs to
+    ServiceType m_serviceType;
+
+    /// Related fields values
+    QList<DICOMValueAttribute*> m_relatedFieldsValues;
+};
+
+} // end namespace udg
+
+#endif
+
+
+/*
+// C-STORE
+    // a) Refused: Out of Resources: This indicates that the peer DIMSE-service-user was unable to store 
+    //      the composite SOP Instance because it was out of resources.
+    // b) Refused: SOP Class Not Supported: This indicates that the peer DIMSE-service-user was unable to store 
+    //      the composite SOP Instance because the SOP Class is not supported,
+    // c) Error: Cannot Understand: This indicates that the peer DIMSE-service-user was unable to store the 
+    //      composite SOP Instance because it cannot understand certain Data Elements.
+    // d) Error: Data Set does not match SOP Class: This indicates that the peer DIMSE-service-user
+    //      was unable to store the composite SOP Instance because the Data Set does not match the SOP Class.
+    // e) Warning: This indicates that the peer DIMSE-service-user was able to store the composite
+    //      SOP Instance, but detected a probable error.
+    // f) Success: This indicates that the composite SOP Instance was successfully stored.
+    enum CStoreStatusType { RefusedOutOfResources, RefusedSOPClassNotSupported, ErrorCannotUndertand, ErrorDataSetDoesNotMatchSOPClass, Warning, Success };
+
+    // C-FIND
+    // a) Success: This indicates that processing of the matches is complete. It shall not contain a matching Identifier.
+    // b) Pending: This indicates that processing of the matches is initiated or continuing. It shall contain a matching Identifier.
+    // c) Refused: Out of Resources: Indicates that processing of the C-FIND has been terminated
+    //      because it was out of resources. This may be the initial response to the C-FIND, or may be
+    //      sent after a number of pending C-FIND responses. This response shall not contain a matching Identifier.
+    // d) Refused: SOP Class Not Supported: Indicates that processing of the C-FIND has been terminated because 
+    //      the SOP Class was not supported. This response shall not contain a matching Identifier.
+    // e) Cancel: Indicates that the processing of the C-FIND has been terminated due to a C-FIND
+    //      Cancel indication primitive. The response shall not contain an Identifier.
+    // f) Failed: Indicates that the C-FIND operation failed at the performing DIMSE-service-user.
+    enum CFindStatusType { Success,
+        Pending,
+        RefusedOutOfResources = 0xa701,
+        RefusedSOPClassNotSupported,
+        IdentifierDoesNotMatchSOPClass = 0xa900,
+        Cancel = 0xfe00,
+        Failed };
+
+    // C-GET
+    // a) Success: This indicates that processing of the matches and all sub-operations are complete.
+    // b) Pending: This indicates that processing of the matches and sub-operations is initiated or continuing.
+    // c) Refused: Out of Resources: Indicates that processing of the C-GET has been terminated because it was out of resources.
+    //      This may be the initial response to the C-GET or may be sent after a number of Pending statuses.
+    // d) Refused: SOP Class Not Supported: Indicates that processing of the C-GET has been terminated because the SOP Class was not supported.
+    // e) Cancel: Indicates that processing of the C-GET has been terminated due to a C-GET Cancel indication primitive.
+    // f) Failed: Indicates that the C-GET operation failed at the performing DIMSE-service-user.
+    enum CGetStatusType { Success, Pending, RefusedOutOfResources, RefusedSOPClassNotSupported, Cancel, Failed };
+
+    // C-MOVE
+    // a) Success: This indicates that processing of the matches and all sub-operations are complete.
+    // b) Pending: This indicates that processing of the matches and sub-operations is initiated or continuing.
+    // c) Refused: Out of Resources: Indicates that processing of the C-MOVE has been terminated because it was out of resources.
+    //      This may be the initial response to the C-MOVE or may be sent after a number of Pending statuses.
+    // d) Refused: SOP Class Not Supported: Indicates that processing of the C-MOVE has been terminated because the SOP Class was not supported.
+    // e) Refused: Move Destination Unknown: Indicates that processing of the C-MOVE has been terminated because the Move destination was unknown.
+    // f) Cancel: Indicates that processing of the C-MOVE has been terminated due to a C-MOVE Cancel indication primitive.
+    // g) Failed: Indicates that the C-MOVE operation failed at the performing DIMSE-service-user.
+    enum CMoveStatusType { Success, Pending, RefusedOutOfResources, RefusedSOPClassNotSupported, Cancel, Failed };
+*/
