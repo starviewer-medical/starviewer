6fe6a50adddfa5f664f1b0ceaec51d455ad6cefb
Roger Bramon <rogerbramon@gmail.com>
2013-07-31 07:09:21 +0000

    ENH: New filter class to apply window-level window

---
+++ starviewer/src/core/windowlevelfilter.h
@@ -0,0 +1,57 @@
+#ifndef WINDOWLEVELFILTER_H
+#define WINDOWLEVELFILTER_H
+
+#include "filter.h"
+
+class vtkImageData;
+class vtkImageMapToWindowLevelColors2;
+
+namespace udg {
+
+class TransferFunction;
+class WindowLevel;
+
+///    This filter applies a window level window to the input.
+class WindowLevelFilter : public Filter {
+
+public:
+    WindowLevelFilter();
+    virtual ~WindowLevelFilter();
+
+    /// Sets the given vtkImageData as input of the filter
+    void setInput(vtkImageData *input);
+    /// Sets the given filter output as input of the filter
+    void setInput(FilterOutput input);
+
+    /// Sets the window-level window
+    void setWindowLevel(const WindowLevel &windowLevel);
+    /// Sets the window-level window
+    void setWindowLevel(double window, double level);
+
+    /// Sets the window value
+    void setWindow(double window);
+    /// Gets the window value
+    double getWindow() const;
+
+    /// Sets the level value
+    void setLevel(double level);
+    /// Gets the level value
+    double getLevel() const;
+
+    /// Sets the transfer funcion
+    void setTransferFunction(const TransferFunction &transferFunction);
+    /// Clears the transfer funcion
+    void clearTransferFunction();
+
+private:
+    /// Returns the vtkAlgorithm used to implement the filter.
+    virtual vtkAlgorithm* getVtkAlgorithm() const;
+
+private:
+    vtkImageMapToWindowLevelColors2* m_filter;
+
+};
+
+}
+
+#endif
