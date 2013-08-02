1e738accc57f82614a4e3f5326d99e2f16af6883
Jesús Spínola <jspinola@gmail.com>
2013-08-02 11:29:28 +0000

    ﻿ENH: Adding SyncActionManager class to manage SyncActions for a set of viewers (#1628)

---
+++ starviewer/src/core/syncactionmanager.cpp
@@ -0,0 +1,153 @@
+#include "syncactionmanager.h"
+
+#include "signaltosyncactionmapperfactory.h"
+#include "syncaction.h"
+#include "synccriterion.h"
+#include "syncactionsconfiguration.h"
+
+namespace udg {
+
+SyncActionManager::SyncActionManager(QObject *parent)
+ : QObject(parent)
+{
+    m_masterViewer = 0;
+    m_enabled = false;
+    
+    setupSignalMappers();
+    setupDefaultSyncActionsConfiguration();
+}
+
+SyncActionManager::~SyncActionManager()
+{
+}
+
+void SyncActionManager::addSyncedViewer(QViewer *viewer)
+{
+    if (!m_syncedViewersSet.contains(viewer))
+    {
+        m_syncedViewersSet << viewer;
+    }
+}
+
+void SyncActionManager::removeSyncedViewer(QViewer *viewer)
+{
+    if (viewer == m_masterViewer)
+    {
+        m_masterViewer = 0;
+        updateMasterViewerMappers();
+    }
+    m_syncedViewersSet.remove(viewer);
+}
+
+void SyncActionManager::setMasterViewer(QViewer *viewer)
+{
+    if (m_syncedViewersSet.contains(viewer))
+    {
+        m_masterViewer = viewer;
+        updateMasterViewerMappers();
+    }
+}
+
+void SyncActionManager::clearSyncedViewersSet()
+{
+    m_syncedViewersSet.clear();
+}
+
+void SyncActionManager::setSyncActionsConfiguration(SyncActionsConfiguration *configuration)
+{
+    if (!configuration)
+    {
+        return;
+    }
+
+    m_syncActionsConfiguration = configuration;
+}
+
+void SyncActionManager::enable(bool enable)
+{
+    m_enabled = enable;
+}
+
+void SyncActionManager::setupSignalMappers()
+{
+    // We create an instance of each of the registered signal mappers, add it to our own list, and then
+    // we create the connection of each mapper with the corresponding slot to apply the SyncAction
+    foreach (const SyncActionMetaData &syncActionMetaData, SignalToSyncActionMapperFactory::instance()->getFactoryIdentifiersList())
+    {
+        SignalToSyncActionMapper *mapper = SignalToSyncActionMapperFactory::instance()->create(syncActionMetaData);
+        
+        m_registeredSignalMappers << mapper;
+        connect(mapper, SIGNAL(actionMapped(SyncAction*)), SLOT(applySyncAction(SyncAction*)));
+    }
+}
+
+void SyncActionManager::setupDefaultSyncActionsConfiguration()
+{
+    m_syncActionsConfiguration = new SyncActionsConfiguration();
+
+    foreach (const SyncActionMetaData &syncActionMetaData, SignalToSyncActionMapperFactory::instance()->getFactoryIdentifiersList())
+    {
+        m_syncActionsConfiguration->enableSyncAction(syncActionMetaData);
+    }
+}
+
+void SyncActionManager::updateMasterViewerMappers()
+{
+    foreach (SignalToSyncActionMapper *mapper, m_registeredSignalMappers)
+    {
+        mapper->setViewer(m_masterViewer);
+    }
+}
+
+void SyncActionManager::applySyncAction(SyncAction *syncAction)
+{
+    if (!syncAction)
+    {
+        return;
+    }
+
+    if (!m_enabled)
+    {
+        return;
+    }
+    
+    foreach (QViewer *viewer, m_syncedViewersSet.values())
+    {
+        if (isSyncActionApplicable(syncAction, viewer))
+        {
+            syncAction->run(viewer);
+        }
+    }
+}
+
+bool SyncActionManager::isSyncActionApplicable(SyncAction *syncAction, QViewer *viewer)
+{
+    if (!viewer || !syncAction)
+    {
+        return false;
+    }
+
+    if (viewer != m_masterViewer && m_syncActionsConfiguration->isSyncActionEnabled(syncAction->getMetaData())
+        && areAllCriteriaSatisfied(m_syncActionsConfiguration->getSyncCriteria(syncAction), viewer))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool SyncActionManager::areAllCriteriaSatisfied(QList<SyncCriterion*> criteria, QViewer *viewer)
+{
+    bool criteriaAreSatisfied = true;
+
+    int i = 0;
+    while (criteriaAreSatisfied && i < criteria.count())
+    {
+        criteriaAreSatisfied = criteria.at(i)->isCriterionSatisfied(m_masterViewer, viewer);
+        ++i;
+    }
+
+    return criteriaAreSatisfied;
+}
+
+} // End namespace udg
