fdb3c375f3823ebead1dd59272df5f4bcd2d27f0
Jesús Spínola <jspinola@gmail.com>
2013-08-06 11:59:08 +0000

    ﻿ENH: Removing distance out parameter from getNearestSlice. We only consider an slice is near if it is within a proximity bounds criterion.

--- starviewer/src/core/slicelocator.h
+++ starviewer/src/core/slicelocator.h
@@ -21,12 +21,20 @@ public:
     /// Sets the plane where the computings will be taken. If no plane set, it will be the XY plane by default.
     void setPlane(OrthogonalPlane::OrthogonalPlaneType plane);
     
-    /// Returns the nearest slice to the given point. The out parameter distance will have the distance from the point to the slice
+    /// Returns the nearest slice to the given point.
     /// The nearest slice will be computed against the given volume and plane from setVolume() and setPlane() methods.
-    /// If no slice is found, -1 will be returned
-    int getNearestSlice(double point[3], double &distance);
+    /// If no slice is found to be considered near, -1 will be returned
+    int getNearestSlice(double point[3]);
 
 private:
+    /// Returns true if the given slice distance could be considered to be within a certain proximity
+    /// regarding the slice spacing values of the current volume, false otherwise
+    bool isWithinProximityBounds(double distanceToSlice);
+
+private:
+    /// This factor keeps the proportion factor of slices to determine if an slice could be considered to be near to another slice or not
+    static const double SliceProximityFactor;
+    
     /// The volume to compute slices from
     Volume *m_volume;
     
