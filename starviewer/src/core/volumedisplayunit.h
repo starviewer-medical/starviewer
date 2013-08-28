49140fa2a6151a4b01f0b5213c2142c2d0e67323
Marc Ruiz <marc.ruiz@gmail.com>
2013-08-28 12:49:06 +0000

    DOC: Add documentation to VolumeDisplayUnit.

--- starviewer/src/core/volumedisplayunit.h
+++ starviewer/src/core/volumedisplayunit.h
@@ -13,6 +13,8 @@ class Volume;
 
 /**
     This class groups together a Volume and the associated objects that a Q2DViewer needs to display a volume.
+    The ImagePipeline, the vtkImageActor and the SliceHandler are created internally in the constructor and destroyed in the destructor, so only the Volume has
+    to be supplied externally.
  */
 class VolumeDisplayUnit {
 
@@ -20,12 +22,23 @@ public:
     VolumeDisplayUnit();
     ~VolumeDisplayUnit();
 
+    /// Returns the volume.
     Volume* getVolume() const;
+    /// Sets a new volume and resets display properties (pipeline and slice handler).
     void setVolume(Volume *volume);
+
+    /// Returns the image pipeline.
     ImagePipeline* getImagePipeline() const;
+
+    /// Returns the image actor.
     vtkImageActor* getImageActor() const;
+
+    /// Returns the slice handler.
     SliceHandler* getSliceHandler() const;
-    const OrthogonalPlane &getViewPlane() const;
+
+    /// Returns the view plane.
+    const OrthogonalPlane& getViewPlane() const;
+    /// Sets a new view plane, resetting some properties of the slice handler.
     void setViewPlane(const OrthogonalPlane &viewPlane);
 
     /// Returns slice thickness of the currently displayed image.
@@ -34,9 +47,16 @@ public:
     double getSliceThickness() const;
 
 private:
+    /// The volume.
     Volume *m_volume;
+
+    /// The image pipeline that processes the volume.
     ImagePipeline *m_imagePipeline;
+
+    /// The image actor where the slices are rendered.
     vtkImageActor *m_imageActor;
+
+    /// The slice handler that controls slices, phases and slabs.
     SliceHandler *m_sliceHandler;
 
 };
