0b62f1cfc9ac468c36563e08970a47ccdda0b5c9
Jesús Spínola <jspinola@gmail.com>
2013-08-29 12:07:52 +0000

    ﻿ENH: Removing minimum and maximum methods from MathTools. QtGlobal methods could be used instead

--- starviewer/tests/auto/unittests/core/test_mathtools.cpp
+++ starviewer/tests/auto/unittests/core/test_mathtools.cpp
@@ -17,12 +17,6 @@ class test_MathTools : public QObject {
 Q_OBJECT
 
 private slots:
-    void minimum_ShouldReturnMinimum_data();
-    void minimum_ShouldReturnMinimum();
-
-    void maximum_ShouldReturnMaximum_data();
-    void maximum_ShouldReturnMaximum();
-
     void truncate_ShouldReturnTruncatedValue_data();
     void truncate_ShouldReturnTruncatedValue();
 
@@ -138,7 +132,6 @@ private slots:
     void copySign_ShouldReturnNaNWithExpectedSign();
 
 private:
-    void setupMinimumMaximumData();
     void setupRadiansAndDegreesConversionData();
     void setupComputeAngleOfAVectorData();
     void setupComputeAngleBetweenTwoVectorsData();
@@ -159,34 +152,6 @@ const double crossProductEpsilon = 0.001;
 const double directorVectorEpsilon = 0.001;
 const double distance3DEpsion = 0.0005;
 const double LineIntersectionEpsilon = 0.0001;
-
-void test_MathTools::minimum_ShouldReturnMinimum_data()
-{
-    setupMinimumMaximumData();
-}
-
-void test_MathTools::minimum_ShouldReturnMinimum()
-{
-    QFETCH(double, value1);
-    QFETCH(double, value2);
-    QFETCH(double, minimum);
-
-    QCOMPARE(MathTools::minimum(value1, value2), minimum);
-}
-
-void test_MathTools::maximum_ShouldReturnMaximum_data()
-{
-    setupMinimumMaximumData();
-}
-
-void test_MathTools::maximum_ShouldReturnMaximum()
-{
-    QFETCH(double, value1);
-    QFETCH(double, value2);
-    QFETCH(double, maximum);
-
-    QCOMPARE(MathTools::maximum(value1, value2), maximum);
-}
 
 void test_MathTools::truncate_ShouldReturnTruncatedValue_data()
 {
@@ -1378,23 +1343,6 @@ void test_MathTools::copySign_ShouldReturnNaNWithExpectedSign()
     std::bitset<8*sizeof(unsigned long long)> fNaNBits(*pNaN);
     bool signBit = fNaNBits.test(fNaNBits.size() - 1);
     QCOMPARE(!signBit, expectedSign);
-}
-
-void test_MathTools::setupMinimumMaximumData()
-{
-    QTest::addColumn<double>("value1");
-    QTest::addColumn<double>("value2");
-    QTest::addColumn<double>("minimum");
-    QTest::addColumn<double>("maximum");
-
-    QTest::newRow("-- first is bigger") << -54.0 << -94.0 << -94.0 << -54.0;
-    QTest::newRow("-- second is bigger") << -77.0 << -38.0 << -77.0 << -38.0;
-    QTest::newRow("++ first is bigger") << 100.0 << 77.0 << 77.0 << 100.0;
-    QTest::newRow("++ second is bigger") << 57.0 << 60.0 << 57.0 << 60.0;
-    QTest::newRow("+-") << 53.0 << -75.0 << -75.0 << 53.0;
-    QTest::newRow("-+") << -95.0 << 59.0 << -95.0 << 59.0;
-    QTest::newRow("-- equal") << -52.0 << -52.0 << -52.0 << -52.0;
-    QTest::newRow("++ equal") << 57.0 << 57.0 << 57.0 << 57.0;
 }
 
 void test_MathTools::setupRadiansAndDegreesConversionData()
