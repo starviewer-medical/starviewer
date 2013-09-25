5085d56f83873807c2ca87d0c68f83189fbb756d
Jesús Spínola <jspinola@gmail.com>
2013-09-25 11:45:41 +0000

    ﻿ENH: Adding new method to get the current z coordinate

--- starviewer/src/core/volumedisplayunit.cpp
+++ starviewer/src/core/volumedisplayunit.cpp
@@ -108,6 +108,22 @@ double VolumeDisplayUnit::getCurrentSpacingBetweenSlices() const
     }
 }
 
+double VolumeDisplayUnit::getCurrentDisplayedImageDepth() const
+{
+    if (getVolume())
+    {
+        double zSpacing = getCurrentSpacingBetweenSlices();
+        int zIndex = getViewPlane().getZIndex();
+        double zOrigin = getVolume()->getOrigin()[zIndex];
+        
+        return zOrigin + zSpacing * getSlice();
+    }
+    else
+    {
+        return 0.0;
+    }
+}
+
 VolumePixelData* VolumeDisplayUnit::getCurrentPixelData()
 {
     if (!m_volume)
