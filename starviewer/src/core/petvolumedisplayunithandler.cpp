42e0e0f3369e1bc5f9919d30120ab44434a74a16
Marc Ruiz <marc.ruiz@gmail.com>
2013-10-18 10:19:28 +0000

    ENH: Set "Black & White Inverse" as the default transfer function when displaying a single PET volume.

--- starviewer/src/core/petvolumedisplayunithandler.cpp
+++ starviewer/src/core/petvolumedisplayunithandler.cpp
@@ -1,8 +1,11 @@
 #include "petvolumedisplayunithandler.h"
 
 #include "transferfunctionmodel.h"
+#include "volumedisplayunit.h"
 
 namespace udg {
+
+const char * const DefaultTransferFunctionName = "Black & White Inverse";
 
 PETVolumeDisplayUnitHandler::PETVolumeDisplayUnitHandler()
 {
@@ -11,6 +14,18 @@ PETVolumeDisplayUnitHandler::PETVolumeDisplayUnitHandler()
 
 PETVolumeDisplayUnitHandler::~PETVolumeDisplayUnitHandler()
 {
+}
+
+void PETVolumeDisplayUnitHandler::setupDefaultTransferFunctions()
+{
+    TransferFunction transferFunction;
+    transferFunction.setName(DefaultTransferFunctionName);
+    int index = m_transferFunctionModel->getIndexOf(transferFunction, true);
+
+    if (index >= 0)
+    {
+        this->getMainVolumeDisplayUnit()->setTransferFunction(m_transferFunctionModel->getTransferFunction(index));
+    }
 }
 
 }
