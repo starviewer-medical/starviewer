ffa7908d88fe56936d0367abf7e4dfdd15d6ced6
Jesús Spínola <jspinola@gmail.com>
2013-10-18 12:56:56 +0000

    ﻿ENH: Add new condition to satisfy conditions. The implied viewers have to have input.

--- starviewer/src/core/synccriterion.cpp
+++ starviewer/src/core/synccriterion.cpp
@@ -1,5 +1,7 @@
 #include "synccriterion.h"
 
+#include "qviewer.h"
+
 namespace udg {
 
 SyncCriterion::SyncCriterion()
@@ -14,7 +16,14 @@ bool SyncCriterion::isCriterionSatisfied(QViewer *viewer1, QViewer *viewer2)
 {
     if (viewer1 && viewer2)
     {
-        return criterionIsMet(viewer1, viewer2);
+        if (!viewer1->hasInput() || !viewer2->hasInput())
+        {
+            return false;
+        }
+        else
+        {
+            return criterionIsMet(viewer1, viewer2);
+        }
     }
     else
     {
