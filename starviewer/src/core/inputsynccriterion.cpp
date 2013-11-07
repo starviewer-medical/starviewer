825213ce6d2c945d93f4886e6b6b1c05ea68d8b4
Marc Ruiz <marc.ruiz@gmail.com>
2013-11-07 11:19:53 +0000

    ENH: Rewrite InputSyncCriterion using the new method Q2DViewer::containsVolume.

--- starviewer/src/core/inputsynccriterion.cpp
+++ starviewer/src/core/inputsynccriterion.cpp
@@ -19,22 +19,10 @@ bool InputSyncCriterion::criterionIsMet(QViewer *viewer1, QViewer *viewer2)
 {
     // TODO Should also check if some reconstruction is applied, i.e. thick slab?
     Q2DViewer *viewer2D = Q2DViewer::castFromQViewer(viewer2);
+
     if (viewer2D)
     {
-        bool found = false;
-        QList<Volume*> volumes = viewer2D->getInputs();
-        int i = 0;
-        while (!found && i < volumes.size())
-        {
-            found = viewer1->getMainInput() == volumes.at(i);
-            i++;
-        }
-
-        if (volumes.size() > 1)
-        {
-            found = found;
-        }
-        return found;
+        return viewer2D->containsVolume(viewer1->getMainInput());
     }
     else
     {
