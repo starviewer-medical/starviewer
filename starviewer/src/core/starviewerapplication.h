2004ec42d110f83f22dc833f2b04a8646ccb8670
Jesús Spínola <jspinola@gmail.com>
2014-05-08 07:38:30 +0000

    ENH: Version and build ID update (0.12.0, 2014051200)

--- starviewer/src/core/starviewerapplication.h
+++ starviewer/src/core/starviewerapplication.h
@@ -5,8 +5,8 @@
 
 namespace udg {
 
-const QString StarviewerVersionString("0.12.0-devel");
-const QString StarviewerBuildID("2012111500");
+const QString StarviewerVersionString("0.12.0");
+const QString StarviewerBuildID("2014051200");
 
 // Indica per aquesta versió d'starviewer quina és la revisió de bd necessària
 const int StarviewerDatabaseRevisionRequired(9591);
