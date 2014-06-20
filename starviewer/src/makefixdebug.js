5b1f20c04d31afa06c1e2f75c0c465b3c1667d31
Marc Ruiz <marc.ruiz@gmail.com>
2014-06-20 10:37:15 +0200

    COMP: Update makefixdebug for Qt5.

--- starviewer/src/makefixdebug.js
+++ starviewer/src/makefixdebug.js
@@ -3,20 +3,26 @@ var ForWriting = 2;
 
 var makefileName = WScript.Arguments(0);
 var fileSystemObject = new ActiveXObject("Scripting.FileSystemObject");
-var inputTextStream = fileSystemObject.OpenTextFile(makefileName, ForReading);
-var makefile = inputTextStream.ReadAll();
-inputTextStream.Close();
 
-var regExp = /(c\:\\Qt\\[0-9]\.[0-9]+\\lib\\)([A-Za-z]+)d(4?)(\.lib)/gi;
-var WshShell = WScript.CreateObject("WScript.Shell");
+if (fileSystemObject.FileExists(makefileName)) {
+    var inputTextStream = fileSystemObject.OpenTextFile(makefileName, ForReading);
+    var makefile = inputTextStream.ReadAll();
+    inputTextStream.Close();
 
-if (regExp.test(makefile)) {
-    WScript.Echo("Fixing " + WshShell.CurrentDirectory + "\\" + makefileName + "...");
-    var modifiedMakefile = makefile.replace(regExp, "$1$2$3$4");
-    var outputTextStream = fileSystemObject.OpenTextFile(makefileName, ForWriting);
-    outputTextStream.Write(modifiedMakefile);
-    outputTextStream.Close();
+    var regExp = /(Qt)(5?)([A-Za-z]+)d(4?)(\.lib)/gi;
+    var WshShell = WScript.CreateObject("WScript.Shell");
+
+    if (regExp.test(makefile)) {
+        WScript.Echo("Fixing " + WshShell.CurrentDirectory + "\\" + makefileName + "...");
+        var modifiedMakefile = makefile.replace(regExp, "$1$2$3$4$5");
+        var outputTextStream = fileSystemObject.OpenTextFile(makefileName, ForWriting);
+        outputTextStream.Write(modifiedMakefile);
+        outputTextStream.Close();
+    }
+    else {
+        WScript.Echo("No need to fix " + WshShell.CurrentDirectory + "\\" + makefileName);
+    }
 }
 else {
-    WScript.Echo("No need to fix " + WshShell.CurrentDirectory + "\\" + makefileName);
+    WScript.Echo("Makefile not found: " + WshShell.CurrentDirectory + "\\" + makefileName);
 }
