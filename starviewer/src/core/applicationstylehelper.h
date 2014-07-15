416449519502c85d2f34ddc78efd25cba3a8c040
Roger Bramon <rogerbramon@gmail.com>
2014-07-15 14:50:07 +0200

    ENH: Improve readability of RelatedStudiesWidget by scaling font size as it is done in PatientBrowserMenu. Ticket #2082

--- starviewer/src/core/applicationstylehelper.h
+++ starviewer/src/core/applicationstylehelper.h
@@ -18,6 +18,7 @@
 class QWidget;
 class QString;
 class QMovie;
+class QTreeWidget;
 
 namespace udg {
 
@@ -49,6 +50,8 @@ public:
     /// Escala el tamany de lletra d'un QWidget al adient a la pantalla on s'ha cridat recomputeStyleToScreenOfWidget
     void setScaledFontSizeTo(QWidget *widget) const;
 
+    /// Escala el tamany de lletra d'un QTreeWidget al adient a la pantalla on s'ha cridat recomputeStyleToScreenOfWidget
+    void setScaledFontSizeTo(QTreeWidget *treeWidget) const;
 
     /// Scales application font size taking into account where recomputeStyleToScreenOfWidget was called
     int getApplicationScaledFontSize() const;
