66f45d6a51ecaf66638c649d9dabdad246049ac6
Marc Ruiz <marc.ruiz@gmail.com>
2014-09-04 12:43:47 +0200

    BUG: Don't remove the queue policy from the job when cancelling it, as this causes some times that volumes that have the same restriction can't load.

--- starviewer/src/core/volumereaderjobfactory.cpp
+++ starviewer/src/core/volumereaderjobfactory.cpp
@@ -221,9 +221,6 @@ void VolumeReaderJobFactory::cancelLoadingAndDeleteVolume(Volume *volume)
         {
             DEBUG_LOG(QString("Volume %1 cannot be dequeued, requesting abort and delete").arg(volume->getIdentifier().getValue()));
             job->requestAbort();
-            // We need to remove the policy from the job so that the job doesn't try to access the policy,
-            // which may be destroyed before the job, from the job destructor
-            job->removeQueuePolicy(&m_resourceRestrictionPolicy);
         }
     }
     else
