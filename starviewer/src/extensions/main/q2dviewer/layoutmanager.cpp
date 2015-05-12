starviewer-medical/starviewer

commit c603bf7c9fdfe91768fe43f0561ebebbc467fafd

BUG: Sometimes when changing the working studies the active hanging protocols were not marked as active because items in the hanging protocols widget were re-created but the signals to activate them were not fired because the active hanging protocols didn't really change. This has been solved by setting the applied hanging protocols to null before applying the proper layout choice for each working study.

starviewer/src/extensions/main/q2dviewer/layoutmanager.cpp
@@ -409,6 +409,11 @@ void LayoutManager::setWorkingStudies(const QString &currentStudyUID, const QStr
 
     emit hangingProtocolCandidatesFound(m_combinedHangingProtocolCandidates, m_currentStudyHangingProtocolCandidates, m_priorStudyHangingProtocolCandidates);
 
+    // Set all applied hanging protocols to null to ensure the new selected ones are correctly marked in the hanging protocols widget
+    setCombinedHangingProtocolApplied(0);
+    setCurrentHangingProtocolApplied(0);
+    setPriorHangingProtocolApplied(0);
+
     applyProperLayoutChoice(changeCurrentStudyLayout, changePriorStudyLayout);
 }
 

commit b526655e70916b80bea4bd033b15312d10dbd018

BUG: The solution to the issue of the unmarked hanging protocols introduced in c603bf7 was not effective in all cases. For example, if the prior study was changed but the current study remained, changeCurrentStudyLayout was false and the current hanging protocol was not updated, leaving it unmarked in the widget. This alternative solution covers all cases: now all three signals are emitted with the current values to ensure that the proper hanging protocols are marked in the widget, before applying the appropriate layout.

starviewer/src/extensions/main/q2dviewer/layoutmanager.cpp
@@ -409,10 +409,10 @@ void LayoutManager::setWorkingStudies(const QString &currentStudyUID, const QStr
 
     emit hangingProtocolCandidatesFound(m_combinedHangingProtocolCandidates, m_currentStudyHangingProtocolCandidates, m_priorStudyHangingProtocolCandidates);
 
-    // Set all applied hanging protocols to null to ensure the new selected ones are correctly marked in the hanging protocols widget
-    setCombinedHangingProtocolApplied(0);
-    setCurrentHangingProtocolApplied(0);
-    setPriorHangingProtocolApplied(0);
+    // Re-emit the signals of the currently applied hanging protocols to ensure that they are marked as active in the hanging protocols widget
+    emit activeCombinedHangingProtocolChanged(m_combinedHangingProtocolApplied);
+    emit activeCurrentHangingProtocolChanged(m_currentHangingProtocolApplied);
+    emit activePriorHangingProtocolChanged(m_priorHangingProtocolApplied);
 
     applyProperLayoutChoice(changeCurrentStudyLayout, changePriorStudyLayout);
 }