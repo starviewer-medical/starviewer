183e4800cceb80281b03f7e8d7b20039b4571c33
Marc Ruiz <marc.ruiz@gmail.com>
2015-12-11 13:34:47 +0100

    ENH: Refactor LocalDatabaseVoiLutDAL.

--- starviewer/src/inputoutput/localdatabasevoilutdal.cpp
+++ starviewer/src/inputoutput/localdatabasevoilutdal.cpp
@@ -14,7 +14,6 @@
 
 #include "localdatabasevoilutdal.h"
 
-#include "databaseconnection.h"
 #include "dicommask.h"
 #include "image.h"
 
@@ -26,57 +25,7 @@ namespace udg {
 
 namespace {
 
-/// Returns a SQL INSERT string with the given image.
-QString buildSqlInsert(Image *image)
-{
-    return QString("INSERT INTO VoiLut (Lut, ImageInstanceUID, ImageFrameNumber) VALUES (?, '%1', %2)")
-            .arg(LocalDatabaseBaseDAL::formatTextToValidSQLSyntax(image->getSOPInstanceUID()))
-            .arg(image->getFrameNumber());
-}
-
-/// Returns a SQL WHERE clause corresponding to the given mask.
-QString buildSqlWhere(const DicomMask &mask)
-{
-    if (!mask.getSOPInstanceUID().isEmpty())
-    {
-        QString where = QString("WHERE ImageInstanceUID = '%1' ").arg(LocalDatabaseBaseDAL::formatTextToValidSQLSyntax(mask.getSOPInstanceUID()));
-
-        if (!mask.getImageNumber().isEmpty())
-        {
-            where += QString("AND ImageFrameNumber = %1").arg(mask.getImageNumber());
-        }
-
-        return where;
-    }
-
-    if (!mask.getSeriesInstanceUID().isEmpty())
-    {
-        return QString("WHERE ImageInstanceUID IN (SELECT SOPInstanceUID FROM Image WHERE SeriesInstanceUID = '%1')")
-                .arg(LocalDatabaseBaseDAL::formatTextToValidSQLSyntax(mask.getSeriesInstanceUID()));
-    }
-
-    if (!mask.getStudyInstanceUID().isEmpty())
-    {
-        return QString("WHERE ImageInstanceUID IN (SELECT SOPInstanceUID FROM Image WHERE StudyInstanceUID = '%1')")
-                .arg(LocalDatabaseBaseDAL::formatTextToValidSQLSyntax(mask.getStudyInstanceUID()));
-    }
-
-    return QString();
-}
-
-/// Returns a SQL DELETE string corresponding to the given mask.
-QString buildSqlDelete(const DicomMask &mask)
-{
-    return "DELETE FROM VoiLut " + buildSqlWhere(mask);
-}
-
-/// Returns a SQL SELECT string corresponding to the given mask.
-QString buildSqlSelect(const DicomMask &mask)
-{
-    return "SELECT Lut, ImageInstanceUID, ImageFrameNumber FROM VoiLut " + buildSqlWhere(mask);
-}
-
-/// Returns the given VOI LUT encoded in a QByteArray using a QDataStream.
+// Returns the given VOI LUT encoded in a QByteArray using a QDataStream.
 QByteArray getByteArray(const VoiLut &voiLut)
 {
     QByteArray byteArray;
@@ -86,10 +35,9 @@ QByteArray getByteArray(const VoiLut &voiLut)
     return byteArray;
 }
 
-/// Returns a VOI LUT constructed from the blob in column 0 of the given prepared statement read using a QDataStream.
-VoiLut getVoiLut(const QSqlQuery &query)
+// Returns a VOI LUT decoded from the given QByteArray using a QDataStream.
+VoiLut getVoiLut(const QByteArray &byteArray)
 {
-    QByteArray byteArray = query.value("Lut").toByteArray();
     QDataStream stream(byteArray);
     stream.setVersion(QDataStream::Qt_5_3);
     TransferFunction voiLut;
@@ -97,68 +45,81 @@ VoiLut getVoiLut(const QSqlQuery &query)
     return voiLut;
 }
 
-}
-
-LocalDatabaseVoiLutDAL::LocalDatabaseVoiLutDAL(DatabaseConnection &dbConnection)
-    : LocalDatabaseBaseDAL(dbConnection)
+// Prepares the given query with the given SQL base command followed by the appropriate where clause according to the given mask.
+void prepareQueryWithMask(QSqlQuery &query, const DicomMask &mask, const QString &sqlCommand)
 {
-}
-
-bool LocalDatabaseVoiLutDAL::insert(const VoiLut &voiLut, Image *image)
-{
-    QString insertStatement = buildSqlInsert(image);
-    QSqlQuery query;
-
-    if (!query.prepare(insertStatement))
+    if (!mask.getSOPInstanceUID().isEmpty())
     {
-//        logError("prepare(" + query.lastQuery() + ")");
-        return false;
-    }
+        QString where(" WHERE ImageInstanceUID = :imageInstanceUID");
+        if (!mask.getImageNumber().isEmpty())
+        {
+            where += " AND ImageFrameNumber = :imageFrameNumber";
+        }
 
+        query.prepare(sqlCommand + where);
+
+        query.bindValue(":imageInstanceUID", mask.getSOPInstanceUID());
+        if (!mask.getImageNumber().isEmpty())
+        {
+            query.bindValue(":imageFrameNumber", mask.getImageNumber());
+        }
+    }
+    else if (!mask.getSeriesInstanceUID().isEmpty())
+    {
+        query.prepare(sqlCommand + " WHERE ImageInstanceUID IN (SELECT SOPInstanceUID FROM Image WHERE SeriesInstanceUID = :seriesInstanceUID)");
+        query.bindValue(":seriesInstanceUID", mask.getSeriesInstanceUID());
+    }
+    else if (!mask.getStudyInstanceUID().isEmpty())
+    {
+        query.prepare(sqlCommand + " WHERE ImageInstanceUID IN (SELECT SOPInstanceUID FROM Image WHERE StudyInstanceUID = :studyInstanceUID)");
+        query.bindValue(":studyInstanceUID", mask.getStudyInstanceUID());
+    }
+    else
+    {
+        query.prepare(sqlCommand);
+    }
+}
+
+}
+
+LocalDatabaseVoiLutDAL::LocalDatabaseVoiLutDAL(DatabaseConnection &databaseConnection)
+    : LocalDatabaseBaseDAL(databaseConnection)
+{
+}
+
+bool LocalDatabaseVoiLutDAL::insert(const VoiLut &voiLut, const Image *image)
+{
+    QSqlQuery query = getNewQuery();
+    query.prepare("INSERT INTO VoiLut (Lut, ImageInstanceUID, ImageFrameNumber) VALUES (:lut, :imageInstanceUID, :imageFrameNumber)");
     QByteArray blob = getByteArray(voiLut);
-    query.bindValue(0, blob);
-
-    if (!query.exec())
-    {
-//        logError("exec(" + query.lastQuery() + ")");
-        return false;
-    }
-
-    return true;
+    query.bindValue(":lut", blob);
+    query.bindValue(":imageInstanceUID", image->getSOPInstanceUID());
+    query.bindValue(":imageFrameNumber", image->getFrameNumber());
+    return executeQueryAndLogError(query);
 }
 
 bool LocalDatabaseVoiLutDAL::del(const DicomMask &mask)
 {
-    QString deleteStatement = buildSqlDelete(mask);
-    QSqlQuery query;
-
-    if (!query.exec(deleteStatement))
-    {
-        logError(query);
-        return false;
-    }
-
-    return true;
+    QSqlQuery query = getNewQuery();
+    prepareQueryWithMask(query, mask, "DELETE FROM VoiLut");
+    return executeQueryAndLogError(query);
 }
 
 QList<VoiLut> LocalDatabaseVoiLutDAL::query(const DicomMask &mask)
 {
-    QList<VoiLut> voiLuts;
-    QString selectStatement = buildSqlSelect(mask);
-    QSqlQuery query;
+    QSqlQuery query = getNewQuery();
+    prepareQueryWithMask(query, mask, "SELECT Lut, ImageInstanceUID, ImageFrameNumber FROM VoiLut");
+    QList<VoiLut> voiLutList;
 
-    if (!query.exec(selectStatement))
+    if (executeQueryAndLogError(query))
     {
-        logError(query);
-        return voiLuts;
+        while (query.next())
+        {
+            voiLutList.append(getVoiLut(query.value("Lut").toByteArray()));
+        }
     }
 
-    while (query.next())
-    {
-        voiLuts.append(getVoiLut(query));
-    }
-
-    return voiLuts;
+    return voiLutList;
 }
 
 } // namespace udg
