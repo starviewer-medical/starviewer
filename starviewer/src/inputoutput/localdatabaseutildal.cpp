1b171264750f5f3a092154680d34df62416a7f2c
Marc Ruiz <marc.ruiz@gmail.com>
2015-12-17 15:26:45 +0100

    ENH: Refactor LocalDatabaseUtilDAL.

--- starviewer/src/inputoutput/localdatabaseutildal.cpp
+++ starviewer/src/inputoutput/localdatabaseutildal.cpp
@@ -12,82 +12,46 @@
   terms contained in the LICENSE file.
  *************************************************************************************/
 
-#include <QString>
-#include <QRegExp>
+#include "localdatabaseutildal.h"
+
 #include <QSqlQuery>
 #include <QVariant>
 
-#include "localdatabaseutildal.h"
-#include "databaseconnection.h"
-#include "logging.h"
-
 namespace udg {
 
-LocalDatabaseUtilDAL::LocalDatabaseUtilDAL(DatabaseConnection &dbConnection)
- : LocalDatabaseBaseDAL(dbConnection)
+LocalDatabaseUtilDAL::LocalDatabaseUtilDAL(DatabaseConnection &databaseConnection)
+ : LocalDatabaseBaseDAL(databaseConnection)
 {
 }
 
-void LocalDatabaseUtilDAL::compact()
+bool LocalDatabaseUtilDAL::compact()
 {
-    QString compactSentence = "vacuum";
-    QSqlQuery query;
-
-    if (!query.exec(compactSentence))
-    {
-        logError(query);
-    }
+    QSqlQuery query = getNewQuery();
+    query.prepare("VACUUM");
+    return executeQueryAndLogError(query);
 }
 
 int LocalDatabaseUtilDAL::getDatabaseRevision()
 {
-    QSqlQuery query;
+    QSqlQuery query = getNewQuery();
+    query.prepare("SELECT Revision FROM DatabaseRevision");
 
-    if (!query.exec(buildSqlGetDatabaseRevision()))
+    if (executeQueryAndLogError(query) && query.next())
     {
-        logError(query);
-        return -1;
-    }
-
-    if (query.next())
-    {
-        QRegExp rexRevisionDatabase("\\d+");//La Revisió es guarda en el format $Revision \d+ $, nosaltres només volem el número per això busquem el \d+
-        int pos = rexRevisionDatabase.indexIn(query.value("Revision").toString());
-
-        if (pos > -1)
-        {
-            return rexRevisionDatabase.cap(0).toInt();
-        }
-        else
-        {
-            return -1;
-        }
+        return query.value(0).toInt();
     }
     else
     {
-        // Si no trobem la fila amb la revisió de la base de dades retornem -1
         return -1;
     }
 }
 
-void LocalDatabaseUtilDAL::updateDatabaseRevision(int databaseRevision)
+bool LocalDatabaseUtilDAL::updateDatabaseRevision(int databaseRevision)
 {
-    QSqlQuery query;
-
-    if (!query.exec(buildSqlUpdateDatabaseRevision(databaseRevision)))
-    {
-        logError(query);
-    }
-}
-
-QString LocalDatabaseUtilDAL::buildSqlGetDatabaseRevision()
-{
-    return "select * from DatabaseRevision";
-}
-
-QString LocalDatabaseUtilDAL::buildSqlUpdateDatabaseRevision(int databaseRevision)
-{
-    return "Update DatabaseRevision Set Revision = " + QString::number(databaseRevision);
+    QSqlQuery query = getNewQuery();
+    query.prepare("UPDATE DatabaseRevision SET Revision = :revision");
+    query.bindValue(0, databaseRevision);
+    return executeQueryAndLogError(query);
 }
 
 }
