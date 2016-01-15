2de3ef147184836947578a621fc30e79b668bd7b
scalahater <scalahater@gmail.com>
2016-01-15 11:20:32 +0100

    ENH: C++11 Support on sdk build scripts on GNU/Linux
    New features on setup.sh are:
    
    - c++11 support
    - Ability to use a different compiler like clang
    - Configurable verbose output
    - Make number of jobs
    - Support for distcc like tools

--- sdk/scripts/linux/libs/cmakebuild.sh
+++ sdk/scripts/linux/libs/cmakebuild.sh
@@ -18,21 +18,21 @@ then
     SOURCE_DIR=$PATCHED_SOURCE_DIR
 fi
 
-cmake $CMAKE_OPTIONS $SOURCE_DIR
+cmake $CMAKE_OPTIONS $CMAKE_DISTCC $CMAKE_COMPILER $SOURCE_DIR
 if [ $? -ne 0 ]
 then
     echo "ERROR: CMake of $LIB failed"
     exit
 fi
 
-make -j4
+make -j$MAKE_CONCURRENCY $MAKE_VERBOSE
 if [ $? -ne 0 ]
 then
     echo "ERROR: Compilation of $LIB failed"
     exit
 fi
 
-make install
+make $MAKE_VERBOSE install
 if [ $? -ne 0 ]
 then
     echo "ERROR: Installation phase of $LIB failed"
