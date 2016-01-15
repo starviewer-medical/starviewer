121767d5bee37699d5edbc47f37e1448bacca38c
scalahater <scalahater@gmail.com>
2016-01-15 11:20:32 +0100

    ENH: C++11 Support on sdk build scripts on GNU/Linux
    New features on setup.sh are:
    
    - c++11 support
    - Ability to use a different compiler like clang
    - Configurable verbose output
    - Make number of jobs
    - Support for distcc like tools

--- sdk/scripts/linux/config/setup.sh
+++ sdk/scripts/linux/config/setup.sh
@@ -26,6 +26,21 @@ SOURCE_DIR_BASE=$SDK_INSTALL_PREFIX/src
 # Location of the pathes to apply on SDK libraries
 PATCHES_ROOT=$SCRIPTS_ROOT/../../patches
 
+# Uncomment if you want to use distcc (distributed C compiler) on CMake.
+#CMAKE_DISTCC='-DCMAKE_CXX_COMPILER_LAUNCHER:STRING=distcc -DCMAKE_C_COMPILER_LAUNCHER:STRING=distcc'
+
+# c++11 support
+CMAKE_CPP11='-DCMAKE_CXX_STANDARD:STRING=11 -DCMAKE_CXX_STANDARD_REQUIRED:BOOL=ON -DCMAKE_C_STANDARD:STRING=11 -DCMAKE_C_STANDARD_REQUIRED:BOOL=ON'
+
+# Uncomment to enable compilation with clang
+#CMAKE_COMPILER='-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang'
+
+# Verbose compilation: Uncomment to output the compiler calls.
+#MAKE_VERBOSE='VERBOSE=yes'
+
+# Number of simultaneous make jobs (-j8)
+MAKE_CONCURRENCY=8
+
 # Because SDK libraries binares are not on a standard location.
 # This environment variable has to be set when starting starviewer binary or
 # when you compile it. (if not the linker will fail).
