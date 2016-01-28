bcecbd554e4831b42b7b30ce9c2f96c51d5a14c5
Marc Ruiz <marc.ruiz@gmail.com>
2016-01-28 12:06:12 +0100

    ENH: Simplify VolumeFillerStep: the volume assignation for enhanced images is the same as for multiframes.

--- starviewer/src/core/volumefillerstep.cpp
+++ starviewer/src/core/volumefillerstep.cpp
@@ -26,13 +26,6 @@ namespace udg {
 
 namespace {
 
-bool isEnhancedImageSOPClass(const QString &sopClassUID)
-{
-    return (sopClassUID == UIDEnhancedCTImageStorage || sopClassUID == UIDEnhancedMRImageStorage || sopClassUID == UIDEnhancedXAImageStorage ||
-        sopClassUID == UIDEnhancedXRFImageStorage || sopClassUID == UIDEnhancedUSVolumeStorage || sopClassUID == UIDEnhancedMRColorImageStorage ||
-        sopClassUID == UIDEnhancedPETImageStorage || sopClassUID == UIDBreastTomosynthesisImageStorage);
-}
-
 // Ens diu si les imatges són de mides diferents
 bool areOfDifferentSize(Image *firstImage, Image *secondImage)
 {
@@ -87,91 +80,64 @@ QString VolumeFillerStep::name()
 
 void VolumeFillerStep::processDICOMFile(const DICOMTagReader *dicomReader)
 {
-    QList<Image*> currentImages = m_input->getCurrentImages();
-
-    // Comprovem si la imatge és enhanced o no per tal de cridar el mètode específic més adient
-    if (isEnhancedImageSOPClass(dicomReader->getValueAttributeAsQString(DICOMSOPClassUID)))
+    int numberOfFrames = 1;
+    int volumeNumber = m_input->getCurrentSingleFrameVolumeNumber();
+    if (dicomReader->tagExists(DICOMNumberOfFrames))
     {
-        processEnhancedDICOMFile(dicomReader);
-    }
-    else
-    {
-        int numberOfFrames = 1;
-        int volumeNumber = m_input->getCurrentSingleFrameVolumeNumber();
-        if (dicomReader->tagExists(DICOMNumberOfFrames))
+        numberOfFrames = dicomReader->getValueAttributeAsQString(DICOMNumberOfFrames).toInt();
+        // Si és la segona imatge multiframe que ens trobem, augmentarem el número que identifica l'actual volum
+        if (m_input->currentSeriesContainsAMultiframeVolume())
         {
-            numberOfFrames = dicomReader->getValueAttributeAsQString(DICOMNumberOfFrames).toInt();
-            // Si és la segona imatge multiframe que ens trobem, augmentarem el número que identifica l'actual volum
-            if (m_input->currentSeriesContainsAMultiframeVolume())
-            {
-                m_input->increaseCurrentMultiframeVolumeNumber();
-            }
-            volumeNumber = m_input->getCurrentMultiframeVolumeNumber();
+            m_input->increaseCurrentMultiframeVolumeNumber();
         }
-
-        for (int frameNumber = 0; frameNumber < numberOfFrames; frameNumber++)
-        {
-            Image *image = currentImages.at(frameNumber);
-
-            // Setting volume number
-
-            // Comprovem si les imatges són de diferents mides per assignar-lis volums diferents
-            // Això només passarà quan les imatges siguin single-frame
-            if (numberOfFrames == 1)
-            {
-                int imagesInCurrentFile = currentImages.size();
-                int imagesInCurrentSeries = m_input->getCurrentSeries()->getImages().size();
-
-                if (imagesInCurrentSeries > imagesInCurrentFile)
-                {
-                    // Si la imatge anterior i l'actual tenen mides diferents, aniran en un volum diferent
-                    Image *lastProcessedImage = m_input->getCurrentSeries()->getImages().at(imagesInCurrentSeries - imagesInCurrentFile - 1);
-                    if (areOfDifferentSize(lastProcessedImage, image) || areOfDifferentPhotometricInterpretation(lastProcessedImage, image)
-                        || areOfDifferentPixelSpacing(lastProcessedImage, image))
-                    {
-                        m_input->increaseCurrentSingleFrameVolumeNumber();
-                        volumeNumber = m_input->getCurrentSingleFrameVolumeNumber();
-                        // Actualitzem el número actual de volum i guardem el corresponent thumbnail
-                        m_input->setCurrentVolumeNumber(volumeNumber);
-                        // HACK Si és la segona imatge de mida diferent, cal generar el propi thumbnail de la imatge anterior
-                        if (volumeNumber == 101)
-                        {
-                            QString path = QString("%1/thumbnail%2.png").arg(QFileInfo(lastProcessedImage->getPath()).absolutePath()).arg(
-                                                   lastProcessedImage->getVolumeNumberInSeries());
-                            ThumbnailCreator().getThumbnail(lastProcessedImage).save(path, "PNG");
-                        }
-                        saveThumbnail(dicomReader);
-                    }
-                }
-            }
-            image->setVolumeNumberInSeries(volumeNumber);
-        }
-        m_input->setCurrentVolumeNumber(volumeNumber);
+        volumeNumber = m_input->getCurrentMultiframeVolumeNumber();
     }
-
-    if (currentImages.count() > 1)
-    {
-        // Com que la imatge és multiframe (tant si és enhanced com si no) creem els corresponents thumbnails i els guardem a la cache
-        saveThumbnail(dicomReader);
-    }
-}
-
-void VolumeFillerStep::processEnhancedDICOMFile(const DICOMTagReader *dicomReader)
-{
-    int numberOfFrames = dicomReader->getValueAttributeAsQString(DICOMNumberOfFrames).toInt();
-    // Si és la segona imatge enhanced que ens trobem, augmentarem el número que identifica l'actual volum
-    if (m_input->currentSeriesContainsAMultiframeVolume())
-    {
-        m_input->increaseCurrentMultiframeVolumeNumber();
-    }
-    m_input->setCurrentVolumeNumber(m_input->getCurrentMultiframeVolumeNumber());
 
     QList<Image*> currentImages = m_input->getCurrentImages();
 
     for (int frameNumber = 0; frameNumber < numberOfFrames; frameNumber++)
     {
         Image *image = currentImages.at(frameNumber);
-        image->setVolumeNumberInSeries(m_input->getCurrentVolumeNumber());
+
+        // Setting volume number
+
+        // Comprovem si les imatges són de diferents mides per assignar-lis volums diferents
+        // Això només passarà quan les imatges siguin single-frame
+        if (numberOfFrames == 1)
+        {
+            int imagesInCurrentFile = currentImages.size();
+            int imagesInCurrentSeries = m_input->getCurrentSeries()->getImages().size();
+
+            if (imagesInCurrentSeries > imagesInCurrentFile)
+            {
+                // Si la imatge anterior i l'actual tenen mides diferents, aniran en un volum diferent
+                Image *lastProcessedImage = m_input->getCurrentSeries()->getImages().at(imagesInCurrentSeries - imagesInCurrentFile - 1);
+                if (areOfDifferentSize(lastProcessedImage, image) || areOfDifferentPhotometricInterpretation(lastProcessedImage, image)
+                    || areOfDifferentPixelSpacing(lastProcessedImage, image))
+                {
+                    m_input->increaseCurrentSingleFrameVolumeNumber();
+                    volumeNumber = m_input->getCurrentSingleFrameVolumeNumber();
+                    // Actualitzem el número actual de volum i guardem el corresponent thumbnail
+                    m_input->setCurrentVolumeNumber(volumeNumber);
+                    // HACK Si és la segona imatge de mida diferent, cal generar el propi thumbnail de la imatge anterior
+                    if (volumeNumber == 101)
+                    {
+                        QString path = QString("%1/thumbnail%2.png").arg(QFileInfo(lastProcessedImage->getPath()).absolutePath()).arg(
+                                               lastProcessedImage->getVolumeNumberInSeries());
+                        ThumbnailCreator().getThumbnail(lastProcessedImage).save(path, "PNG");
+                    }
+                    saveThumbnail(dicomReader);
+                }
+            }
+        }
+        image->setVolumeNumberInSeries(volumeNumber);
+    }
+    m_input->setCurrentVolumeNumber(volumeNumber);
+
+    if (currentImages.count() > 1)
+    {
+        // Com que la imatge és multiframe (tant si és enhanced com si no) creem els corresponents thumbnails i els guardem a la cache
+        saveThumbnail(dicomReader);
     }
 }
 
