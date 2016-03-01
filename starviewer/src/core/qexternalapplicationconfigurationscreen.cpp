c72b10555dbbc0ca3137d05c491d597ece326518
scalahater <scalahater@gmail.com>
2016-03-01 13:07:52 +0100

    ENH: External application configuration table is editable.
    The UI implementation works, and the buttons are disabled and enabled when necessary.

--- starviewer/src/core/qexternalapplicationconfigurationscreen.cpp
+++ starviewer/src/core/qexternalapplicationconfigurationscreen.cpp
@@ -12,6 +12,7 @@
   terms contained in the LICENSE file.
  *************************************************************************************/
 
+#include "logging.h"
 #include "qexternalapplicationconfigurationscreen.h"
 
 namespace udg {
@@ -20,6 +21,7 @@ QExternalApplicationConfigurationScreen::QExternalApplicationConfigurationScreen
  : QWidget(parent)
 {
     setupUi(this);
+    checkGrayeds();
 }
 
 QExternalApplicationConfigurationScreen::~QExternalApplicationConfigurationScreen()
@@ -27,5 +29,73 @@ QExternalApplicationConfigurationScreen::~QExternalApplicationConfigurationScree
 
 }
 
+void QExternalApplicationConfigurationScreen::moveItem(int shift)
+{
+    //This wants to be understandable code:
+    //Do not blame me for efficiency, compiler will make it efficient.
+    int currentRow = tableWidget->currentRow();
+    int currentCol = tableWidget->currentColumn();
+    if (currentRow >= 0) {
+        int maxRow = tableWidget->rowCount()-1;
+        int minRow = 0;
+        int swappingRow = qMin(maxRow,qMax(currentRow + shift,minRow));
+
+        QTableWidgetItem* currentRowCol0 = tableWidget->takeItem(currentRow,0);
+        QTableWidgetItem* currentRowCol1 = tableWidget->takeItem(currentRow,1);
+        QTableWidgetItem* swappingRowCol0 = tableWidget->takeItem(swappingRow,0);
+        QTableWidgetItem* swappingRowCol1 = tableWidget->takeItem(swappingRow,1);
+
+        tableWidget->setItem(currentRow,0,swappingRowCol0);
+        tableWidget->setItem(currentRow,1,swappingRowCol1);
+        tableWidget->setItem(swappingRow,0,currentRowCol0);
+        tableWidget->setItem(swappingRow,1,currentRowCol1);
+
+        tableWidget->setCurrentItem(tableWidget->item(swappingRow,currentCol));
+    }
+}
+
+void QExternalApplicationConfigurationScreen::checkGrayeds()
+{
+    int maxRow = tableWidget->rowCount() - 1;
+    int currentRow = tableWidget->currentRow();
+
+    btnDelete->setEnabled(currentRow >= 0);
+    btnUp->setEnabled(currentRow > 0 && currentRow <= maxRow);
+    btnDown->setEnabled(currentRow >= 0 && currentRow < maxRow);
+}
+
+void QExternalApplicationConfigurationScreen::on_btnUp_clicked() 
+{
+    moveItem(-1);
+}
+
+void QExternalApplicationConfigurationScreen::on_btnDown_clicked() 
+{
+    moveItem(1);
+}
+
+void QExternalApplicationConfigurationScreen::on_btnAdd_clicked() 
+{
+    QTableWidgetItem* nameWidget = new QTableWidgetItem("New application");
+    QTableWidgetItem* urlWidget = new QTableWidgetItem("http://www.starviewer.org");
+    tableWidget->insertRow(tableWidget->rowCount());
+    tableWidget->setItem(tableWidget->rowCount()-1,0,nameWidget);
+    tableWidget->setItem(tableWidget->rowCount()-1,1,urlWidget);
+    checkGrayeds();
+}
+
+void QExternalApplicationConfigurationScreen::on_btnDelete_clicked() 
+{
+    int rowToRemove = tableWidget->currentRow();
+    if (rowToRemove >= 0) {
+        tableWidget->removeRow(rowToRemove);
+        checkGrayeds();
+    }
+}
+
+void QExternalApplicationConfigurationScreen::on_tableWidget_currentItemChanged(QTableWidgetItem* current, QTableWidgetItem* previous)
+{
+     checkGrayeds();
+}
 
 }
