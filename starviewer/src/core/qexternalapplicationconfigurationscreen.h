0040d1e195f4d81aa56b9e399d5246232bc469a5
scalahater <scalahater@gmail.com>
2016-03-01 13:07:52 +0100

    ENH: External application configuration table is editable.
    The UI implementation works, and the buttons are disabled and enabled when necessary.

--- starviewer/src/core/qexternalapplicationconfigurationscreen.h
+++ starviewer/src/core/qexternalapplicationconfigurationscreen.h
@@ -20,13 +20,41 @@
 namespace udg {
 
 /**
-    Pantalla de configuració de layouts del visor 2D
+    @brief External application configuration GUI
+
+    This GUI is used to edit the external applications list. When properly
+    configured, the entries set here appear on a menu.
  */
 class QExternalApplicationConfigurationScreen : public QWidget, private Ui::QExternalApplicationConfigurationScreenBase {
 Q_OBJECT
 public:
     QExternalApplicationConfigurationScreen(QWidget *parent = 0);
     ~QExternalApplicationConfigurationScreen();
+
+private:
+    /**
+     * @brief Moves a table row up or down according to a given shift.
+     * @param shift Amount of
+     */
+    void moveItem(int shift = -1);
+
+    /**
+     * @brief Enables or disables the buttons.
+     *
+     * Checks the situation with the table widget and enables or disables up,
+     * down and delete buttons.
+     * They are only enabled when needed.
+     *
+     * You should invoke this method when the table data changes.
+     */
+    void checkGrayeds();
+
+private slots:
+    void on_btnUp_clicked();
+    void on_btnDown_clicked();
+    void on_btnAdd_clicked();
+    void on_btnDelete_clicked();
+    void on_tableWidget_currentItemChanged(QTableWidgetItem* current, QTableWidgetItem* previous);
 
 };
 
