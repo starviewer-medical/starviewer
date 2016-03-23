84282848b3f72f55051c5e7f0edc3c94ea9c8a83
scalahater <scalahater@gmail.com>
2016-03-23 12:15:43 +0100

    BUG: External applications parameters, and special UIDs.
    Parameter sanitization feature has been removed because some patients UIDs contained special characters.
    
    The security warning on the UI has been modified.

--- starviewer/src/interface/interface_en_GB.ts
+++ starviewer/src/interface/interface_en_GB.ts
@@ -269,7 +269,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="548"/>
+        <location filename="qapplicationmainwindow.cpp" line="550"/>
         <source>Language Switch</source>
         <translation type="unfinished"></translation>
     </message>
@@ -463,30 +463,30 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="515"/>
+        <location filename="qapplicationmainwindow.cpp" line="517"/>
         <source>Switch to %1 language</source>
         <oldsource>Switch to %1 Language</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="548"/>
+        <location filename="qapplicationmainwindow.cpp" line="550"/>
         <source>Changes will take effect the next time you start the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="707"/>
+        <location filename="qapplicationmainwindow.cpp" line="710"/>
         <source>Beta Version</source>
         <oldsource>Beta version</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="708"/>
+        <location filename="qapplicationmainwindow.cpp" line="711"/>
         <source>&lt;h2&gt;%1&lt;/h2&gt;&lt;p align=&apos;justify&apos;&gt;This is a preview release of %1 used exclusively for testing purposes.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;This version is intended for radiologists and our test-team members. Users of this version should not expect extensions to function properly.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;If you want to help us to improve %1, please report any found bug or any feature request you may have by sending an e-mail to: &lt;a href=&quot;mailto:%2&quot;&gt;%2&lt;/a&gt;&lt;/p&gt;&lt;h3&gt;We really appreciate your feedback!&lt;/h3&gt;</source>
         <oldsource>&lt;h2&gt;%1&lt;/h2&gt;&lt;p align=&apos;justify&apos;&gt;This is a preview release of %1 used exclusively for testing purposes.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;This version is intended for radiologists and our test-team members. Users of this version should not expect the extensions function properly.&lt;/p&gt;&lt;p align=&apos;justify&apos;&gt;If you want to help us to improve %1, please report any found bug or any feature request you may have by sending an e-mail to: &lt;a href=&quot;mailto:%2&quot;&gt;%2&lt;/a&gt;&lt;/p&gt;&lt;h3&gt;We really appreciate your feedback!&lt;/h3&gt;</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qapplicationmainwindow.cpp" line="779"/>
+        <location filename="qapplicationmainwindow.cpp" line="782"/>
         <source>Please activate &quot;Listen to RIS requests&quot; option in %1 configuration to retrieve studies from SAP.</source>
         <oldsource>Please activate &quot;Listen RIS Request&quot; option in %1 configuration to retrieve studies from SAP.</oldsource>
         <translation type="unfinished"></translation>
