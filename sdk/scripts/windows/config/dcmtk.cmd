b32a23a89901f5ded6592fddbceb4c1020663ed1
Marc Ruiz <marc.ruiz@gmail.com>
2016-05-23 18:44:41 +0200

    ENH: Update SDK Windows scripts to build with Qt 5.6.
    Some default paths have also been changed.

--- sdk/scripts/windows/config/dcmtk.cmd
+++ sdk/scripts/windows/config/dcmtk.cmd
@@ -1,23 +1,25 @@
+REM Call zlib configuration script to make sure ZlibDir is set
 call "%ScriptsRoot%\config\zlib.cmd"
 
-set SourceDir=%SourceDirBase%\dcmtk-3.6.1_20120515\dcmtk-3.6.1_20120515
+set SourceDir=%SourceDirPrefix%\dcmtk-3.6.1_20120515
+set BuildDir=%BuildDirPrefix%\dcmtk-3.6.1_20120515
 
 if %BuildType% == debug (
     set CMakeBuildType=Debug
-    set BuildDir=%SourceDir%-build-debug
-    set InstallPrefix=%SystemDrive%/dcmtk/3.6.1_20120515d-%Arch%
+    set BuildDir=%BuildDir%-deb
+    set InstallPrefix=%SdkInstallPrefix%/dcmtk/3.6.1_20120515d
 )
 if %BuildType% == release (
     set CMakeBuildType=RelWithDebInfo
-    set BuildDir=%SourceDir%-build-release
-    set InstallPrefix=%SystemDrive%/dcmtk/3.6.1_20120515-%Arch%
+    set BuildDir=%BuildDir%-rel
+    set InstallPrefix=%SdkInstallPrefix%/dcmtk/3.6.1_20120515
 )
 
 REM ============== Nothing should need to be changed below this line ==============
 
 set CMakeOptions=-DCMAKE_BUILD_TYPE:STRING=%CMakeBuildType% ^
                  -DCMAKE_INSTALL_PREFIX:PATH="%InstallPrefix%" ^
-				 -DDCMTK_WITH_PRIVATE_TAGS:BOOL="1" ^
-				 -DDCMTK_WITH_ZLIB:BOOL="1" ^
-				 -DWITH_ZLIBINC:PATH="%ZlibDir%" ^
-				 -DDCMTK_WITH_SNDFILE:BOOL="0" 
+                 -DDCMTK_WITH_PRIVATE_TAGS:BOOL="1" ^
+                 -DDCMTK_WITH_ZLIB:BOOL="1" ^
+                 -DWITH_ZLIBINC:PATH="%ZlibDir%" ^
+                 -DDCMTK_WITH_SNDFILE:BOOL="0" 
