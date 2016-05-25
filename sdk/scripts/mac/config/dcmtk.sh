4e40465e306c805626589ee743db8ce2d50af6c6
Marc Ruiz <marc.ruiz@gmail.com>
2016-05-25 13:39:06 +0200

    ENH: Update SDK Mac scripts to build with Qt 5.6.
    Some default paths have also been changed.

--- sdk/scripts/mac/config/dcmtk.sh
+++ sdk/scripts/mac/config/dcmtk.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-SourceDir="$SourceDirBase/dcmtk-3.6.1_20120515/dcmtk-3.6.1_20120515"
+SourceDir="$SourceDirBase/dcmtk-3.6.1_20120515"
 
 if [ $BuildType = "debug" ]
 then
     CMakeBuildType="Debug"
     BuildDir="$SourceDir-build-debug"
-    InstallPrefix="/usr/local/"
 fi
 if [ $BuildType = "release" ]
 then
     CMakeBuildType="RelWithDebInfo"
     BuildDir="$SourceDir-build-release"
-    InstallPrefix="/usr/local/"
 fi
 
 # ============== Nothing should need to be changed below this line ==============
 
 CMakeOptions="-DCMAKE_BUILD_TYPE:STRING=$CMakeBuildType \
-                -DCMAKE_INSTALL_PREFIX:PATH=$InstallPrefix \
-                -DDCMTK_WITH_TIFF:BOOL=OFF \
-                -DDCMTK_WITH_ICONV:BOOL=OFF"
+              -DCMAKE_INSTALL_PREFIX:PATH=$SdkInstallPrefix \
+              -DCMAKE_PREFIX_PATH:PATH=$QTDIR \
+              -DDCMTK_WITH_PRIVATE_TAGS:BOOL=TRUE \
+              -DDCMTK_WITH_ZLIB:BOOL=TRUE \
+              -DDCMTK_WITH_SNDFILE:BOOL=FALSE \
+              -DDCMTK_WITH_TIFF:BOOL=FALSE \
+              -DDCMTK_WITH_ICONV:BOOL=FALSE"
