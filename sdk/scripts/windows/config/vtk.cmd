2a9b70720fbd458933ce1b217dd63faf001c6a82
Marc Ruiz <marc.ruiz@gmail.com>
2016-06-21 18:45:55 +0200

    ENH: Update SDK Windows scripts to build with VTK 7.0.

--- sdk/scripts/windows/config/vtk.cmd
+++ sdk/scripts/windows/config/vtk.cmd
@@ -1,15 +1,15 @@
-set SourceDir=%SourceDirPrefix%\VTK-6.1.0
-set BuildDir=%BuildDirPrefix%\VTK-6.1.0
+set SourceDir=%SourceDirPrefix%\VTK-7.0.0
+set BuildDir=%BuildDirPrefix%\VTK-7.0.0
 
 if %BuildType% == debug (
     set CMakeBuildType=Debug
     set BuildDir=%BuildDir%-deb
-    set InstallPrefix=%SdkInstallPrefix%/VTK/6.1.0d
+    set InstallPrefix=%SdkInstallPrefix%/VTK/7.0.0d
 )
 if %BuildType% == release (
     set CMakeBuildType=RelWithDebInfo
     set BuildDir=%BuildDir%-rel
-    set InstallPrefix=%SdkInstallPrefix%/VTK/6.1.0
+    set InstallPrefix=%SdkInstallPrefix%/VTK/7.0.0
 )
 
 REM ============== Nothing should need to be changed below this line ==============
@@ -24,4 +24,4 @@ set CMakeOptions=-DCMAKE_BUILD_TYPE:STRING=%CMakeBuildType% ^
                  -DModule_vtkViewsQt:BOOL=TRUE ^
                  -DVTK_QT_VERSION:STRING=5
 
-set VtkDir=%InstallPrefix%/lib/cmake/vtk-6.1
+set VtkDir=%InstallPrefix%/lib/cmake/vtk-7.0
