4a8cb871cc0a85d5524b4c88640bea55abda6aa9
Scala Hater <scalahater@gmail.com>
2016-09-05 18:35:35 +0200

    ENH: Update crash reporter translations.

--- starviewer/src/crashreporter/crashreporter_ca_ES.ts
+++ starviewer/src/crashreporter/crashreporter_ca_ES.ts
@@ -4,60 +4,61 @@
 <context>
     <name>QCrashReporterBase</name>
     <message>
-        <location filename="qcrashreporterbase.ui" line="28"/>
         <source>The application Starviewer crashed</source>
         <translation>L&apos;aplicació Starviewer s&apos;ha tancat inesperadament</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="41"/>
         <source>We&apos;re sorry Starviewer had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
         <translation>L&apos;Starviewer ha tingut algun problema i s&apos;ha tancat inesperadament. Dediqueu un moment per enviar un informe de la incidència per ajudar l&apos;equip de desenvolupament a diagnosticar i arreglar el problema. La vostra informació personal no s&apos;enviarà en aquest informe.</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="95"/>
         <source>Tell Starviewer about this crash</source>
         <translation>Informa d&apos;aquesta incidència</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="112"/>
         <source>Comments</source>
         <translation>Comentaris</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="118"/>
         <source>Please describe any steps needed to trigger the problem (Optional):</source>
         <translation>Descriviu els passos per poder reproduir el problema (opcional):</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="132"/>
         <source>Email me when more information is available (Optional):</source>
         <translation>Vull rebre un correu electrònic quan hi hagi més informació disponible sobre aquest problema (opcional):</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="156"/>
         <source>Send additional host information. This information will be sent:</source>
         <translation>Envia informació addicional sobre l&apos;ordinador. S&apos;enviarà la següent informació:</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="194"/>
         <source>Sending report...</source>
         <translation>S&apos;està enviant l&apos;informe...</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="221"/>
         <source>Quit Starviewer</source>
         <translation>Surt de l&apos;Starviewer</translation>
     </message>
     <message>
-        <location filename="qcrashreporterbase.ui" line="228"/>
         <source>Restart Starviewer</source>
         <translation>Reinicia l&apos;Starviewer</translation>
+    </message>
+    <message>
+        <source>Crash report could not be sent.</source>
+        <translation>No s&apos;ha pogut enviar l&apos;informe de fallada.</translation>
+    </message>
+    <message>
+        <source>Crash report sent.</source>
+        <translation>S&apos;ha enviat l&apos;informe de fallada.</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel·la</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../core/coresettings.cpp" line="129"/>
         <source>PNG (*.png)</source>
         <translation>PNG (*.png)</translation>
     </message>
@@ -65,46 +66,54 @@
 <context>
     <name>udg::QCrashReporter</name>
     <message>
-        <location filename="qcrashreporter.cpp" line="35"/>
         <source>%1</source>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="36"/>
         <source>Quit %1</source>
         <translation>Surt de l&apos;%1</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="37"/>
         <source>Restart %1</source>
         <translation>Reinicia l&apos;%1</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="38"/>
         <source>Tell %1 about this crash</source>
         <translation>Notifica l&apos;%1 sobre aquest problema</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="39"/>
         <source>We&apos;re sorry %1 had a problem and crashed. Please take a moment to send us a crash report to help us diagnose and fix the problem. Your personal information is not sent with this report.  </source>
         <translation>L&apos;%1 ha tingut algun problema i s&apos;ha tancat inesperadament. Dediqueu un moment per enviar un informe de la incidència per ajudar l&apos;equip de desenvolupament a diagnosticar i arreglar el problema. La vostra informació personal no s&apos;enviarà en aquest informe.</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="75"/>
         <source>IP</source>
         <translation>IP</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="82"/>
         <source>User Domain</source>
         <translatorcomment>No s&apos;ha de traduir</translatorcomment>
         <translation>User Domain</translation>
     </message>
     <message>
-        <location filename="qcrashreporter.cpp" line="139"/>
         <source>Host information</source>
         <translatorcomment>No s&apos;ha de traduir</translatorcomment>
         <translation>Host information</translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <translation>Correcte</translation>
+    </message>
+    <message>
+        <source>The crash report has been sent successfully.</source>
+        <translation>L&apos;informe de fallada s&apos;ha enviat.</translation>
+    </message>
+    <message>
+        <source>Send error</source>
+        <translation>Error en enviar</translation>
+    </message>
+    <message>
+        <source>The crash report could not be sent to Starviewer developers because [%1] error happened, do you want to try it again?</source>
+        <translation>L&apos;informe de fallada no s&apos;ha pogut enviar correctament als desenvolupadors de Starviewer degut al següent error [%1], voleu intentar-ho de nou?</translation>
     </message>
 </context>
 </TS>
