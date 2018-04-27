df5afc10189b93d106dc928a7cce5a0862ab2943
Marc Ruiz <marc.ruiz@gmail.com>
2018-04-27 13:09:17 +0200

    ENH: Unify SDK build script (part 1: dcmtk). [skip ci]

---
+++ sdk/scripts/unix/config/dcmtk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SOURCE_DIR="$SOURCE_DIR_BASE/dcmtk-3.6.1_20120515"
+
+if [ $BUILD_TYPE = debug ]
+then
+    CMAKE_BUILD_TYPE=Debug
+    BUILD_DIR="$SOURCE_DIR-build-debug"
+fi
+if [ $BUILD_TYPE = release ]
+then
+    CMAKE_BUILD_TYPE=RelWithDebInfo
+    BUILD_DIR="$SOURCE_DIR-build-release"
+fi
+
+################ Nothing should need to be changed below this line ################
+
+CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE:STRING=$CMAKE_BUILD_TYPE \
+               -DCMAKE_INSTALL_PREFIX:PATH=$SDK_INSTALL_PREFIX \
+               -DDCMTK_WITH_PRIVATE_TAGS:BOOL=TRUE \
+               -DDCMTK_WITH_ZLIB:BOOL=TRUE \
+               -DDCMTK_WITH_SNDFILE:BOOL=FALSE \
+               -DDCMTK_WITH_OPENSSL:BOOL=FALSE \
+               -DDCMTK_WITH_TIFF:BOOL=FALSE \
+               -DDCMTK_WITH_ICONV:BOOL=FALSE"
+
+DCMTKLIBDIR="$SDK_INSTALL_PREFIX/lib"
+DCMTKINCLUDEDIR="$SDK_INSTALL_PREFIX/include/dcmtk"
---
+++ sdk/scripts/unix/libs/cmakebuild.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PATCHES_ROOT="$SCRIPTS_ROOT/../../patches"
+
+mkdir -p "$BUILD_DIR"
+pushd "$BUILD_DIR"
+
+if [[ -d "$PATCHES_ROOT/$LIB" ]]
+then
+    PATCHED_SOURCE_DIR="$SOURCE_DIR-patched"
+    rsync -a --delete "$SOURCE_DIR/" "$PATCHED_SOURCE_DIR"
+    pushd "$PATCHED_SOURCE_DIR"
+
+    for PATCH in "$PATCHES_ROOT/$LIB"/*
+    do
+        git apply "$PATCH" --ignore-whitespace -v
+    done
+
+    popd
+    SOURCE_DIR="$PATCHED_SOURCE_DIR"
+fi
+
+$CMAKE -Wno-dev $CMAKE_OPTIONS $CMAKE_DISTCC $CMAKE_COMPILER "$SOURCE_DIR"
+if [[ $? -ne 0 ]]
+then
+    echo "ERROR: CMake of $LIB failed"
+    exit
+fi
+
+make $MAKE_CONCURRENCY $MAKE_VERBOSE
+if [[ $? -ne 0 ]]
+then
+    echo "ERROR: Compilation of $LIB failed"
+    exit
+fi
+
+make $MAKE_VERBOSE install
+if [[ $? -ne 0 ]]
+then
+    echo "ERROR: Installation phase of $LIB failed"
+    exit
+fi
+
+popd
