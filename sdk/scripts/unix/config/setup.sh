e4074626ac8ae052164d86eea29720fb6c5e6744
Marc Ruiz <marc.ruiz@gmail.com>
2018-04-27 13:09:17 +0200

    ENH: Unify SDK build script (part 1: dcmtk). [skip ci]

--- sdk/scripts/unix/config/setup.sh
+++ sdk/scripts/unix/config/setup.sh
@@ -16,9 +16,33 @@ SDK_INSTALL_PREFIX=${SDK_INSTALL_PREFIX:-"$SDK_BASE_PREFIX/usr/local"}
 SOURCE_DIR_BASE=${SOURCE_DIR_BASE:-"$SDK_INSTALL_PREFIX/src"}
 
 # List of build types to use. Possible values: debug, release.
-BUILD_TYPES="release"
+BUILD_TYPES=${BUILD_TYPES:-"release"}
+
 # List of libs to build. Possible values: dcmtk, vtk, gdcm, itk, ecm, threadweaver.
-LIBS="dcmtk vtk gdcm itk ecm threadweaver"
+LIBS=${LIBS:-"dcmtk vtk gdcm itk ecm threadweaver"}
+
+# CMake executable
+if [[ $(uname) == 'Linux' ]]
+then
+    CMAKE=${CMAKE:-cmake}
+elif [[ $(uname) == 'Darwin' ]]
+then
+    CMAKE=${CMAKE:-"/Applications/CMake.app/Contents/bin/cmake"}
+fi
+
+# Set to appropriate value (example below) if you want to use distcc (distributed C compiler) on CMake.
+CMAKE_DISTCC=${CMAKE_DISTCC:-}
+#CMAKE_DISTCC='-DCMAKE_CXX_COMPILER_LAUNCHER:STRING=distcc -DCMAKE_C_COMPILER_LAUNCHER:STRING=distcc'
+
+# Set to appropriate value (example below) to compile with non-default compiler (default is gcc on Linux and clang in Mac).
+CMAKE_COMPILER=${CMAKE_COMPILER:-}
+#CMAKE_COMPILER='-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang'
+
+# Number of simultaneous make jobs.
+MAKE_CONCURRENCY=${MAKE_CONCURRENCY:--j4}
+
+# Verbose compilation: set to 'VERBOSE=yes' to output the compiler calls.
+MAKE_VERBOSE=${MAKE_VERBOSE:-}
 
 # Where to write the SDK environment configuration script.
 SDK_ENVIRONMENT_FILE=$SCRIPTS_ROOT/../../../environment.sh
@@ -38,23 +62,8 @@ DPKG_TMP=/tmp/starviewer-dpkg
 # Where to place the packages
 DPKG_DESTINATION=$SCRIPTS_ROOT/../../../
 
-# Location of the pathes to apply on SDK libraries
-PATCHES_ROOT=$SCRIPTS_ROOT/../../patches
-
-# Uncomment if you want to use distcc (distributed C compiler) on CMake.
-#CMAKE_DISTCC='-DCMAKE_CXX_COMPILER_LAUNCHER:STRING=distcc -DCMAKE_C_COMPILER_LAUNCHER:STRING=distcc'
-
 # c++11 support
 CMAKE_CPP11='-DCMAKE_CXX_STANDARD:STRING=11 -DCMAKE_CXX_STANDARD_REQUIRED:BOOL=ON -DCMAKE_C_STANDARD:STRING=11 -DCMAKE_C_STANDARD_REQUIRED:BOOL=ON'
-
-# Uncomment to enable compilation with clang
-#CMAKE_COMPILER='-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang'
-
-# Verbose compilation: Uncomment to output the compiler calls.
-MAKE_VERBOSE='VERBOSE=yes'
-
-# Number of simultaneous make jobs (-j8)
-MAKE_CONCURRENCY=4
 
 # Because SDK libraries binares are not on a standard location.
 # This environment variable has to be set when starting starviewer binary or
