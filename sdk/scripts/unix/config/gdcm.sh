089e298dc76ac79495eb592b1e7aec5ed2251c19
Marc Ruiz <marc.ruiz@gmail.com>
2018-05-03 10:47:16 +0200

    ENH: Unify SDK build script (part 3: gdcm). [skip ci]

---
+++ sdk/scripts/unix/config/gdcm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Call vtk configuration script to make sure VTKCMAKEDIR is set
+. "$SCRIPTS_ROOT/config/vtk.sh"
+
+SOURCE_DIR="$SOURCE_DIR_BASE/GDCM-2.6.4"
+
+if [ $BUILD_TYPE = debug ]
+then
+    CMAKE_BUILD_TYPE=Debug
+    BUILD_DIR="$SOURCE_DIR-build-debug"
+fi
+if [ $BUILD_TYPE = release ]
+then
+    CMAKE_BUILD_TYPE=RelWithDebInfo
+    BUILD_DIR="$SOURCE_DIR-build-release"
+fi
+
+################ Nothing should need to be changed below this line ################
+
+CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE:STRING=$CMAKE_BUILD_TYPE \
+               -DCMAKE_INSTALL_PREFIX:PATH=$SDK_INSTALL_PREFIX \
+               -DGDCM_BUILD_SHARED_LIBS:BOOL=TRUE \
+               -DGDCM_USE_VTK:BOOL=TRUE \
+               -DVTK_DIR:PATH=$VTKCMAKEDIR"
+
+GDCMCMAKEDIR="$SDK_INSTALL_PREFIX/lib/gdcm-2.6"
+GDCMLIBDIR="$SDK_INSTALL_PREFIX/lib"
+GDCMINCLUDEDIR="$SDK_INSTALL_PREFIX/include/gdcm-2.6"
