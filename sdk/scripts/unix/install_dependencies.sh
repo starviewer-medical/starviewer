2d968e2d711b679d4024ad0cb8a001067ad4b0d5
Scala Hater <scalahater@gmail.com>
2020-04-09 18:12:18 +0200

    COMP: Add xsltproc tool dependency. #2822

--- sdk/scripts/unix/install_dependencies.sh
+++ sdk/scripts/unix/install_dependencies.sh
@@ -14,7 +14,7 @@ then
         then
             echo "Debian 10 (Buster) detected."
             echo "Please, input the password to install the dependencies required to run and compile Starviewer."
-            sudo apt-get install build-essential cmake git wget rsync
+            sudo apt-get install build-essential cmake git wget rsync xsltproc
             sudo apt-get install libssl1.1 libssl-dev zlib1g zlib1g-dev libgl1 libgl1-mesa-dev libxt6 libxt-dev libglu1-mesa libglu1-mesa-dev
         fi
     fi
