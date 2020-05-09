630d83c40d06ef025c8fcb97985c735d73c47d44
Scala Hater <scalahater@gmail.com>
2020-05-09 17:24:21 +0200

    ENH: Quit if Starvierwer compilation fails.
    Abort the compilation script if a Make or QMake error is detected.

--- sdk/scripts/unix/starviewer_build.sh
+++ sdk/scripts/unix/starviewer_build.sh
@@ -10,5 +10,17 @@ export LD_LIBRARY_PATH
 echo "Invoking QMake with the following extra arguments: ${STARVIEWER_QMAKE_ARGUMENTS[@]}"
 $QTDIR/bin/qmake -r ${STARVIEWER_QMAKE_ARGUMENTS[@]} $STARVIEWER_SOURCE_DIR_BASE/starviewer.pro
 
+if [[ $? -ne 0 ]]
+then
+    echo "[!] Error when trying to compile Starviewer. Quitting... [!]"
+    exit 1
+fi
+
+$MAKE -j$MAKE_CONCURRENCY VERBOSE=$MAKE_VERBOSE
+if [[ $? -ne 0 ]]
+then
+    echo "[!] Error when trying to compile Starviewer. Quitting... [!]"
+    exit 1
+fi
 
 popd
