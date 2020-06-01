starviewer-medical/starviewer

commit 96dbd02bea1992a140df7d4c8d9fa308b7f0d014

COMP: Fix warnings from deprecated declarations. #25

starviewer/src/inputoutput/qrelatedstudieswidget.cpp
@@ -20,11 +20,11 @@
 #include "relatedstudiesmanager.h"
 #include "queryscreen.h"
 #include "singleton.h"
-#include "screenmanager.h"
 #include "qtreewidgetwithseparatorline.h"
 
 #include <QVBoxLayout>
 #include <QMovie>
+#include <QScreen>
 #include <QTreeWidgetItem>
 #include <QScrollBar>
 
@@ -42,9 +42,6 @@ QRelatedStudiesWidget::QRelatedStudiesWidget(RelatedStudiesManager *relatedStudi
 
     m_lookingForStudiesWidget = new QWidget(this);
     m_relatedStudiesTree = new QTreeWidgetWithSeparatorLine(this);
-    m_signalMapper = new QSignalMapper(this);
-    m_currentStudySignalMapper = new QSignalMapper(this);
-    m_priorStudySignalMapper = new QSignalMapper(this);
     m_queryScreen = SingletonPointer<QueryScreen>::instance();
     m_numberOfDownloadingStudies = 0;
 
@@ -67,7 +64,6 @@ QRelatedStudiesWidget::~QRelatedStudiesWidget()
         delete m_infomationPerStudy.take(key);
     }
     delete m_lookingForStudiesWidget;
-    delete m_signalMapper;
 }
 
 void QRelatedStudiesWidget::updateList()
@@ -256,9 +252,6 @@ void QRelatedStudiesWidget::initializeSearch()
 void QRelatedStudiesWidget::createConnections()
 {
     connect(m_relatedStudiesManager, SIGNAL(queryStudiesFinished(QList<Study*>)), SLOT(queryStudiesFinished(QList<Study*>)));
-    connect(m_signalMapper, SIGNAL(mapped(const QString&)), SLOT(retrieveAndLoadStudy(const QString&)));
-    connect(m_currentStudySignalMapper, SIGNAL(mapped(const QString&)), SLOT(currentStudyRadioButtonClicked(const QString&)));
-    connect(m_priorStudySignalMapper, SIGNAL(mapped(const QString&)), SLOT(priorStudyRadioButtonClicked(const QString&)));
     connect(m_queryScreen, SIGNAL(studyRetrieveStarted(QString)), SLOT(studyRetrieveStarted(QString)));
     connect(m_queryScreen, SIGNAL(studyRetrieveFinished(QString)), SLOT(studyRetrieveFinished(QString)));
     connect(m_queryScreen, SIGNAL(studyRetrieveFailed(QString)), SLOT(studyRetrieveFailed(QString)));
@@ -310,6 +303,7 @@ void QRelatedStudiesWidget::initializeLookingForStudiesWidget()
 
 void QRelatedStudiesWidget::insertStudyToTree(Study *study)
 {
+    const QString &studyInstanceUid = study->getInstanceUID();
     QTreeWidgetItem *item = new QTreeWidgetItem();
 
     // Afegim l'item al widget
@@ -331,8 +325,7 @@ void QRelatedStudiesWidget::insertStudyToTree(Study *study)
     currentStudyLayout->setMargin(0);
     m_currentStudyRadioGroup.addButton(currentRadioButton);
     m_relatedStudiesTree->setItemWidget(item, CurrentStudy, currentStudyWidget);
-    connect(currentRadioButton, SIGNAL(clicked()), m_currentStudySignalMapper, SLOT(map()));
-    m_currentStudySignalMapper->setMapping(currentRadioButton, study->getInstanceUID());
+    connect(currentRadioButton, &QRadioButton::clicked, [=] { currentStudyRadioButtonClicked(studyInstanceUid); });
 
     // Add Radio Button to select prior study
     QWidget *priorStudyWidget = new QWidget(m_relatedStudiesTree);
@@ -344,8 +337,7 @@ void QRelatedStudiesWidget::insertStudyToTree(Study *study)
     priorStudyLayout->setMargin(0);
     m_priorStudyRadioGroup.addButton(priorRadioButton);
     m_relatedStudiesTree->setItemWidget(item, PriorStudy, priorStudyWidget);
-    connect(priorRadioButton, SIGNAL(clicked()), m_priorStudySignalMapper, SLOT(map()));
-    m_priorStudySignalMapper->setMapping(priorRadioButton, study->getInstanceUID());
+    connect(priorRadioButton, &QRadioButton::clicked, [=] { priorStudyRadioButtonClicked(studyInstanceUid); });
 
     QWidget *statusWidget = new QWidget(m_relatedStudiesTree);
     QHBoxLayout *statusLayout = new QHBoxLayout(statusWidget);
@@ -366,8 +358,7 @@ void QRelatedStudiesWidget::insertStudyToTree(Study *study)
     downloadButtonLayout->setContentsMargins(0, 2, 0, 1);
     downloadButtonLayout->addWidget(downloadButton);
 
-    connect(downloadButton, SIGNAL(clicked()), m_signalMapper, SLOT(map()));
-    m_signalMapper->setMapping(downloadButton, study->getInstanceUID());
+    connect(downloadButton, &QPushButton::clicked, [=] { retrieveAndLoadStudy(studyInstanceUid); });
 
     if (hasToHighlightStudy(study))
     {
@@ -409,8 +400,7 @@ void QRelatedStudiesWidget::updateWidgetWidth()
 
 void QRelatedStudiesWidget::updateWidgetHeight()
 {
-    ScreenManager screen;
-    int screenAvailableHeight = screen.getScreenLayout().getScreen(screen.getScreenID(this)).getAvailableGeometry().height();
+    int screenAvailableHeight = QGuiApplication::screenAt(this->mapToGlobal(QPoint(0, 0)))->availableSize().height();
     int topAndMargins = this->geometry().top() + m_relatedStudiesTree->geometry().top() * 2; // Es multiplica per 2 pel marge inferior.
     int maxHeight = screenAvailableHeight - topAndMargins;
     int minHeight = m_relatedStudiesTree->sizeHint().height();

starviewer/src/core/displayableid.h
@@ -41,7 +41,7 @@ namespace udg {
 class DisplayableID {
 
 public:
-    DisplayableID(const QString &id, const QString &name = QString::null);
+    explicit DisplayableID(const QString &id, const QString &name = QString());
 
     ~DisplayableID();
 

starviewer/src/core/hoverpoints.cpp
@@ -372,16 +372,6 @@ void HoverPoints::movePoint(int index, const QPointF &point, bool emitUpdate)
     }
 }
 
-inline static bool x_less_than(const QPointF &p1, const QPointF &p2)
-{
-    return p1.x() < p2.x();
-}
-
-inline static bool y_less_than(const QPointF &p1, const QPointF &p2)
-{
-    return p1.y() < p2.y();
-}
-
 void HoverPoints::firePointChange()
 {
 //    printf("HoverPoints::firePointChange(), current=%d\n", m_currentIndex);
@@ -396,11 +386,15 @@ void HoverPoints::firePointChange()
 
         if (m_sortType == XSort)
         {
-            qSort(m_points.begin(), m_points.end(), x_less_than);
+            std::sort(m_points.begin(), m_points.end(), [](const QPointF &p1, const QPointF &p2) {
+                return p1.x() < p2.x();
+            });
         }
         else if (m_sortType == YSort)
         {
-            qSort(m_points.begin(), m_points.end(), y_less_than);
+            std::sort(m_points.begin(), m_points.end(), [](const QPointF &p1, const QPointF &p2) {
+                return p1.y() < p2.y();
+            });
         }
 
         // Compensate for changed order...

starviewer/src/core/itkVolumeCalculatorImageFilter.cpp
@@ -106,7 +106,7 @@ void
 VolumeCalculatorImageFilter<TInputImage>
 ::BeforeThreadedGenerateData()
 {
-  int numberOfThreads = this->GetNumberOfThreads();
+  int numberOfThreads = this->GetNumberOfWorkUnits();
 
   // Create the thread temporaries
   m_ThreadVol = std::vector<unsigned int>(numberOfThreads, 0);

starviewer/src/core/qfusionbalancewidget.cpp
@@ -37,7 +37,7 @@ QFusionBalanceWidget::QFusionBalanceWidget(QWidget *parent)
     m_firstVolumeLabel->setAlignment(Qt::AlignCenter);
     m_firstVolumePercentLabel = new QLabel(this);
     m_firstVolumePercentLabel->setAlignment(Qt::AlignCenter);
-    m_firstVolumePercentLabel->setMinimumWidth(m_firstVolumePercentLabel->fontMetrics().width("100%"));
+    m_firstVolumePercentLabel->setMinimumWidth(m_firstVolumePercentLabel->fontMetrics().horizontalAdvance("100%"));
     QVBoxLayout *firstVolumeLayout = new QVBoxLayout();
     firstVolumeLayout->addWidget(m_firstVolumeLabel);
     firstVolumeLayout->addWidget(m_firstVolumePercentLabel);
@@ -46,7 +46,7 @@ QFusionBalanceWidget::QFusionBalanceWidget(QWidget *parent)
     m_secondVolumeLabel->setAlignment(Qt::AlignCenter);
     m_secondVolumePercentLabel = new QLabel(this);
     m_secondVolumePercentLabel->setAlignment(Qt::AlignCenter);
-    m_secondVolumePercentLabel->setMinimumWidth(m_secondVolumePercentLabel->fontMetrics().width("100%"));
+    m_secondVolumePercentLabel->setMinimumWidth(m_secondVolumePercentLabel->fontMetrics().horizontalAdvance("100%"));
     QVBoxLayout *secondVolumeLayout = new QVBoxLayout();
     secondVolumeLayout->addWidget(m_secondVolumeLabel);
     secondVolumeLayout->addWidget(m_secondVolumePercentLabel);

starviewer/src/core/qopacitytransferfunctiongraphicalview.cpp
@@ -219,9 +219,9 @@ QOpacityTransferFunctionGraphicalViewNode* QOpacityTransferFunctionGraphicalView
     // Filtrem per quedar-nos només amb els nodes
     QtConcurrent::blockingFilter(items, isNode);
     // Ordenem per x
-    qSort(items.begin(), items.end(), lessThan);
+    std::sort(items.begin(), items.end(), lessThan);
     // Busquem el nou node (ja l'hem inserit abans)
-    QList<QGraphicsItem*>::const_iterator it = qBinaryFind(items.begin(), items.end(), node, lessThan);
+    QList<QGraphicsItem*>::const_iterator it = std::lower_bound(items.begin(), items.end(), node, lessThan);
     Q_ASSERT(it != items.end());
     Q_ASSERT(*it == node);
     // (ln)
@@ -311,12 +311,12 @@ void QOpacityTransferFunctionGraphicalView::removeNode(const QPoint &position)
     else
     {
         // Ordenem per x
-        qSort(items.begin(), items.end(), lessThan);
+        std::sort(items.begin(), items.end(), lessThan);
         // Node auxiliar per fer la cerca
         QOpacityTransferFunctionGraphicalViewNode *auxiliarNode = new QOpacityTransferFunctionGraphicalViewNode();
         auxiliarNode->setPos(mapToScene(position));
         // Busquem el node més proper a x
-        QList<QGraphicsItem*>::const_iterator it = qLowerBound(items.begin(), items.end(), auxiliarNode, lessThan);
+        QList<QGraphicsItem*>::const_iterator it = std::lower_bound(items.begin(), items.end(), auxiliarNode, lessThan);
         delete auxiliarNode;
         if (it == items.begin())
         {

starviewer/src/core/qscreendistribution.cpp
@@ -14,15 +14,15 @@
 
 #include "qscreendistribution.h"
 
+#include <QGuiApplication>
 #include <QPainter>
-#include <QDesktopWidget>
-#include <QApplication>
 #include <QSize>
 #include <QMouseEvent>
 #include <QPaintEvent>
 #include <QWidgetAction>
 #include <QLinearGradient>
 #include <QRadialGradient>
+#include <QScreen>
 
 namespace udg {
 
@@ -181,19 +181,17 @@ void QScreenDistribution::paintEvent(QPaintEvent *event)
 
 void QScreenDistribution::computeSizesAndPositions()
 {
-    QDesktopWidget *desktop = QApplication::desktop();
-
     int minimumX = 0;
     int minimumY = 0;
     int maximumX = 0;
     int maximumY = 0;
 
     // Per cada pantalla
-    for (int i = 0; i < desktop->screenCount(); i++)
+    foreach (QScreen *screen, QGuiApplication::screens())
     {
         // Agafem les seves coordenades
-        QPoint topLeft = desktop->screenGeometry(i).topLeft();
-        QPoint bottomRight = desktop->screenGeometry(i).bottomRight();
+        QPoint topLeft = screen->geometry().topLeft();
+        QPoint bottomRight = screen->geometry().bottomRight();
 
         m_screens.append(QRect(topLeft, bottomRight));
 

starviewer/src/core/qtransferfunctioneditorbygradient.cpp
@@ -132,11 +132,6 @@ const TransferFunction &QTransferFunctionEditorByGradient::getTransferFunction()
     return m_transferFunction;
 }
 
-inline static bool x_less_than(const QPointF &p1, const QPointF &p2)
-{
-    return p1.x() < p2.x();
-}
-
 void QTransferFunctionEditorByGradient::pointsUpdated()
 {
     double w = m_alpha_shade->width();
@@ -150,7 +145,9 @@ void QTransferFunctionEditorByGradient::pointsUpdated()
     points += m_blue_shade->points();
     points += m_alpha_shade->points();
 
-    qSort(points.begin(), points.end(), x_less_than);
+    std::sort(points.begin(), points.end(), [](const QPointF &p1, const QPointF &p2) {
+        return p1.x() < p2.x();
+    });
 
     for (int i = 0; i < points.size(); ++i)
     {

commit 99cdca23578bd780197b9d0596b3a52e816f802a

BUG: Avoid crashes in certain screen configurations. #2850
This bug was introduced recently in commit 96dbd02 but only manifested
when the global point (0, 0) was not contained in any screen. Since the
height is also adjusted when the widget is shown this should not create
any size issue.

starviewer/src/inputoutput/qrelatedstudieswidget.cpp
@@ -400,7 +400,14 @@ void QRelatedStudiesWidget::updateWidgetWidth()
 
 void QRelatedStudiesWidget::updateWidgetHeight()
 {
-    int screenAvailableHeight = QGuiApplication::screenAt(this->mapToGlobal(QPoint(0, 0)))->availableSize().height();
+    QScreen *screen = QGuiApplication::screenAt(this->mapToGlobal(QPoint(0, 0)));
+
+    if (!screen)    // screen may be null if the widget is not visible
+    {
+        return;
+    }
+
+    int screenAvailableHeight = screen->availableSize().height();
     int topAndMargins = this->geometry().top() + m_relatedStudiesTree->geometry().top() * 2; // Es multiplica per 2 pel marge inferior.
     int maxHeight = screenAvailableHeight - topAndMargins;
     int minHeight = m_relatedStudiesTree->sizeHint().height();