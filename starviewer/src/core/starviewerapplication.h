9454f5a005259794a80cb273a6eecb80bfd4f3c2
Marc Ruiz <marc.ruiz@gmail.com>
2021-04-09 14:36:15 +0200

    ENH: Version and build id update (1.1.0, 2021040900).

--- starviewer/src/core/starviewerapplication.h
+++ starviewer/src/core/starviewerapplication.h
@@ -25,8 +25,8 @@ namespace udg {
     #define CE_SUFFIX ""
 #endif
 
-const QString StarviewerVersionString("1.1.0-devel" CE_SUFFIX);
-const QString StarviewerBuildID("2020071400");
+const QString StarviewerVersionString("1.1.0" CE_SUFFIX);
+const QString StarviewerBuildID("2021040900");
 
 #undef CE_SUFFIX
 
