95faa5ae84781d498c99dc4147f2b6de5935c5a9
Marc Ruiz <marc.ruiz@gmail.com>
2021-09-15 14:15:41 +0200

    BUG: Improve implementation to get a MAC address. #2750

--- starviewer/src/core/machineinformation.cpp
+++ starviewer/src/core/machineinformation.cpp
@@ -25,51 +25,73 @@ MachineInformation::MachineInformation()
 
 QString MachineInformation::getMACAddress()
 {
-    QString macAdress;
-    // Primer de tot mirar si hi ha interficia de xarxa local
-    bool found = false;
-    int index = 0;
-    QList<QNetworkInterface> allInterfaces = QNetworkInterface::allInterfaces();
-    while (!found && index < allInterfaces.count())
+    QNetworkInterface candidates[4];
+    // These numbers define the priority order for the candidates
+    constexpr int ConnectedEthernet = 0, ConnectedWifi = 1, DisconnectedEthernet = 2, DisconnectedWifi = 3;
+
+    foreach (const QNetworkInterface &interface, QNetworkInterface::allInterfaces())
     {
-        QNetworkInterface inter = allInterfaces[index++];
-
-        bool isMainInterface = false;
-#ifdef WIN32
-        isMainInterface = inter.humanReadableName().contains("local");
-#else
-        isMainInterface = inter.humanReadableName().contains("eth");
-#endif
-        QNetworkInterface::InterfaceFlags f = inter.flags();
-        bool flagsOk = f.testFlag(QNetworkInterface::IsUp) && f.testFlag(QNetworkInterface::IsRunning) && !f.testFlag(QNetworkInterface::IsLoopBack);
-
-        if (isMainInterface && inter.isValid() && flagsOk)
+        if (!interface.isValid())
         {
-            macAdress += inter.hardwareAddress();
-            found = true;
+            continue;
+        }
+
+        QNetworkInterface::InterfaceFlags flags = interface.flags();
+        bool flagsOk = flags.testFlag(QNetworkInterface::IsUp) && flags.testFlag(QNetworkInterface::IsRunning) &&
+                       !flags.testFlag(QNetworkInterface::IsLoopBack);
+        int ips = interface.addressEntries().size();
+
+        if (flagsOk)
+        {
+            int index = -1;
+
+            if (interface.type() == QNetworkInterface::Ethernet)
+            {
+                if (ips > 0)
+                {
+                    index = ConnectedEthernet;
+                }
+                else
+                {
+                    index = DisconnectedEthernet;
+                }
+            }
+            else if (interface.type() == QNetworkInterface::Wifi)
+            {
+                if (ips > 0)
+                {
+                    index = ConnectedWifi;
+                }
+                else
+                {
+                    index = DisconnectedWifi;
+                }
+            }
+
+            if (index >= 0)
+            {
+                if (!candidates[index].isValid())
+                {
+                    candidates[index] = interface;
+
+                    if (index == 0)
+                    {
+                        // This is the best possible option, no need to continue
+                        break;
+                    }
+                }
+            }
         }
     }
 
-    // Dono prioritat a la interfície d'àrea local i després busco la primera interfície vàlida
-    if (macAdress == "")
+    QString macAdress;
+
+    for (int i = 0; i < 4; i++)
     {
-        found = false;
-        index = 0;
-        while (!found && index < allInterfaces.count())
+        if (candidates[i].isValid())
         {
-            QNetworkInterface interface = allInterfaces[index++];
-
-            QNetworkInterface::InterfaceFlags flags = interface.flags();
-            bool flagsOk = flags.testFlag(QNetworkInterface::IsUp) && flags.testFlag(QNetworkInterface::IsRunning) &&
-                           !flags.testFlag(QNetworkInterface::IsLoopBack);
-
-            // Per si de cas el bluetooth està engegat i foncionant, fer que no l'agafi
-            // Rarament trobarem una connexió de xarxa que vagi a través d'un dispositiu bluetooth
-            if (interface.isValid() && flagsOk && !interface.humanReadableName().toLower().contains("bluetooth"))
-            {
-                macAdress += interface.hardwareAddress();
-                found = true;
-            }
+            macAdress = candidates[i].hardwareAddress();
+            break;
         }
     }
 
