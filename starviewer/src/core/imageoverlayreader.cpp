298ee6675f1f37fd384f50e8b97fe6573dcbd93f
Marc Ruiz <marc.ruiz@gmail.com>
2022-04-21 16:47:24 +0200

    BUG: Fix several problems when reading overlays.
    Problem 1 (#2154): very long time to read overlays in multiframes.
    Problem 2 (#2155): multiframe overlays ignore Image Frame Origin.
    Problem 3 (#2986): overlay origin read as if it was x\y instead of
    row\column.
    
    Overlays are now read with DCMTK, allowing to read only one frame of the
    file each time. This greatly reduces the reading time, although there's
    still room for improvement. The other problems have also been fixed when
    doing the new implementation.
    
    However a new limitation has been introduced (#2987) due to a bug in the
    currently used version of DCMTK (3.6.5). The limitation is that all
    overlays are cropped to fit in the image confines. This will be fixed
    when we upgrade DCMTK to a newer version (DCMTK 3.6.6 has already fixed
    its bug).

--- starviewer/src/core/imageoverlayreader.cpp
+++ starviewer/src/core/imageoverlayreader.cpp
@@ -14,18 +14,18 @@
 
 #include "imageoverlayreader.h"
 
+#include "dicomtagreader.h"
+//#include "dicomvaluerepresentationconverter.h"
+#include "imageoverlay.h"
 #include "logging.h"
 
-#include <gdcmImageReader.h>
-#include <gdcmOverlay.h>
+#include <dcdatset.h>   // DcmDataset
+#include <dcmimage.h>   // DicomImage
 
 namespace udg {
 
 ImageOverlayReader::ImageOverlayReader()
-{
-}
-
-ImageOverlayReader::~ImageOverlayReader()
+    : m_numberOfOverlays(0), m_frameNumber(0)
 {
 }
 
@@ -33,38 +33,92 @@ void ImageOverlayReader::setFilename(const QString &filename)
 {
     m_filename = filename;
 }
-  
+
+void ImageOverlayReader::setNumberOfOverlays(int numberOfOverlays)
+{
+    m_numberOfOverlays = numberOfOverlays;
+}
+
+void ImageOverlayReader::setFrameNumber(int frameNumber)
+{
+    m_frameNumber = frameNumber;
+}
+
 bool ImageOverlayReader::read()
 {
     m_overlaysList.clear();
 
-    gdcm::Image image = getGDCMImageFromFile(m_filename);
-    
-    for (size_t overlayIndex = 0; overlayIndex < image.GetNumberOfOverlays(); ++overlayIndex)
+    DICOMTagReader dicomReader(m_filename);
+    DicomImage dicomImage(dicomReader.getDcmDataset(), dicomReader.getDcmDataset()->getOriginalXfer(), CIF_UsePartialAccessToPixelData, 0, 1);
+
+    for (int plane = 0; plane < m_numberOfOverlays; plane++)
     {
-        m_overlaysList << ImageOverlay::fromGDCMOverlay(image.GetOverlay(overlayIndex));
+        int frameToRead = 0;
+
+        DICOMTag numberOfFramesInOverlayTag(DICOMNumberOfFramesInOverlay);
+        numberOfFramesInOverlayTag.setGroup(dicomImage.getOverlayGroupNumber(plane));
+
+        if (dicomReader.hasAttribute(numberOfFramesInOverlayTag))
+        {
+            // Multi-frame Overlay Module (PS3.3§C.9.3)
+            int numberOfFramesInOverlay = dicomReader.getValueAttributeAsQString(numberOfFramesInOverlayTag).toInt();
+
+            DICOMTag imageFrameOriginTag(DICOMImageFrameOrigin);
+            imageFrameOriginTag.setGroup(dicomImage.getOverlayGroupNumber(plane));
+            int imageFrameOrigin = 0;
+
+            if (dicomReader.hasAttribute(imageFrameOriginTag))
+            {
+                imageFrameOrigin = dicomReader.getValueAttributeAsQString(imageFrameOriginTag).toInt() - 1; // in DICOM first frame is 1; for us is 0
+            }
+
+            frameToRead = m_frameNumber - imageFrameOrigin;
+
+            if (frameToRead < 0 || frameToRead >= numberOfFramesInOverlay)
+            {
+                continue;   // no overlay for this frame and plane
+            }
+        }
+
+        // TODO Use the below code after upgrading to DCMTK 3.6.6 or later
+        //      The below code gets the full uncropped overlay but crashes causes a segmentation fault in DCMTK 3.6.5 if overlay origin is not (0,0).
+//        DICOMTag overlayOriginTag(DICOMOverlayOrigin);
+//        overlayOriginTag.setGroup(dicomImage.getOverlayGroupNumber(plane));
+//        QString overlayOrigin = dicomReader.getValueAttributeAsQString(overlayOriginTag);
+//        const QVector<int64_t> &origin = DICOMValueRepresentationConverter::signed64BitVeryLongToInt64Vector(overlayOrigin);
+//        int x = origin[1];  // column
+//        int y = origin[0];  // row
+
+//        uint width, height;
+//        const void *data = dicomImage.getFullOverlayData(plane, width, height, frameToRead);
+
+        uint x, y, width, height;
+        EM_Overlay mode;
+        const void *data = dicomImage.getOverlayData(plane, x, y, width, height, mode, frameToRead);
+
+        if (!data)
+        {
+            WARN_LOG(QString("Could not fetch overlay data for plane %1 and frame %2.").arg(plane).arg(m_frameNumber));
+            continue;
+        }
+
+        ImageOverlay overlay;
+        overlay.setRows(height);
+        overlay.setColumns(width);
+        overlay.setOrigin(x, y);
+        unsigned char *dataCopy = new unsigned char[width * height];
+        std::memcpy(dataCopy, data, width * height);
+        overlay.setData(dataCopy);
+
+        m_overlaysList.append(overlay);
     }
 
-    return true;
+    return !m_overlaysList.isEmpty();
 }
 
 QList<ImageOverlay> ImageOverlayReader::getOverlays() const
 {
     return m_overlaysList;
-}
-
-gdcm::Image ImageOverlayReader::getGDCMImageFromFile(const QString &filename)
-{
-    gdcm::ImageReader imageReader;
-    imageReader.SetFileName(qPrintable(filename));
-    if (!imageReader.Read())
-    {
-        ERROR_LOG("Ha fallat la lectura del fitxer: " + filename + " [ImageOverlayReader]");
-        DEBUG_LOG("Ha fallat la lectura del fitxer: " + filename);
-        return gdcm::Image();
-    }
-    
-    return imageReader.GetImage();
 }
 
 }
--- starviewer/src/core/imageoverlayreader.h
+++ starviewer/src/core/imageoverlayreader.h
@@ -17,12 +17,6 @@
 
 #include <QList>
 
-#include "imageoverlay.h"
-
-namespace gdcm {
-class Image;
-}
-
 namespace udg {
 
 class ImageOverlay;
@@ -30,24 +24,17 @@ class ImageOverlay;
 /**
     Classe per llegir overlays a través d'un arxiu. Per llegir els overlays caldrà assignar primer el nom de l'arxiu
     del que volem llegir els overlays i després fer-ne la lectura. Un cop feta la lectura podrem obtenir els overlays amb getOverlays()
-    
-    Exemple:
-    \code
-    ImageOverlayReader reader;
-    reader.setFilename("C:\samples\image.dcm");
-    if (read())
-    {
-        return reader.getOverlays();
-    }
-    \endcode
  */
 class ImageOverlayReader {
 public:
     ImageOverlayReader();
-    ~ImageOverlayReader();
 
     /// Nom del fitxer a partir del que hem de llegir l'overlay
     void setFilename(const QString &filename);
+    /// Sets the number of overlays in the image.
+    void setNumberOfOverlays(int numberOfOverlays);
+    /// Sets the frame number of the image. Needed to get the proper overlay in multiframe overlays.
+    void setFrameNumber(int frameNumber);
 
     /// Un cop donat un nom d'arxiu, en llegeix els overlays. Retorna fals en cas d'error, cert altrament
     bool read();
@@ -56,12 +43,12 @@ public:
     QList<ImageOverlay> getOverlays() const;
 
 private:
-    /// Ens retorna una gdcm::Image a partir del nom de fitxer especificat. Retornarà nul en cas d'error
-    virtual gdcm::Image getGDCMImageFromFile(const QString &filename);
-
-private:
     /// Nom de l'arxiu del que hem de llegir els overlays
     QString m_filename;
+    /// Number of overlays in the image.
+    int m_numberOfOverlays;
+    /// Frame number of the image.
+    int m_frameNumber;
 
     /// Llista d'overlays llegits
     QList<ImageOverlay> m_overlaysList;
