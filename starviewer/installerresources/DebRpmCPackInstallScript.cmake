22e739b5543a2827a119af8bc225f981db8ca94b
Marc Ruiz <marc.ruiz@gmail.com>
2022-11-02 19:24:36 +0100

    ENH: Configure URL handler with DEB installer. #3028

--- starviewer/installerresources/DebRpmCPackInstallScript.cmake
+++ starviewer/installerresources/DebRpmCPackInstallScript.cmake
@@ -1,3 +1,5 @@
+# TODO This won't work properly for Starviewer Lite because starviewer.sh is still hardcoded pointing to full Starviewer
+
 execute_process(COMMAND mkdir -p "${CMAKE_CURRENT_BINARY_DIR}/usr/bin" "${CMAKE_CURRENT_BINARY_DIR}/usr/share/applications")
 
 execute_process(COMMAND ln -s /opt/starviewer/starviewer.sh "${CMAKE_CURRENT_BINARY_DIR}/usr/bin/starviewer")
@@ -5,3 +7,7 @@ execute_process(COMMAND ln -s /opt/starviewer/starviewer.sh "${CMAKE_CURRENT_BIN
 list(GET CPACK_BUILD_SOURCE_DIRS 0 SOURCE_DIR)
 string(REPLACE " " "" OUTPUT_FILENAME ${CPACK_PACKAGE_NAME})    # "Starviewer" for "Starviewer" and "StarviewerLite" for "Starviewer Lite"
 configure_file("${SOURCE_DIR}/installerresources/Starviewer.desktop.in" "${CMAKE_CURRENT_BINARY_DIR}/usr/share/applications/${OUTPUT_FILENAME}.desktop")
+
+# URL handler
+configure_file("${SOURCE_DIR}/installerresources/StarviewerUrlHandler.desktop.in"
+               "${CMAKE_CURRENT_BINARY_DIR}/usr/share/applications/${OUTPUT_FILENAME}UrlHandler.desktop")
